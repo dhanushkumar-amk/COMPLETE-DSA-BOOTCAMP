d6d84167be35e5c8b563041875718cd5112df1ed
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-25 18:19:40 +0530

    modified

--- src/MATHS/CountTheNumberInDigit.java
+++ src/MATHS/CountTheNumberInDigit.java
@@ -1,13 +1,15 @@
 package MATHS;
 
+import java.util.Scanner;
+
 public class CountTheNumberInDigit {
     public static void main(String[] args) {
-       int n = 1234;
+        Scanner sc = new 
        System.out.println(countNumber(n));
     }
     public static int countNumber(int n){
         // init the count variable
-      
+
         int count = 0;
         while(n > 0){
          n = n /10;
