3345bee1c9bd7de2ff2e76b782ee4ae45b6a0590
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-25 18:26:36 +0530

    modified

--- src/ARRAYS/MinimumNumberInArray.java
+++ src/ARRAYS/MinimumNumberInArray.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class MinimumNumberInArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number : ");
         
     }
 
