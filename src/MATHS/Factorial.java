a41851312bcb0edd991a2bd605c0e21a5974cca8
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-25 18:23:23 +0530

    modified

--- src/MATHS/Factorial.java
+++ src/MATHS/Factorial.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Factorial {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println();
          int n = sc.nextInt();
         System.out.println(fact(n));
         }
