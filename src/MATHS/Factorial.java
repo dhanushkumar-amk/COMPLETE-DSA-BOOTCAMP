49223c8edf0cda27a33428a4c40a7093b6b477e3
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-25 18:23:30 +0530

    modified

--- src/MATHS/Factorial.java
+++ src/MATHS/Factorial.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Factorial {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number");
+        System.out.println("Enter the number : ");
          int n = sc.nextInt();
         System.out.println(fact(n));
         }
