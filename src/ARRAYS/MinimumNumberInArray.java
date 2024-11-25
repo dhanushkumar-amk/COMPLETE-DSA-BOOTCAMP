21b48846b17e46fa4782d5d400e812255b5b3bdc
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-25 18:27:12 +0530

    modified

--- src/ARRAYS/MinimumNumberInArray.java
+++ src/ARRAYS/MinimumNumberInArray.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class MinimumNumberInArray {
     public static void main(String[] args) {
-        int[] arr = {12,4,23,123,24,5}
+        int[] arr = {12,4,23,123,24,5,1};
+        
     }
 
 }
