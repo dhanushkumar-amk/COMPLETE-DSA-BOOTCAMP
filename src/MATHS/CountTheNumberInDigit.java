e67d20df80ecf5a5fd61234e6b7f9bc3196dc6c7
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-25 18:19:51 +0530

    modified

--- src/MATHS/CountTheNumberInDigit.java
+++ src/MATHS/CountTheNumberInDigit.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class CountTheNumberInDigit {
     public static void main(String[] args) {
-        Scanner sc = new 
+        Scanner sc = new Scanner(Ss)
        System.out.println(countNumber(n));
     }
     public static int countNumber(int n){
