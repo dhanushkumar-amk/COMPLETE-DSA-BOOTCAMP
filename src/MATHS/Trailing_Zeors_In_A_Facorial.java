7442bee0c7abf258776d02ea2fd69972dc78dd17
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-26 11:02:53 +0530

    modified

--- src/MATHS/Trailing_Zeors_In_A_Facorial.java
+++ src/MATHS/Trailing_Zeors_In_A_Facorial.java
@@ -1,7 +1,7 @@
 package MATHS;
 public class Trailing_Zeors_In_A_Facorial {
 public static void main(String[] args) {
-    int n = 200;
+    int n = 5;
     System.out.println(ZerosInFactorial(n));
 }
 
