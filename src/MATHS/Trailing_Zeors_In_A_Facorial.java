4f942a1320c9d9c5d1449986f18e9b79f75d5619
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-26 11:00:40 +0530

    modified

--- src/MATHS/Trailing_Zeors_In_A_Facorial.java
+++ src/MATHS/Trailing_Zeors_In_A_Facorial.java
@@ -1,10 +1,19 @@
 package MATHS;
 public class Trailing_Zeors_In_A_Facorial {
 public static void main(String[] args) {
-
+    int n = 20;
+    System.out.println(ZerosInFactorial(n));
 }
 
 public  static  int ZerosInFactorial(int n){
+    int result = 0;
+    int powerOf5 = 5;
 
+    while( n >= powerOf5){
+        result += n / powerOf5;
+        powerOf5 = powerOf5 * 5;
+    }
+
+    return  result;
     }
 }
