4229ec04f9e169de2b9f96c75f2e529505ce38fd
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-27 19:06:53 +0530

    modified

--- src/MATHS/PrimeNumberInGivenRange.java
+++ src/MATHS/PrimeNumberInGivenRange.java
@@ -1,5 +1,7 @@
 package MATHS;
 
+import static MATHS.PrimeNumber.isPrime2;
+
 public class PrimeNumberInGivenRange {
     public static void main(String[] args) {
         int n =14;
@@ -9,7 +11,7 @@ public class PrimeNumberInGivenRange {
 
    public static void printPrime(int n){
         for (int i =2; i <= n ; i++) {
-            if (isPrime(n))
+            if (isPrime2(n))
                 System.out.println(i);
         }
     }
