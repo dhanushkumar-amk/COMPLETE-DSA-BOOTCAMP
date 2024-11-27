9df0605a04105c4bfd43468b5c9b7088c4d09ead
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-27 16:41:56 +0530

    modified

--- src/MATHS/GCD_and_HCF_of_tow_numbers.java
+++ src/MATHS/GCD_and_HCF_of_tow_numbers.java
@@ -1,2 +1,21 @@
-package MATHS;public class GCD_and_HCF_of_tow_numbers {
+package MATHS;
+
+public class GCD_and_HCF_of_tow_numbers {
+    public static void main(String[] args) {
+        int n = 20;
+        int m = 15;
+
+    }
+
+    public static  int gcd(int n, int m){
+        int min = Math.min(n, m);
+            int result ;
+        for (int i = min; i <= 1; i--) {
+            if (m % i == 1 && n % i == 1){
+                result  =  i;
+            }
+            break;
+        }
+        return  result;
+    }
 }
