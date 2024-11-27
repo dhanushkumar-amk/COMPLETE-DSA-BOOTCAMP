9718dc9163e5e0e909cc5cad2a9d31f6f50c3d6e
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-27 20:08:26 +0530

    modified

--- src/MATHS/SieveOfEratosthenes.java
+++ src/MATHS/SieveOfEratosthenes.java
@@ -3,14 +3,52 @@ package MATHS;
 public class SieveOfEratosthenes {
     public static void main(String[] args) {
     int n = 21;
-    isPrime(n);
+//    isPrime1(n);
+//    isPrime2(n);
+    isPrime3(n);
     }
 
-    static  void isPrime(int n){
+    // method 1
+    static  void isPrime1(int n){
         boolean[] arr = new boolean[n + 1];
         for (int i = 2; i <=n ; i++) {
             if (arr[i] == false){
                 for (int j = i * 2; j <= n ; j+= i) {
+                    arr[j] = true;
+                }
+            }
+        }
+        for (int i = 2; i < arr.length-1; i++) {
+            if (arr[i] == false)
+                System.out.println(i);
+        }
+    }
+
+
+    // method 2
+    static  void isPrime2(int n){
+        boolean[] arr = new boolean[n + 1];
+        for (int i = 2; i <=n ; i++) {
+            if (arr[i] == false){
+//                 just i * i instant i * 2
+                for (int j = i * i; j <= n ; j+= i) {
+                    arr[j] = true;
+                }
+            }
+        }
+        for (int i = 2; i < arr.length-1; i++) {
+            if (arr[i] == false)
+                System.out.println(i);
+        }
+    }
+
+
+    // method 3
+    static  void isPrime3(int n){
+        boolean[] arr = new boolean[n + 1];
+        for (int i = 2; i <=Math.sqrt(n) ; i++) {
+            if (arr[i] == false){
+                for (int j = i * i; j <= n ; j+= i) {
                     arr[j] = true;
                 }
             }
