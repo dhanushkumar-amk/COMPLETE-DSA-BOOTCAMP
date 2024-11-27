dd7ec325d62e7891b813fdc1237c95f143507be1
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-27 17:46:10 +0530

    modified

--- src/MATHS/LCM.java
+++ src/MATHS/LCM.java
@@ -1,2 +1,19 @@
-package MATHS;public class LCM {
+package MATHS;
+
+public class LCM {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5;
+        lcm(a, b);
+    }
+    static  int lcm(int a, int b){
+        int result = Math.max(a, b);
+        while (true){
+            if (result % a == 0 && result % b == 0){
+                break;
+            }
+            result++;
+        }
+        return result;
+    }
 }
