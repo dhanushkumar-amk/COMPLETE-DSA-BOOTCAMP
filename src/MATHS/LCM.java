54fd4b3efac37788e8df937be98a1112881e73cd
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-27 17:46:25 +0530

    modified

--- src/MATHS/LCM.java
+++ src/MATHS/LCM.java
@@ -4,7 +4,7 @@ public class LCM {
     public static void main(String[] args) {
         int a = 2;
         int b = 5;
-        lcm(a, b);
+        System.out.println(lcm(a, b));
     }
     static  int lcm(int a, int b){
         int result = Math.max(a, b);
