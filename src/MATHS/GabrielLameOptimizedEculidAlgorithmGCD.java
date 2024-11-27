2a1e81a7c0b9eba0e18e73fcd4ac0376a6c47312
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-27 17:35:00 +0530

    modified

--- src/MATHS/GabrielLameOptimizedEculidAlgorithmGCD.java
+++ src/MATHS/GabrielLameOptimizedEculidAlgorithmGCD.java
@@ -1,2 +1,21 @@
-package MATHS;public class GabrielLameOptimizedEculidAlgorithmGCD {
+package MATHS;
+
+public class GabrielLameOptimizedEculidAlgorithmGCD {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 15;
+        System.out.println(gcd(a,b));
+    }
+    public  static  int gcd(int a , int b) {
+        while (a != 0 && b != 0) {
+            if (a < b)
+                a = a % b;
+            else
+                b = b % a;
+        }
+        if (a != 0)
+            return a;
+        else
+            return b;
+    }
 }
