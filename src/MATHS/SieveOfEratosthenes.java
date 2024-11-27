c60f6a5d71919a4308b3745b2a10397c353c7731
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-27 19:53:24 +0530

    modified

--- src/MATHS/SieveOfEratosthenes.java
+++ src/MATHS/SieveOfEratosthenes.java
@@ -2,7 +2,22 @@ package MATHS;
 
 public class SieveOfEratosthenes {
     public static void main(String[] args) {
-
+    int n = 21;
+    isPrime(n);
     }
-    
+
+    static  void isPrime(int n){
+        boolean[] arr = new boolean[n + 1];
+        for (int i = 2; i <=n ; i++) {
+            if (arr[i] == false){
+                for (int j = i * 2; j <= n ; j+= i) {
+                    arr[j] = true;
+                }
+            }
+        }
+        for (int i = 2; i <=n; i++) {
+            if (arr[i] == false)
+                System.out.println(i);
+        }
+    }
 }
