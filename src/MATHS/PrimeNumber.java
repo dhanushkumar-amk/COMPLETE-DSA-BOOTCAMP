d8e1b5db91be8262eae6f2416e84abb67fa81503
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-27 18:25:16 +0530

    modified

--- src/MATHS/PrimeNumber.java
+++ src/MATHS/PrimeNumber.java
@@ -2,6 +2,14 @@ package MATHS;
 
 public class PrimeNumber {
     public static void main(String[] args) {
-        
+        int  n = 10;
+        System.out.println(isPrime(n));
+    }
+    static  boolean isPrime(int n){
+        for (int i = 2; i <n/2 ; i++) {
+            if ( n % i == 0)
+                return false;
+        }
+        return true;
     }
 }
