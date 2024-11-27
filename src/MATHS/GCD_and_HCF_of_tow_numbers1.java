60e26f6359ddaf7a9b4daf968dc3363369a8fc28
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-27 17:01:32 +0530

    modified

--- src/MATHS/GCD_and_HCF_of_tow_numbers1.java
+++ src/MATHS/GCD_and_HCF_of_tow_numbers1.java
@@ -1,6 +1,6 @@
 package MATHS;
 
-public class GCD_and_HCF_of_tow_numbers {
+public class GCD_and_HCF_of_tow_numbers1 {
     public static void main(String[] args) {
         int a = 20;
         int b = 15;
@@ -15,7 +15,7 @@ public class GCD_and_HCF_of_tow_numbers {
             min = b;
 
             for (int i = min; i >= 1;  i--) {
-            if (a / i == 1 && b / i == 1) {
+            if (a % i == 1 && b % i == 1) {
                 System.out.println(i);
                 break;
             }
