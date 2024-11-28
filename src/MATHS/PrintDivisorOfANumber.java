394b83e737da00764706697de8a473bdd31f76eb
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-28 10:39:42 +0530

    modified

--- src/MATHS/PrintDivisorOfANumber.java
+++ src/MATHS/PrintDivisorOfANumber.java
@@ -3,7 +3,7 @@ package MATHS;
 public class PrintDivisorOfANumber {
     public static void main(String[] args) {
         int n = 25;
-        divisor3(n);
+        divisor4(n);
     }
 
     // brute force method1
@@ -32,9 +32,27 @@ public class PrintDivisorOfANumber {
         for (int i = 1; i* i <= n ; i++) {
             if (n % i == 0){
                 System.out.print(i + " ");
+                // remove the duplicates of an numbers
                 if ( i != n/i)
                     System.out.println(n/ i);
             }
+        }
+    }
+
+    // print the divisor in ascending order
+    public static  void divisor4(int n){
+        int i = 0;
+        for ( i = 1; i* i <= n ; i++) {
+            if (n % i == 0){
+                System.out.println(i);
+            }
+        }
+
+        // we can use empty to an initialization path in for loop
+        // because we initialize the i in above so if i exit that same value exits here it helps us to reverse the for lpop
+        for ( ;i >=1;i--){
+            if (n % i == 0)
+                System.out.println(n / i);
         }
     }
 
