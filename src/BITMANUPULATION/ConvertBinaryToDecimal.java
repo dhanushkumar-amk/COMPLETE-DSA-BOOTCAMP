57e41ef32ace8dd78bf720b98ba91964be1a1216
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-28 13:27:48 +0530

    modified

--- src/BITMANUPULATION/ConvertBinaryToDecimal.java
+++ src/BITMANUPULATION/ConvertBinaryToDecimal.java
@@ -2,8 +2,20 @@ package BITMANUPULATION;
 
 public class ConvertBinaryToDecimal {
     public static void main(String[] args) {
-        int n = 101101;
+        String n = "101101";
+        System.out.println(btod(n));
+    }
+    static  int btod(String n){
+        int result = 0;
+        int powerOfTwo = 1;  // value of 2 to the power 0 is 1
+        for (int i = n.length()-1; i >= 0; i--){
+            if (n.charAt(i) == '1') {
+                result += powerOfTwo;
+            }
+            powerOfTwo *= 2;
+        }
 
+        return result;
     }
 
 }
