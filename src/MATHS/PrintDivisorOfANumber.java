673476554cbe5a3f74379a678839da6429f1bf28
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-28 10:28:15 +0530

    modified

--- src/MATHS/PrintDivisorOfANumber.java
+++ src/MATHS/PrintDivisorOfANumber.java
@@ -26,6 +26,16 @@ public class PrintDivisorOfANumber {
         }
     }
 
-
+    // method 3
+    // efficient  solution
+    public static  void divisor3(int n){
+        for (int i = 1; i* i <= n ; i++) {
+            if (n % i == 0){
+                System.out.print(i + " ");
+                if (n/ i != i)
+                    System.out.println(n/ i);
+            }
+        }
+    }
 
 }
