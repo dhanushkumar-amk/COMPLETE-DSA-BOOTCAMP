c4de1f5081e9b7e460d2aa08009b49ebc7c5efc5
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-28 10:15:20 +0530

    modified

--- src/MATHS/PrintDivisorOfANumber.java
+++ src/MATHS/PrintDivisorOfANumber.java
@@ -3,14 +3,20 @@ package MATHS;
 public class PrintDivisorOfANumber {
     public static void main(String[] args) {
         int n = 20;
-        divisor(n);
+        divisor1(n);
     }
 
-    public static  void divisor(int n){
+    // brute force method1
+    public static  void divisor1(int n){
         for (int i = 1; i <=n ; i++) {
             if (n % i == 0){
                 System.out.println(i);
             }
         }
     }
+
+
+    // method 2
+    
+
 }
