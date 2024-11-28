7245897f30637e27321eecc09f5029e00607e8ed
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-28 11:18:53 +0530

    modified

--- src/MATHS/PrimeFactors.java
+++ src/MATHS/PrimeFactors.java
@@ -1,2 +1,19 @@
-package MATHS;public class PrimeFactors {
+package MATHS;
+
+public class PrimeFactors {
+    public static void main(String[] args) {
+
+    }
+
+
+    // method 1
+    public  static  void factorsPrime(int n){
+        int i = 2;
+        while(n % i == 0){
+            System.out.println(i);
+            n = n / i;
+        }
+        i++;
+
+    }
 }
