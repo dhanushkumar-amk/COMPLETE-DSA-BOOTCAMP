14ddb15961e2271c2ffedc4bc3cb45f044af5bf3
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-28 11:28:43 +0530

    modified

--- src/MATHS/PrimeFactors.java
+++ src/MATHS/PrimeFactors.java
@@ -3,12 +3,12 @@ package MATHS;
 public class PrimeFactors {
     public static void main(String[] args) {
 int n = 900;
-factorsPrime(n);
+factorsPrime1(n);
     }
 
 
     // method 1
-    public  static  void factorsPrime(int n){
+    public  static  void factorsPrime1(int n){
         int i = 2;
         while (n > 1){
             while(n % i == 0){
@@ -18,4 +18,7 @@ factorsPrime(n);
             i++;
         }
       }
+
+      // method 2
+    
 }
