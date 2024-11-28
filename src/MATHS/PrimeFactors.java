c5883327292ba5bc59a978f84e886be3de1f8b47
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-28 11:21:02 +0530

    modified

--- src/MATHS/PrimeFactors.java
+++ src/MATHS/PrimeFactors.java
@@ -2,18 +2,20 @@ package MATHS;
 
 public class PrimeFactors {
     public static void main(String[] args) {
-
+int n = 900;
+factorsPrime(n);
     }
 
 
     // method 1
     public  static  void factorsPrime(int n){
         int i = 2;
-        while(n % i == 0){
-            System.out.println(i);
-            n = n / i;
+        while (n > 1){
+            while(n % i == 0){
+                System.out.println(i);
+                n = n / i;
+            }
+            i++; 
         }
-        i++;
-
-    }
+      }
 }
