00def52d10a7d65b1b45982dce548b773dce598c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-28 16:41:18 +0530

    modified

--- src/BITMANUPULATION/SwitchOnTheIthBit.java
+++ src/BITMANUPULATION/SwitchOnTheIthBit.java
@@ -3,6 +3,12 @@ package BITMANUPULATION;
 public class SwitchOnTheIthBit {
     public static void main(String[] args) {
         int n =36;
-        
+        int i = 3;
+
+        int onMask = 1 << i;
+
+        int result = n | onMask;
+        System.out.println(result);
+
     }
 }
