490b9cace56cfa706d2cc97ee1fa75a375576c88
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-29 12:45:11 +0530

    modified

--- src/BITMANUPULATION/SwapAllOddAndEvenBits.java
+++ src/BITMANUPULATION/SwapAllOddAndEvenBits.java
@@ -1,2 +1,12 @@
-package BITMANUPULATION;public class SwapAllOddAndEvenBits {
+package BITMANUPULATION;
+
+public class SwapAllOddAndEvenBits {
+    public static void main(String[] args) {
+        int n = 181;
+        System.out.println(swapOddEven(n));
+    }
+
+    static  int swapOddEven(int n){
+        return (( n & 0xaaaaaaaa) >>> 1) | ((n & 0x55555555) << 1);
+    }
 }
