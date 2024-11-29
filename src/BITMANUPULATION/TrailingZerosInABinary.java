58ffb1ffe29817f9cffd43ae43de4de1a51dbca1
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-29 12:57:34 +0530

    modified

--- src/BITMANUPULATION/TrailingZerosInABinary.java
+++ src/BITMANUPULATION/TrailingZerosInABinary.java
@@ -1,2 +1,11 @@
-package BITMANUPULATION;public class TrailingZerosInABinary {
+package BITMANUPULATION;
+
+public class TrailingZerosInABinary {
+    public static void main(String[] args) {
+        int n = 168;
+        System.out.println(trailing(n));
+    }
+    static  int trailing(int n){
+        return (int) (Math.log10((n & n - 1) ^ n) / Math.log10(2));
+    }
 }
