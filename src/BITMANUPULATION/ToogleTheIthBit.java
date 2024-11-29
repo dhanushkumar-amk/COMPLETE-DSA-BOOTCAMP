52980d06284392339c7c145b68bfceedef8c0caa
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-29 07:45:28 +0530

    modified

--- src/BITMANUPULATION/ToogleTheIthBit.java
+++ src/BITMANUPULATION/ToogleTheIthBit.java
@@ -1,2 +1,12 @@
-package BITMANUPULATION;public class ToogleTheIthBit {
+package BITMANUPULATION;
+
+public class ToogleTheIthBit {
+    public static void main(String[] args) {
+        int n = 36;
+        int i = 5;
+
+        int mask = n << i;
+
+        System.out.println(n ^ mask);
+    }
 }
