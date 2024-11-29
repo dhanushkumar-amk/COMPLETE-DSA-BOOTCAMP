7adaf5f73041fee783ff9492277e20eda86cffe1
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-29 10:52:38 +0530

    modified

--- src/BITMANUPULATION/FindWhetherTheNumberIsAPowerOf2.java
+++ src/BITMANUPULATION/FindWhetherTheNumberIsAPowerOf2.java
@@ -2,6 +2,14 @@ package BITMANUPULATION;
 
 public class FindWhetherTheNumberIsAPowerOf2 {
     public static void main(String[] args) {
-        
+        int n = 8;
+        System.out.println(powerOf2(n));
+    }
+    static  boolean powerOf2(int n){
+        // base case
+        if (n==0)
+            return false;
+
+        return  (n &(n -1 ))==0;
     }
 }
