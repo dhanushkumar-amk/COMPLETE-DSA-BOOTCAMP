89cf30f11a5a3237159e4e65cdc4766a62012482
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-29 09:52:35 +0530

    modified

--- src/BITMANUPULATION/ProgramToFindTheRightMostBit.java
+++ src/BITMANUPULATION/ProgramToFindTheRightMostBit.java
@@ -3,11 +3,11 @@ package BITMANUPULATION;
 public class ProgramToFindTheRightMostBit {
     public static void main(String[] args) {
         int n = 0;
-        System.out.println(rightMostSetBitPosition(n));
+        System.out.println(rightMostSetBitPosition2(n));
     }
 
     // method 1
-    static  int rightMostSetBitPosition(int n){
+    static  int rightMostSetBitPosition1(int n){
         int mask = 1;
         int position = 0;// assume intialize it zero
 
@@ -23,6 +23,8 @@ public class ProgramToFindTheRightMostBit {
     }
 
     // method 2
-    
+    static  int rightMostSetBitPosition2(int n){
+         return (int) (Math.log10(n ^ (n &(n-1)))/ Math.log10(2));
+    }
 
 }
