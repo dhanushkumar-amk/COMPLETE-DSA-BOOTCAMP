7040f80cd53c4e81109187df3ca1a404af10953b
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-29 12:07:42 +0530

    modified

--- src/BITMANUPULATION/CountTheLongestConsecutiveOnesInBinaryRepresentationINGivenNumber.java
+++ src/BITMANUPULATION/CountTheLongestConsecutiveOnesInBinaryRepresentationINGivenNumber.java
@@ -1,2 +1,17 @@
-package BITMANUPULATION;public class CountTheLongestConsecutiveOnesInNinaryRepresentationINGivenNumber {
+package BITMANUPULATION;
+
+public class CountTheLongestConsecutiveOnesInBinaryRepresentationINGivenNumber {
+    public static void main(String[] args) {
+        int n = 101;
+        System.out.println(countTheValues(n));
+
+    }
+    static  int countTheValues(int n){
+        int count = 0;
+         while (n != 0){
+             n = n & (n << 1);
+             count++;
+         }
+         return count;
+    }
 }
