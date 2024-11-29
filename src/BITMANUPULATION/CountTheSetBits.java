31cd7710d6ecc1178bae359d1636cc60d6f65ff4
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-29 10:25:30 +0530

    modified

--- src/BITMANUPULATION/CountTheSetBits.java
+++ src/BITMANUPULATION/CountTheSetBits.java
@@ -1,2 +1,18 @@
-package BITMANUPULATION;public class CountTheSetBits {
+package BITMANUPULATION;
+
+public class CountTheSetBits {
+    public static void main(String[] args) {
+        int n = 42;
+        System.out.println(countSetBits(n));
+    }
+
+    static  int countSetBits(int n){
+        int count = 0;
+
+        while (n != 0) {
+            n = n & (n - 1);
+            count++;
+        }
+        return count;
+    }
 }
