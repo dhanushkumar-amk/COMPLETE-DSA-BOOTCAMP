6070734174fab70eb0b8ed9606e68c199838e3d5
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-29 16:26:33 +0530

    modified

--- src/BITMANUPULATION/NthPalindromicRepresentation.java
+++ src/BITMANUPULATION/NthPalindromicRepresentation.java
@@ -1,2 +1,23 @@
-package BITMANUPULATION;public class NthPalindromicRepresentation {
+package BITMANUPULATION;
+
+public class NthPalindromicRepresentation {
+    public static void main(String[] args) {
+
+    }
+    public static  int palindromic(int n){
+        int length = 0;
+        int count = 0;
+
+        while( count < n){
+            length++;
+            count = count + (int)Math.pow(2, (length - 1)/ 2);
+        }
+        count = count - (int)Math.pow(2, (length - 1)/ 2);
+
+        int element = n - count - 1;
+
+        int answer = (1 << (length - 1)) | (element << (length/2));
+
+        answer = answer | 
+    }
 }
