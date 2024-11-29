6444971cd61900d589a92cacfb048a9d42a87772
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-29 11:24:14 +0530

    modified

--- src/BITMANUPULATION/LonelyDigits.java
+++ src/BITMANUPULATION/LonelyDigits.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class LonelyDigits {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,3,2,1};
+        int[] arr = {1,2,3,4,3,2,4,5,1};
         System.out.println(lonelyInteger2(arr));
     }
 
