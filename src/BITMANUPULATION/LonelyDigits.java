f9efdb733b39765453eb3505061d8f10dcd74c94
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-29 11:17:02 +0530

    modified

--- src/BITMANUPULATION/LonelyDigits.java
+++ src/BITMANUPULATION/LonelyDigits.java
@@ -5,16 +5,20 @@ import java.util.Arrays;
 public class LonelyDigits {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,3,2,1};
-        System.out.println(lonelyInteger(arr));
+        System.out.println(lonelyInteger1(arr));
     }
-    public  static  int lonelyInteger(int[] arr){
 
+    // method 1
+    public  static  int lonelyInteger1(int[] arr){
         Arrays.sort(arr);
-
         for (int i = 0; i <arr.length-1 ; i+= 2) {
             if (arr[i] != arr[i + 1])
                 return arr[i];
         }
         return arr[arr.length-1];
     }
+
+
+    // method 2
+
 }
