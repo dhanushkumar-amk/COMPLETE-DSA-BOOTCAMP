6c4afe568ded8fafdf2d13f5fc948e9e7a7a7fc9
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-30 21:15:54 +0530

    modified

--- src/BITMANUPULATION/TwosComplement.java
+++ src/BITMANUPULATION/TwosComplement.java
@@ -1,2 +1,25 @@
-package BITMANUPULATION;public class TwosComplement {
+package BITMANUPULATION;
+
+public class TwosComplement {
+    public static void main(String[] args) {
+
+    }
+
+    public static String twosComplement(String str){
+        StringBuilder builder = new StringBuilder();
+    }
+
+    public  static  String OnesComplement(String str){
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (char i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '1')
+                stringBuilder.append('0');
+            else
+                stringBuilder.append('1');
+        }
+
+        return stringBuilder.toString();
+    }
 }
