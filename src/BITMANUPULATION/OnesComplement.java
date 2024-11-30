fcca3b2d36a0248ebe798e2cf9bd8a5f0abc3fad
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-30 20:59:10 +0530

    modified

--- src/BITMANUPULATION/OnesComplement.java
+++ src/BITMANUPULATION/OnesComplement.java
@@ -1,2 +1,13 @@
-package BITMANUPULATION;public class OnesComplement {
+package BITMANUPULATION;
+
+
+public class OnesComplement {
+    public static void main(String[] args) {
+        String num = "111111";
+
+    }
+   public static String Complement(String num){
+
+
+   }
 }
