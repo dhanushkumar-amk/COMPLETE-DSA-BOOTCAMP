d50245f7dff526b5449b601a523ae4f44fc22e8f
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-30 22:22:54 +0530

    modified

--- src/BITMANUPULATION/swap.java
+++ src/BITMANUPULATION/swap.java
@@ -1,2 +1,17 @@
-package BITMANUPULATION;public class swap {
+package BITMANUPULATION;
+
+public class swap {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        swapp(a, b);
+    }
+    static void swapp(int a, int b){
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+
+        System.out.println("After swapping: A = " + a + "B = " + b);
+    }
 }
