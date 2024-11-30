6650e00aa41380d404f2f214371e642765aeceef
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-30 22:09:41 +0530

    modified

--- src/BITMANUPULATION/SubtractTwoNumber.java
+++ src/BITMANUPULATION/SubtractTwoNumber.java
@@ -1,2 +1,22 @@
-package BITMANUPULATION;public class SubtractTwoNumber {
+package BITMANUPULATION;
+
+public class SubtractTwoNumber {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        System.out.println(sub(a, b));
+    }
+
+    static int sub(int a, int b){
+
+        while (b != 0){
+            int carry = ~(a) & b;
+
+            a = a ^ b;
+
+            b = carry << 1;
+        }
+
+        return a;
+    }
 }
