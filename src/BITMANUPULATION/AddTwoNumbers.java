55c9bd77ad4345c80516964e1ed96d09194e0e42
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-30 21:55:24 +0530

    modified

--- src/BITMANUPULATION/AddTwoNumbers.java
+++ src/BITMANUPULATION/AddTwoNumbers.java
@@ -1,2 +1,22 @@
-package BITMANUPULATION;public class AddTwoNumbers {
+package BITMANUPULATION;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(add(a, b));
+    }
+
+    static int add(int a, int b){
+
+        while (b != 0){
+            int carry = a & b;
+
+            a = a ^ b;
+
+            b = carry << 1;
+        }
+
+        return a;
+    }
 }
