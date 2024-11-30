24cc87da78cecb407ad00ed0b1fcc1cd69b37593
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-11-30 21:01:22 +0530

    modified

--- src/BITMANUPULATION/OnesComplement.java
+++ src/BITMANUPULATION/OnesComplement.java
@@ -4,10 +4,18 @@ package BITMANUPULATION;
 public class OnesComplement {
     public static void main(String[] args) {
         String num = "111111";
-
+        System.out.println(Complement(num));
     }
    public static String Complement(String num){
 
+       StringBuilder builder = new StringBuilder();
 
+       for (char i = 0; i <num.length() ; i++) {
+           if (num.charAt(i) == '0')
+               builder.append('1');
+           else
+               builder.append('0');
+       }
+    return  builder.toString();
    }
 }
