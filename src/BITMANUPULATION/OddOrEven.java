eaf821a7ef6334ff35d93dc5501028fb27f17aa2
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-01 20:59:08 +0530

    modified

--- src/BITMANUPULATION/OddOrEven.java
+++ src/BITMANUPULATION/OddOrEven.java
@@ -1,2 +1,12 @@
-package BITMANUPULATION;public class OddOrEven {
+package BITMANUPULATION;
+
+public class OddOrEven {
+    public static void main(String[] args) {
+        int n = 6;
+
+        if ((n & 1) == 0)
+            System.out.println("even");
+        else
+            System.out.println("odd");
+    }
 }
