9e4b59a074b71d8cb350f5462505b13592a3edec
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-04 19:25:56 +0530

    modified

--- src/SEARCHING/LinearSearch.java
+++ src/SEARCHING/LinearSearch.java
@@ -10,7 +10,7 @@ public class LinearSearch {
     static  int linearSearch(int[] arr, int target){
         for (int i = 0; i <arr.length ; i++) {
             if (arr[i] == target)
-                return i;
+                return i + 1;
         }
         return  -1;
     }
