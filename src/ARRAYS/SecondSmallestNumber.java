ad936ff404de86eb7eb34009916991f3626754ed
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-05 05:21:46 +0530

    modified

--- src/ARRAYS/SecondSmallestNumber.java
+++ src/ARRAYS/SecondSmallestNumber.java
@@ -7,18 +7,18 @@ public class SecondSmallestNumber {
     }
 
     static  int smallest(int[] arr){
-        int max1 = arr[0];
-        int max2 = arr[0];
+        int min1 = arr[0];
+        int min2 = arr[0];
 
         for (int i = 1; i <arr.length ; i++) {
-            if (arr[i] < max1){
-                max2 = max1;
-                max1 = arr[i];
-            } else if (arr[i] < max2) {
-                max2 = arr[i];
+            if (arr[i] < min1){
+                min2 = min1;
+                min1 = arr[i];
+            } else if (arr[i] < min2) {
+                min2 = arr[i];
             }
         }
-        return  max2;
+        return  min2;
 
     }
 }
