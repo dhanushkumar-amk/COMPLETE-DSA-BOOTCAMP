7865843af874745916e50c270c425c8fb41a11c0
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-05 05:13:37 +0530

    modified

--- src/ARRAYS/SecondLargestElement.java
+++ src/ARRAYS/SecondLargestElement.java
@@ -2,10 +2,23 @@ package ARRAYS;
 
 public class SecondLargestElement {
     public static void main(String[] args) {
-
+    int[] arr = {23,24,25,26,27,28};
+        System.out.println(largest(arr));
     }
 
     static  int largest(int[] arr){
-        int tmax
+        int max1 = arr[0];
+        int max2 = arr[0];
+
+        for (int i = 1; i <arr.length ; i++) {
+            if (arr[i] > max1){
+                max2 = max1;
+                max1 = arr[i];
+            } else if (arr[i] > max2) {
+                max2 = arr[i];
+            }
+        }
+        return  max2;
+
     }
 }
