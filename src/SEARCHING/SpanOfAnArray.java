f113b10289b9b3158c8745b582b74536e15fa2c1
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-04 20:16:08 +0530

    modified

--- src/SEARCHING/SpanOfAnArray.java
+++ src/SEARCHING/SpanOfAnArray.java
@@ -2,6 +2,27 @@ package SEARCHING;
 
 public class SpanOfAnArray {
     public static void main(String[] args) {
-        
+        int[] arr ={12,23,43,12,65,34};
+        System.out.println(span(arr));
+    }
+    static  int span(int[] arr){
+        int max = arr[0];
+
+        for (int i = 0; i <arr.length ; i++) {
+            if (arr[i] > max)
+                max = arr[i];
+        }
+
+        int min = arr[0];
+
+        for (int i = 0; i < arr.length ; i++) {
+            if (arr[i] < min)
+                min = arr[i];
+        }
+
+        int span = max - min;
+
+        return span;
+
     }
 }
