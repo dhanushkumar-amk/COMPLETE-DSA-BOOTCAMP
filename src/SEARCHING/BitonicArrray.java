9e6eab31f1ebdc22e9230b28872ebdb74d024187
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-07 11:49:31 +0530

    modified

--- src/SEARCHING/BitonicArrray.java
+++ src/SEARCHING/BitonicArrray.java
@@ -58,7 +58,7 @@ public class BitonicArrray {
 
             if (arr[mid] > arr[mid -1 ] && arr[mid] > arr[mid + 1])
                 return mid;
-            else if (arr[mid] > arr[mid - 1] && arr[mid] < arr[mid - 1])
+            else if (arr[mid] > arr[mid - 1] && arr[mid] < arr[mid + 1])
                 left = mid;
             else
                 right = mid;
