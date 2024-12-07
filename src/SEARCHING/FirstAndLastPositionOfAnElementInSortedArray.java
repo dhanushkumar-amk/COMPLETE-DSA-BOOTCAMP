c61611bfd1baf1f8fc1fd5f648fcbde13361bfdb
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-07 18:40:55 +0530

    modified

--- src/SEARCHING/FirstAndLastPositionOfAnElementInSortedArray.java
+++ src/SEARCHING/FirstAndLastPositionOfAnElementInSortedArray.java
@@ -1,2 +1,10 @@
-package SEARCHING;public class FirstAndLastPositionOfAnElementInSortedArray {
+package SEARCHING;
+
+public class FirstAndLastPositionOfAnElementInSortedArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,4,4,4,5,6,7};
+        int target = 4;
+        System.out.println();
+    }
+    static int
 }
