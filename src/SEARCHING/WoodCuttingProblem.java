c6439c5b92418d96bd6638b3a50473b315abac6b
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-09 09:08:47 +0530

    modified

--- src/SEARCHING/WoodCuttingProblem.java
+++ src/SEARCHING/WoodCuttingProblem.java
@@ -2,6 +2,19 @@ package SEARCHING;
 
 public class WoodCuttingProblem {
     public static void main(String[] args) {
-        
+        int[] height = {20,15,10,17};
+        int targetWood = 7;
+        System.out.println(machines(height, targetWood));
+
+    }
+    static  int machines(int[] height, int targetWood){
+
+        // find maximum value
+        int max = 0;
+        for (int i = 0; i < height.length ; i++) {
+            if (height[i] > max)
+                max =height[i];
+        }
+
     }
 }
