3cec8236d8de44de1597d4c722ed0eb61ffa9246
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-10 19:20:52 +0530

    modified

--- src/SEARCHING/BookAllocationProblem.java
+++ src/SEARCHING/BookAllocationProblem.java
@@ -2,6 +2,14 @@ package SEARCHING;
 
 public class BookAllocationProblem {
     public static void main(String[] args) {
-        
+        int[] pages = {12,34,67,90};
+        int student = 2;
+
     }
+
+    static boolean isPossibleSolution(int mid){
+
+    }
+
+    
 }
