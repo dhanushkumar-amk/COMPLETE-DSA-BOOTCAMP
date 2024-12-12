e4e005ba3b2fbe55220e980d22cf00b9c5cef5c3
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-12 06:36:12 +0530

    modified

--- src/SEARCHING/PainterPartitionProblem.java
+++ src/SEARCHING/PainterPartitionProblem.java
@@ -1,2 +1,9 @@
-package SEARCHING;public class PainterPartitionProlem {
+package SEARCHING;
+
+public class PainterPartitionProblem {
+    public static void main(String[] args) {
+        int[] boards  = {10,20,30,40};
+        
+
+    }
 }
