c12aa1eda866d30571a3badeaedeffdca9a9ece6
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-13 09:24:32 +0530

    modified

--- src/ARRAYS/CheckIfArrayIsSortedOrNOt.java
+++ src/ARRAYS/CheckIfArrayIsSortedOrNOt.java
@@ -1,2 +1,17 @@
-package ARRAYS;public class CheckIfArrayIsSortedOrNOt {
+package ARRAYS;
+
+public class CheckIfArrayIsSortedOrNOt {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        System.out.println(isSorted(arr));
+
+    }
+    static boolean isSorted(int[] arr){
+
+        for (int i = 1; i <arr.length ; i++) {
+            if (arr[i] < arr[i - 1])
+                return false;
+        }
+        return true;
+    }
 }
