88cc17f0e365f6665891b039577a1f02637d668c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-14 08:53:17 +0530

    modified

--- src/ARRAYS/RemoveDuplicatesFromSortedArray.java
+++ src/ARRAYS/RemoveDuplicatesFromSortedArray.java
@@ -2,6 +2,32 @@ package ARRAYS;
 
 public class RemoveDuplicatesFromSortedArray {
     public static void main(String[] args) {
+        int[] arr = {2,2,3,3,4,5,6,6};
+
+        System.out.println("Before");
+        for (int i = 0; i <arr.length ; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        removeDuplicates(arr);
+
         
+
+        for (int i = 0; i <arr.length ; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+    static void removeDuplicates(int[] arr){
+
+        int j = 0;
+
+        for (int i = 1; i <arr.length ; i++) {
+            if (arr[j] != arr[i]){
+                j++;
+                arr[j] = arr[i];
+            }
+        }
     }
 }
