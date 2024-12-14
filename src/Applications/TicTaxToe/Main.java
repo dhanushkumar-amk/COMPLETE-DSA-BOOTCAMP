f73b8ff19cc4579133c80baf13f71e851ff687e8
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-14 12:27:37 +0530

    modified

--- src/Applications/TicTaxToe/Main.java
+++ src/Applications/TicTaxToe/Main.java
@@ -1,7 +1,26 @@
 package Applications.TicTaxToe;
 
+import java.util.*;
+
 public class Main {
+
+
+    // for overall class
+    private static char[][] board;
+    private static int n;
+    private static char currentPlayer;
+
     public static void main(String[] args) {
-        System.out.println("hello world");
-            }
+
+        // get scanner class
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the grid (n * n): ");
+        n = sc.nextInt();
+
+        // create the board
+        board = new char[n][n];
+        
+
+
+    }
 }
