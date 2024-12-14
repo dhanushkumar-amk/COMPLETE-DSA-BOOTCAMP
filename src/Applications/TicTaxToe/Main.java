ff7c170a96686606aaf7ceccb6fe0c8a59ffb643
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-14 12:07:13 +0530

    modified

--- src/Applications/TicTaxToe/Main.java
+++ src/Applications/TicTaxToe/Main.java
@@ -1,2 +1,7 @@
-package Applications.TicTaxToe;public class Main {
+package Applications.TicTaxToe;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 }
