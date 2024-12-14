cea8252decb335356ff8a87675cdc47abe4e5ecb
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-14 08:28:27 +0530

    modified

--- src/SEARCHING/FindSquareOrFloorRootOfAnNumber.java
+++ src/SEARCHING/FindSquareOrFloorRootOfAnNumber.java
@@ -2,6 +2,32 @@ package SEARCHING;
 
 public class FindSquareOrFloorRootOfAnNumber {
     public static void main(String[] args) {
-        
+        int n =  25;
+        System.out.println(sqrt(n));
+    }
+    static int sqrt(int n){
+
+        // base case
+        if (n == 1 || n == 0)
+            return n;
+
+        int low = 2;
+        int high = n/2;
+
+        int result = 0;
+        while (low <= high){
+            int mid = (low + high)/2;
+
+            if (mid * mid == n)
+                return mid;
+            else if (mid * mid < n) {
+                low = mid + 1;
+                result = mid;  // for floor
+            }else {
+                high = mid - 1;
+                //result = mid // for ceil;
+            }
+        }
+        return result;
     }
 }
