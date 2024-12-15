64a204574c16b05bb30ad1cb70b8cf94c7b5d322
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-15 11:47:16 +0530

    modified

--- src/ARRAYS/InverseOfAnArray.java
+++ src/ARRAYS/InverseOfAnArray.java
@@ -1,2 +1,31 @@
-package ARRAYS;public class InverseOfAnArray {
+package ARRAYS;
+
+public class InverseOfAnArray {
+    public static void main(String[] args) {
+        int[] arr = {2,3,1,0,4};
+
+        for (int i = 0; i < args.length ; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+        int[] answer = inverse(arr);
+
+        for (int i = 0; i < answer.length ; i++) {
+            System.out.print(answer[i] + " ");
+        }
+    }
+
+    static int[] inverse(int[] arr){
+
+        int[] ans = new int[arr.length];
+
+        for (int i = 0; i < arr.length ; i++) {
+            int value = arr[i];
+            ans[value] = i;
+        }
+        return ans;
+    }
 }
+
+
