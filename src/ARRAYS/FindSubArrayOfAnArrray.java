16fed37a20e27f0cacc1c67bfada104b9b765a42
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-15 11:30:04 +0530

    modified

--- src/ARRAYS/FindSubArrayOfAnArrray.java
+++ src/ARRAYS/FindSubArrayOfAnArrray.java
@@ -15,7 +15,7 @@ public class FindSubArrayOfAnArrray {
 
         for (int i = 0; i < arr.length ; i++) {
             for (int j = i; j < arr.length ; j++) {
-                for (int k = i; k <j ; k++) {
+                for (int k = i; k <=j ; k++) {
                     System.out.print(arr[k] + " ");
                 }
                 System.out.println();
