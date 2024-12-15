7a0e73841764fdfb15dadcfad7bd841bfd6f8a84
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-15 23:37:20 +0530

    modified

--- src/ARRAYS/LeadersOfAnArray.java
+++ src/ARRAYS/LeadersOfAnArray.java
@@ -13,13 +13,13 @@ public class LeadersOfAnArray {
         for (int i = 0; i <arr.length ; i++) {
             boolean isLeader = true;
             for (int j = i + 1; j < arr.length ; j++) {
-                if (arr[j] > arr[i]){
+                if (arr[j] >= arr[i]){
                     isLeader = false;
                     break;
                 }
             }
             if (isLeader)
-                System.out.println(arr[i] + " ");
+                System.out.print(arr[i] + " ");
         }
     }
 }
