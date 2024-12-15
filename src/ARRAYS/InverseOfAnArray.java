a44f844a9168dd9bb6dcf6847d242bdb1c11cd5c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-15 11:47:34 +0530

    modified

--- src/ARRAYS/InverseOfAnArray.java
+++ src/ARRAYS/InverseOfAnArray.java
@@ -4,7 +4,7 @@ public class InverseOfAnArray {
     public static void main(String[] args) {
         int[] arr = {2,3,1,0,4};
 
-        for (int i = 0; i < args.length ; i++) {
+        for (int i = 0; i < arr.length ; i++) {
             System.out.print(arr[i] + " ");
         }
 
