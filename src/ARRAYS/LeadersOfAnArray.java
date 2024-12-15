9a0209efab7caaceb3ec275ab97aa66c8cb1c59e
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-15 23:36:56 +0530

    modified

--- src/ARRAYS/LeadersOfAnArray.java
+++ src/ARRAYS/LeadersOfAnArray.java
@@ -2,7 +2,24 @@ package ARRAYS;
 
 public class LeadersOfAnArray {
     public static void main(String[] args) {
-
+    int[] arr = {8,11,5,11,7,6,3};
+    leaders1(arr);
     }
-    
+
+
+    // brute force
+    static void leaders1(int[] arr){
+
+        for (int i = 0; i <arr.length ; i++) {
+            boolean isLeader = true;
+            for (int j = i + 1; j < arr.length ; j++) {
+                if (arr[j] > arr[i]){
+                    isLeader = false;
+                    break;
+                }
+            }
+            if (isLeader)
+                System.out.println(arr[i] + " ");
+        }
+    }
 }
