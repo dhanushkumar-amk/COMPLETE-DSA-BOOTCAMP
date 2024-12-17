7694d721fd1cddcd897e19b38844f6ab3297ece6
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-17 21:33:42 +0530

    modified

--- src/ARRAYS/MajorityElements.java
+++ src/ARRAYS/MajorityElements.java
@@ -2,7 +2,7 @@ package ARRAYS;
 
 public class MajorityElements {
     public static void main(String[] args) {
-        int[] arr = {3,1,3};
+        int[] arr = {3,2,3};
         System.out.println(majorityElements1(arr));
     }
 
