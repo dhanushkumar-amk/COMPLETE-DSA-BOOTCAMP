230c518b770cd6aa26b77a6e7c46f4b4ed05e76e
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-17 21:15:14 +0530

    modified

--- src/ARRAYS/MaximumSubArray.java
+++ src/ARRAYS/MaximumSubArray.java
@@ -24,7 +24,7 @@ public class MaximumSubArray {
         int sum = arr[0];
         int maxSum = arr[0];
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 1; i < arr.length; i++) {
             if (sum > 0){
                 sum+= arr[i];
             }else{
