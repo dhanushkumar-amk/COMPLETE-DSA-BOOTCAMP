56006428ea9dae7a9ea8f0ea81f19b56cf34c13b
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-17 10:17:22 +0530

    modified

--- src/ARRAYS/TrappingRainWaterSum.java
+++ src/ARRAYS/TrappingRainWaterSum.java
@@ -3,7 +3,7 @@ package ARRAYS;
 public class TrappingRainWaterSum {
     public static void main(String[] args) {
     int[] height = {4,2,0,3,2,5};
-    
+
     }
 
     static void trap(int[] height){
