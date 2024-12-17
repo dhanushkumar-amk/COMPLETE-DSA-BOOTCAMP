d99001585e89bf46fbc2e3a7daf118cf5cc791a5
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-17 21:34:35 +0530

    modified

--- src/ARRAYS/MajorityElements.java
+++ src/ARRAYS/MajorityElements.java
@@ -10,10 +10,10 @@ public class MajorityElements {
 
         for (int i = 0; i < arr.length; i++) {
             int count  = 0;
-            for (int j = i+ 1; j < arr.length ; j++) {
-
+            for (int j = i + 1; j < arr.length ; j++) {
                 if (arr[i] == arr[j])
-                    count++;
+                    count += 1;
+
             }
             if (count > arr.length /2)
                 return arr[i];
