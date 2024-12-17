3f73761fac6cfd3de851ad7ef63d3b256f2182f1
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-17 09:16:01 +0530

    modified

--- src/ARRAYS/TrappingRainWaterSum.java
+++ src/ARRAYS/TrappingRainWaterSum.java
@@ -2,17 +2,11 @@ package ARRAYS;
 
 public class TrappingRainWaterSum {
     public static void main(String[] args) {
-    int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
-    trap(height);
+    int[] height = {4,2,0,3,2,5};
+    
     }
 
     static void trap(int[] height){
-        int maxmin = Math.min(height[0] , height[height.length - 1]);
 
-        int result = 0;
-        for (int i = 1; i <height.length -1; i++) {
-            result +=  maxmin - height[i];
-        }
-        System.out.println(result);
     }
 }
