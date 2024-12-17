55d87b4611d4c64a3f56f9b48e9fa50c26c67aed
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-17 20:37:44 +0530

    modified

--- src/ARRAYS/MaximumSubArray.java
+++ src/ARRAYS/MaximumSubArray.java
@@ -1,2 +1,21 @@
-package ARRAYS;public class MaximumSubArray {
+package ARRAYS;
+
+public class MaximumSubArray {
+    public static void main(String[] args) {
+        int[] arr = {-1,3,4,-2};
+        System.out.println(maximumSum(arr));
+
+    }
+    static int maximumSum(int[] arr){
+        int maxSum = 0;
+        int sum = 0;
+
+        for (int i = 0; i <arr.length ; i++) {
+            for (int j = i; j < arr.length ; j++) {
+                sum += arr[j];
+            }
+            maxSum = Math.max(sum, maxSum);
+        }
+        return maxSum;
+    }
 }
