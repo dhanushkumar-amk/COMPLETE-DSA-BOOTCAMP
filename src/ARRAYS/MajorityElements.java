3a6dc49cdc053ccdb275e6b179091235972c46c6
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-17 21:31:54 +0530

    modified

--- src/ARRAYS/MajorityElements.java
+++ src/ARRAYS/MajorityElements.java
@@ -1,2 +1,24 @@
-package ARRAYS;public class MajorityElements {
+package ARRAYS;
+
+public class MajorityElements {
+    public static void main(String[] args) {
+        int[] arr = {3,1,3};
+        System.out.println(majorityElements1(arr));
+    }
+
+    static int majorityElements1(int[] arr){
+
+        for (int i = 0; i < arr.length; i++) {
+            int count  = 0;
+            for (int j = i+ 1; j < arr.length ; j++) {
+
+                if ((arr[i] == arr[j]))
+                    count++;
+            }
+
+            if (count > arr.length /2)
+                return arr[i];
+        }
+        return -1;
+    }
 }
