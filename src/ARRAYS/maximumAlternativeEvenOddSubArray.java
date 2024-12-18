3d6f57eb35b5f7cd1cab6bb279da1908ec37bde8
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-18 10:44:09 +0530

    modified

--- src/ARRAYS/maximumAlternativeEvenOddSubArray.java
+++ src/ARRAYS/maximumAlternativeEvenOddSubArray.java
@@ -1,2 +1,23 @@
-package ARRAYS;public class maximumAlternativeEvenOddSubArray {
+package ARRAYS;
+
+public class maximumAlternativeEvenOddSubArray {
+    public static void main(String[] args) {
+        int[] arr = {8,10,11,12};
+        System.out.println(evenOdd(arr));
+    }
+
+
+    static int evenOdd(int[] arr){
+        int maxCount = 0;
+
+        for (int i = 0; i < arr.length ; i++) {
+            int count = 1;
+            for (int j = i + 1; j < arr.length ; j++) {
+                if (arr[j] % 2 == 0 && arr[j - 1]%2  != 0   || arr[j]%2 != 0 && arr[j-1] %2 == 0)
+                    count++;
+            }
+            maxCount = Math.max(maxCount, count);
+        }
+        return ,maxCount;
+    }
 }
