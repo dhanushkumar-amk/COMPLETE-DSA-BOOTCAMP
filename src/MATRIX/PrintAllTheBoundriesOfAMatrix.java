658eef8fe8b2e53ee33900dd46186b082302fbd3
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-19 03:57:46 +0530

    modified

--- src/MATRIX/PrintAllTheBoundriesOfAMatrix.java
+++ src/MATRIX/PrintAllTheBoundriesOfAMatrix.java
@@ -1,2 +1,25 @@
-package MATRIX;public class PrintAllTheBoundriesOfAMatrix {
+package MATRIX;
+
+public class PrintAllTheBoundriesOfAMatrix {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1,2,3,4,5},
+                {6,7,8,9,10},
+                {11,12,13,14,15},
+                {16,17,18,19,20},
+                {21,22,23,24,25},
+        };
+
+
+        int i=0;
+        int j=0;
+
+        // print left to right
+        for (j = 0; j <arr[i].length ; j++) {
+            System.out.print(arr[i][j] + " ");
+        }
+
+        
+
+    }
 }
