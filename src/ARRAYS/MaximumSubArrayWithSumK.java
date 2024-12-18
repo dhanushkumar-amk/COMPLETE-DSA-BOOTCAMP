1a184347ddda5721aa8d9667e73f084fc9604890
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-18 13:58:52 +0530

    modified

--- src/ARRAYS/MaximumSubArrayWithSumK.java
+++ src/ARRAYS/MaximumSubArrayWithSumK.java
@@ -8,12 +8,12 @@ public class MaximumSubArrayWithSumK {
     }
 
     static int sum1(int[] arr, int k){
-
+        int CurrentSum  = 0;
         int MaxSum = Integer.MIN_VALUE;
 
         for (int i = 0; i < arr.length - k ; i++) {
-            int CurrentSum  = 0;
-            for (int j = i; j <= k ; j++) {
+
+            for (int j = i; j < k ; j++) {
                 CurrentSum += arr[j];
             }
 
