ec0112538a9cb1204d5f8f0ce03ef437a1e5c7ad
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-18 14:27:40 +0530

    modified

--- src/ARRAYS/MaximumSubArrayWithSumK.java
+++ src/ARRAYS/MaximumSubArrayWithSumK.java
@@ -4,7 +4,7 @@ public class MaximumSubArrayWithSumK {
     public static void main(String[] args) {
         int[] arr = {2,9,31,-4,21,7};
         int k = 3;
-        System.out.println(sum1(arr, k));
+        System.out.println(sum2(arr, k));
     }
 
     static int sum1(int[] arr, int k){
@@ -20,5 +20,23 @@ public class MaximumSubArrayWithSumK {
         }
 
         return MaxSum;
+    }
+
+
+    // efficient solution  using sliding windows
+    static int sum2(int[] arr, int k){
+
+        int windowSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < k ; i++) {
+            windowSum += arr[i];
+        }
+
+        for (int i = k; i < arr.length ; i++) {
+            windowSum = windowSum - arr[i - k] + arr[i];
+            maxSum = Math.max(maxSum,windowSum);
+        }
+        return maxSum;
     }
 }
