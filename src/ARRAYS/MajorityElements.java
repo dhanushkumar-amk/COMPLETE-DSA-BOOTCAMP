3d8265812a350435aafe0067058495a6510b9053
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-18 09:48:37 +0530

    modified

--- src/ARRAYS/MajorityElements.java
+++ src/ARRAYS/MajorityElements.java
@@ -3,7 +3,7 @@ package ARRAYS;
 public class MajorityElements {
     public static void main(String[] args) {
         int[] arr = {3,2,3};
-        System.out.println(majorityElements1(arr));
+        System.out.println(majorityElements2(arr));
     }
 
     static int majorityElements1(int[] arr){
@@ -19,4 +19,27 @@ public class MajorityElements {
         }
         return -1;
     }
+
+    //efficient solution
+    static int majorityElements2(int[] arr){
+
+        int maj = arr[0];
+        int count = 0;
+
+        for (int i = 0; i <arr.length ; i++) {
+            if (arr[i] == maj)
+                count++;
+            else
+                count--;
+
+
+            if (count == 0){
+                maj = arr[i];
+                count = 1;
+            }
+        }
+
+        return maj;
+    }
+
 }
