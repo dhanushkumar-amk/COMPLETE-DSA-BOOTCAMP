600fd6eeabc051421ff1ed64f960fd7c96deaa55
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-19 02:57:16 +0530

    modified

--- src/MATRIX/PrintMatrixInZigZagForm.java
+++ src/MATRIX/PrintMatrixInZigZagForm.java
@@ -13,12 +13,12 @@ public class PrintMatrixInZigZagForm {
         for (int i = 0; i < arr.length ; i++) {
 
             if (i % 2 == 0){
-                for (int j = 0; j < arr.length; j++) {
+                for (int j = 0; j < arr[i].length; j++) {
                     System.out.print(arr[i][j] + " ");
                 }
             }
             else{
-                for (int j= arr.length - 1; j > 0;j++)
+                for (int j= arr[i].length - 1; j >= 0;j--)
                     System.out.print(arr[i][j] + " ");
             }
             System.out.println();
