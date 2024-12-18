9187b30ac9cc422145efd9fd088ea6a5f25e2860
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-18 16:00:47 +0530

    modified

--- src/ARRAYS/MaximumConsecutiveFlips.java
+++ src/ARRAYS/MaximumConsecutiveFlips.java
@@ -1,2 +1,21 @@
-package ARRAYS;public class MaximumConsecutiveFlips {
+package ARRAYS;
+
+public class MaximumConsecutiveFlips {
+    public static void main(String[] args) {
+        int[] arr =
+    }
+
+    static void maximumFlips(int[] arr){
+
+        for (int i = 0; i < arr.length ; i++) {
+            if (arr[i] != arr[i - 1]){
+                if (arr[i] != arr[0])
+                    System.out.print(i + " ");
+                else
+                    System.out.print(i - 1);
+            }
+
+            System.out.println();
+        }
+    }
 }
