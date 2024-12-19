fb6169d237bd788057e05a5cc0f5c7cb776c3977
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-19 16:32:51 +0530

    modified

--- src/MATRIX/MedianOfTheMatrix.java
+++ src/MATRIX/MedianOfTheMatrix.java
@@ -1,2 +1,15 @@
-package MATRIX;public class MedianOfTheMatrix {
+package MATRIX;
+
+public class MedianOfTheMatrix {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {5,11,20,35,45},
+                {1,2,3,4,6},
+                {13,15,16,19,21},
+        };
+    }
+
+    static int median1(int[][] arr){
+        
+    }
 }
