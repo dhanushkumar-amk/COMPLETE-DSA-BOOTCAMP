d46b7b9392760cbc283d3c4f14f4159984dc1778
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-19 21:43:33 +0530

    modified

--- src/MATRIX/TransposeMatrix.java
+++ src/MATRIX/TransposeMatrix.java
@@ -48,5 +48,13 @@ public class TransposeMatrix {
 
 
     // efficient solution
-    
+    static void transpose2(int[][] arr){
+        for (int i = 0; i < arr.length - 1 ; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+    }
 }
