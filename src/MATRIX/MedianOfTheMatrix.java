df75013b2aa6ee57cec3bd5e0a0601d46ee10aee
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-19 16:50:14 +0530

    modified

--- src/MATRIX/MedianOfTheMatrix.java
+++ src/MATRIX/MedianOfTheMatrix.java
@@ -10,6 +10,32 @@ public class MedianOfTheMatrix {
     }
 
     static int median1(int[][] arr){
-        
+
+        int min =arr[0][0];
+        int max =arr[0][0];
+
+
+        for (int i = 0; i < arr.length; i++) {
+
+            // min value
+            if (arr[i][0] < min)
+                min = arr[i][0];
+
+            // max value
+            if (arr[i][arr[0].length - 1] > max)
+                max =  arr[i][arr[0].length - 1];
+        }
+
+
+
+        while(min < max){
+            int mid = (min + max)/2;
+
+            int count = 0;
+            
+        }
+
+
+
     }
 }
