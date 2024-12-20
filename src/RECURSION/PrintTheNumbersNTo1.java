f888dba025ecb77d4ee702c70ced7753c34bbf94
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-20 11:00:56 +0530

    modified

--- src/RECURSION/PrintTheNumbersNTo1.java
+++ src/RECURSION/PrintTheNumbersNTo1.java
@@ -1,2 +1,16 @@
-package RECURSION;public class PrintTheNumbersNTo1 {
+package RECURSION;
+
+public class PrintTheNumbersNTo1 {
+    public static void main(String[] args) {
+        numbers(5);
+    }
+
+    static void numbers(int n){
+
+        if (n < 1)
+            return;
+
+        System.out.println(n);
+            numbers(n - 1);
+    }
 }
