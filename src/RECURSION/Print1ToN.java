23efa994b7b7ecf0b6bb4ae593e16cc6fd41fc5e
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-20 11:26:02 +0530

    modified

--- src/RECURSION/Print1ToN.java
+++ src/RECURSION/Print1ToN.java
@@ -1,2 +1,16 @@
-package RECURSION;public class Print1ToN {
+package RECURSION;
+
+public class Print1ToN {
+    public static void main(String[] args) {
+        numbers(5);
+    }
+
+    static void numbers(int n){
+
+        if (n < 1)
+            return;
+
+        numbers(n - 1);
+        System.out.println(n);
+    }
 }
