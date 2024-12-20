5fa5d83d52c29cf0988717b76a64d47a844400bc
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-20 08:07:42 +0530

    modified

--- src/MATRIX/ReverseAColumnOfAMatrix.java
+++ src/MATRIX/ReverseAColumnOfAMatrix.java
@@ -9,6 +9,14 @@ public class ReverseAColumnOfAMatrix {
             {4,9,15,19,24},
             {5,10,15,20,25},
         };
+        for (int i = 0; i < arr.length ; i++) {
+            for (int j = 0; j <arr[i].length ; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
 
     reverseColumn(arr);
 
