725e844235382c4eb20e848887b6fc26ac2c63f9
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-20 11:22:17 +0530

    modified

--- src/MATHS/Fiboacci.java
+++ src/MATHS/Fiboacci.java
@@ -3,12 +3,13 @@ package MATHS;
 public class Fiboacci {
     public static void main(String[] args) {
 
+        int n = 4;
         int n1 = 0;
         int n2 = 1;
         int n3 = 0;
         System.out.println(n1);
         System.out.println(n2);
-        for (int i = 1; i <=n; i++) {
+        for (int i = 1; i <= n; i++) {
              n3 = n1 + n2;
             n1 = n2;
             n2 = n3;
