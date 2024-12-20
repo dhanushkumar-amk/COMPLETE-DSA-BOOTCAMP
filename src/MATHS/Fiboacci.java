335b4565ec914f3221af01cac43b491473c76c22
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-20 11:20:26 +0530

    modified

--- src/MATHS/Fiboacci.java
+++ src/MATHS/Fiboacci.java
@@ -1,2 +1,17 @@
-package MATHS;public class Fiboacci {
+package MATHS;
+
+public class Fiboacci {
+    public static void main(String[] args) {
+        int n = 5;
+
+        int n1 = 0;
+        int n2 = 1;
+        for (int i = 1; i <=n; i++) {
+            int n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+
+            System.out.println(n3);
+        }
+    }
 }
