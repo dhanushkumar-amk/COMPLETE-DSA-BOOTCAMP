84aa767a2a0b3b76df8b0fe25298c336fa7e81b3
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-21 17:18:26 +0530

    modified

--- src/RECURSION/ReverseAString.java
+++ src/RECURSION/ReverseAString.java
@@ -1,2 +1,17 @@
-package RECURSION;public class ReverseAString {
+package RECURSION;
+
+public class ReverseAString {
+    public static void main(String[] args) {
+     String s = "hello";
+     String r = "";
+     int index  = s.length()-1;
+        System.out.println(reverse(s,r,index));
+    }
+
+    static  String reverse(String s, String r, int index){
+        if (index < 0)
+            return r;
+
+        return reverse(s, r+s.charAt(index), index - 1);
+    }
 }
