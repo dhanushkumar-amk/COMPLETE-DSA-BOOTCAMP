3f91ceec6507bb9a71caed9394abadefd53f7f8f
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-21 09:45:23 +0530

    modified

--- src/RECURSION/SumOfAllDigits.java
+++ src/RECURSION/SumOfAllDigits.java
@@ -1,2 +1,14 @@
-package RECURSION;public class SumOfAllDigits {
+package RECURSION;
+
+public class SumOfAllDigits {
+    public static void main(String[] args) {
+        int n = 1234;
+        System.out.println(sum(n));
+    }
+    static int sum(int n){
+        if (n == 0)
+            return 0;
+
+        return sum(n/10) + n%10;
+    }
 }
