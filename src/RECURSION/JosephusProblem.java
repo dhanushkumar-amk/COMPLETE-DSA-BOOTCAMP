72146a85cdf6f625ca9ca0ec986311eefa3f5a90
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-21 19:18:54 +0530

    modified

--- src/RECURSION/JosephusProblem.java
+++ src/RECURSION/JosephusProblem.java
@@ -2,9 +2,15 @@ package RECURSION;
 
 public class JosephusProblem {
     public static void main(String[] args) {
-        
+        int n = 7;
+        int k = 3;
+
+        System.out.println(joes(n,k));
     }
     static int joes(int n, int k){
+    if (n == 1)
+        return 0;   // o is an index of a current D position
 
+        return (joes(n -1, k) + k) % n;
     }
 }
