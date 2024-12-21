e3c6ea25c08d37210eeddfdb2929aa9cf7d77273
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-21 18:26:15 +0530

    modified

--- src/RECURSION/SumOfAllElementsInArray.java
+++ src/RECURSION/SumOfAllElementsInArray.java
@@ -1,2 +1,17 @@
-package RECURSION;public class SumOfAllElementsInArray {
+package RECURSION;
+
+public class SumOfAllElementsInArray {
+    public static void main(String[] args) {
+       int[] arr = {1,2,3,4};
+       int i = arr.length - 1;
+        System.out.println(sum(arr, i));
+    }
+    static int sum(int[] arr, int i){
+
+
+        if (i == 0)
+            return 0;
+
+        return sum(arr, i -1) +arr[i];
+    }
 }
