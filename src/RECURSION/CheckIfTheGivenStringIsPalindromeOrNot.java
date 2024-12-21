55a9e332ea12d9eadc2102021914761a73d8b5ed
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-21 18:08:42 +0530

    modified

--- src/RECURSION/CheckIfTheGivenStringIsPalindromeOrNot.java
+++ src/RECURSION/CheckIfTheGivenStringIsPalindromeOrNot.java
@@ -3,6 +3,18 @@ package RECURSION;
 public class CheckIfTheGivenStringIsPalindromeOrNot {
     public static void main(String[] args) {
         String s = "madam";
-        
+        int i = 0;
+        int j= s.length()-1;
+        System.out.println(isPalindrome(s, i,j));
+
+    }
+    static boolean isPalindrome(String s, int i, int j){
+        if (i >= j)
+            return true;
+
+        if (s.charAt(i) != s.charAt(j))
+            return false;
+
+        return isPalindrome(s, i+1, j-1);
     }
 }
