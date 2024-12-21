0e61b6ad95b3b635f47d75ac7d4caec233ddbba8
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-21 09:27:55 +0530

    modified

--- src/RECURSION/CountNumberOfDigitsInAGivenValue.java
+++ src/RECURSION/CountNumberOfDigitsInAGivenValue.java
@@ -1,2 +1,15 @@
-package RECURSION;public class CountNumberOfDigitsInAGivenValue {
+package RECURSION;
+
+public class CountNumberOfDigitsInAGivenValue {
+    public static void main(String[] args) {
+        int n = 12345;
+        System.out.println(count(n));
+    }
+    static int count(int n){
+
+        if (n == 0)
+            return 0;
+
+        return count(n / 10) + 1;
+    }
 }
