bc84758537afd2ffd447996c90cc604688ed570b
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-21 07:48:41 +0530

    modified

--- src/RECURSION/FactorialOfANumber.java
+++ src/RECURSION/FactorialOfANumber.java
@@ -6,9 +6,11 @@ public class FactorialOfANumber {
     }
     static int factorial(int n){
 
+        // base case
         if (n == 1)
             return 1;
 
+        // recursive case
         return n * factorial(n - 1);
 
 
