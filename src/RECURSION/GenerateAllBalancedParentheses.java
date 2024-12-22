e252fb3688685cb1c7e30867957aa2c8326b4d02
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-22 09:16:08 +0530

    modified

--- src/RECURSION/GenerateAllBalancedParentheses.java
+++ src/RECURSION/GenerateAllBalancedParentheses.java
@@ -1,2 +1,32 @@
-package RECURSION;public class GenerateAllBalancedParentheses {
+package RECURSION;
+
+public class GenerateAllBalancedParentheses {
+    public static void main(String[] args) {
+
+        int n = 2;
+        char[] arr = new char[n * 2];
+        int openingBraceCount = 0;
+        int closingBraceCount = 0;
+        int index = 0;
+
+        generate(arr, n, index, openingBraceCount, closingBraceCount);
+
+    }
+
+    static void generate(char[] arr, int n, int index, int openingBraceCount, int closingBraceCount){
+
+        // base case
+        if (index == arr.length)
+            System.out.println("The possible answer is : " + arr);
+
+        if(openingBraceCount < n){
+            arr[index] = 'C';
+            generate(arr, n, index + 1, openingBraceCount + 1, closingBraceCount);
+        }
+
+        if (closingBraceCount < n){
+            arr[index] = ')';
+            generate(arr, n, index + 1, openingBraceCount, closingBraceCount + 1);
+        }
+    }
 }
