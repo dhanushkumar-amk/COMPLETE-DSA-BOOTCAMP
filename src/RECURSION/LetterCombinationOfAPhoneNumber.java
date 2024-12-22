d088592293df3696e867f9dc9fbac37991653aed
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-22 12:43:58 +0530

    modified

--- src/RECURSION/LetterCombinationOfAPhoneNumber.java
+++ src/RECURSION/LetterCombinationOfAPhoneNumber.java
@@ -2,11 +2,11 @@ package RECURSION;
 
 public class LetterCombinationOfAPhoneNumber {
 
-    static String[] keypad = {"", "", "abc", "def", "ghi", "jkl",""}
+    static String[] keypad = {"", "", "abc", "def", "ghi", "jkl","mno", "pqrs", "tuv", "wxyz"};
 
     public static void main(String[] args) {
 
     }
 
-    static void
+    static void possibleWords(String s, A)
 }
