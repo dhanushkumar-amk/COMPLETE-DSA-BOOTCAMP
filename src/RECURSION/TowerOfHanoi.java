31e925d2d99b5a32c8bdfb24cba641049791edc9
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-23 20:20:09 +0530

    modified

--- src/RECURSION/TowerOfHanoi.java
+++ src/RECURSION/TowerOfHanoi.java
@@ -1,2 +1,21 @@
-package RECURSION;public class TowerOfHanoi {
+package RECURSION;
+
+public class TowerOfHanoi {
+    public static void main(String[] args) {
+        tower(3, 'A', 'B', 'C');
+    }
+
+    static void tower(int n , char source, char auxiliary, char destination){
+
+        // base case
+        if (n == 1){
+            System.out.println(source + " -> " + destination);
+            return;
+        }
+
+        tower(n-1,source, destination, auxiliary );
+        tower(n, source, auxiliary, destination);
+        tower(n-1, auxiliary,source,destination);
+
+    }
 }
