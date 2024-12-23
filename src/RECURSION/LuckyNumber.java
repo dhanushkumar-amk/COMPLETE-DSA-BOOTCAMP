fa31173ee0c4fd1d1d92e2363e1f5a39c694ef27
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-23 19:55:08 +0530

    modified

--- src/RECURSION/LuckyNumber.java
+++ src/RECURSION/LuckyNumber.java
@@ -2,7 +2,19 @@ package RECURSION;
 
 public class LuckyNumber {
     public static void main(String[] args) {
+    int n = 13;
+    int counter = 2;
 
+        System.out.println(isLuckyNumber(n, counter));
     }
-    
+    static boolean isLuckyNumber(int n, int counter){
+
+        if (n < counter)
+            return true;
+
+        if (n % counter == 0)
+            return false;
+
+        return isLuckyNumber(n - (n/counter), counter + 1);
+    }
 }
