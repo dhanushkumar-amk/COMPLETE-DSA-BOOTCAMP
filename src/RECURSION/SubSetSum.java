11f271c05116fd1e344b93ae9708190146bd03f1
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-23 19:29:29 +0530

    modified

--- src/RECURSION/SubSetSum.java
+++ src/RECURSION/SubSetSum.java
@@ -3,7 +3,25 @@ package RECURSION;
 public class SubSetSum {
     public static void main(String[] args) {
 
+        int[] arr = {10,20,1,5};
+        int sum = 25;
+
+        System.out.println(countSubSets(arr, sum, 0));
     }
 
-    static int 
+    static int countSubSets(int[] arr, int sum, int i){
+
+        // base case
+        if (sum < 0)
+            return 0;
+
+        if (sum == 0)
+            return 1;
+
+        if (i == arr.length)
+            return 0;
+
+        return countSubSets(arr, sum - arr[i], i++) + countSubSets(arr, sum, i++);
+
+    }
 }
