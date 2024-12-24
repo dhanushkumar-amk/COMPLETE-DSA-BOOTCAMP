39cce4f7ef246501c03932c5245e2eb712adfe4d
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-24 19:21:48 +0530

    modified

--- src/RECURSION/PowerOfNumbers.java
+++ src/RECURSION/PowerOfNumbers.java
@@ -1,2 +1,17 @@
-package RECURSION;public class PowerOfNumbers {
+package RECURSION;
+
+public class PowerOfNumbers {
+    public static void main(String[] args) {
+        System.out.println(power(5,8));
+    }
+
+    static long power(int x, int y){
+
+
+        if (y == 0)
+            return 1;
+
+        return power( x, y-1) * x;
+
+    }
 }
