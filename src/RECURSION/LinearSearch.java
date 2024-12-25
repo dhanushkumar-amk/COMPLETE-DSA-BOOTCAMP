d8422055598d0eac0eb344d93c5b64cff347d3f8
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-25 18:03:22 +0530

    modified

--- src/RECURSION/LinearSearch.java
+++ src/RECURSION/LinearSearch.java
@@ -4,7 +4,7 @@ public class LinearSearch {
     public static void main(String[] args) {
 
         int[] arr = {1,2,3,4,5};
-        System.out.println(search(arr, 4, 0));
+        System.out.println(search(arr, 41, 0));
     }
     static boolean search(int [] arr, int target, int i){
 
