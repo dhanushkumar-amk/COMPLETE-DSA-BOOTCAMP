03b71a6c6205b52969728f0b5c13bf0ceaa12321
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-25 14:52:13 +0530

    modified

--- src/RECURSION/CountNoOfZerosOfAnNumber.java
+++ src/RECURSION/CountNoOfZerosOfAnNumber.java
@@ -1,2 +1,22 @@
-package RECURSION;public class CountNoOfZerosOfAnNumber {
+package RECURSION;
+
+public class CountNoOfZerosOfAnNumber {
+    public static void main(String[] args) {
+
+        int n = 1000;
+
+        int count = 0;
+        while(n != 0){
+            int rem = n % 10;
+            if (rem == 0) {
+                count++;
+                n = n / 10;
+            }else {
+                n = n/10;
+            }
+        }
+        System.out.println(count);
+
+    }
+
 }
