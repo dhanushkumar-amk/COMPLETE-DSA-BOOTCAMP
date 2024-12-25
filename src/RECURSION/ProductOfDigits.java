e7e74849d408e5c61fe6c1cf94de068851b4fd58
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-25 08:22:52 +0530

    modified

--- src/RECURSION/ProductOfDigits.java
+++ src/RECURSION/ProductOfDigits.java
@@ -6,8 +6,8 @@ public class ProductOfDigits {
         System.out.println(product(n));
     }
     static int product(int n){
-        if (n == 0)
-            return 0;
+        if (n% 10  == n)
+            return n;
 
         return product(n/ 10) * product(n % 10);
     }
