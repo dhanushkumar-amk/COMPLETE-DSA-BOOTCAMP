06236d1dd6ca503c14bae521b21d88329cad5042
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-25 17:39:16 +0530

    modified

--- src/RECURSION/ArrayIsSortedOrNot.java
+++ src/RECURSION/ArrayIsSortedOrNot.java
@@ -2,7 +2,7 @@ package RECURSION;
 
 public class ArrayIsSortedOrNot {
     public static void main(String[] args) {
-        int[] arr = {1,2,31,4,5};
+        int[] arr = {1,2,3,4,5};
         System.out.println(isSorted(arr, 0));
     }
     static boolean isSorted(int[] arr, int i){
