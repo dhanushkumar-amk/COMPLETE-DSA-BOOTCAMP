d60209feee39c4b714eb4370ecaa6d38c5edbeff
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-25 14:05:57 +0530

    modified

--- src/RECURSION/ReverseANumber.java
+++ src/RECURSION/ReverseANumber.java
@@ -3,14 +3,22 @@ package RECURSION;
 public class ReverseANumber {
     public static void main(String[] args) {
 
+        int n = 522;
+        reverse1(n);
+        System.out.println(sum);
     }
-    static int  sum  = 0;
-    static void reverse(int n){
 
-        if (sum == 0)
+    static int  sum  = 0;
+    static void reverse1(int n){
+
+        if (n == 0)
             return;
 
 
+        int remainder = n % 10;
+        sum = sum * 10 + remainder;
+
+        reverse1(n / 10);
 
 
     }
