38b2a8c47415320fda5d053ef64847917df121f6
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-25 14:42:46 +0530

    modified

--- src/RECURSION/PalindromeNumberOrNot.java
+++ src/RECURSION/PalindromeNumberOrNot.java
@@ -1,17 +1,30 @@
 package RECURSION;
 
+
 public class PalindromeNumberOrNot {
     public static void main(String[] args) {
-
+        int n = 1234;
+        System.out.println(isPalindrome(n));
     }
 
     static boolean isPalindrome(int n){
         return n == reverse(n);
     }
 
-    static void reverse(int n){
+    static int reverse(int n){
+        int digits = (int) (Math.log10(n)) + 1;
+
+        return helper(n, digits);
+    }
+
+   private static int helper(int n, int digits){
+
         if (n % 10 == n)
-            return;
+            return n;
+
+        int rem = n % 10;
+
+        return rem * (int) Math.pow(10, digits) + helper(n / 10, digits - 1);
     }
 
 }
