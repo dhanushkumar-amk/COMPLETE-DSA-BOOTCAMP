241f4ce03d11897162991d4d36ac306e2390c06e
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-25 18:11:28 +0530

    modified

--- src/RECURSION/LinearSearch.java
+++ src/RECURSION/LinearSearch.java
@@ -4,7 +4,7 @@ public class LinearSearch {
     public static void main(String[] args) {
 
         int[] arr = {1,2,3,4,5};
-        System.out.println(search(arr, 4, 0));
+        System.out.println(searc1(arr, 4, 0));
     }
 
     // basic linear search
@@ -14,6 +14,17 @@ public class LinearSearch {
             return false;
 
         return arr[i] == target || search(arr, target, i+1);
+    }
+
+    // return an index value
+    static int search1(int[] arr, int target, int i){
+        if (i == arr.length)
+            return -1;
+
+        if (arr[i] == target)
+            return i;
+        else
+            return search1(arr, target, i + 1);
     }
 
     // return as a list
