8725e2e93f8cdf5910cea67ee6df42a3907f874b
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-25 09:01:21 +0530

    modified

--- src/RECURSION/ReverseANumber.java
+++ src/RECURSION/ReverseANumber.java
@@ -1,2 +1,14 @@
-package RECURSION;public class ReverseANumber {
+package RECURSION;
+
+public class ReverseANumber {
+    public static void main(String[] args) {
+
+    }
+    int  sum  = 0;
+    static void reverse(int n){
+
+        if (sum == 0)
+            
+
+    }
 }
