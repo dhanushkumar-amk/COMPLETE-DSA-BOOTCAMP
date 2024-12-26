0853b9db3c1cfb430d0d67b648845b7bd28618ea
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-26 20:21:42 +0530

    modified

--- src/ARRAYS/ReverseArrayAndRemoveDuplicates.java
+++ src/ARRAYS/ReverseArrayAndRemoveDuplicates.java
@@ -21,7 +21,8 @@ public class ReverseArrayAndRemoveDuplicates {
         int j=0;
 
         for (int i = 0; i < arr.length; i++) {
-
+            if(arr[j] != arr[i])
+                j++;
         }
     }
 
