087eba923faa472f30bcd59fdd635fdded6b16af
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-26 20:35:43 +0530

    modified

--- src/ARRAYS/ReverseArrayAndRemoveDuplicates.java
+++ src/ARRAYS/ReverseArrayAndRemoveDuplicates.java
@@ -1,62 +1,53 @@
 package ARRAYS;
 
-import java.util.Arrays;
-
 public class ReverseArrayAndRemoveDuplicates {
 
     public static void main(String[] args) {
 
-
         int[] arr = {1, 2, 3, 3, 4, 5, 6, 6};
 
+        // Step 1: Reverse the array
         reverse(arr);
 
-        for (int i = 0; i < arr.length ; i++) {
+        System.out.println("Reversed Array:");
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
 
         System.out.println();
 
-        int ans = removeDuplicates(arr);
-        for (int i = ans - 1; i >=0  ; i--) {
+        // Step 2: Remove duplicates from the reversed array
+        int newSize = removeDuplicates(arr);
+
+        System.out.println("Array after removing duplicates (reversed):");
+        for (int i = 0; i < newSize; i++) {
             System.out.print(arr[i] + " ");
         }
-
-
-
     }
 
+    static int removeDuplicates(int[] arr) {
+        int j = 0; // Pointer for the position of unique elements
 
-
-
-
-
-    static  int removeDuplicates(int[] arr){
-
-        Arrays.sort(arr);
-
-        int j=0;
-            for (int i = 1; i < arr.length; i++) {
-                if (arr[i] != arr[j]) { // Only add unique elements
-                    j++;
-                    arr[j] = arr[i];
-                }
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[j]) { // Compare with the last unique element
+                j++;
+                arr[j] = arr[i];
             }
-            return j + 1;
+        }
+
+        return j + 1; // New size of the array with unique elements
     }
 
-     static void reverse(int[] arr){
+    static void reverse(int[] arr) {
         int start = 0;
-        int end = arr.length -1;
+        int end = arr.length - 1;
 
-        while (start <= end){
+        while (start < end) {
             int temp = arr[start];
             arr[start] = arr[end];
             arr[end] = temp;
             start++;
             end--;
         }
-     }
+    }
 }
-
-
