ce8a61a29c4727d34867ad896a60e7af2d7e5b97
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-28 07:58:10 +0530

    modified

--- src/RECURSION/SelectionSort.java
+++ src/RECURSION/SelectionSort.java
@@ -2,6 +2,26 @@ package RECURSION;
 
 public class SelectionSort {
     public static void main(String[] args) {
+        int[] arr = {2,8,5,9,22,2,1};
+        selectionSort(arr);
+    }
+
+    static void selectionSort(int[] arr,int row, int column, int max){
+
+        if (row == 0)
+            return;
+
+        if (column < row){
+            if (arr[column] > arr[max]){
+                selectionSort(arr, row, column + 1, column);
+            }
+            else{
+                selectionSort(arr, row, column +1, max);
+            }
+        }else{
+            
+        }
 
     }
+
 }
