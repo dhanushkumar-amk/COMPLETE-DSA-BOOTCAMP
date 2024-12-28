e22da15e0265331918fd783acdd58d5bc3cb8079
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-28 07:43:31 +0530

    modified

--- src/RECURSION/BubbleSort.java
+++ src/RECURSION/BubbleSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = {4,3,2,1};
-        bubbleSort(arr, 4, 0);
+        bubbleSort(arr, arr.length - 1, 0);
         System.out.println(Arrays.toString(arr));
     }
 
