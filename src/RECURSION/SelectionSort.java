ac4d5a22f8fba24ee848ee2de4a1eff235db4475
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-28 08:43:51 +0530

    modified

--- src/RECURSION/SelectionSort.java
+++ src/RECURSION/SelectionSort.java
@@ -1,9 +1,12 @@
 package RECURSION;
 
+import java.util.Arrays;
+
 public class SelectionSort {
     public static void main(String[] args) {
         int[] arr = {2,8,5,9,22,2,1};
-        selectionSort(arr);
+        selectionSort(arr, arr.length,0,0);
+        System.out.println(Arrays.toString(arr));
     }
 
     static void selectionSort(int[] arr,int row, int column, int max){
@@ -19,7 +22,11 @@ public class SelectionSort {
                 selectionSort(arr, row, column +1, max);
             }
         }else{
-            
+            int temp = arr[max];
+            arr[max]  = arr[row -1];
+            arr[row - 1] = temp;
+
+            selectionSort(arr, row-1, 0, 0);
         }
 
     }
