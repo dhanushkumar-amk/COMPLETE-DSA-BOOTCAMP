15dcfd92d50ecb12c71c1d9ca91aefdcfcc10cc2
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-28 07:28:39 +0530

    modified

--- src/RECURSION/PatternPrinting.java
+++ src/RECURSION/PatternPrinting.java
@@ -6,5 +6,16 @@ public class PatternPrinting {
     }
     static void pattern(int row, int column){
 
+        if( row == 0)
+            return;
+
+        if (column < row){
+            System.out.print(" * ");
+            pattern(row, column + 1);
+        }
+        else{
+            System.out.println();
+            pattern(row -1, 0);
+        }
     }
 }
