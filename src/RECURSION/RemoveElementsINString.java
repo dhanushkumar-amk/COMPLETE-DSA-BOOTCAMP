bb00fbe21ef91ed9bf8991c6513d735cabeca542
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-28 20:05:56 +0530

    modified

--- src/RECURSION/RemoveElementsINString.java
+++ src/RECURSION/RemoveElementsINString.java
@@ -3,6 +3,25 @@ package RECURSION;
 public class RemoveElementsINString {
     public static void main(String[] args) {
 
+        skip("", "abacde");
     }
-    
+
+
+    static void skip(String result, String original){
+
+        if (original.isEmpty()){
+            System.out.println(result);
+            return;
+        }
+
+        char character  = original.charAt(0);
+
+        if (character == 'a'){
+            skip(result, original.substring(1));
+        }
+        else{
+            skip(result + character, original.substring(1));
+        }
+    }
+
 }
