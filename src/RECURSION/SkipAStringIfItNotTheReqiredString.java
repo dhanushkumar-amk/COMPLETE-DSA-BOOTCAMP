5c44a7be11a0a0743db050ab8aaa3c8d81b645e6
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-29 07:12:03 +0530

    modified

--- src/RECURSION/SkipAStringIfItNotTheReqiredString.java
+++ src/RECURSION/SkipAStringIfItNotTheReqiredString.java
@@ -1,2 +1,19 @@
-package RECURSION;public class SkipAStringIfItNotTheReqiredString {
+package RECURSION;
+
+public class SkipAStringIfItNotTheReqiredString {
+    public static void main(String[] args) {
+        String s = "helloappapple";
+        System.out.println(skipRequired(s));
+    }
+
+    static String skipRequired(String s){
+
+        if (s.isEmpty())
+            return "";
+
+        if (s.startsWith("app") && !s.startsWith("apple"))
+            return skipRequired(s.substring(3));
+        else
+            return s.charAt(0) + skipRequired(s.substring(1));
+    }
 }
