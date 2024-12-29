d8504a814fdc028ed166384558daaf29584189df
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-29 07:00:38 +0530

    modified

--- src/RECURSION/SkipAString.java
+++ src/RECURSION/SkipAString.java
@@ -2,8 +2,20 @@ package RECURSION;
 
 public class SkipAString {
     public static void main(String[] args) {
-
+        String s = "helloappleworld";
+        System.out.println(SkipApple(s));
     }
 
+
+    static String SkipApple(String s){
+
+        if (s.isEmpty())
+            return "";
+
+        if (s.startsWith("apple"))
+            return SkipApple(s.substring(5));
+        else
+            return s.charAt(0) + SkipApple(s.substring(1));
+    }
 
 }
