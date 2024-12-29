6a4af9cf13f66ca59416603f26e95998dc5345a7
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-29 10:37:43 +0530

    modified

--- src/RECURSION/MAZE/PrintingAllPossiblePathsINOnlyRightAndDownDirections.java
+++ src/RECURSION/MAZE/PrintingAllPossiblePathsINOnlyRightAndDownDirections.java
@@ -1,12 +1,16 @@
 package RECURSION.MAZE;
 
+import java.util.ArrayList;
+
 public class PrintingAllPossiblePathsINOnlyRightAndDownDirections {
     public static void main(String[] args) {
         int row = 3;
         int column = 3;
         String answer = "";
 
-        printPaths(answer, row, column);
+//        printPaths(answer, row, column);
+
+        System.out.println(path("", 3,3));
 
     }
 
@@ -22,5 +26,29 @@ public class PrintingAllPossiblePathsINOnlyRightAndDownDirections {
 
         if (column > 1 )
             printPaths(answer + 'R', row, column - 1); // going right
+    }
+
+
+
+    // added to arraylist
+    static ArrayList<String> path(String answer, int row, int column){
+        if (row == 1 && column == 1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(answer);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+
+        if (row > 1){
+            list.addAll(path(answer + 'D', row - 1, column));
+        }
+
+        if (column > 1){
+            list.addAll(path(answer + 'R', row, column - 1));
+        }
+
+
+        return list;
     }
 }
