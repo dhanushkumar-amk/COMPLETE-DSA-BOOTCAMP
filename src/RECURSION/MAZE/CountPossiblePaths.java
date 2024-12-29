5aeab270c33fdda13021510568a2cc3ef3ea4b73
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-29 09:52:58 +0530

    modified

--- src/RECURSION/MAZE/CountPossiblePaths.java
+++ src/RECURSION/MAZE/CountPossiblePaths.java
@@ -1,2 +1,21 @@
-package RECURSION.MAZE;public class CountPossiblePaths {
+package RECURSION.MAZE;
+
+public class CountPossiblePaths {
+    public static void main(String[] args) {
+
+        int row = 3;
+        int column = 3;
+        System.out.println( countPath(row, column));
+    }
+    static  int countPath(int row, int column){
+
+        if (row == 1 || column == 1)
+            return 1;
+
+        int right = countPath(row, column - 1);
+        int down = countPath(row - 1, column);
+
+        return down + right;
+
+    }
 }
