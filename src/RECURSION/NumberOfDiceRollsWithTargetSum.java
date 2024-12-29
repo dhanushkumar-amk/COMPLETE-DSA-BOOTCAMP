23259494306bb2560a8e4489c5ccbf3c679bc55e
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-29 08:16:03 +0530

    modified

--- src/RECURSION/NumberOfDiceRollsWithTargetSum.java
+++ src/RECURSION/NumberOfDiceRollsWithTargetSum.java
@@ -2,7 +2,8 @@ package RECURSION;
 
 public class NumberOfDiceRollsWithTargetSum {
     public static void main(String[] args) {
-        dice("", 4);
+       // dice("", 4);
+        diceCustomFace("", 4, 9);
     }
 
     static void dice(String answer, int target){
@@ -14,6 +15,23 @@ public class NumberOfDiceRollsWithTargetSum {
 
         for (int i = 1; i <= 6 && i <= target ; i++) {
             dice(answer + i, target - i);
+        }
+
+    }
+
+
+
+
+    // multiple face dice based problem
+    static void diceCustomFace(String answer, int target, int face){
+
+        if (target == 0){
+            System.out.println(answer);
+            return;
+        }
+
+        for (int i = 1; i <= 6 && i <= target ; i++) {
+            diceCustomFace(answer + i, target - i, face);
         }
 
     }
