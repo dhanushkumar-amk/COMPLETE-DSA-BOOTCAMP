c552a91bdf571a8cb97b5157ad1a8b2df12a9def
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-29 17:56:22 +0530

    modified

--- src/RECURSION/BACKTRACKING/NQueens.java
+++ src/RECURSION/BACKTRACKING/NQueens.java
@@ -2,6 +2,75 @@ package RECURSION.BACKTRACKING;
 
 public class NQueens {
     public static void main(String[] args) {
-        
+        int n = 4;
+        boolean[][] board = new boolean[n][n];
+        queens(board, 0);
+    }
+
+    static int queens(boolean[][] board, int row){
+
+        if (row == board.length){
+            display(board);
+            return 1;
+        }
+
+
+        int count = 0;
+
+        // placing the queen and checking for every row and column
+        for (int column = 0; column < board.length; column++) {
+            // place the queen if it is safe
+
+            if (isSafe(board, row, column)){
+                board[row][column] = true;
+                count += queens(board, row + 1);
+                board[row][column] = false;
+            }
+        }
+        return count;
+
+    }
+
+    private static boolean isSafe(Boolean[][] board, int row, int column) {
+        // check vertical row
+        for (int i = 0; i < row ; i++) {
+            if (board[i][column]) // its true means already has  queen
+                return false;
+        }
+
+        // diagonal left
+
+        int maxLeft = Math.min(row, column);
+        for (int i = 1; i <= maxLeft ; i++) {
+            if (board[row - i][column - 1])
+                return false;
+        }
+
+        // diagonal right
+        int maxRight = Math.min(row, board.length - column - 1);
+        for (int i = 1; i <= maxRight ; i++) {
+            if (board[row - i][column + i])
+                return false;
+        }
+
+        return true;
+
+
+    }
+
+
+
+    private static void display(Boolean[][] board) {
+        for (int i = 0; i <board.length ; i++) {
+            for (int j = 0; j <board[0].length ; j++) {
+                if (board[i][j]){
+                    System.out.print("Q ");
+                }
+                else {
+                    System.out.print("X ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
