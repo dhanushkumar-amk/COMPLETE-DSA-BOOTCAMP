f7f19a7ef57cadee4816433454f9a6bd14ee20c0
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-29 13:29:57 +0530

    modified

--- src/RECURSION/BACKTRACKING/PrintAllPaths.java
+++ src/RECURSION/BACKTRACKING/PrintAllPaths.java
@@ -2,7 +2,34 @@ package RECURSION.BACKTRACKING;
 
 public class PrintAllPaths {
     public static void main(String[] args) {
-        
+
+    }
+
+    static void path(String answer, boolean[][] maze, int row, int column){
+
+        if (row == maze.length -1 && column == maze[0].length - 1){
+            System.out.println(answer);
+            return;
+        }
+
+        if (!maze[row][column])
+            return;
+
+        // if not false make it as false  so next time we can'tgo
+        else
+           maze[row][column] = false;
+
+
+        if (row < maze.length - 1)
+            path(answer + "D", maze, row + 1, column);
+
+        if (column < maze.length - 1)
+            path(answer + "R", maze, row, column + 1);
+
+        if (row >)
+
+
+
     }
 
 }
