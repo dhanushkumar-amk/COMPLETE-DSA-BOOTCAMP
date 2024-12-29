e3c01a05d1dadc71c76534215eff462d7a87888b
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-29 10:54:33 +0530

    modified

--- src/RECURSION/MAZE/IncludingPrintDiagonals.java
+++ src/RECURSION/MAZE/IncludingPrintDiagonals.java
@@ -1,9 +1,35 @@
 package RECURSION.MAZE;
 
+
+import java.util.ArrayList;
+
 public class IncludingPrintDiagonals {
     public static void main(String[] args) {
 
+        ArrayList<String> ans = diagonals("", 3,3);
+        System.out.println(ans);
     }
 
-    
+    static ArrayList<String> diagonals(String answer, int row, int column){
+
+        if (row == 1 && column == 1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(answer);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+
+        if (row > 1 && column > 1)
+            list.addAll(diagonals(answer + "D", row - 1, column -1));
+
+        if (row > 1)
+            list.addAll(diagonals(answer + "V", row - 1, column));
+
+        if (column > 1)
+            list.addAll(diagonals(answer + "H", row, column -1));
+
+        return list;
+
+    }
 }
