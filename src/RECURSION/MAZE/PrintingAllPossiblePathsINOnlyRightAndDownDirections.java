e36a99f447f1ce83811e288048132e1797c77443
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-29 10:20:05 +0530

    modified

--- src/RECURSION/MAZE/PrintingAllPossiblePathsINOnlyRightAndDownDirections.java
+++ src/RECURSION/MAZE/PrintingAllPossiblePathsINOnlyRightAndDownDirections.java
@@ -2,6 +2,23 @@ package RECURSION.MAZE;
 
 public class PrintingAllPossiblePathsINOnlyRightAndDownDirections {
     public static void main(String[] args) {
+        int row = 3;
+        int column = 3;
+        String answer = "";
+
+        printPaths(answer, row, column);
+
+    }
+
+    static void printPaths(String answer, int row, int column){
+
+        if (row == 1 && column == 1){
+            System.out.println(answer);
+            return;
+        }
+
+        printPaths(answer + 'R', row, column - 1); // going right
+        printPaths(answer + 'D', row - 1, column); // going down
 
     }
 }
