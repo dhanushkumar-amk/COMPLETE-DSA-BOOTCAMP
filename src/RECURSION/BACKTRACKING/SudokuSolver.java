4cf5fc94a0f8ae2abb6751f28eb8fa8f9977efb5
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-29 18:36:37 +0530

    modified

--- src/RECURSION/BACKTRACKING/SudokuSolver.java
+++ src/RECURSION/BACKTRACKING/SudokuSolver.java
@@ -3,6 +3,34 @@ package RECURSION.BACKTRACKING;
 public class SudokuSolver {
     public static void main(String[] args) {
 
+        int[][] board = {
+                {5,3,0,0,7,0,0,0,0},
+                {6,0,0,1,9,5,0,0,0},
+                {0,9,8,0,0,0,0,6,0},
+                {8,0,0,0,6,0,0,0,3},
+                {4,0,0,8,0,3,0,0,1},
+                {7,0,0,0,2,0,0,0,6},
+                {0,6,0,0,0,0,2,8,0},
+                {0,0,0,4,1,9,0,0,5},
+                {0,0,0,0,8,0,0,7,9}
+        };
+
+        System.out.println(solve(board));
+
+        if (solve(board)) {
+            Display(board);
+        } else {
+            System.out.println("Can't be solved");
+        }
+    }
+
+
+
+    private static boolean solve(int[][] board) {
+    }
+
+
+    private static void Display(int[][] board) {
     }
 
 }
