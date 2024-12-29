a110432fd50a394815aa265f5e6e2b5738de4529
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-29 19:03:03 +0530

    modified

--- src/RECURSION/BACKTRACKING/SudokuSolver.java
+++ src/RECURSION/BACKTRACKING/SudokuSolver.java
@@ -15,17 +15,47 @@ public class SudokuSolver {
                 {0,0,0,0,8,0,0,7,9}
         };
 
-//        System.out.println(solve(board));
-//
-//        if (solve(board)) {
-//            Display(board);
-//        } else {
-//            System.out.println("Can't be solved");
-//        }
+        System.out.println(solve(board));
+
+        if (solve(board)) {
+            Display(board);
+        } else {
+            System.out.println("Can't be solved");
+        }
     }
 
 
-    static  boolean isSafe(int[] board)
+    static boolean isSafe(int[][] board, int row, int column, int num) {
+        // Check the row
+        for (int i = 0; i < board.length; i++) {
+            if (board[row][i] == num) {
+                return false;
+            }
+        }
+
+        // Check the column
+        for (int i = 0; i < board.length; i++) {
+            int[] nums = board[i];
+            if (nums[column] == num) {
+                return false;
+            }
+        }
+
+        // Check the 3x3 sub-grid
+        int sqrt = (int) Math.sqrt(board.length);
+        int rowStart = row - row % sqrt;
+        int colStart = column - column % sqrt;
+
+        for (int r = rowStart; r < rowStart + sqrt; r++) {
+            for (int c = colStart; c < colStart + sqrt; c++) {
+                if (board[r][c] == num) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 
     private static void Display(int[][] board) {
     }
