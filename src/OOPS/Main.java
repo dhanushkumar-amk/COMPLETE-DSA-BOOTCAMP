4d4c8c96d0a2878d3405f544f0fd40f00643f0d9
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-31 19:22:06 +0530

    modified

--- src/OOPS/Main.java
+++ src/OOPS/Main.java
@@ -2,9 +2,23 @@ package OOPS;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,5,6};
-        for (int i = 0; i < arr.length ; i++) {
-            System.out.println(arr[i]);
-        }
+
+        Students student = new Students();
+
+        String str= "";
+        System.out.println(str);
+
     }
 }
+
+    // craete a class
+
+    // we can create own data type using class
+    class Students{
+        int[] roll = new int[5];
+        String[] name = new String[5];
+        float[] marks = new float[5];
+     }
+
+
+
