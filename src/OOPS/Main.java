098fcdd4bc7d244bb2f6dc828196ef3702eb1e40
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-31 19:09:31 +0530

    modified

--- src/OOPS/Main.java
+++ src/OOPS/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = {1,2,3,5,6};
         for (int i = 0; i < arr.length ; i++) {
-            System.out.println(i);
+            System.out.println(arr[i]);
         }
     }
 }
