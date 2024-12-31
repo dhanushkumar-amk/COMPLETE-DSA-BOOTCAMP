39b106472c809e1aa2dac9a971fe3ead5279c354
Dhanushkumar G <dhanushkumaramk@gmail.com>
2024-12-31 19:49:29 +0530

    modified

--- src/OOPS/Main.java
+++ src/OOPS/Main.java
@@ -6,7 +6,9 @@ public class Main {
     public static void main(String[] args) {
 
         Students student = new Students();
+        student.marks = 10.5f;
 
+        System.out.println(student.marks);
 
     }
 }
@@ -19,12 +21,12 @@ public class Main {
         int rollNo;
         String name;
         float marks;
-
-        public Students(int rollNo, String name, float marks) {
-            this.rollNo = rollNo;
-            this.name = name;
-            this.marks = marks;
-        }
+//
+//        public Students(int rollNo, String name, float marks) {
+//            this.rollNo = rollNo;
+//            this.name = name;
+//            this.marks = marks;
+//        }
     }
 
 
