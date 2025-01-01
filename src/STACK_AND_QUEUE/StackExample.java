e8392f433663fec2e02303bb69b884c36be28e6e
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-01 07:43:30 +0530

    modified

--- src/STACK_AND_QUEUE/StackExample.java
+++ src/STACK_AND_QUEUE/StackExample.java
@@ -1,2 +1,20 @@
-package STACK_AND_QUEUE;public class Stack {
+package STACK_AND_QUEUE;
+
+import java.util.Stack;
+
+public class StackExample {
+    public static void main(String[] args) {
+
+        Stack<Integer> stack = new Stack();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+
+        System.out.println(stack.pop());  // return as a integer
+
+    }
 }
