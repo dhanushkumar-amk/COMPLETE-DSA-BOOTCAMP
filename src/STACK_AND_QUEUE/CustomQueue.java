c4b401071cbab89e841b8477efc638cccef7c2ac
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-01 10:45:22 +0530

    modified

--- src/STACK_AND_QUEUE/CustomQueue.java
+++ src/STACK_AND_QUEUE/CustomQueue.java
@@ -2,4 +2,8 @@ package STACK_AND_QUEUE;
 
 public class CustomQueue {
 
+    public static void main(String[] args) {
+
+    }
+
 }
--- src/STRING/CountVowelsInString.java
+++ src/STRING/CountVowelsInString.java
@@ -1,2 +1,19 @@
-package STRING;public class CountVowelsInString {
+package STRING;
+
+public class CountVowelsInString {
+    public static void main(String[] args) {
+        String s = "dhanushkumar";
+        System.out.println(count(s));
+    }
+
+    static int count(String s){
+
+        int count = 0;
+
+        for (char i = 0; i <s.length(); i++) {
+            if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u')
+                count++;
+        }
+        return count;
+    }
 }
