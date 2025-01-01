ba44922f73df53f8348901d46a37b81330a07194
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-01 11:21:35 +0530

    modified

--- src/STACK_AND_QUEUE/CustomQueue.java
+++ src/STACK_AND_QUEUE/CustomQueue.java
@@ -2,10 +2,40 @@ package STACK_AND_QUEUE;
 
 public class CustomQueue {
 
-    public static void main(String[] args) {
+   private  int[] data;
+    private static  final int DEFAULT_SIZE = 10;
+
+
+    int end = 0;
+
+    public CustomQueue() {
+        this(DEFAULT_SIZE);
+    }
+
+    public CustomQueue(int size) {
+        this.data = new int[size];
+    }
+
+    // is full
+    public boolean isFull(){
+        return end == data.length;  // pointer in the last index``
+    }
+
+    // is empty
+    public boolean isEmpty(){
+        return end == 0;
+    }
+
+
+    public boolean insert(int item){
+        if (isFull())
+            return false;
+
+        data[end]
+
 
     }
 
-    
+
 
 }
