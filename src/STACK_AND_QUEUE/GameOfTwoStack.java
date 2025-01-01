2ea42d11bc1d79ee3d0d69c6a32d465636bd19ef
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-01 19:23:33 +0530

    modified

--- src/STACK_AND_QUEUE/GameOfTwoStack.java
+++ src/STACK_AND_QUEUE/GameOfTwoStack.java
@@ -3,8 +3,11 @@ package STACK_AND_QUEUE;
 public class GameOfTwoStack {
     public static void main(String[] args) {
 
-        
 
+    }
+
+    static int twoStack(int target, int[] a, int b){
+        
     }
 
 
