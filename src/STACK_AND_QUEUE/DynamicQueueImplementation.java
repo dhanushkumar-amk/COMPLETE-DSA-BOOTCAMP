7b215ffb42f3b4ee1c4542b02867e4d41907b983
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-01 12:32:38 +0530

    modified

--- src/STACK_AND_QUEUE/DynamicQueueImplementation.java
+++ src/STACK_AND_QUEUE/DynamicQueueImplementation.java
@@ -1,5 +1,30 @@
 package STACK_AND_QUEUE;
 
-public class DynamicQueueImplementation {
+public class DynamicQueueImplementation extends CircularQueue {
+    public DynamicQueueImplementation() {
+        super();
+    }
 
+    public DynamicQueueImplementation(int size) {
+        super(size);
+    }
+
+    @Override
+    public boolean insert(int item) {
+
+        if (this.isFull()){
+            int[] temp = new int[data.length * 2];
+
+            for (int i = 0; i < data.length; i++) {
+                temp[i] = data[(front + i) % data.length];
+            }
+
+            front = 0;
+            end = data.length;
+            data = temp;
+
+        }
+
+        return super.insert(item);
+    }
 }
