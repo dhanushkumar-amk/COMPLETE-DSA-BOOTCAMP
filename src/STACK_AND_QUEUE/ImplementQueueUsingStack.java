a219eda3d59a26b823740e6448c711749b474cd0
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-01 13:27:06 +0530

    modified

--- src/STACK_AND_QUEUE/ImplementQueueUsingStack.java
+++ src/STACK_AND_QUEUE/ImplementQueueUsingStack.java
@@ -6,4 +6,25 @@ public class ImplementQueueUsingStack {
     public static void main(String[] args) {
 
     }
+
+    public MyQueue() {
+
+    }
+
+    public void push(int x) {
+
+    }
+
+    public int pop() {
+
+    }
+
+    public int peek() {
+
+    }
+
+    public boolean empty() {
+
+    }
+
 }
