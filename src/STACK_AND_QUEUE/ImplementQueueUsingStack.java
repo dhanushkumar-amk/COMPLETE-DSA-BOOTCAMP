3ed77eff5c1266bf7a9dcad542570cf9bdf4f6b9
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-01 16:01:06 +0530

    modified

--- src/STACK_AND_QUEUE/ImplementQueueUsingStack.java
+++ src/STACK_AND_QUEUE/ImplementQueueUsingStack.java
@@ -7,13 +7,12 @@ import java.util.Stack;
 public class ImplementQueueUsingStack {
     public static void main(String[] args) {
 
-        Stack<Integer> stack = new Stack<>();
 
-        stack.push(5);
-        System.out.println(stack.pop());
 
     }
 
 
 
 }
+
+class 
