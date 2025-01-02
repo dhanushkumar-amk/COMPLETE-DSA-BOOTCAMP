25749565050b571ea894b7aa0a5abde80e8e3063
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-02 05:46:07 +0530

    modified

--- src/STACK_AND_QUEUE/ValidParentheses.java
+++ src/STACK_AND_QUEUE/ValidParentheses.java
@@ -1,2 +1,38 @@
-package STACK_AND_QUEUE;public class ValidParentheses {
+package STACK_AND_QUEUE;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+        String s = "({[]})";
+        System.out.println(isValid(s));
+    }
+
+    static boolean isValid(String s){
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[')
+                stack.push(ch);
+            else{
+                if (ch == '}'){
+                    if (stack.isEmpty() || stack.pop() != '{')
+                        return false;
+                }
+
+                if (ch == ']'){
+                    if (  stack.isEmpty() || stack.pop() != '[')
+                        return false;
+                }
+
+
+                if (ch == ')'){
+                    if (   stack.isEmpty() ||  stack.pop() != '(')
+                        return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
 }
