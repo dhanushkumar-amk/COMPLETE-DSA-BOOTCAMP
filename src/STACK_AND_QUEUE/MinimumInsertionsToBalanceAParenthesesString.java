409791d1bb4a5a986341a5368f14b36e36140b78
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-02 10:52:57 +0530

    modified

--- src/STACK_AND_QUEUE/MinimumInsertionsToBalanceAParenthesesString.java
+++ src/STACK_AND_QUEUE/MinimumInsertionsToBalanceAParenthesesString.java
@@ -1,2 +1,26 @@
-package STACK_AND_QUEUE;public class MinimuminsertionsToBalanceAParenthesesString {
+package STACK_AND_QUEUE;
+
+import java.util.Stack;
+
+public class MinimumInsertionsToBalanceAParenthesesString {
+
+    public static void main(String[] args) {
+        String s = "{}}";
+        System.out.println(minInsertions(s));
+    }
+    static  int minInsertions(String s){
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()){
+            if (ch == '{'){
+                if (!stack.isEmpty() || stack.peek() == '}')
+                    stack.pop();
+                else
+                    stack.push(ch);
+            }else
+                stack.push(ch);
+        }
+        return stack.size();
+    }
 }
