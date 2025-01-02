5d1e83a29a86dee5782c7fc978e424d3208a328e
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-02 05:30:37 +0530

    modified

--- src/STACK_AND_QUEUE/LargestRectangleInHistogram.java
+++ src/STACK_AND_QUEUE/LargestRectangleInHistogram.java
@@ -1,11 +1,48 @@
 package STACK_AND_QUEUE;
 
+import java.util.Stack;
+
 public class LargestRectangleInHistogram {
 
     public static void main(String[] args) {
 
     }
 
-    
+    public int largestRectangleArea(int[] heights) {
+
+        Stack<Integer> stack = new Stack<>();
+        int max = 0;
+
+        stack.push(0);
+
+        for (int i = 1; i < heights.length; i++) {
+            while (!stack.empty() && heights[i] < heights[stack.peek()]){
+                max  = getMax(heights, stack, max, i);
+            }
+            stack.push(i);
+        }
+
+        int i = heights.length;
+
+        while (!stack.empty()){
+            max  = getMax(heights, stack, max, i);
+        }
+
+        return max;
+    }
+
+
+    private int getMax(int[] heights, Stack<Integer> stack, int max, int i) {
+        int area;
+        int popped = stack.pop();
+
+        if (stack.empty()){
+            area = heights[popped] * i;
+        }else{
+            area = heights[popped] * (i - 1 - stack.peek());
+        }
+
+        return Math.max(max, area);
+    }
 
 }
