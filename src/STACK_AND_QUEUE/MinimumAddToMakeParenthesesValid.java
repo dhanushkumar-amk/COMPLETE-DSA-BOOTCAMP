cbc224885d5bfed156a5c1cf6eb3ab2a0e4384d3
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-02 11:36:46 +0530

    modified

--- src/STACK_AND_QUEUE/MinimumAddToMakeParenthesesValid.java
+++ src/STACK_AND_QUEUE/MinimumAddToMakeParenthesesValid.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 
 public class MinimumAddToMakeParenthesesValid {
     public static void main(String[] args) {
-        String s = "(()))";
+        String s = "))())(";
         System.out.println(minAddToMakeValid(s));
     }
 
