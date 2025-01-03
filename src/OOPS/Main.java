fb1b00cc54fa13bfd4cc415db0aff21070eb4c19
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-03 19:36:19 +0530

    modified

--- src/OOPS/Main.java
+++ src/OOPS/Main.java
@@ -6,11 +6,17 @@ public class Main {
     public static void main(String[] args) {
 
         Students student = new Students(10, "dhanushkumar", 10.8f);
+        Students students = new Students(20, "ram", 45.0f);
+
 
 
         System.out.println(student.marks);
         System.out.println(student.rollNo);
         System.out.println(student.name);
+
+        System.out.println(students.marks);
+        System.out.println(students.rollNo);
+        System.out.println(students.name);
 
 
     }
@@ -30,6 +36,8 @@ public class Main {
             this.rollNo = rollNo;
             this.name = name;
             this.marks = mark;
+
+            // this.marks = marks == marks = marks
         }
 
 
