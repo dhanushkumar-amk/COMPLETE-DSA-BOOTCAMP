c3f68b4eabcc41d53097782d89b4ec0ed4a78fa9
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-04 19:33:21 +0530

    modified

--- src/OOPS/SINGLETONCLASS/SingletonClass.java
+++ src/OOPS/SINGLETONCLASS/SingletonClass.java
@@ -1,5 +1,19 @@
 package OOPS.SINGLETONCLASS;
 
 public class SingletonClass {
-            
+    SingletonClass() {
+        String name = "dhanush";
+    }
+
+    private static SingletonClass instance;
+
+    public static SingletonClass getInstance(){
+        // check whether 1 obj only is created or not
+
+        if (instance == null){
+            instance = new SingletonClass();
+        }
+        return instance;
+    }
+
 }
--- src/OOPS/SINGLETONCLASS/main.java
+++ src/OOPS/SINGLETONCLASS/main.java
@@ -1,2 +1,10 @@
-package OOPS.SINGLETONCLASS;public class main {
+package OOPS.SINGLETONCLASS;
+
+public class main {
+    public static void main(String[] args) {
+        SingletonClass obj1 =  SingletonClass.getInstance();
+        SingletonClass ob2 = SingletonClass.getInstance();
+
+        System.out.println(obj1.na);
+    }
 }
