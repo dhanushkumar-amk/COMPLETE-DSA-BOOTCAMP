8e90c4d1ff8369c2ae3ac6f830c6c5c73b2d73a1
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-04 17:17:41 +0530

    modified

--- src/OOPS/Static/Main.java
+++ src/OOPS/Static/Main.java
@@ -12,7 +12,8 @@ public class Main {
         System.out.println(Human.population);
         System.out.println(Human.population);
 
-        
+
+        System.out.println(dhanush.name);
 
     }
 }
