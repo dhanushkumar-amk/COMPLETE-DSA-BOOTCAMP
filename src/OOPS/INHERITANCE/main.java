099b230560ee49107e32ce97d8e0c2a40307bd7d
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-05 02:49:21 +0530

    modified

--- src/OOPS/INHERITANCE/main.java
+++ src/OOPS/INHERITANCE/main.java
@@ -3,7 +3,7 @@ package OOPS.INHERITANCE;
 public class main {
     public static void main(String[] args) {
 
-        Box box = new Box();
+        Box box = new Box(10, 20, 30);
 
         System.out.println(box.l);
         System.out.println(box.h);
