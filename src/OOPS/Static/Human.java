04f1c8baa3359edd23137716aed4a36d64f6dbf6
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-04 17:08:53 +0530

    modified

--- src/OOPS/Static/Human.java
+++ src/OOPS/Static/Human.java
@@ -1,16 +1,21 @@
 package OOPS.Static;
 
+
+
 public class Human {
     int age;
     String name;
     int salary;
     boolean isMarried;
+    long population;
 
     public Human(int age, boolean isMarried, String name, int salary) {
         this.age = age;
         this.isMarried = isMarried;
         this.name = name;
         this.salary = salary;
+
+        this.population =+1;
     }
 
 
--- src/OOPS/Static/Main.java
+++ src/OOPS/Static/Main.java
@@ -5,7 +5,8 @@ public class Main {
         Human dhanush  = new Human(10, false, "dhanushkumar", 100000);
         Human sanjay  = new Human(11, true, "sanjaykumar", 200000);
 
-        System.out.println(dhanush.name);
+        System.out.println(dhanush.population);
+        System.out.println(sanjay.population);
 
     }
 }
