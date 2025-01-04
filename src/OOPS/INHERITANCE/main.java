5b3d6005d137cdf4e9616be577bc3a5531b83c7d
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-05 02:51:59 +0530

    modified

--- src/OOPS/INHERITANCE/main.java
+++ src/OOPS/INHERITANCE/main.java
@@ -4,6 +4,11 @@ public class main {
     public static void main(String[] args) {
 
         Box box = new Box(4);
+        Box box2 = new Box(box);
+
+        System.out.println(box2.l);
+        System.out.println(box2.h);
+        System.out.println(box2.w);
 
         System.out.println(box.l);
         System.out.println(box.h);
