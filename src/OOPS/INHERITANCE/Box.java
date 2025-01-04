c9bb8c515cce24d2f3cce5cb86638eb7782811a3
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-05 02:48:59 +0530

    modified

--- src/OOPS/INHERITANCE/Box.java
+++ src/OOPS/INHERITANCE/Box.java
@@ -1,2 +1,44 @@
-package OOPS.INHERITANCE;public class Box {
+package OOPS.INHERITANCE;
+
+public class Box {
+
+    double l;
+    double h;
+    double w;
+
+
+    Box(){
+        this.h = -1;
+        this.l = -1;
+        this.w = -1;
+    }
+
+     Box(double h, double l, double w) {
+        this.h = h;
+        this.l = l;
+        this.w = w;
+    }
+
+
+    // cube
+    Box(double side){
+        this.l = side;
+        this.w = side;
+        this.h = side;
+    }
+
+
+
+    // copy Constructor
+    Box(Box old){
+        this.h = old.h;
+        this.w = old.w;
+        this.l = old.l;
+    }
+
+
+    public  void information(){
+        System.out.println("Running the box");
+    }
+
 }
--- src/OOPS/INHERITANCE/main.java
+++ src/OOPS/INHERITANCE/main.java
@@ -1,2 +1,12 @@
-package OOPS.INHERITANCE;public class main {
+package OOPS.INHERITANCE;
+
+public class main {
+    public static void main(String[] args) {
+
+        Box box = new Box();
+
+        System.out.println(box.l);
+        System.out.println(box.h);
+        System.out.println(box.w);
+    }
 }
