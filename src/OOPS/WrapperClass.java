f1a92aa41a9f1b214c1fe2ad29dd5d5f16e737c3
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-04 08:16:17 +0530

    modified

--- src/OOPS/WrapperClass.java
+++ src/OOPS/WrapperClass.java
@@ -1,2 +1,15 @@
-package OOPS;public class WrapperClass {
+
+package OOPS;
+
+import java.lang.Integer;
+
+public class WrapperClass {
+    public static void main(String[] args) {
+    Integer num = 10;
+
+        System.out.println(num);
+
+    }
+
+
 }
