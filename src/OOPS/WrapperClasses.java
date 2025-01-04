a12a3d61d8351575613f772f697530a836205c1a
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-04 08:28:48 +0530

    modified

--- src/OOPS/WrapperClasses.java
+++ src/OOPS/WrapperClasses.java
@@ -6,16 +6,30 @@ public class WrapperClasses {
         int a = 10;
         int b = 20;
         // it will created as a object
-        Integer num = 20;
+        Integer num1= 20;
+        Integer num2= 10;
 
+
+        // ut can't be swap because java is a pass by value
         swap(a, b);
         System.out.println(a + " " +  b);
+
+
+        swap1(num1, num2);
+        System.out.println(num1 + " " + num2);
     }
 
     static void swap(int a, int b){
         int temp = a;
         a = b;
         b = temp;
+    }
+
+
+    static void swap1(Integer num1, Integer num2){
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
     }
 
 }
