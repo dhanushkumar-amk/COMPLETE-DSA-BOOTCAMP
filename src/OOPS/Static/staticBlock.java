103f6f2eccdd561e727599ecd5b436f91208d8a9
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-04 17:45:36 +0530

    modified

---
+++ src/OOPS/Static/staticBlock.java
@@ -0,0 +1,25 @@
+package OOPS.Static;
+
+
+
+public class staticBlock {
+    static int a = 4;
+    static int b;
+
+    static {
+        System.out.println("i am from the static block");
+        b = a * 5;
+    }
+
+    public static void main(String[] args) {
+        staticBlock obj = new staticBlock();
+        System.out.println(staticBlock.a + " " + staticBlock.b);
+
+    staticBlock.b += 3;
+
+    staticBlock obj2 = new staticBlock();
+        System.out.println(staticBlock.b);
+
+    }
+
+}
