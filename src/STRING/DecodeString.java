fae4c0ffff328a31663cbdb0a34af34ebbe8e369
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-04 20:01:36 +0530

    modified

--- src/STRING/DecodeString.java
+++ src/STRING/DecodeString.java
@@ -2,7 +2,7 @@ package STRING;
 
 public class DecodeString {
     public static void main(String[] args) {
-
+    String s = "3[a]2[bc]";
     }
 
     public static String decodeString(String s) {
