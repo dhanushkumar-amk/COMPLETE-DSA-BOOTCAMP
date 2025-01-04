06cd29e99919a1caf7386b183b6189339851a28c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-04 11:21:50 +0530

    modified

--- src/OOPS/PACKAGE/a/Greeting.java
+++ src/OOPS/PACKAGE/a/Greeting.java
@@ -1,2 +1,10 @@
-package OOPS.PACKAGE.a;public class Greeting {
+package OOPS.PACKAGE.a;
+
+import static OOPS.PACKAGE.b.Message.message;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println();
+        message();
+    }
 }
--- src/OOPS/PACKAGE/b/Message.java
+++ src/OOPS/PACKAGE/b/Message.java
@@ -1,2 +1,11 @@
-package OOPS.PACKAGE.b;public class Message {
+package OOPS.PACKAGE.b;
+
+public class Message {
+    public static void main(String[] args) {
+
+    }
+
+    public static void message(){
+        System.out.println("this is message from package b");
+    }
 }
