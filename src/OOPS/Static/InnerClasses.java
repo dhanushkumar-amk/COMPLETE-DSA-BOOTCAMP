9a7d7823c9946d3f0a783e6b9152423eb15796c5
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-04 18:59:03 +0530

    modified

--- src/OOPS/Static/InnerClasses.java
+++ src/OOPS/Static/InnerClasses.java
@@ -18,20 +18,9 @@ public class InnerClasses {
         }
     }
 
-    static class B{
-
-    }
-
-
-    static class C{
-
-    }
-
-
-
     public static void main(String[] args) {
         A a = new A("dhanush");
-        A b = new A("sanjay");
+
 
 
         // it contain a.toString() by default; ig it has own the it will use that otherwise use default method
--- src/OOPS/Static/Tostring.java
+++ src/OOPS/Static/Tostring.java
@@ -1,2 +1,23 @@
-package OOPS.Static;public class Tostring {
+package OOPS.Static;
+
+public class Tostring {
+    public static void main(String[] args) {
+        fun a = new fun("dhanush");
+        System.out.println(a);
+    }
 }
+
+class fun{
+
+    String name;
+
+    public fun(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
+
