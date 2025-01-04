67e7157b95f0d2e2c33485a8fd2ef74c10ea6214
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-05 02:53:09 +0530

    modified

--- src/OOPS/INHERITANCE/main.java
+++ src/OOPS/INHERITANCE/main.java
@@ -4,12 +4,16 @@ public class main {
     public static void main(String[] args) {
 
         Box box = new Box(4);
-        Box box2 = new Box(box);
 
+        /*
+        Box box2 = new Box(box);
+        // it consist all the property of the old constructor
         System.out.println(box2.l);
         System.out.println(box2.h);
         System.out.println(box2.w);
 
+
+         */
         System.out.println(box.l);
         System.out.println(box.h);
         System.out.println(box.w);
