c2f37c45b8aa428e53790b2153a0dfe22491f407
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-04 17:45:17 +0530

    modified

--- src/OOPS/Static/static2.java
+++ src/OOPS/Static/static2.java
@@ -14,6 +14,12 @@ public class static2 {
     public static void main(String[] args) {
         static2 obj = new static2();
         System.out.println(static2.a + " " + static2.b);
+
+    static2.b += 3;
+
+    static2 obj2 = new static2();
+        System.out.println(static2.b);
+
     }
 
 }
