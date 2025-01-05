143825c50eb179e6c7d6256fd5dd345206bbe86b
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-05 23:48:10 +0530

    modified

--- src/OOPS/ACCESS/Main.java
+++ src/OOPS/ACCESS/Main.java
@@ -1,9 +1,13 @@
 package OOPS.ACCESS;
 
+
+
+
 public class Main {
     public static void main(String[] args) {
         A obj = new A("dhnaush", 10);
         obj.getNum();
-        
+
+
     }
 }
--- src/OOPS/ACCESS/ObjectDemo.java
+++ src/OOPS/ACCESS/ObjectDemo.java
@@ -1,2 +1,45 @@
-package OOPS.ACCESS;public class ObjectDemo {
+package OOPS.ACCESS;
+
+public class ObjectDemo {
+
+    int num;
+
+    public ObjectDemo(int num) {
+        this.num = num;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    public ObjectDemo() {
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+
+    public static void main(String[] args) {
+        ObjectDemo obj = new ObjectDemo(10);
+        System.out.println(obj.hashCode());
+    }
 }
