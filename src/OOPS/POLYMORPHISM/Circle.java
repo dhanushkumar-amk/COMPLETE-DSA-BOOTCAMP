d4edf60d7950010ba50d6e70db10f1c5b08a4c79
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-05 13:59:15 +0530

    modified

--- src/OOPS/POLYMORPHISM/Circle.java
+++ src/OOPS/POLYMORPHISM/Circle.java
@@ -5,6 +5,7 @@ public class Circle extends Shapes {
     @Override
     void area(){
         System.out.println("I am in circle");
+        System.out.println("It is override the paren class with child class");
     }
 
     /*
--- src/OOPS/POLYMORPHISM/PrintOver.java
+++ src/OOPS/POLYMORPHISM/PrintOver.java
@@ -1,2 +1,15 @@
-package OOPS.POLYMORPHISM;public class PrintOver {
+package OOPS.POLYMORPHISM;
+
+public class PrintOver {
+
+   int num;
+
+    public PrintOver(int num) {
+        this.num = num;
+    }
+
+    public static void main(String[] args) {
+        PrintOver print = new PrintOver(5);
+        System.out.println(print);
+    }
 }
