84286d8d29f0ff12da25c390fb9bb6fc7fba3090
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-05 11:50:14 +0530

    modified

--- src/OOPS/POLYMORPHISM/Numbers.java
+++ src/OOPS/POLYMORPHISM/Numbers.java
@@ -1,2 +1,27 @@
-package OOPS.POLYMORPHISM;public class Numbers {
+package OOPS.POLYMORPHISM;
+
+public class Numbers {
+
+
+    // ex
+
+    int sum(int a, int b){
+        return  a + b;
+    }
+
+    int sum(int a, int b, int c){
+        return a + b + c;
+    }
+    
+
+
+    public static void main(String[] args) {
+
+        Numbers obj = new Numbers();
+
+        obj.sum(10,20,30);
+        obj.sum(10,20);
+
+    }
+
 }
