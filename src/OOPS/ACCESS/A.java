e9ca6a331e954a58bd25ffaabcdd86cd2a9e9c3b
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-05 23:04:39 +0530

    modified

--- src/OOPS/ACCESS/A.java
+++ src/OOPS/ACCESS/A.java
@@ -1,2 +1,13 @@
-package OOPS.ACCESS;public class A {
+package OOPS.ACCESS;
+
+public class A {
+    int num;
+    String name;
+    int[] arr;
+
+    public A( String name, int num) {
+        this.num = num;
+        this.name = name;
+        this.arr = new int[num];
+    }
 }
