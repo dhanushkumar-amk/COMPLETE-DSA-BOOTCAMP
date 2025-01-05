98faeff65ab4315a5e5357b94315a43564338f60
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-05 13:50:42 +0530

    modified

--- src/OOPS/POLYMORPHISM/Circle.java
+++ src/OOPS/POLYMORPHISM/Circle.java
@@ -2,7 +2,15 @@ package OOPS.POLYMORPHISM;
 
 public class Circle extends Shapes {
 
+    @Override
     void area(){
         System.out.println("I am in circle");
     }
+
+    /*
+    void area1(){
+        System.out.println("Not override");
+    }
+     */
+
 }
