038841d6a9106e8e913f630312ad8b08b55f3dae
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-05 11:16:35 +0530

    modified

--- src/OOPS/POLYMORPHISM/Main.java
+++ src/OOPS/POLYMORPHISM/Main.java
@@ -7,8 +7,11 @@ public class Main {
     public static void main(String[] args) {
 
         Shapes shape = new Shapes();
-        shape.area();
         Circle circle = new Circle();
         Square square = new Square();
+
+        shape.area();
+        circle.area();
+        square.area();
     }
 }
