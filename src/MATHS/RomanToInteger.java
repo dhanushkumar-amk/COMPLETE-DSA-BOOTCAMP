e8a21ad600c9255fd0a002ed779f24bcf9d85e37
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-05 20:35:11 +0530

    modified

--- src/MATHS/RomanToInteger.java
+++ src/MATHS/RomanToInteger.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class RomanToInteger {
     public static void main(String[] args) {
-        String s = "III";
+        String s =  "MCMXCIV";
         System.out.println(romanToInt(s));
     }
 
