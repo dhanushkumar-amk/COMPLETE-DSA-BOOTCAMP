76993566b3d8238efc85b3e38bed76e1cb092024
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-07 21:09:15 +0530

    modified

--- src/MATHS/Sqrt.java
+++ src/MATHS/Sqrt.java
@@ -1,2 +1,11 @@
-package MATHS;public class Sqrt {
+package MATHS;
+
+public class Sqrt {
+    public static void main(String[] args) {
+        mySqrt(8);
+    }
+
+    public static int mySqrt(int x) {
+        return (int) Math.sqrt(x);
+    }
 }
