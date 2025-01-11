ac5c95f4c7684401367e58a0f4330293fdbd8dcb
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-11 22:55:45 +0530

    modified

--- src/OOPS/INTERFACES/NESTEDINTERFACE/Main.java
+++ src/OOPS/INTERFACES/NESTEDINTERFACE/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         B obj = new B();
-        System.out.println(obj.isOddOREven(10));
+        System.out.println(obj.isOddOREven(7));
 
 
     }
