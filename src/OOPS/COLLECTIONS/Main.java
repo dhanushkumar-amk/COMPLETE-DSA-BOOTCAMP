4f6e67e271b086e62caf633775fa745439d85752
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-12 13:10:03 +0530

    modified

--- src/OOPS/COLLECTIONS/Main.java
+++ src/OOPS/COLLECTIONS/Main.java
@@ -1,9 +1,19 @@
 package OOPS.COLLECTIONS;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Vector;
 
 public class Main {
     public static void main(String[] args) {
-        
+
+        List<Integer> vector = new Vector<>();
+        vector.add(1);
+        vector.add(2);
+        vector.add(3);
+        vector.add(4);
+
+        System.out.println(vector);
+
     }
 }
