213339ad56c95e30bb7b51a4ad8add1690567468
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-12 13:16:39 +0530

    modified

--- src/OOPS/ENUMEXAMPLES/Main.java
+++ src/OOPS/ENUMEXAMPLES/Main.java
@@ -1,2 +1,26 @@
-package OOPS.ENUMEXAMPLES;public class Main {
+package OOPS.ENUMEXAMPLES;
+
+public class Main {
+
+    enum week{
+        monday,
+        tuesday,
+        wednesday,
+        thursday,
+        friday,
+        saturday,
+        sunday
+    }
+    // these are called enum constants
+    // every one in enum is public static and final one
+    // since it is final so we can't inheritance
+    // type is week
+
+    public static void main(String[] args) {
+        week weeks;
+        weeks = week.friday;
+
+        for (week day : week.values())
+            System.out.println(day);
+    }
 }
