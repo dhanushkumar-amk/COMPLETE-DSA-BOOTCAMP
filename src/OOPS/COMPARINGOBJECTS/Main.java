b6c1a9b50f1fd6363f0ab8ef1c3d29080a7e6d6c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-12 09:37:48 +0530

    modified

--- src/OOPS/COMPARINGOBJECTS/Main.java
+++ src/OOPS/COMPARINGOBJECTS/Main.java
@@ -1,10 +1,21 @@
 package OOPS.COMPARINGOBJECTS;
 
+import java.util.Arrays;
+
 public class Main  {
     public static void main(String[] args) {
 
         Student dhanush = new Student(10, 20);
         Student kumar = new Student(102, 21);
+        Student sanjay = new Student(106, 22);
+        Student ashwin = new Student(104, 51);
+        Student arun = new Student(103, 28);
+
+
+        Student[] list = {dhanush, kumar, sanjay, ashwin, arun};
+
+        System.out.println(Arrays.toString(list));
+        
 
 
        // we can't directly compare objects because it consists more than one variables
