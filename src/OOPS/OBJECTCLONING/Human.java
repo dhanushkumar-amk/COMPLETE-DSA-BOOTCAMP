198cf0c7880ef663d3da92404b7e9a82399435b4
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-12 12:38:56 +0530

    modified

--- src/OOPS/OBJECTCLONING/Human.java
+++ src/OOPS/OBJECTCLONING/Human.java
@@ -4,10 +4,12 @@ public class Human implements Cloneable {
 
     int age;
     String name;
+    int[] arr;
 
     public Human(int age, String name) {
         this.age = age;
         this.name = name;
+        this.arr = new int[]{1,2,3,4,5};
     }
 
 
@@ -16,6 +18,7 @@ public class Human implements Cloneable {
         this.age = other.age;
     }
 
+    @Override
     public Object clone() throws CloneNotSupportedException {
         return super.clone();
     }
