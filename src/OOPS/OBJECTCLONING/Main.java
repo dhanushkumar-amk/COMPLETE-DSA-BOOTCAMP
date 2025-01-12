94ebcd279dab325e9b03499e67ae228b2314309b
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-12 12:29:58 +0530

    modified

--- src/OOPS/OBJECTCLONING/Main.java
+++ src/OOPS/OBJECTCLONING/Main.java
@@ -2,14 +2,16 @@ package OOPS.OBJECTCLONING;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException {
 
 
         Human dhanush = new Human(20, "dhanushkumar");
 //        Human kumar = new Human(dhanush);
 
-        Human kumar = (Human) dhanush
+        Human kumar = (Human) dhanush.clone();
 
+        System.out.println(kumar.name);
+        System.out.println(kumar.age);
     }
 
 }
