6314e718752a2c329f28830bfec5de64118e4d61
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-12 07:49:31 +0530

    modified

--- src/OOPS/GENERIC/CustomArrayList.java
+++ src/OOPS/GENERIC/CustomArrayList.java
@@ -1,2 +1,16 @@
-package OOPS.GENERIC;public class CustomArrayList {
+package OOPS.GENERIC;
+
+import java.util.ArrayList;
+
+public class CustomArrayList {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+    }
 }
