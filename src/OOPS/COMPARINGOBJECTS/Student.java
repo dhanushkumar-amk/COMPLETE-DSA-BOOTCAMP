160f0b14a0d17ab776f687bbdda39248d8eb817e
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-12 09:40:20 +0530

    modified

--- src/OOPS/COMPARINGOBJECTS/Student.java
+++ src/OOPS/COMPARINGOBJECTS/Student.java
@@ -11,7 +11,7 @@ public class Student  implements Comparable<Student>{
 
     @Override
     public String toString() {
-        return rollNo + " ";
+        return marks + " " + rollNo;
     }
 
     @Override
