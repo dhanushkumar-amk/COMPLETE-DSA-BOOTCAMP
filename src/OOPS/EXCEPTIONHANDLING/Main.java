b9876388fdecace544aa0a897fa79c0d70b5172d
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-12 10:33:53 +0530

    modified

--- src/OOPS/EXCEPTIONHANDLING/Main.java
+++ src/OOPS/EXCEPTIONHANDLING/Main.java
@@ -1,2 +1,12 @@
-package OOPS.EXCEPTIONHANDLING;public class Main {
+package OOPS.EXCEPTIONHANDLING;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 0;
+
+        int c = a / b;
+
+        System.out.println(c);
+    }
 }
