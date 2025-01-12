aa79aa95fe09e42300fb2b51b8e904182ed60121
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-12 12:12:43 +0530

    modified

--- src/OOPS/EXCEPTIONHANDLING/CustomException.java
+++ src/OOPS/EXCEPTIONHANDLING/CustomException.java
@@ -6,5 +6,4 @@ public class CustomException extends Exception {
         super(message);
     }
 
-
 }
--- src/OOPS/OBJECTCLONING/Human.java
+++ src/OOPS/OBJECTCLONING/Human.java
@@ -1,2 +1,17 @@
-package OOPS.OBJECTCLONING;public class Human {
+package OOPS.OBJECTCLONING;
+
+public class Human {
+
+    int age;
+    String name;
+
+    public Human(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public Human(Human other){
+        this.name
+    }
+
 }
