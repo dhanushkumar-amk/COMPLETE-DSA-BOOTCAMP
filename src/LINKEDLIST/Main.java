4af62ab490f696d6cf9f5929d0ecdbd151017b25
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-13 09:29:42 +0530

    modified

--- src/LINKEDLIST/Main.java
+++ src/LINKEDLIST/Main.java
@@ -5,7 +5,11 @@ public class Main {
 
         LL list = new LL();
 
-
+        list.insertFirst(1);
+        list.insertFirst(2);
+        list.insertFirst(3);
+        list.insertFirst(4);
+        list.insertFirst(5);
 
 
         list.display();
