ef5d6bcb27f0beabccaf5a26d6f60ad8ac6250d6
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-13 09:01:16 +0530

    modified

--- src/LINKEDLIST/LL.java
+++ src/LINKEDLIST/LL.java
@@ -1,2 +1,28 @@
-package LINKEDLIST;public class LL {
+package LINKEDLIST;
+
+public class LL {
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public LL(){
+        this.size = 0;
+    }
+
+
+     private class Node{
+         private int value;
+         private  Node next;
+
+         public Node(Node next, int val) {
+             this.next = next;
+             this.value = val;
+         }
+
+         public Node(int val) {
+             this.value = val;
+         }
+
+     }
 }
--- src/LINKEDLIST/Main.java
+++ src/LINKEDLIST/Main.java
@@ -1,2 +1,8 @@
-package LINKEDLIST;public class Main {
+package LINKEDLIST;
+
+public class Main {
+    public static void main(String[] args) {
+        LL list = new LL();
+
+    }
 }
