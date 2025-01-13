392d27d64bc2e2276e4cab85a3735a1da88c2a67
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-13 20:27:57 +0530

    modified

--- src/LINKEDLIST/InsertTheElementUsingRecursion.java
+++ src/LINKEDLIST/InsertTheElementUsingRecursion.java
@@ -9,6 +9,57 @@ public class InsertTheElementUsingRecursion {
 
     public static void main(String[] args) {
 
+        InsertTheElementUsingRecursion list = new InsertTheElementUsingRecursion();
+
+        list.insertFirst(1);
+        list.insertFirst(2);
+        list.insertFirst(3);
+        list.insertFirst(4);
+        list.insertFirst(5);
+
+        list.display();
+
+        list.insertRecursion(200, 2);
+
+        list.display();
+
+    }
+
+    public  void insertRecursion(int val, int index){
+        head = insertRecursion(val, index, head);
+    }
+
+    private  Node insertRecursion(int val, int index, Node node){
+
+        if (val == 0){
+            Node temp = new Node(node,val);
+            size++;
+            return temp;
+        }
+
+        node.next = insertRecursion(val, index--, node.next);
+        return node;
+    }
+
+
+    public void insertFirst(int val){
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+
+        if (tail == null)
+            tail  = head;
+
+        size += 1;
+    }
+
+    public void display(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.val +  " --> " );
+            temp = temp.next;
+        }
+        System.out.println("end");
     }
 
     class Node{
