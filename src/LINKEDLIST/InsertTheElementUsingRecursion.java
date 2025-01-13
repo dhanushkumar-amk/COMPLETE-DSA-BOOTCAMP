86b4565fb8c3d83287f7ea9e259b4dee3b213632
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-13 19:24:05 +0530

    modified

--- src/LINKEDLIST/InsertTheElementUsingRecursion.java
+++ src/LINKEDLIST/InsertTheElementUsingRecursion.java
@@ -1,2 +1,11 @@
-package LINKEDLIST;public class InsertTheElementUsingRecursion {
+package LINKEDLIST;
+
+public class InsertTheElementUsingRecursion {
+    public static void main(String[] args) {
+
+    }
+
+    public void insertRecursion(int val, int index){
+        
+    }
 }
