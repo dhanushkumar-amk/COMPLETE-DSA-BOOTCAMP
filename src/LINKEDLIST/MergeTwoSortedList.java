8d78628d42f8e76743baf347e0e730d2da8ed9af
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-14 09:26:35 +0530

    modified

--- src/LINKEDLIST/MergeTwoSortedList.java
+++ src/LINKEDLIST/MergeTwoSortedList.java
@@ -18,7 +18,7 @@ public class MergeTwoSortedList {
         list1.display();
         list2.display();
 
-        LL.merge(list1, list2);
+        System.out.println(LL.merge(list1, list2));
 
     }
 }
