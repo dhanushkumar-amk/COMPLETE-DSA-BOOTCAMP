218a8b16bccd1eef38c67d34b1bf1c5149cdce50
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-14 09:17:55 +0530

    modified

--- src/LINKEDLIST/MergeTwoSortedList.java
+++ src/LINKEDLIST/MergeTwoSortedList.java
@@ -1,5 +1,23 @@
 package LINKEDLIST;
 
 public class MergeTwoSortedList {
-    
+    public static void main(String[] args) {
+
+        LL list1 = new LL();
+        LL list2 = new LL();
+
+        list1.insertFirst(1);
+        list1.insertFirst(3);
+        list1.insertFirst(5);
+
+        list2.insertFirst(1);
+        list2.insertFirst(2);
+        list2.insertFirst(9);
+        list2.insertFirst(14);
+
+        list1.display();
+        list2.display();
+
+
+    }
 }
