6a5d8a217fdb0943fc9cbbe95cc042c0fad74c05
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-15 23:36:02 +0530

    modified

--- src/STRING/StringBuilderandStrings.java
+++ src/STRING/StringBuilderandStrings.java
@@ -74,12 +74,27 @@ public class StringBuilderandStrings {
         System.out.println();
         System.out.println(series);
 
+//        every time we create a seperate object so it become o(n2)
+
     }
 
 
 }
 
-
+// placeHolder in java
+/*
+%c - Character
+%d - Decimal number (base 10)
+%e - Exponential floating-point number
+%f - Floating-point number
+%i - Integer (base 10)
+%o - Octal number (base 8)
+%s - String
+%u - Unsigned decimal (integer) number
+%x - Hexadecimal number (base 16)
+%t - Date/time
+%n - Newline
+ */
 
 
 
