e4beac0efd297f23e1efdc91abdbd976ded71da6
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-15 22:09:42 +0530

    modified

--- src/STRING/StringBuilderandStrings.java
+++ src/STRING/StringBuilderandStrings.java
@@ -12,11 +12,20 @@ public class StringBuilderandStrings {
         System.out.println(b);
 
     }
+
+
+    public class comparison{
+        public static void main(String[] args) {
+
+            String a = "ram";
+            String b = "ram";
+
+            System.out.println(a == b);
+        }
+    }
 }
 
-public class comparison{
 
-}
 
 
 
