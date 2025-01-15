5b3c42f8680449e460a308f1635c30c0dcd7c253
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-15 22:02:14 +0530

    modified

--- src/STRING/StringBuilderandStrings.java
+++ src/STRING/StringBuilderandStrings.java
@@ -3,10 +3,11 @@ package STRING;
 public class StringBuilderandStrings {
     public static void main(String[] args) {
 
-        String name = "dhanushkumar";
-        
+        String a = "dhanushkumar";
+        String b = "dhanushkumar";
 
-        System.out.println(name);
+        System.out.println(a);
+        System.out.println(a);
     }
 }
 
