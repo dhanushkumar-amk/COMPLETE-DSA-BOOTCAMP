f4c8485cc2390d93f04fb0fa239e5cbeef4f0589
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-17 05:15:14 +0530

    modified

--- src/SORTING/InsertionSort.java
+++ src/SORTING/InsertionSort.java
@@ -21,8 +21,8 @@ public class InsertionSort {
                     arr[j] = arr[j-1];
                     arr[j-1] = temp;
                 }
-                else
-                    break;
+//                else
+//                    break;
             }
         }
     }
