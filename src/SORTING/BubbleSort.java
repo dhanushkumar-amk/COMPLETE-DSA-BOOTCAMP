ebfd5c626952b1d1f0e12f5e7d2c0853206ffe46
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-16 12:03:10 +0530

    modified

--- src/SORTING/BubbleSort.java
+++ src/SORTING/BubbleSort.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args) {
     int[] arr = {3,1,5,4,2};
+    System.out.println(Arrays.toString(arr));
+    
     bubbleSort(arr);
     System.out.println(Arrays.toString(arr));
     }
