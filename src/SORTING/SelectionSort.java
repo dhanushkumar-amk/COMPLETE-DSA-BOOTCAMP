9a2128c6f6db2e16e733ebc8eaccb958d6e5ed99
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-17 03:40:33 +0530

    modified

--- src/SORTING/SelectionSort.java
+++ src/SORTING/SelectionSort.java
@@ -1,2 +1,18 @@
-package SORTING;public class SelectionSort {
+package SORTING;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {4,5,1,2,3};
+
+        System.out.println(Arrays.toString(arr));
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+        
+    }
+
+    static void selectionSort(int[] arr){
+
+    }
 }
