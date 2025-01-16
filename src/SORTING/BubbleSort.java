6bdc2e84d4c0a32e3caf88960b2423d16df89f25
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-16 12:02:03 +0530

    modified

--- src/SORTING/BubbleSort.java
+++ src/SORTING/BubbleSort.java
@@ -1,7 +1,31 @@
- package SORTING; 
+package SORTING;
+
+import java.util.Arrays;
 
 public class BubbleSort {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+    int[] arr = {3,1,5,4,2};
 
+    bubbleSort(arr);
+
+        System.out.println(Arrays.toString(arr));
     }
+
+    static void bubbleSort(int[] arr){
+
+        // run the steps n - 1 times
+        for (int i = 0; i < arr.length; i++) {
+            // for each  steps the max items comes to the end
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] < arr[j - 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j = 1] = temp;
+                }
+            }
+        }
+    }
+
+
+
 }
