3c9d270d8e1f1144f4012bd93068e337766e3264
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-17 05:15:23 +0530

    modified

--- src/SORTING/InsertionSort.java
+++ src/SORTING/InsertionSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class InsertionSort {
     public static void main(String[] args) {
 
-        int[] arr = {5,4,3,2,1};
+        int[] arr = {};
         System.out.println(Arrays.toString(arr));
 
         insertionSort(arr);
