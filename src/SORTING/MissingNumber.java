db42b72f25b0545940b2fa411d61f460bc3dedd5
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-17 14:18:57 +0530

    modified

--- src/SORTING/MissingNumber.java
+++ src/SORTING/MissingNumber.java
@@ -1,7 +1,30 @@
- package SORTING; 
+package SORTING;
 
 public class MissingNumber {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        int[] arr = {4,0,2,1};
+        System.out.println(missingNumber(arr));
+    }
 
+    static int missingNumber(int[] arr){
+        int i = 0;
+        while(i < arr.length){
+            int correctIndex = arr[i];
+            if (arr[i] < arr.length && arr[i] != arr[correctIndex]){
+                int temp = arr[i];
+                arr[i] = arr[correctIndex];
+                arr[correctIndex] = temp;
+            }else{
+                i++;
+            }
+        }
+        // search for first missing number
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index)
+                return index;
+        }
+
+        // case 2 if th n is missing numbers
+        return arr.length;
     }
 }
