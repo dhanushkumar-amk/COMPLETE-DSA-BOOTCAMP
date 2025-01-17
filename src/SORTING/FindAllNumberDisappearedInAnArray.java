fddacbb4fba22023939cb1ee6210b569a1b3dc6f
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-17 15:14:30 +0530

    modified

--- src/SORTING/FindAllNumberDisappearedInAnArray.java
+++ src/SORTING/FindAllNumberDisappearedInAnArray.java
@@ -7,6 +7,9 @@ public class FindAllNumberDisappearedInAnArray {
     public static void main(String[] args) {
     int[] arr = {4,3,2,7,8,2,3,1};
     List<Integer> ans = findDisappearedNumbers(arr);
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.println(ans.get(i));
+        }
 
 
     }
