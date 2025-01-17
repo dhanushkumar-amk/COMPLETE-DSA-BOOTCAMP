5c725a922081e945eb9528104c16bc721078bcf5
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-17 14:03:29 +0530

    modified

--- src/ARRAYS/MissingNumbers.java
+++ src/ARRAYS/MissingNumbers.java
@@ -1,2 +1,21 @@
-package ARRAYS;public class MissingNumbers {
+package ARRAYS;
+
+public class MissingNumbers {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,5};
+        int n = arr.length;
+        System.out.println(missingNumbers(arr, n));
+    }
+
+    static int missingNumbers(int[] arr, int n){
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+
+        int actualSum = (n * n + 1)/2;
+        int missingNumber = actualSum - sum;
+        return missingNumber;
+    }
 }
