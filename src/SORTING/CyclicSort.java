24c58a7bb5d4b992c266fafe07a4389464498cb9
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-17 13:51:22 +0530

    modified

--- src/SORTING/CyclicSort.java
+++ src/SORTING/CyclicSort.java
@@ -12,6 +12,20 @@ public class CyclicSort {
     }
 
     static void cyclicSort(int[] arr){
-        
+        int i = 0;
+
+        while(i < arr.length){
+            int correctIndex = arr[i] - 1;
+            if (arr[i] != arr[correctIndex]){
+                int temp =arr[i];
+                arr[i] = arr[correctIndex];
+                arr[correctIndex] = temp;
+            }else {
+                i++;
+            }
+
+        }
     }
 }
+
+
