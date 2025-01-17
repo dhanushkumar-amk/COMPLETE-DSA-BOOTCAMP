0393357b27ddf13f56a0c87c4c92fd7df58d1f1e
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-17 16:50:38 +0530

    modified

--- src/SORTING/FirstMissingPositiveNumber.java
+++ src/SORTING/FirstMissingPositiveNumber.java
@@ -6,6 +6,19 @@ public class FirstMissingPositiveNumber {
     }
 
     public static int firstMissingPositive(int[] arr) {
+        int i = 0;
+        while(i < arr.length){
+            int correctIndex = arr[i] - 1;
+            if (arr[i] != arr[correctIndex]){
+                int temp =arr[i];
+                arr[i] = arr[correctIndex];
+                arr[correctIndex] = temp;
+            }else {
+                i++;
+            }
+        }
+
         
+
     }
 }
