3e355e5c49bc32e6c6fdb06b84d4130eba360d00
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-17 16:00:06 +0530

    modified

--- src/SORTING/FindAllDuplicatesElementsInArray.java
+++ src/SORTING/FindAllDuplicatesElementsInArray.java
@@ -1,7 +1,32 @@
 package SORTING;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FindAllDuplicatesElementsInArray {
     public static void main(String[] args) {
+        int[] arr = {};
+        System.out.println(duplicateNumber(arr));
+    }
 
+    static List<Integer> duplicateNumber(int[] arr){
+        int i = 0;
+            while(i < arr.length) {
+                int correctIndex = arr[i] - 1;
+                if (arr[i] != arr[correctIndex]) {
+                    int temp = arr[i];
+                    arr[i] = arr[correctIndex];
+                    arr[correctIndex] = temp;
+                } else {
+                    i++;
+                }
+            }
+
+        List<Integer> list = new ArrayList<>();
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index + 1)
+                list.add(index + 1);
+        }
+        return list;
     }
 }
