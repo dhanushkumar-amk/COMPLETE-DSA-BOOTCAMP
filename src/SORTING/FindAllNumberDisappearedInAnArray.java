4268d4afa300e85b8d7dcb753534624c72e4bd1f
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-17 14:57:09 +0530

    modified

--- src/SORTING/FindAllNumberDisappearedInAnArray.java
+++ src/SORTING/FindAllNumberDisappearedInAnArray.java
@@ -1,2 +1,9 @@
-package SORTING;public class FindAllNumberdisapperingInAnArray {
+package SORTING;
+
+public class FindAllNumberDisappearedInAnArray {
+    public static void main(String[] args) {
+    int[] arr = {4,3,2,7,8,2,3,1};
+
+    }
+
 }
