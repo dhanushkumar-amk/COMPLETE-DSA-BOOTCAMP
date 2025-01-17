7ead0d9398aa631a1920671b2964fb39a1d18d2c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-17 16:18:00 +0530

    modified

--- src/SORTING/SetMisMatch.java
+++ src/SORTING/SetMisMatch.java
@@ -6,8 +6,24 @@ public class SetMisMatch {
 
     }
 
-   static int[] findErrorNums(int[] nums) {
-        int[] ans = new int[nums.length];
-        
+   static int[] findErrorNums(int[] arr) {
+
+       int i = 0;
+       while(i < arr.length){
+           int correctIndex = arr[i] - 1;
+           if (arr[i] != arr[correctIndex]){
+               int temp =arr[i];
+               arr[i] = arr[correctIndex];
+               arr[correctIndex] = temp;
+           }else {
+               i++;
+           }
+       }
+
+       for (int index = 0; index < arr.length; index++) {
+           if (arr[index] != index + 1)
+               return new int[]{arr[index], index + 1};
+       }
+        return new int[]{-1,-1};
     }
 }
