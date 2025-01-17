5cbe551bb6fb910e1e0f53c3867e158da0613a7c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-17 15:02:13 +0530

    modified

--- src/SORTING/FindAllNumberDisappearedInAnArray.java
+++ src/SORTING/FindAllNumberDisappearedInAnArray.java
@@ -1,9 +1,34 @@
 package SORTING;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FindAllNumberDisappearedInAnArray {
     public static void main(String[] args) {
     int[] arr = {4,3,2,7,8,2,3,1};
-
+        System.out.println(Arrays.toString(arr));
     }
+    static List<Integer> findDisappearedNumbers(int[] arr){
+        ArrayList<Integer> list = new ArrayList<>();
 
+        int i = 0;
+        while( i < arr.length){
+            int correctIndex = arr.length - 1;
+            if (arr[i] != correctIndex){
+                int temp = arr[i];
+                arr[i] = arr[correctIndex];
+                arr[correctIndex] = temp;
+            }else{
+                i++;
+            }
+        }
+
+        // just find the missing number and add as index + 1
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index)
+                list.add(index + 1);
+        }
+        return list;
+    }
 }
