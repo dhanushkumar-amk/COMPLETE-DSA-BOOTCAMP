54c88976fc0afad569e205920e149f60391abf0a
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-17 14:04:10 +0530

    modified

--- src/ARRAYS/MissingNumbers.java
+++ src/ARRAYS/MissingNumbers.java
@@ -10,7 +10,7 @@ public class MissingNumbers {
     static int missingNumbers(int[] arr, int n){
 
         int sum = 0;
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
         }
 
