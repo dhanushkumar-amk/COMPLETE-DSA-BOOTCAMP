117295e6483b8df05821f5063cc0bc2072c5ff81
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-17 15:43:26 +0530

    modified

--- src/SORTING/FindAllDuplicateNumbers.java
+++ src/SORTING/FindAllDuplicateNumbers.java
@@ -2,6 +2,25 @@ package SORTING;
 
 public class FindAllDuplicateNumbers {
     public static void main(String[] args) {
+    int[] arr = {1,3,4,2,2};
+        System.out.println();
+    }
+    static int duplicateNumber(int[] arr){
 
+        int i = 0;
+        while(i < arr.length){
+            if (arr[i] != i + 1) {
+                int correctIndex = arr[i] - 1;
+                if (arr[i] != arr[correctIndex]) {
+                    int temp = arr[i];
+                    arr[i] = arr[correctIndex];
+                    arr[correctIndex] = temp;
+                } else {
+                    return arr[i];
+                }
+            }
+            else i++;
+        }
+        return -1;
     }
 }
