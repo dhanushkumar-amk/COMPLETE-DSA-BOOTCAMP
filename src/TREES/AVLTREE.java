74d5e82d4e197071b3a5d1121efdf5712baace42
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-19 03:45:32 +0530

    modified

--- src/TREES/AVLTREE.java
+++ src/TREES/AVLTREE.java
@@ -1,6 +1,8 @@
 package TREES;
 
 public class AVLTREE {
+    private Node left;
+
     public class Node{
         private int value;
         private int height;
@@ -76,12 +78,69 @@ public class AVLTREE {
         // the max height is left node and right node + 1 => is a new height
         node.height = Math.max(height(node.left), height(node.right)) + 1;
 
-        // after rotate the node return itself as a root root up up and finally return root
+        // after insertion of the node then it check and it unbalanced then it will rotated and return as a root node
         return rotate(node);
     }
 
     // rotate function
     private Node rotate(Node node) {
+
+        if (height(node.left)  - height(node.right) > 1){
+            //left heavy
+            if (height(node.left.left) - height(node.left.right) > 0){
+                // left left case => right rotate  node
+                return rightRotate(node);
+            }
+
+            if (height(node.left.left) - height(node.left.right) < 0){
+                // left right case => left rotate on child and do right rotate on  node
+                node.left = leftRotate(node.left);
+                return rightRotate(node);
+            }
+        }
+
+        if (height(node.left)  - height(node.right) < -1){
+            //right  heavy
+            if (height(node.right.left) - height(node.right.right) < 0){
+                // right right case => right rotate  node
+                return leftRotate(node);
+            }
+
+            if (height(node.right.left) - height(node.right.right) > 0){
+                // left right case => left rotate on child and do right rotate on  node
+                node.right = rightRotate(node.right);
+                return leftRotate(node);
+            }
+        }
+        return node;
+    }
+
+    private Node rightRotate(Node p) {
+        Node c = p.left;
+        Node t = c.right;
+
+        c.right = p;
+        p.left = t;
+
+        p.height = Math.max(height(p.left), height(p.right) + 1);
+        c.height = Math.max(height(c.left), height(c.right) + 1);
+
+        return c;
+
+    }
+
+    private Node leftRotate(Node c) {
+        Node p = c.right;
+        Node t = p.left;
+
+
+        p.left = c;
+        c.right = t;
+
+        p.height = Math.max(height(p.left), height(p.right) + 1);
+        c.height = Math.max(height(c.left), height(c.right) + 1);
+
+        return p;
     }
 
     // insert through array
