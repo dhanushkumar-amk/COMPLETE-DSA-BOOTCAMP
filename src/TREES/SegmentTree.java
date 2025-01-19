65f9806b5eeb09472876829739ca1b8cf86a1321
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-19 10:00:06 +0530

    modified

--- src/TREES/SegmentTree.java
+++ src/TREES/SegmentTree.java
@@ -63,8 +63,8 @@ public class SegmentTree {
          }
 
          //no current child
-        str = str + " Interval = [ " + node..startIndex + " - " + node.left.endIndex + " ] and the " +
-                "data : " + node.left.data  + "+ -> ";
+        str = str + " Interval = [ " + node.startIndex + " - " + node.endIndex + " ] and the " +
+                "data : " + node.data  + "+ -> ";
 
         if (node.right != null){
             str = str + " Interval = [ " + node.right.startIndex + " - " + node.right.endIndex + " ] and the " +
@@ -75,14 +75,62 @@ public class SegmentTree {
         }
         System.out.println(str);
 
+        if(node.left != null)
+            display(node.left);
 
+        if(node.right != null)
+            display(node.right);
     }
+
+
+    // query
+    public int query(int queryStartIndex, int queryEndIndex){
+        return this.query(this.root, queryStartIndex, queryEndIndex);
+    }
+
+    private int query(Node node, int queryStartIndex, int queryEndIndex) {
+        if (node.startIndex >= queryStartIndex && node.endIndex <= queryEndIndex){
+            // node completely lie inside the query
+            return node.data;
+        } else if (node.startIndex > queryEndIndex || node.endIndex < queryStartIndex) {
+            // completely outside
+            return 0;
+        }else{
+            return this.query(node.left, queryStartIndex, queryEndIndex) + this.query(node.right, queryStartIndex, queryEndIndex);
+        }
+    }
+
+    // update value in particular index
+
+    public void update(int index, int value){
+        this.root.data = update(index, value, this.root);
+    }
+
+    private int update(int index, int value, Node node){
+        if (index >= node.startIndex && index <= node.endIndex){
+            if (index == node.startIndex  && index == node.endIndex){
+             // base case
+             node.data = value;
+             return node.data;
+            }else{
+                int leftAnswer = update( index, value, node.left);
+                int rightAnswer = update( index, value, node.right);
+
+                node.data = leftAnswer + rightAnswer;
+                return node.data;
+            }
+        }
+        return node.data;
+    }
+
 
 
     // main function
     public static void main(String[] args) {
         int[] arr = {3,8,7,6,-2,-8,4,9};
         SegmentTree tree = new SegmentTree(arr);
+
+        tree.display();
     }
 
 }
