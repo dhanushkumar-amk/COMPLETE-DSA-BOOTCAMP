41542e7cd2ce7ade52db4dba5932bbe56be5b266
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-19 10:06:10 +0530

    modified

--- src/TREES/SegmentTree.java
+++ src/TREES/SegmentTree.java
@@ -126,17 +126,6 @@ public class SegmentTree {
 
 
     // main function
-    public static void main(String[] args) {
-        int[] arr = {3,8,7,6,-2,-8,4,9};
-        SegmentTree tree = new SegmentTree(arr);
-
-
-        System.out.println(tree.query(2,6));
-        //tree.update(0, 1);
-
-        tree.display();
-
-
-    }
+   
 
 }
