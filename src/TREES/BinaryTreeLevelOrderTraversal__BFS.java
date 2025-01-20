1960296c5b8d3108598214e9fe676b4e9d32aafa
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-20 14:24:19 +0530

    modified

--- src/TREES/BinaryTreeLevelOrderTraversal__BFS.java
+++ src/TREES/BinaryTreeLevelOrderTraversal__BFS.java
@@ -10,6 +10,26 @@ public class BinaryTreeLevelOrderTraversal__BFS {
 
         List<List<Integer>> result = new ArrayList<>();
 
-        
+
     }
+
+    public class TreeNode{
+        private int value;
+        private int height;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode(int value) {
+            this.value = value;
+        }
+
+        public int getValue(){
+            return value;
+        }
+    }
+
+    private TreeNode root;
+
+
+
 }
