1c82818f77c2fbdb962f1559c9ae24ef0afffa94
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-20 15:10:33 +0530

    modified

--- src/TREES/AverageOfLevelsInBinaryTree.java
+++ src/TREES/AverageOfLevelsInBinaryTree.java
@@ -7,4 +7,24 @@ package TREES;
 
 public class AverageOfLevelsInBinaryTree {
 
+
+    public class TreeNode {
+       int val;
+       TreeNode left;
+       TreeNode right;
+
+       TreeNode() {
+
+       }
+
+       TreeNode(int val) {
+           this.val = val;
+       }
+       
+       TreeNode(int val, TreeNode left, TreeNode right) {
+           this.val = val;
+           this.left = left;
+           this.right = right;
+       }
+   }
 }
