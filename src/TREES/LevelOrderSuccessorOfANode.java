dd172350c0c31dc9b6153c54b8463cfd0d664e79
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-21 13:40:56 +0530

    modified

--- src/TREES/LevelOrderSuccessorOfANode.java
+++ src/TREES/LevelOrderSuccessorOfANode.java
@@ -1,2 +1,27 @@
-package TREES;public class LevelOrderSuccessorOfANode {
+package TREES;
+
+public class LevelOrderSuccessorOfANode {
+
+
+
+    public class TreeNode {
+        int val;
+         TreeNode left;
+         TreeNode right;
+        TreeNode() {
+
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val,  TreeNode left,  TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private TreeNode root;
 }
