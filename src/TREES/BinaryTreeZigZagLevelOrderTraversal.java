a796a57557c0f9c3b1ee3f140db27331d973e5a5
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-21 14:44:59 +0530

    modified

--- src/TREES/BinaryTreeZigZagLevelOrderTraversal.java
+++ src/TREES/BinaryTreeZigZagLevelOrderTraversal.java
@@ -3,6 +3,8 @@ package TREES;
 // https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/description/
 // leetcode 103
 
+import java.util.*;
+
 public class BinaryTreeZigZagLevelOrderTraversal {
 
     public class TreeNode {
@@ -29,8 +31,50 @@ public class BinaryTreeZigZagLevelOrderTraversal {
 
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
 
-        
+        List<List<Integer>> result = new ArrayList<>();
 
+        if (root == null)
+            return result;
+
+        Deque<TreeNode> deque = new LinkedList<>();
+        deque.offer(root);
+
+        boolean isReverse = false;
+
+        while(!deque.isEmpty()){
+            int currentLevelSize = deque.size();
+            List<Integer> currentLevelList = new ArrayList<>(currentLevelSize);
+
+            for (int i = 0; i < currentLevelSize; i++) {
+
+                if (!isReverse){
+                    TreeNode currentNode = deque.pollFirst();
+                    currentLevelList.add(currentNode.val);
+
+                    if (currentNode.left != null)
+                        deque.addLast(currentNode.left);
+
+                    if (currentNode.right != null)
+                        deque.addLast(currentNode.right);
+                }else{
+
+                    TreeNode currentNode = deque.pollLast();
+                    currentLevelList.add(currentNode.val);
+
+                    if (currentNode.right != null)
+                        deque.addFirst(currentNode.right);
+
+                    if (currentNode.left != null)
+                        deque.addFirst(currentNode.left);
+
+                }
+                // if true means false || false means true conversion on every iteration
+                isReverse = !isReverse;
+            }
+
+            result.add(currentLevelList);
+        }
+        return result;
     }
 
 
