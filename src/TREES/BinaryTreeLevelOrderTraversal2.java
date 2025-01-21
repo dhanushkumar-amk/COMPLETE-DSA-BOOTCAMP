365baf60c2cbb4419bfb23b30207072f947a099f
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-22 00:34:48 +0530

    modified

--- src/TREES/BinaryTreeLevelOrderTraversal2.java
+++ src/TREES/BinaryTreeLevelOrderTraversal2.java
@@ -1,5 +1,7 @@
 package TREES;
 
+import java.util.*;
+
 public class BinaryTreeLevelOrderTraversal2 {
 
 
@@ -27,7 +29,36 @@ public class BinaryTreeLevelOrderTraversal2 {
     private TreeNode root;
 
 
-    
-    
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
 
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null)
+            return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()){
+
+            int currentLevelSize = queue.size();
+
+            List<Integer> currentLevelList = new ArrayList<>(currentLevelSize);
+
+            for (int i = 0; i < currentLevelSize ; i++) {
+
+                TreeNode currentNode = queue.poll();
+                currentLevelList.add(currentNode.val);
+
+                if (currentNode.left != null)
+                    queue.offer(currentNode.left);
+
+                if (currentNode.right != null)
+                    queue.offer(currentNode.right);
+            }
+
+            result.add(0, currentLevelList);
+        }
+        return result;
+    }
 }
