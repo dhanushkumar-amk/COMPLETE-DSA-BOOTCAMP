f3f8302c5831fcfaf6d3b48090e849711e547685
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-22 00:28:31 +0530

    modified

--- src/TREES/BinaryTreeLevelOrderTraversal2.java
+++ src/TREES/BinaryTreeLevelOrderTraversal2.java
@@ -1,5 +1,33 @@
 package TREES;
 
 public class BinaryTreeLevelOrderTraversal2 {
+
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
     
+    private TreeNode root;
+
+
+    
+    
+
 }
