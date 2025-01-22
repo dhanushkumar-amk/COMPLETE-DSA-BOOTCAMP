4144dc248932ae677d9e65bef00096012a28a5c8
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-22 23:48:47 +0530

    modified

--- src/TREES/BinaryTreeRightSideView.java
+++ src/TREES/BinaryTreeRightSideView.java
@@ -4,6 +4,13 @@ package TREES;
 // https://leetcode.com/problems/binary-tree-right-side-view/description/
 // leetcode 199
 
+import com.sun.source.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class BinaryTreeRightSideView {
 
     public class TreeNode {
@@ -28,6 +35,35 @@ public class BinaryTreeRightSideView {
 
     private TreeNode root;
 
-    
+    public List<Integer> rightSideView(TreeNode root) {
+
+        List<Integer> result = new ArrayList<>();
+
+        if (root == null)
+            return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()){
+            int currentLevelSize = queue.size();
+
+            for (int i = 0; i <currentLevelSize ; i++) {
+                TreeNode currentNode  = queue.poll();
+
+                // only added lst element in queue consider as a answer
+                if (i == currentLevelSize - 1)
+                    result.add(currentNode.val);
+
+                if (currentNode.left != null)
+                    queue.offer(currentNode.left);
+
+                if (currentNode.right != null)
+                    queue.offer(currentNode.right);
+            }
+        }
+
+        return result;
+    }
 
 }
