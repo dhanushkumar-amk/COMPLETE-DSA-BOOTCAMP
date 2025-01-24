2ad0b6efc40c5a7fcdc7345e6109d553318b368d
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-24 15:20:46 +0530

    modified

--- src/TREES/SymmetricTree.java
+++ src/TREES/SymmetricTree.java
@@ -6,8 +6,27 @@ package TREES;
 // leetcode 101
 
 
+
+
 public class SymmetricTree {
 
 
+     public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+  private TreeNode root;
+        
+
+    
 
 }
