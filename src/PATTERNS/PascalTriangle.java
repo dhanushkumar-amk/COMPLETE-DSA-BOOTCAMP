4a152afa3a3ebced693bef8595ec4af77573bb3c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-25 14:43:59 +0530

    modified

--- src/PATTERNS/PascalTriangle.java
+++ src/PATTERNS/PascalTriangle.java
@@ -4,22 +4,26 @@ import java.util.*;
 
 public class PascalTriangle {
     public static void main(String[] args) {
-
+        int n = 5;
     }
 
-    public List<List<Integer>> generate(int numRows) {
+    public static List<List<Integer>> generate(int numRows) {
         List<List<Integer>> result = new ArrayList<>();
 
         for (int i = 0; i < numRows; i++) {
             List<Integer> currentRowList = new ArrayList<>();
 
             for (int j = 0; j <= i; j++) {
-                
-
+                if (j == 0 || j == i){
+                    currentRowList.add(1);
+                }
+                else {
+                    List<Integer> previousRow = result.get(i - 1);
+                    currentRowList.add(previousRow.get(j) + previousRow.get(j - 1));
+                }
             }
+            result.add(currentRowList);
         }
-
+        return result;
     }
-
-
 }
