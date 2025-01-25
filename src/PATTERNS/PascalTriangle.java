7772571bfd069337dfa2d8c397daba0edc197ffe
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-25 14:37:32 +0530

    modified

--- src/PATTERNS/PascalTriangle.java
+++ src/PATTERNS/PascalTriangle.java
@@ -1,2 +1,25 @@
-package PATTERNS;public class PascalTriangle {
+package PATTERNS;
+
+import java.util.*;
+
+public class PascalTriangle {
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> currentRowList = new ArrayList<>();
+
+            for (int j = 0; j <= i; j++) {
+                
+
+            }
+        }
+
+    }
+
+
 }
