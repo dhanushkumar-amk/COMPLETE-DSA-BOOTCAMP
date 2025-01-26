2c7323ac41d4cd5c07fc405a1a2788f150eb2e77
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-27 00:24:47 +0530

    modified

--- src/TREES/InvertBinaryTree.java
+++ src/TREES/InvertBinaryTree.java
@@ -1,2 +1,32 @@
-package TREES;public class InvertBinaryTree {
+package TREES;
+
+
+public class InvertBinaryTree {
+
+    class   TreeNode{
+
+         int val;
+       TreeNode left;
+        TreeNode right;
+
+        public TreeNode(TreeNode left, TreeNode right, int val) {
+            this.left = left;
+            this.right = right;
+            this.val = val;
+        }
+
+        public TreeNode() {
+        }
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    private TreeNode root;
+
+    public TreeNode invertTree(TreeNode root) {
+
+        return root;
+    }
 }
