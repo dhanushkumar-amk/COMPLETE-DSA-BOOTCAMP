f3772d81dd26237b6a6f052a940ee0255dbfc49e
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-27 00:39:16 +0530

    modified

--- src/TREES/MaximumDepthOfBinaryTree.java
+++ src/TREES/MaximumDepthOfBinaryTree.java
@@ -1,2 +1,28 @@
-package TREES;public class MaximumDepthOfBinaryTree {
+package TREES;
+
+public class MaximumDepthOfBinaryTree {
+
+    public class TreeNode{
+
+        int val;
+         TreeNode left;
+         TreeNode right;
+
+        public TreeNode( TreeNode left,  TreeNode right, int val) {
+            this.left = left;
+            this.right = right;
+            this.val = val;
+        }
+
+        public TreeNode() {
+        }
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    private  TreeNode root;
+
+    
 }
