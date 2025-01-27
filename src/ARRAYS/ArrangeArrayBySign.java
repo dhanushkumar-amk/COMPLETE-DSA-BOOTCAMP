c3acf16e9580f0229a267b1f4add9029f343d7ba
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-28 02:53:25 +0530

    modified

--- src/ARRAYS/ArrangeArrayBySign.java
+++ src/ARRAYS/ArrangeArrayBySign.java
@@ -1,9 +1,37 @@
 package ARRAYS;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ArrangeArrayBySign {
     public static void main(String[] args) {
-
+        int[] arr = {3,1,-2, -5, 2, -4};
+        int[] answer = arrange(arr);
+        System.out.println(Arrays.toString(answer));
     }
 
-    stat
+    static int[] arrange(int[] arr){
+
+        ArrayList<Integer> positiveValue = new ArrayList<>();
+        ArrayList<Integer> negativeValue = new ArrayList<>();
+
+        for (int i = 0; i < arr.length ; i++) {
+            if (arr[i] > 0)
+                positiveValue.add(arr[i]);
+            else
+                negativeValue.add(arr[i]);
+        }
+
+        int[] answer = new int[arr.length];
+        int k = 0;
+        int c = 0;
+
+        while (c < positiveValue.size()){
+            answer[k] = positiveValue.get(c);
+            answer[k+1] = negativeValue.get(c);
+            k+=2;
+            c++;
+        }
+        return answer;
+    }
 }
