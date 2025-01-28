13a38195b27b07f5fde97f53823890ea38e1533e
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-28 20:47:32 +0530

    modified

--- src/MATHS/PercentageCalculation.java
+++ src/MATHS/PercentageCalculation.java
@@ -1,9 +1,17 @@
 package MATHS;
 
+import java.util.Scanner;
+
 public class PercentageCalculation {
     public static void main(String[] args) {
 
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the salary amount : ");
+        int salary = sc.nextInt();
+
     }
+
+    public static 
 
 
 }
