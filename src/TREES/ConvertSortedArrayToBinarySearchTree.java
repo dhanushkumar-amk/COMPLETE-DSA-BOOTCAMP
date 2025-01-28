01cfad024306354e36e94175d5126b52f6f65bf0
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-28 20:04:25 +0530

    modified

--- src/TREES/ConvertSortedArrayToBinarySearchTree.java
+++ src/TREES/ConvertSortedArrayToBinarySearchTree.java
@@ -5,6 +5,19 @@ package TREES;
 
 public class ConvertSortedArrayToBinarySearchTree {
 
+    public class TreeNode {
+       int val;
+       TreeNode left;
+       TreeNode right;
+       TreeNode() {}
+       TreeNode(int val) { this.val = val; }
+       TreeNode(int val, TreeNode left, TreeNode right) {
+           this.val = val;
+           this.left = left;
+           this.right = right;
+       }
+   }
 
+   
 
 }
