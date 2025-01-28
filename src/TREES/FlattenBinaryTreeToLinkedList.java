7cbed25e42675ca932fb0fa5a338d167519fcde9
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-28 20:32:23 +0530

    modified

--- src/TREES/FlattenBinaryTreeToLinkedList.java
+++ src/TREES/FlattenBinaryTreeToLinkedList.java
@@ -1,2 +1,22 @@
-package TREES;public class FlattenBinaryTreeToLinkedList {
+package TREES;
+
+//
+
+public class FlattenBinaryTreeToLinkedList {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private TreeNode root;
+
+    
 }
