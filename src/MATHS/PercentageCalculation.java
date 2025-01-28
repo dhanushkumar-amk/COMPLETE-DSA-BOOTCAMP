6b30eeb866ce61850b23243bab4696e2cbbecdfe
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-28 20:55:56 +0530

    modified

--- src/MATHS/PercentageCalculation.java
+++ src/MATHS/PercentageCalculation.java
@@ -8,11 +8,23 @@ public class PercentageCalculation {
         Scanner sc =new Scanner(System.in);
         System.out.println("Enter the salary amount : ");
         int salary = sc.nextInt();
-
+        percentage(salary);
     }
 
     public static void percentage(int salary){
-        
+
+        double needs = 0.4; // 50%
+        double wants = 0.3; // 30%
+        double savings = 0.2; // 20%
+
+        int need = (int) (salary * needs);
+        int want = (int) (salary * wants);
+        int saving = (int) (salary * savings);
+
+        System.out.println("the total salary " + salary);
+        System.out.println("the amount for needs " +  need);
+        System.out.println("the amount for wants " +  want);
+        System.out.println("the amount for savings " +  saving);
     }
 
 
