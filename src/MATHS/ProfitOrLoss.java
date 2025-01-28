a015790ebcb22daebad1dffaf2e7bcb08c01089b
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-28 20:41:49 +0530

    modified

--- src/MATHS/ProfitOrLoss.java
+++ src/MATHS/ProfitOrLoss.java
@@ -1,2 +1,31 @@
-package MATHS;public class ProfitOrLoss {
+package MATHS;
+
+import java.util.Scanner;
+
+public class ProfitOrLoss {
+    public static void main(String[] args) {
+
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the cost price : ");
+        int costPrice = sc.nextInt();
+
+        System.out.println("Enter the selling price : ");
+        int sellingPrice = sc.nextInt();
+
+        profitOrLoss(costPrice,sellingPrice);
+    }
+
+    static void profitOrLoss(int costPrice, int sellingPrice){
+
+        int result = Math.abs(costPrice - sellingPrice);
+
+        if (result == 0)
+            System.out.println("Nothing profit or loss " + result);
+
+        if (result > 0)
+            System.out.println("Profit " + result);
+        else
+            System.out.println("Loss " + result);
+
+    }
 }
