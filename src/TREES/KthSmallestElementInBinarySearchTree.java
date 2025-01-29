657bbd7732dc60468e97c8096e8ce0f939b7c041
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-30 05:16:51 +0530

    modified

--- src/TREES/KthSmallestElementInBinarySearchTree.java
+++ src/TREES/KthSmallestElementInBinarySearchTree.java
@@ -4,5 +4,16 @@ package TREES;
 // leetcode 230
 
 public class KthSmallestElementInBinarySearchTree {
-    
+    public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
 }
