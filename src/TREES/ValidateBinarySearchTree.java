9980d22785048132c5f571e0a7aa675726da36cc
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-29 19:23:11 +0530

    modified

--- src/TREES/ValidateBinarySearchTree.java
+++ src/TREES/ValidateBinarySearchTree.java
@@ -7,7 +7,18 @@ package TREES;
 
 public class ValidateBinarySearchTree {
 
-
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
 
     public boolean isValidBST(TreeNode root) {
 
