838ac49a2792c4c964c9bd63644ad9f728c185be
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-30 06:08:12 +0530

    modified

--- src/TREES/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ src/TREES/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -3,6 +3,8 @@ package TREES;
 // leetcode 105
 // https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
 
+import java.util.Arrays;
+
 public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
 
     public class TreeNode {
@@ -18,6 +20,30 @@ public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
       }
   }
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+        if (preorder.length == 0)
+            return null;
+
+        int root = preorder[0];
+        int index = 0;
+
+        for (int i = 0; i <inorder.length; i++) {
+            if (inorder[i] == root)
+                index = i;
+        }
+
+        TreeNode node = new TreeNode(root);
+
+         node.left = buildTree(Arrays.copyOfRange(preorder, 1, index + 1),
+                 Arrays.copyOfRange(inorder, 0, index)); // from 1 because 0 is a root element
+
+         node.right = buildTree(Arrays.copyOfRange(preorder, index + 1, preorder.length),
+                 Arrays.copyOfRange(inorder, index + 1, inorder.length));
+
+
+         return node;
+
+
 
     }
     
