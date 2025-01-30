03813aed963f38679ff0e66bb7399a2d4ca25326
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-30 15:44:47 +0530

    modified

--- src/STRING/CountString.java
+++ src/STRING/CountString.java
@@ -2,8 +2,30 @@ package STRING;
 
 public class CountString {
     public static void main(String[] args) {
-
+        String s = "xxyyyyyzzzzzzz";
+        count(s);
     }
 
-    sta
+    static  void count(String s){
+
+        int xcount  = 0;
+        int ycount = 0;
+        int zcount = 0;
+
+        for (char i = 0; i <s.length() ; i++) {
+            if (s.charAt(i) == 'x')
+                xcount++;
+
+            if (s.charAt(i) == 'y')
+                ycount++;
+
+            if (s.charAt(i) == 'z')
+                zcount++;
+        }
+
+        System.out.println(xcount);
+        System.out.println(ycount);
+        System.out.println(zcount);
+    }
+
 }
