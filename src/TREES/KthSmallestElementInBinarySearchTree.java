8475eb6ea0706b194cd6885fb649e00bcee91f41
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-30 05:32:49 +0530

    modified

--- src/TREES/KthSmallestElementInBinarySearchTree.java
+++ src/TREES/KthSmallestElementInBinarySearchTree.java
@@ -17,9 +17,30 @@ public class KthSmallestElementInBinarySearchTree {
       }
   }
 
-    public int kthSmallest(TreeNode root, int k) {
 
-    }
+
+    int count = 0;
+
     
+
+    public TreeNode helper(TreeNode root, int k) {
+
+        if (root == null)
+            return null;
+
+        TreeNode left = helper(root.left, k);
+
+        if (left != null){
+            return left;
+        }
+
+        count++;
+
+        if (count == k)
+            return root;
+
+        return helper(root.right, k);
+    }
+
 
 }
