f70e1306c3d3738978485e842ba6178a1f0610d5
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-31 12:45:23 +0530

    modified

--- src/TREES/SerializeAndDeserializeBinaryTree.java
+++ src/TREES/SerializeAndDeserializeBinaryTree.java
@@ -1,6 +1,9 @@
 package TREES;
 
 // https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
+// leetcode 297
+
+import java.util.*;
 
 public class SerializeAndDeserializeBinaryTree {
 
@@ -14,12 +17,44 @@ public class SerializeAndDeserializeBinaryTree {
 
   }
 
-  public String serialize(TreeNode root) {
-
+  public List<String> serialize(TreeNode root) {
+    List<String> list = new ArrayList<>();
+    helper(root, list);
+    return list;
   }
 
-  public TreeNode deserialize(String data) {
+  void helper(TreeNode node, List<String> list){
 
+      if (node == null) {
+        list.add(null);
+        return;
+      }
+
+      list.add(String.valueOf(node.val));
+
+      helper(node.left, list);
+      helper(node.right, list);
+  }
+
+  public TreeNode deserialize(List<String> list) {
+    Collections.reverse(list);
+
+    TreeNode node = helper2(list);
+    return node;
+  }
+
+  private TreeNode helper2(List<String> list) {
+      String val = list.remove(list.size() - 1);
+
+      if (val.charAt(0) == 'n')
+          return null;
+
+      TreeNode node = new TreeNode(Integer.parseInt(val));
+
+      node.left = helper2(list);
+      node.right = helper2(list);
+
+      return node;
   }
 }
 
