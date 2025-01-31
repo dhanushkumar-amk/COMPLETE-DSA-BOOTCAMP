6c31d26b8d32604aa58a9360e29cda772779ccb8
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-31 13:05:42 +0530

    modified

--- src/TREES/PathSum.java
+++ src/TREES/PathSum.java
@@ -4,5 +4,16 @@ package TREES;
 // leetcode 112
 
 public class PathSum {
-
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }    
 }
