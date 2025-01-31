7721e1a72e1a49dd83995182e6b954a109216b83
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-31 21:30:44 +0530

    modified

--- src/TREES/VerticalOrderTraversalOfABinaryTree.java
+++ src/TREES/VerticalOrderTraversalOfABinaryTree.java
@@ -3,6 +3,8 @@ package TREES;
 // leetcode 987
 // https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/description/
 
+import java.util.*;
+
 public class VerticalOrderTraversalOfABinaryTree {
 
     public class TreeNode {
@@ -18,5 +20,46 @@ public class VerticalOrderTraversalOfABinaryTree {
       }
   }
 
-  
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<List<Integer>> answer = new ArrayList<List<Integer>>();
+
+        if (root == null)
+            return answer;
+
+        int column = 0;
+        Queue<Map.Entry<TreeNode, Integer>> queue = new ArrayDeque<>();
+        Map<Integer, ArrayList<Integer>> map = new HashMap<>();
+
+        queue.offer(new AbstractMap.SimpleEntry<>(root, column));
+
+        int min = 0;
+        int max = 0;
+
+        while (!queue.isEmpty()){
+            Map.Entry<TreeNode, Integer> removed = queue.poll();
+            root = removed.getKey();
+            column = removed.getValue();
+
+
+            if (root != null){
+                if (!map.containsKey(column)){
+                    map.put(column, new ArrayList<Integer>());
+                }
+                map.get(column).add(root.val);
+
+                min = Math.min(min, column);
+                max = Math.max(max, column);
+
+                queue.offer(new AbstractMap.SimpleEntry<>(root.left, column- 1));
+                queue.offer(new AbstractMap.SimpleEntry<>(root.right, column + 1));
+
+            }
+        }
+
+        for (int i = min; i <= max; i++) {
+            answer.add(map.get(i));
+        }
+
+        return answer;
+    }
 }
