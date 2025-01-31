aa31154198ad4d4519c1944c31e827d4fb072142
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-31 13:38:26 +0530

    modified

--- src/TREES/BinaryTreeMaximumPathSum.java
+++ src/TREES/BinaryTreeMaximumPathSum.java
@@ -1,4 +1,17 @@
 package TREES;
 
 public class BinaryTreeMaximumPathSum {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+  
 }
