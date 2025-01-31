5344dbd0f72d6259d03edf2d4397479e3cd768b1
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-31 20:51:07 +0530

    modified

--- src/TREES/ConstructBinaryTreeFromPreorderAndInorderTraversalUsingHashMap.java
+++ src/TREES/ConstructBinaryTreeFromPreorderAndInorderTraversalUsingHashMap.java
@@ -2,6 +2,8 @@ package TREES;
 // leetcode 105
 // https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
 
+import java.util.HashMap;
+
 public class ConstructBinaryTreeFromPreorderAndInorderTraversalUsingHashMap {
     public class TreeNode {
         int val;
@@ -16,6 +18,36 @@ public class ConstructBinaryTreeFromPreorderAndInorderTraversalUsingHashMap {
         }
     }
 
-    
+    static int index = 0;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+
+       return helper(preorder, inorder, 0, preorder.length, map);
+    }
+
+    private TreeNode helper(int[] preorder, int[] inorder, int left, int right, HashMap<Integer, Integer> map) {
+
+        if (left > right)
+            return null;
+
+        int current = preorder[index];
+        index++;
+
+        TreeNode node = new TreeNode(current);
+
+        // it becomes leaf node
+        if (left == right)
+            return node;
+
+        node.left = helper(preorder, inorder, left, index - 1, map);
+        node.left = helper(preorder, inorder, index + 1, right, map);
+
+        return node;
+    }
 
 }
