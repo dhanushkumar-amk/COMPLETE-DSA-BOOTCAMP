12c0ca2c7acebc216feed8825debac7eae7f5ca8
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-31 14:04:04 +0530

    modified

--- src/TREES/BinaryTreeMaximumPathSum.java
+++ src/TREES/BinaryTreeMaximumPathSum.java
@@ -8,6 +8,7 @@ public class BinaryTreeMaximumPathSum {
       int val;
       TreeNode left;
       TreeNode right;
+
       TreeNode() {}
       TreeNode(int val) { this.val = val; }
       TreeNode(int val, TreeNode left, TreeNode right) {
@@ -16,7 +17,29 @@ public class BinaryTreeMaximumPathSum {
           this.right = right;
       }
   }
-    public int maxPathSum(TreeNode root) {
+
+  int answer = Integer.MIN_VALUE;
+  public int maxPathSum(TreeNode root) {
+      helper(root);
+      return answer;
+    }
+
+    private int helper(TreeNode node) {
+      if (node == null)
+          return 0;
+
+      int left = helper(node.left);
+      int right = helper(node.right);
+
+      // if it value was negative
+      left = Math.max(0, left);
+      right = Math.max(0, right);
+
+      int sum = left + right + node.val;
+
+      answer = Math.max(answer, sum);
+
+      return Math.max(left, right) + node.val;
 
     }
 }
