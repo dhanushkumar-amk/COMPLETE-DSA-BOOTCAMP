d96c5b084560d3091d05373ac28f0f6f5a4f4f62
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-31 12:33:39 +0530

    modified

--- src/TREES/SerializeAndDeserializeBinaryTree.java
+++ src/TREES/SerializeAndDeserializeBinaryTree.java
@@ -2,7 +2,18 @@ package TREES;
 
 public class SerializeAndDeserializeBinaryTree {
 
-    
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) {
+        val = x;
+      }
+
+  }
+
+  
+
 
 }
 
