627110abc8d116879351eefa16547f3aa1fe1e77
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-01-31 14:31:09 +0530

    modified

--- src/TREES/PathExitsInBinaryTreeFromRootToLeaf.java
+++ src/TREES/PathExitsInBinaryTreeFromRootToLeaf.java
@@ -22,13 +22,22 @@ public class PathExitsInBinaryTreeFromRootToLeaf {
     }
 
     public boolean findPath(TreeNode node, int[] arr){
-
         if (node == null)
             return arr.length == 0;
-
-        return helper()
-
-
+        
+        return helper(node, arr ,0);
     }
 
+    private boolean helper(TreeNode node, int[] arr, int index) {
+        if (node == null)
+            return false;
+
+        if (index >= arr.length || node.val != arr[index])
+            return false;
+
+        if (node.left == null && node.right == null && index == arr.length - 1)
+            return true;
+
+        return helper(node.left, arr, index + 1) || helper(node.right, arr, index + 1);
+    }
 }
