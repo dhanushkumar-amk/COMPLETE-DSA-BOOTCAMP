b3e9b6d404186236ba3d3fe950d334c137ca7a02
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-01 16:31:28 +0530

    modified

--- src/HEAPS/Heap.java
+++ src/HEAPS/Heap.java
@@ -12,12 +12,64 @@ public class Heap<T extends  Comparable<T>> {
     }
 
 
-
+    // parent
     private int parent(int index){
         return  (index - 1)/2;
     }
 
-    
+    // left
+    private int left(int index){
+        return (index * 2) + 1;
+    }
+
+    // right
+    private int right(int index){
+        return (index * 2) + 2;
+    }
+
+
+    // insert
+    public  void insert(T value){
+        // add the element in last
+        list.add(value);
+
+        // search towards up and swap
+        upHeap(list.size() - 1);
+    }
+
+    // upHeap
+    private void upHeap(int index) {
+
+        if (index == 0) return;
+
+        int p = parent(index);
+
+        if (list.get(index).compareTo(list.get(p)) < 0){
+            swap(index, p);
+            upHeap(p);
+        }
+    }
+
+    // remove
+    private T remove() throws Exception{
+
+        if (list.isEmpty()){
+            throw new Exception("Removing from the empty heap");
+        }
+
+        // return 0th index
+        T temp = list.get(0);
+
+        // swap the last index with oth index
+        T last = list.remove(list.size()- 1);
+
+        if (!list.isEmpty()){
+            list.set(0, last);
+        }
+
+        return temp;
+    }
+
 
     // swap function
     private void swap(int first, int second) {
