edd0ec03510574d1828c4b1ab05d6cb59a7ba71b
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-01 16:04:23 +0530

    modified

--- src/HEAPS/Heap.java
+++ src/HEAPS/Heap.java
@@ -1,2 +1,34 @@
-package HEAPS;public class Heap {
+package HEAPS;
+
+
+import java.util.ArrayList;
+
+public class Heap<T extends  Comparable<T>> {
+
+    private ArrayList<T> list;
+
+    public Heap() {
+        list = new ArrayList<>();
+    }
+
+
+
+    private int parent(int index){
+        return  (index - 1)/2;
+    }
+
+    
+
+    // swap function
+    private void swap(int first, int second) {
+        T temp = list.get(first);
+        list.set(first, list.get(second));
+        list.set(second, temp);
+    }
+}
+
+
+
+class main{
+
 }
