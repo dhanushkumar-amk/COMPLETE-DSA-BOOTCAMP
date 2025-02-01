e88bda6c6c106f1f7055f413c1e114da9263f60e
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-01 10:14:20 +0530

    modified

--- src/TREES/WordLadder.java
+++ src/TREES/WordLadder.java
@@ -8,10 +8,20 @@ public class WordLadder {
     public static void main(String[] args) {
         List<String> wordList = new ArrayList<>();
         wordList.add("hot");
-        
+        wordList.add("dot");
+        wordList.add("dog");
+        wordList.add("lot");
+        wordList.add("log");
+        wordList.add("cog");
+
+        String beginWord = "hit";
+        String endWord = "cog";
+
+        System.out.println(ladderLength(beginWord, endWord,));
+
     }
 
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+    public  static  int ladderLength(String beginWord, String endWord, List<String> wordList) {
 
     }
 }
