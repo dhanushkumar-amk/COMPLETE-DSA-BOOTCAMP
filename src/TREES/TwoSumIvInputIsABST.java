d2e41b94654d2d65ab882877d95c1034c42dddcf
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-01 11:16:00 +0530

    modified

--- src/TREES/TwoSumIvInputIsABST.java
+++ src/TREES/TwoSumIvInputIsABST.java
@@ -3,6 +3,8 @@ package TREES;
 //leetcode 653
 // https://leetcode.com/problems/two-sum-iv-input-is-a-bst/description/
 
+import java.util.HashSet;
+
 public class TwoSumIvInputIsABST {
 
     class TreeNode{
@@ -26,6 +28,21 @@ public class TwoSumIvInputIsABST {
 
     public boolean findTarget(TreeNode root, int k) {
 
+        HashSet<Integer> set = new HashSet<>();
+        return helper(root, k, set);
+
+    }
+
+    private boolean helper(TreeNode node, int k, HashSet<Integer> set) {
+        if (node == null)
+            return false;
+
+        if (set.contains(k - node.val))
+            return true;
+
+        set.add(node.val);
+
+        return helper(node.left, k, set) || helper(node.right, k, set);
     }
 
 }
