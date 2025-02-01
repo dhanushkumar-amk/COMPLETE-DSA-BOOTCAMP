b7ef9d653fbd5dca699a6ca4a65898d5c7a9ee0b
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-01 12:06:40 +0530

    modified

--- src/TREES/CorrectBinaryTreeThatHasAnyTwoNodesSwapped.java
+++ src/TREES/CorrectBinaryTreeThatHasAnyTwoNodesSwapped.java
@@ -1,2 +1,54 @@
-package TREES;public class CorrectBinaryTreeThatHasAnyTwoNodesSwapped {
+package TREES;
+
+public class CorrectBinaryTreeThatHasAnyTwoNodesSwapped {
+    class Node{
+        Node left;
+        Node right;
+        int val;
+
+        public Node() {
+        }
+
+        public Node(Node left, Node right, int val) {
+            this.left = left;
+            this.right = right;
+            this.val = val;
+        }
+
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+
+    Node first;
+    Node second;
+    Node previous;
+
+    public void helper(Node root){
+        inOderTraversal(root);
+
+        // swap
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+
+        return;
+    }
+
+    private void inOderTraversal(Node root) {
+            if (root == null)
+                return;
+
+            inOderTraversal(root.left);
+
+            if (previous.val > root.val && previous != null) {
+                if (first == null) {
+                    first = previous;
+                }
+                second = root;
+            }
+            previous = root;
+
+            inOderTraversal(root.right);
+    }
 }
