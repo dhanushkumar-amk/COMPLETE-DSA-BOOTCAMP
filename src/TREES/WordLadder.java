1619493dc706e5297e322c1d9cce85416d5dcc8a
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-01 10:09:22 +0530

    modified

--- src/TREES/WordLadder.java
+++ src/TREES/WordLadder.java
@@ -7,7 +7,8 @@ public class WordLadder {
 
     public static void main(String[] args) {
         List<String> wordList = new ArrayList<>();
-        wordList.add()
+        wordList.add("hot");
+        
     }
 
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
