eb5af02f6bb7101f9de871a7d1ee30d06b321ba3
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-01 10:07:09 +0530

    modified

--- src/TREES/WordLadder.java
+++ src/TREES/WordLadder.java
@@ -1,5 +1,16 @@
 package TREES;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WordLadder {
-    
+
+    public static void main(String[] args) {
+        List<String> wordList = new ArrayList<>();
+        wordList.add()
+    }
+
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+
+    }
 }
