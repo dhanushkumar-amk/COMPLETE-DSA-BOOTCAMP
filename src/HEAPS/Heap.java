ebf1527b0a761d8be4e3886ab6d12eebf10a353d
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-01 17:29:17 +0530

    modified

--- src/HEAPS/Heap.java
+++ src/HEAPS/Heap.java
@@ -65,9 +65,30 @@ public class Heap<T extends  Comparable<T>> {
 
         if (!list.isEmpty()){
             list.set(0, last);
+            // top to bottom
+            downHeap(0);
         }
 
         return temp;
+    }
+
+    //downHeap
+    private void downHeap(int index) {
+        int min = index;
+
+        int left = left(index);
+        int right = right(index);
+
+        if (left < list.size() && list.get(min).compareTo(list.get(left)) > 0)
+            min = left;
+
+        if (right < list.size() && list.get(min).compareTo(list.get(right)) > 0)
+            min = right;
+
+        if (min != index){
+            swap(min, index);
+            downHeap(min);
+        }
     }
 
 
@@ -77,10 +98,31 @@ public class Heap<T extends  Comparable<T>> {
         list.set(first, list.get(second));
         list.set(second, temp);
     }
+
+
+    // heapSort
+    public  ArrayList<T> heapSort() throws Exception{
+        ArrayList<T> data = new ArrayList<>();
+
+        while (!list.isEmpty()){
+            data.add(this.remove());
+        }
+        return data;
+    }
 }
 
+class Main{
+    public static void main(String[] args) {
+        Heap<Integer> heap = new Heap<>();
 
+        heap.insert(34);
+        heap.insert(45);
+        heap.insert(22);
+        heap.insert(89);
+        heap.insert(76);
+        heap.insert(54);
+        heap.insert(97);
 
-class main{
-
+        System.out.println(heap.);
+    }
 }
