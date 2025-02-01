83f331d98b5ce3e4347f3e9c36e33069226538b6
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-01 10:55:52 +0530

    modified

--- src/TREES/TwoSumIvInputIsABST.java
+++ src/TREES/TwoSumIvInputIsABST.java
@@ -4,6 +4,26 @@ package TREES;
 // https://leetcode.com/problems/two-sum-iv-input-is-a-bst/description/
 
 public class TwoSumIvInputIsABST {
+
+    class TreeNode{
+       TreeNode left;
+       TreeNode right;
+       int val;
+
+        public TreeNode() {
+        }
+
+        public TreeNode(TreeNode left, TreeNode right, int val) {
+            this.left = left;
+            this.right = right;
+            this.val = val;
+        }
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
     
 
 }
