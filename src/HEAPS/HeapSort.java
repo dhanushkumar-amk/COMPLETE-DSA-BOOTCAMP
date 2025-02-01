d7188c55b331fdd70037027551f2bff68a47f6d1
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-01 17:43:32 +0530

    modified

---
+++ src/HEAPS/HeapSort.java
@@ -0,0 +1,48 @@
+package HEAPS;
+
+import java.util.Arrays;
+
+public class HeapSort {
+
+    private void  buildHeap(int [] arr,int n)
+    {
+        for(int i=n/2-1;i>=0;--i)
+            heapify(arr,n,i);
+        for(int i=n-1;i>0;--i)
+        {
+            int temp=arr[0];
+            arr[0]=arr[i];
+            arr[i]=temp;
+            heapify(arr,i,0);
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private void heapify(int [] arr,int n,int i)
+    {
+        while(true)
+        {
+            int largest=i;
+            int leftChild=2*i+1;
+            int rightChild=2*i+2;
+
+            if(leftChild<n && arr[leftChild]>arr[largest])
+                largest=leftChild;
+            if(rightChild<n && arr[rightChild]>arr[largest])
+                largest=rightChild;
+            if(largest==i)
+                return;
+            int temp=arr[i];
+            arr[i]=arr[largest];
+            arr[largest]=temp;
+
+            i=largest;
+        }
+    }
+    public static void main(String[] args) {
+        int [] arr ={4,1,3,5,6,9};
+        HeapSort heapSort =new HeapSort();
+        heapSort.buildHeap(arr,arr.length);
+    }
+
+}
