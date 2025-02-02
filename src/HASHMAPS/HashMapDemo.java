05f5c94aa629c5008955573cd163e6d7340dd1df
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-02 13:18:46 +0530

    modified

--- src/HASHMAPS/HashMapDemo.java
+++ src/HASHMAPS/HashMapDemo.java
@@ -2,6 +2,7 @@ package HASHMAPS;
 
 import java.awt.*;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class HashMapDemo {
     public static void main(String[] args) {
@@ -26,6 +27,15 @@ public class HashMapDemo {
         // check if the key is present or not
         System.out.println(map.containsKey("dhanush"));
 
+        HashSet<Integer> set = new HashSet<>();
+        set.add(56);
+        set.add(56); // not included duplicates
+        set.add(54);
+        set.add(53);
+        set.add(52);
+        set.add(51);
+
+        System.out.println(set);
     }
 }
 
