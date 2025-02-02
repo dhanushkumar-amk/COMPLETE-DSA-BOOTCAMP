70add40eb7a2bdbef2148220e46f4f9063a3565a
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-02 08:20:43 +0530

    modified

--- src/GREEDY/HuffmanCoder.java
+++ src/GREEDY/HuffmanCoder.java
@@ -1,2 +1,40 @@
-package GREEDY;public class HuffmsnCoder {
+package GREEDY;
+
+import java.util.*;
+public class HuffmanCoder {
+
+    HashMap<Character, String> encoder;
+    HashMap<String, Character> decoder;
+
+    private class Node implements Comparable<Node>{
+
+        Character data;
+        int cost;
+        Node left;
+        Node right;
+
+        public Node(int cost, Character data) {
+            this.cost = cost;
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+
+        // java is confuse so we tell what to compare
+        @Override
+        public int compareTo(Node other) {
+            return this.cost - other.cost;
+        }
+        // negative means smaller , 0 means equal, positive means larger
+    }
+
+    public HuffmanCoder(String feeder) throws Exception {
+
+        HashMap<Character, Integer> frequency = new HashMap<>();
+
+        for (int i = 0; i <feeder.length() ; i++) {
+            char character = feeder.charAt(i);
+
+        }
+    }
 }
