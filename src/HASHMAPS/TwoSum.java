5176148bad9deb1900dc39b0f85333a0a82c3324
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-02 19:58:50 +0530

    modified

--- src/HASHMAPS/TwoSum.java
+++ src/HASHMAPS/TwoSum.java
@@ -3,14 +3,18 @@ package HASHMAPS;
 //leetcode 1
 // https://leetcode.com/problems/two-sum/
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class TwoSum {
     public static void main(String[] args) {
-        int[] arr = {}
+        int[] arr = {2,7,11,15};
+        int target = 9;
+        int[] ans = twoSum(arr, target);
+        System.out.println(Arrays.toString(ans));
     }
 
-    public int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
 
         HashMap<Integer, Integer> map = new HashMap<>();
         int[] result = new int[2];
