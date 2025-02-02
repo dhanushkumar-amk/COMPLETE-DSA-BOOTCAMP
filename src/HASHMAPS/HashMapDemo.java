f1665738bd6191d116404d5baa2286bc7eefdbc0
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-02 13:12:10 +0530

    modified

--- src/HASHMAPS/HashMapDemo.java
+++ src/HASHMAPS/HashMapDemo.java
@@ -1,13 +1,25 @@
 package HASHMAPS;
 
 import java.awt.*;
+import java.util.HashMap;
 
 public class HashMapDemo {
     public static void main(String[] args) {
-        String name = "dhanush";
+       /* String name = "dhanush";
         int code = name.hashCode();
         System.out.println(code);
         System.out.println(code);
+
+        */
+        HashMap<String, Integer> map = new HashMap<>();
+
+        map.put("dhanush", 100);
+        map.put("ram", 56);
+        map.put("kunal", 87);
+        map.put("raj", 20);
+
+        System.out.println(map.get("raj"));
+
     }
 }
 
