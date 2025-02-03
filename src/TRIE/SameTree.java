590d5197768cf04b5d8b982c49fba7f757de7a5c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-03 21:12:19 +0530

    modified

--- src/TRIE/SameTree.java
+++ src/TRIE/SameTree.java
@@ -1,5 +1,8 @@
 package TRIE;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class SameTree {
     public class TreeNode {
       int val;
@@ -14,5 +17,30 @@ public class SameTree {
       }
   }
 
+  private TreeNode root1;
+  private TreeNode root2;
 
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root1);
+        queue.offer(root2);
+
+        while (!queue.isEmpty()){
+            TreeNode first = queue.poll();
+            TreeNode second = queue.poll();
+
+            if (first == null && second == null)
+                continue;
+            else if (first == null || second == null || first.val != second.val)
+                return false;
+
+            queue.add(root1.left);
+            queue.add(root2.left);
+            queue.add(root1.right);
+            queue.add(root2.right);
+        }
+        return true;
+    }
 }
