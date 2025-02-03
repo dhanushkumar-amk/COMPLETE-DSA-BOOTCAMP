5486a65d07b0f5f920ac53cbcc79d1bc3bbc3f42
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-03 20:20:41 +0530

    modified

--- src/TRIE/Trie2.java
+++ src/TRIE/Trie2.java
@@ -1,2 +1,15 @@
-package TRIE;public class Trie2 {
+package TRIE;
+
+public class Trie2 {
+     class Node{
+
+         Node[] links = new Node[26];
+         int countEndWith = 0;
+         int countPrefix = 0;
+
+         public Node() {
+         }
+
+         
+     }
 }
