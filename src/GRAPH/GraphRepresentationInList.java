339f6cb2405f28bc3dfc562f4c29b1184723de67
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-06 08:52:23 +0530

    modified

--- src/GRAPH/GraphRepresentationInList.java
+++ src/GRAPH/GraphRepresentationInList.java
@@ -1,2 +1,42 @@
-package GRAPH;public class GraphRepresentationInList {
+package GRAPH;
+
+import java.util.ArrayList;
+
+public class GraphRepresentationInList {
+    public static void main(String[] args) {
+        int n = 5;
+        int m = 6;
+
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+
+        for (int i = 0; i <=n; i++) {
+            list.add(new ArrayList<>());
+        }
+
+            list.get(1).add(2);
+            list.get(2).add(1);
+
+            list.get(1).add(3);
+            list.get(3).add(1);
+
+            list.get(3).add(4);
+            list.get(4).add(3);
+
+            list.get(2).add(4);
+            list.get(4).add(2);
+
+            list.get(2).add(5);
+            list.get(5).add(2);
+
+            list.get(4).add(5);
+            list.get(5).add(4);
+
+
+        for (int i = 0; i <=n ; i++) {
+            for (int j = 0; j < list.get(i).size(); j++) {
+                System.out.println(list.get(i).get(j) + " ");
+            }
+            System.out.println();
+        }
+    }
 }
