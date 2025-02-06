a491fbcdaf16e9272351152033e46a5a58d4c0a4
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-07 04:51:34 +0530

    modified

--- src/GRAPH/NumberOfProvinces.java
+++ src/GRAPH/NumberOfProvinces.java
@@ -57,4 +57,35 @@ public class NumberOfProvinces {
         NumberOfProvinces ob = new NumberOfProvinces();
         System.out.println(ob.numProvinces(adj, 3));
     }
+
+
+
+    /*
+       public void dfs(int[][] isConnected,int u,boolean[] flag){
+        if(flag[u] == true){
+            return;
+        }
+
+        flag[u] = true;
+
+        for(int i = 0; i < isConnected.length; i++){
+            if(isConnected[u][i] == 1){
+                dfs(isConnected,i,flag);
+            }
+        }
+    }
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        boolean[] flag = new boolean[n];
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            if(flag[i] == false){
+                dfs(isConnected,i,flag);
+                ans += 1;
+            }
+        }
+
+        return ans;
+    }
+     */
 }
