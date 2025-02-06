a8fb6599b65ae2084172049936292d576240d304
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-07 04:19:19 +0530

    modified

--- src/GRAPH/DFSImplementation.java
+++ src/GRAPH/DFSImplementation.java
@@ -1,6 +1,11 @@
 package GRAPH;
 
+import java.util.ArrayList;
+
 public class DFSImplementation {
 
-    public static void dfs()
+    public static void dfs(int node, boolean[] vis, ArrayList<ArrayList<Integer>> adj,
+                           ArrayList<Integer> list){
+        
+    }
 }
