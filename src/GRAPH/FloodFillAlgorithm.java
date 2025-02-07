80d41b034c83bc7547f2349a12beecafa40069a1
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-07 19:04:17 +0530

    modified

--- src/GRAPH/FloodFillAlgorithm.java
+++ src/GRAPH/FloodFillAlgorithm.java
@@ -31,4 +31,24 @@ public class FloodFillAlgorithm {
                 dfs(startRow, startCol, answerMatrix, image, newColor, deltaRow, deltaCol, initialColor);
         }
     }
+
+
+    public static void main(String[] args)
+    {
+        int[][] image =  {
+                {1,1,1},
+                {1,1,0},
+                {1,0,1}
+        };
+
+        // sr = 1, sc = 1, newColor = 2
+        FloodFillAlgorithm obj = new FloodFillAlgorithm();
+        int[][] ans = obj.floodFill(image, 1, 1, 2);
+        for(int i = 0; i < ans.length; i++){
+            for(int j = 0; j < ans[i].length; j++)
+                System.out.print(ans[i][j] + " ");
+            System.out.println();
+        }
+    }
+
 }
