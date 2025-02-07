7ad8ee3936fe241d915dde7fb4f9e1ba9ce74956
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-07 19:07:36 +0530

    modified

--- src/GRAPH/FloodFillAlgorithm.java
+++ src/GRAPH/FloodFillAlgorithm.java
@@ -2,37 +2,34 @@ package GRAPH;
 
 public class FloodFillAlgorithm {
 
-    public int[][]  floodFill(int[][] image, int startRow, int startCol, int newColor){
-        int initialColor = image[startRow][startCol];
-
-        int[][] answerMatrix = image;
-
-        int[] deltaRow = {-1, 0, 1, 0};
-        int[] deltaCol = {0, -1, 0, 1};
-
-
-        dfs(startRow, startCol, answerMatrix, image, newColor, deltaRow, deltaCol, initialColor);
-        return answerMatrix;
-    }
-
-    private void dfs(int startRow, int startCol, int[][] answerMatrix, int[][] image, int newColor, int[] deltaRow, int[] deltaCol, int initialColor) {
-
-        answerMatrix[startRow][startCol] = newColor;
-
+    private void dfs(int row, int col, int[][] ans, int[][] image, int newColor, int delRow[], int delCol[], int iniColor) {
+        // color with new color
+        ans[row][col] = newColor;
         int n = image.length;
         int m = image[0].length;
-
-        // because for 4 => direction
-        for (int i = 0; i < 4; i++) {
-            int neighbourRow = startRow + deltaRow[i];
-            int neighbourCol = startCol + deltaCol[i];
-
-            if (neighbourRow >= 0 && neighbourRow < n && neighbourCol >= 0 && neighbourCol < m && image[neighbourRow][neighbourCol] == initialColor && answerMatrix[neighbourRow][neighbourCol] != newColor)
-                dfs(startRow, startCol, answerMatrix, image, newColor, deltaRow, deltaCol, initialColor);
+        // there are exactly 4 neighbours
+        for(int i = 0;i<4;i++) {
+            int neighbourRow = row + delRow[i];
+            int neighbourCol = col + delCol[i];
+            // check for valid coordinate 
+            // then check for same initial color and unvisited pixel
+            if(neighbourRow>=0 && neighbourRow<n && neighbourCol>=0 && neighbourCol < m &&
+                    image[neighbourRow][neighbourCol] == iniColor && ans[neighbourRow][neighbourCol] != newColor) {
+                dfs(neighbourRow, neighbourCol, ans, image, newColor, delRow, delCol, iniColor);
+            }
         }
     }
-
-
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor)
+    {
+        // get initial color
+        int iniColor = image[sr][sc];
+        int[][] ans = image;
+        // delta row and delta column for neighbours
+        int[] delRow = {-1, 0, +1, 0};
+        int[] delCol = {0, +1, 0, -1};
+        dfs(sr, sc, ans, image, newColor, delRow, delCol, iniColor);
+        return ans;
+    }
     public static void main(String[] args)
     {
         int[][] image =  {
@@ -41,7 +38,7 @@ public class FloodFillAlgorithm {
                 {1,0,1}
         };
 
-        // sr = 1, sc = 1, newColor = 2
+        // sr = 1, sc = 1, newColor = 2       
         FloodFillAlgorithm obj = new FloodFillAlgorithm();
         int[][] ans = obj.floodFill(image, 1, 1, 2);
         for(int i = 0; i < ans.length; i++){
