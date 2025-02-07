62e8e2316cd435939718420f02364bc9ef745026
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-07 20:12:46 +0530

    modified

--- src/GRAPH/RottingOranges.java
+++ src/GRAPH/RottingOranges.java
@@ -6,4 +6,21 @@ package GRAPH;
 
 public class RottingOranges {
 
+
+    class Pair{
+        int row;
+        int colum;
+        int time;
+
+        public Pair(int colum, int row, int time) {
+            this.colum = colum;
+            this.row = row;
+            this.time = time;
+        }
+    }
+
+    
+
+
 }
+
