b4c814e11cc96cf94396be1711834319d90be5d9
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-08 05:15:45 +0530

    modified

--- src/GRAPH/DetectCycleInUndirectedGraphUsingDFS.java
+++ src/GRAPH/DetectCycleInUndirectedGraphUsingDFS.java
@@ -20,4 +20,34 @@ public class DetectCycleInUndirectedGraphUsingDFS {
         return false;
     }
 
+
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        int vis[] = new int[V];
+        for(int i = 0;i<V;i++) {
+            if(vis[i] == 0) {
+                if(dfs(i, -1, vis, adj))
+                    return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args)
+    {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            adj.add(new ArrayList < > ());
+        }
+        adj.get(1).add(2);
+        adj.get(2).add(1);
+        adj.get(2).add(3);
+        adj.get(3).add(2);
+
+        DetectCycleInUndirectedGraphUsingDFS obj = new DetectCycleInUndirectedGraphUsingDFS();
+        boolean ans = obj.isCycle(4, adj);
+        if (ans)
+            System.out.println("1");
+        else
+            System.out.println("0");
+    }
+
 }
