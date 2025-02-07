5051842f19be1e79fe9b28012985d3ac6bbc544b
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-08 05:11:19 +0530

    modified

--- src/GRAPH/DetectCycleInUndirectedGraphUsingDFS.java
+++ src/GRAPH/DetectCycleInUndirectedGraphUsingDFS.java
@@ -1,2 +1,19 @@
-package GRAPH;public class DetectcycleInUndirectedGraphUsingDFS {
+package GRAPH;
+
+import java.util.ArrayList;
+
+public class DetectCycleInUndirectedGraphUsingDFS {
+
+
+    private  boolean dfs(int node, int parent, int[] visited, ArrayList<ArrayList<Integer>> list){
+        visited[node] = 1;
+
+        for(int adjacentNode : list.get(node)){
+
+            if (visited[adjacentNode]) {
+            }
+            visited[adjacentNode]++;
+        }
+    }
+
 }
