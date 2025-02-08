9bce5a0b1d3361afdfaf02d9457603e2c99b7da8
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-08 20:57:27 +0530

    modified

--- src/GRAPH/DetectCycleInDirectedGraph.java
+++ src/GRAPH/DetectCycleInDirectedGraph.java
@@ -31,7 +31,7 @@ public class DetectCycleInDirectedGraph {
             if (visited[it] == 0){
                 if (dfs(it, list, visited, pathVisited))
                     return true;
-            } 
+            }
             // if the node has been previous visited but it has been visited on the same path
             else if (pathVisited[it] == 1)
                 return true;
@@ -41,4 +41,31 @@ public class DetectCycleInDirectedGraph {
         pathVisited[node] = 0;
         return false;
     }
+
+
+    public static void main(String[] args) {
+            int V = 11;
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) {
+                adj.add(new ArrayList<>());
+            }
+            adj.get(1).add(2);
+            adj.get(2).add(3);
+            adj.get(3).add(4);
+            adj.get(3).add(7);
+            adj.get(4).add(5);
+            adj.get(5).add(6);
+            adj.get(7).add(5);
+            adj.get(8).add(9);
+            adj.get(9).add(10);
+            adj.get(10).add(8);
+
+            DetectCycleInDirectedGraph obj = new DetectCycleInDirectedGraph();
+            boolean ans = obj.isCyclic(V, adj);
+            if (ans)
+                System.out.println("True");
+            else
+                System.out.println("False");
+
+        }
 }
