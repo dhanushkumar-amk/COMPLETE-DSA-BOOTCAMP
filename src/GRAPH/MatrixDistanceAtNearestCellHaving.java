2b107661d6fa3d0205c617da884b79cf2bf8a5c0
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-08 05:35:00 +0530

    modified

--- src/GRAPH/MatrixDistanceAtNearestCellHaving.java
+++ src/GRAPH/MatrixDistanceAtNearestCellHaving.java
@@ -19,12 +19,31 @@ public class MatrixDistanceAtNearestCellHaving {
         }
     }
 
-    
+
 
     public int[][] updateMatrix(int[][] mat) {
 
     }
 
 
+    public static void main(String[] args)
+    {
+        int[][] grid = {
+                {0,1,1,0},
+                {1,1,0,0},
+                {0,0,1,1}
+        };
+
+        MatrixDistanceAtNearestCellHaving obj = new MatrixDistanceAtNearestCellHaving();
+        int[][] ans = obj.nearest(grid);
+        for(int i = 0; i < ans.length; i++){
+            for(int j = 0; j < ans[i].length; j++){
+                System.out.print(ans[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+ 
 
 }
