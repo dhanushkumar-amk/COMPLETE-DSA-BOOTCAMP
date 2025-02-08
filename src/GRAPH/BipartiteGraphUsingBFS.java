ba1f4b57b6b042e7f0412c2052e2ca4b369c7ff4
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-08 15:35:17 +0530

    modified

--- src/GRAPH/BipartiteGraphUsingBFS.java
+++ src/GRAPH/BipartiteGraphUsingBFS.java
@@ -1,10 +1,51 @@
 package GRAPH;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class BipartiteGraphUsingBFS {
 
-    boolean isBipartite(int v, )
+    boolean isBipartite(int v,  ArrayList <ArrayList< Integer >> list){
+
+        int[] color = new int[v];
+
+        Arrays.fill(color, -1);
+
+        for (int i = 0; i < v; i++) {
+            if (color[i] == -1){
+                if (!check(i, v, list, color))
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean check(int start, int v, ArrayList<ArrayList<Integer>> list, int[] color) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+
+        // initial color is 0 thn alternatively changed
+        color[start] = 0;
+
+        while(!queue.isEmpty()){
+            int node = queue.peek();
+            queue.poll();
+
+            for(int i : list.get(node)){
+                if (color[i] == -1) {
+                    color[i] = 1 - color[node];
+                    queue.add(i);
+                    // both are same color adjacent element
+                } else if (color[i] == color[node]) {
+                    return false;
+
+                }
+            }
+        }
+        return true;
+    }
 
 
     public static void main(String[] args)
