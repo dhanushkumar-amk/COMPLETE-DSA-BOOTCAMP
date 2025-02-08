abb51bbf7f32598082c5343143358e03c3f2f9e8
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-08 05:36:48 +0530

    modified

--- src/GRAPH/MatrixDistanceAtNearestCellHaving.java
+++ src/GRAPH/MatrixDistanceAtNearestCellHaving.java
@@ -27,7 +27,13 @@ public class MatrixDistanceAtNearestCellHaving {
         int m = mat[0].length;
 
         int[][] visited = new int[n][m];
-        int[][] 
+        int[][] distance = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                
+            }
+        }
     }
 
 
