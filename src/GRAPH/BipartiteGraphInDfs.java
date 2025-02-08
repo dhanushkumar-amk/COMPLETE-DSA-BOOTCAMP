af6f3973bb4aee895e970ff2f6ce6cdf56d62e7d
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-08 16:18:56 +0530

    modified

--- src/GRAPH/BipartiteGraphInDfs.java
+++ src/GRAPH/BipartiteGraphInDfs.java
@@ -5,9 +5,35 @@ import java.util.*;
 
 public class BipartiteGraphInDfs {
 
-    public boolean isBipartite(int V, ArrayList<ArrayList<Integer>>adj){
+    public boolean isBipartite(int v, ArrayList<ArrayList<Integer>>adj){
+
+        int[] color = new int[v];
+        
 
     }
 
-    
+
+    public static void main(String[] args)
+    {
+        // V = 4, E = 4
+        ArrayList < ArrayList < Integer >> adj = new ArrayList < > ();
+        for (int i = 0; i < 4; i++) {
+            adj.add(new ArrayList < > ());
+        }
+        adj.get(0).add(2);
+        adj.get(2).add(0);
+        adj.get(0).add(3);
+        adj.get(3).add(0);
+        adj.get(1).add(3);
+        adj.get(3).add(1);
+        adj.get(2).add(3);
+        adj.get(3).add(2);
+
+        BipartiteGraphInDfs obj = new BipartiteGraphInDfs();
+        boolean ans = obj.isBipartite(4, adj);
+        if(ans)
+            System.out.println("1");
+        else System.out.println("0");
+    }
+
 }
