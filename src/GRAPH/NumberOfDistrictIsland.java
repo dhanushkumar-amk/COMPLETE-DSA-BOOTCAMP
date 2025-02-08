9514539b84d7f23b8c7fa70c3d782b2953cae5a7
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-08 12:20:27 +0530

    modified

--- src/GRAPH/NumberOfDistrictIsland.java
+++ src/GRAPH/NumberOfDistrictIsland.java
@@ -1,2 +1,18 @@
-package GRAPH;public class NumberOfDistrictIsland {
+package GRAPH;
+
+public class NumberOfDistrictIsland {
+
+
+    class Pair{
+        int first;
+        int second;
+
+        public Pair(int first, int second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+
+    public int countDistinctIslands()
+
 }
