c5f73e40fc365545dc96a5fede001e6c6d9bedf8
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-08 20:44:29 +0530

    modified

--- src/GRAPH/DetectCycleInDirectedGraph.java
+++ src/GRAPH/DetectCycleInDirectedGraph.java
@@ -1,5 +1,23 @@
 package GRAPH;
 
+import java.util.ArrayList;
+
 public class DetectCycleInDirectedGraph {
-    
+
+
+    public boolean isCyclic(int v, ArrayList<ArrayList<Integer>> list){
+
+        int[] visited = new int[v];
+        int[] pathVisited = new int[v];
+
+        for (int i = 0; i < v; i++) {
+            if (visited[i] == 0) {
+                // it is cycl then it ist true
+                if (dfs(i, list, visited, pathVisited))
+                    return true;
+            }
+
+
+        }
+    }
 }
