cf36a5691b27aee271e09f51ed60a26a8946619d
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-09 12:21:42 +0530

    modified

--- src/GRAPH/TopologicalSort.java
+++ src/GRAPH/TopologicalSort.java
@@ -4,5 +4,10 @@ import java.util.ArrayList;
 
 public class TopologicalSort {
 
-    public int[] topologicalSort(int v, ArrayList<ArrayList<Integer>> list)
+    public int[] topologicalSort(int v, ArrayList<ArrayList<Integer>> list){
+
+        int[] visited = new int[v];
+
+        S
+    }
 }
