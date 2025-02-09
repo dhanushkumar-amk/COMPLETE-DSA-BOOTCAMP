34f7d774286c920756f1b75a4517d1db4727ecb8
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-10 04:52:33 +0530

    modified

--- src/GRAPH/AlienDictionary.java
+++ src/GRAPH/AlienDictionary.java
@@ -1,5 +1,73 @@
 package GRAPH;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class AlienDictionary {
-    
+
+
+    static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) {
+        int indegree[] = new int[V];
+        for (int i = 0; i < V; i++) {
+            for (int it : adj.get(i)) {
+                indegree[it]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<Integer>();
+
+        for (int i = 0; i < V; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        int topo[] = new int[V];
+        int i = 0;
+        while (!q.isEmpty()) {
+            int node = q.peek();
+            q.remove();
+            topo[i++] = node;
+            // node is in your topo sort
+            // so please remove it from the indegree
+
+            for (int it : adj.get(node)) {
+                indegree[it]--;
+                if (indegree[it] == 0) {
+                    q.add(it);
+                }
+            }
+        }
+
+        return topo;
+    }
+
+    public String findDictionary(String[] dictionary, int n, int k){
+
+        List<List<Integer>> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            String string1 = dictionary[i];
+            String string2 = dictionary[i + 1];
+
+            int length = Math.min(string1.length(), string2.length());
+
+            for (int pointer = 0; pointer < length; pointer++) {
+                if (string1.charAt(pointer) != string2.charAt(pointer)){
+                    list.get(string1.charAt(pointer) - 'a').add(string2.charAt(pointer) - 'a');
+                    break;
+                }
+            }
+        }
+
+        List<Integer> answer = topoSort(k, list);
+        String answer
+
+    }
 }
