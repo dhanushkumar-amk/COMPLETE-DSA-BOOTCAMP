ea5f4c984c6f12d81d11796b0cf355e516e39bdc
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-09 20:27:31 +0530

    modified

--- src/GRAPH/CourseSchedule1.java
+++ src/GRAPH/CourseSchedule1.java
@@ -1,10 +1,17 @@
 package GRAPH;
 
+import java.util.ArrayList;
+
 // https://leetcode.com/problems/course-schedule/description/
 //leetcode  207
 public class CourseSchedule1 {
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            list.get(new ArrayList<>());
+        }
 
     }
 }
