206ec7f254844a091c58fd90bf461ee7d8f9dcc1
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-09 12:27:22 +0530

    modified

--- src/GRAPH/TopologicalSort.java
+++ src/GRAPH/TopologicalSort.java
@@ -1,6 +1,7 @@
 package GRAPH;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class TopologicalSort {
 
@@ -8,6 +9,35 @@ public class TopologicalSort {
 
         int[] visited = new int[v];
 
-        S
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < v; i++) {
+            if (visited[i] == 0){
+                dfs(i, list, visited, stack);
+            }
+        }
+
+        int[] answer = new int[v];
+        int i= 0;
+
+        while (!stack.isEmpty()){
+            answer[i] = stack.peek();
+            stack.peek();
+            i++;
+        }
+        return answer;
+    }
+
+    private void dfs(int node, ArrayList<ArrayList<Integer>> list, int[] visited, Stack<Integer> stack) {
+
+        visited[node] = 1;
+
+        for(int it : list.get(node)){
+            if (visited[it] == 0){
+                dfs(it, list, visited, stack);
+            }
+        }
+        // before returning store the element  on the stack
+        stack.push(node);
     }
 }
