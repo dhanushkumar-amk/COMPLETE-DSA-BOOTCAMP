61de50f215f4be37b24c9cdfd6a4125cdd438cf4
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-09 20:08:05 +0530

    modified

--- src/GRAPH/DetectCycleInDirectedGraphTopoSort.java
+++ src/GRAPH/DetectCycleInDirectedGraphTopoSort.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class DetectCycleInDirectedGraphTopoSort {
-    public  boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj){
+    public static   boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj){
         int indegree[] = new int[V];
         for (int i = 0; i < V; i++) {
             for (int it : adj.get(i)) {
@@ -42,5 +42,23 @@ public class DetectCycleInDirectedGraphTopoSort {
             return false;
 
         return true;
+    }
+
+
+    public static void main(String[] args) {
+        int V = 6;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+        adj.get(2).add(3);
+        adj.get(3).add(1);
+        adj.get(4).add(0);
+        adj.get(4).add(1);
+        adj.get(5).add(0);
+        adj.get(5).add(2);
+
+       boolean ans = DetectCycleInDirectedGraphTopoSort.isCyclic(V, adj);
+        System.out.println(ans);
     }
 }
