5f338082b6725628ec74c6eb52a1036786924245
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-10 04:53:00 +0530

    modified

--- src/GRAPH/AlienDictionary.java
+++ src/GRAPH/AlienDictionary.java
@@ -8,41 +8,7 @@ import java.util.Queue;
 public class AlienDictionary {
 
 
-    static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) {
-        int indegree[] = new int[V];
-        for (int i = 0; i < V; i++) {
-            for (int it : adj.get(i)) {
-                indegree[it]++;
-            }
-        }
-
-        Queue<Integer> q = new LinkedList<Integer>();
-
-        for (int i = 0; i < V; i++) {
-            if (indegree[i] == 0) {
-                q.add(i);
-            }
-        }
-
-        int topo[] = new int[V];
-        int i = 0;
-        while (!q.isEmpty()) {
-            int node = q.peek();
-            q.remove();
-            topo[i++] = node;
-            // node is in your topo sort
-            // so please remove it from the indegree
-
-            for (int it : adj.get(node)) {
-                indegree[it]--;
-                if (indegree[it] == 0) {
-                    q.add(it);
-                }
-            }
-        }
-
-        return topo;
-    }
+    
 
     public String findDictionary(String[] dictionary, int n, int k){
 
