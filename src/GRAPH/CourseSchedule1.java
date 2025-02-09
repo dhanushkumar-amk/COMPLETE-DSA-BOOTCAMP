6a359291767aed0ebd9a1ba718a273009f7f0402
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-09 20:36:08 +0530

    modified

--- src/GRAPH/CourseSchedule1.java
+++ src/GRAPH/CourseSchedule1.java
@@ -1,6 +1,8 @@
 package GRAPH;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 // https://leetcode.com/problems/course-schedule/description/
 //leetcode  207
@@ -18,5 +20,38 @@ public class CourseSchedule1 {
             list.get(prerequisites[i][0]).add(prerequisites[i][1]);
         }
 
+
+        int[] inDegree = new int[n];
+        for (int i = 0; i < n; i++) {
+            for(int it : list.get(i)){
+                inDegree[i]++;
+            }
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (inDegree[i] == 0)
+                queue.add(i);
+        }
+
+        int i = 0;
+        int[] answer = new int[numCourses];
+        while (!queue.isEmpty()){
+            int node = queue.poll();
+            answer[i] = node;
+            i++;
+
+            for(int it : list.get(node)){
+                inDegree[it]--;
+
+                if (inDegree[it] == 0)
+                    queue.add(it);
+            }
+        }
+        
+        if (answer.length == numCourses)
+            return true;
+
+        return false;
     }
 }
