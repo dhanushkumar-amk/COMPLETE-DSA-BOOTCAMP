7d5a133c781f231b675d75693e50872d3e932aa0
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-09 20:42:58 +0530

    modified

--- src/GRAPH/CourseScheduleII.java
+++ src/GRAPH/CourseScheduleII.java
@@ -4,9 +4,22 @@ package GRAPH;
 // https://leetcode.com/problems/course-schedule-ii/description/
 // leetcode 210
 
+import java.util.ArrayList;
+
 public class CourseScheduleII {
 
     public int[] findOrder(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        for (int i = 0; i <numCourses; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        int n = prerequisites.length;
+        for (int i = 0; i <n ; i++) {
+            list.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+
+
 
     }
 }
