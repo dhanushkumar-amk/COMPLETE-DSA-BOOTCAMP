cf5af35c924601897928f7c7b6686de209520012
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-09 11:14:57 +0530

    modified

--- src/GRAPH/EventualSafeStates.java
+++ src/GRAPH/EventualSafeStates.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class EventualSafeStates {
 
-    public List<Integer> eventualSafeNodes(ArrayList<ArrayList<Integer>> list) {
+    public List<Integer> eventualSafeNodes(List<List<Integer>> list) {
 
         int v = list.size();
 
@@ -25,7 +25,7 @@ public class EventualSafeStates {
         return safeNodes;
     }
 
-    private boolean dfs(int node, ArrayList<ArrayList<Integer>> list, int[] visited, int[] pathVisited, int[] check) {
+    private boolean dfs(int node, List<List<Integer>> list, int[] visited, int[] pathVisited, int[] check) {
 
         visited[node] = 1;
 
@@ -48,5 +48,36 @@ public class EventualSafeStates {
         pathVisited[node] = 0;
         check[node] = 1;
         return false;
+    }
+
+
+    public static void main(String[] args) {
+        int V = 12;
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+        adj.get(0).add(1);
+        adj.get(1).add(2);
+        adj.get(2).add(3);
+        adj.get(2).add(4);
+        adj.get(3).add(4);
+        adj.get(3).add(5);
+        adj.get(4).add(6);
+        adj.get(5).add(6);
+        adj.get(6).add(7);
+        adj.get(8).add(1);
+        adj.get(8).add(9);
+        adj.get(9).add(10);
+        adj.get(10).add(8);
+        adj.get(11).add(9);
+
+        EventualSafeStates obj = new EventualSafeStates();
+        List<Integer> safeNodes = obj.eventualSafeNodes(adj);
+
+        for (int node : safeNodes) {
+            System.out.print(node + " ");
+        }
+        System.out.println("");
     }
 }
