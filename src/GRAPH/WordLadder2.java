52bc910557ba20af547fd4a97169a0d2ea007fc9
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-11 04:34:31 +0530

    modified

--- src/GRAPH/WordLadder2.java
+++ src/GRAPH/WordLadder2.java
@@ -3,6 +3,48 @@ package GRAPH;
 // https://leetcode.com/problems/word-ladder-ii/
 // letocde 126
 
+import java.util.*;
+
 public class WordLadder2 {
-        
+        public ArrayList<ArrayList<String>> findSequence(String startWord, String targetWord, String[] wordList){
+
+            // create the set and ad elements in the set
+            Set<String> set = new HashSet<>();
+
+            int len = wordList.length;
+            for (int i = 0; i < len; i++)
+                set.add(wordList[i]);
+
+            // create the queue and arraylist add startWord to it
+            Queue<ArrayList<String>> queue = new LinkedList<>();
+            ArrayList<String> list = new ArrayList<>();
+
+            list.add(startWord);
+            queue.add(list);
+
+            // create the array list to add every level for operations
+            ArrayList<String> everyLevelList = new ArrayList<>();
+            everyLevelList.add(startWord);
+
+            int level = 0;
+            ArrayList<ArrayList<String>> answer = new ArrayList<>();
+
+            while (!queue.isEmpty()){
+                ArrayList<String > subAnswer = queue.poll();
+
+                // if level will be increased hen earse the element from list
+                if (subAnswer.size() > level){
+                    level++;
+                    for(String it : everyLevelList)
+                        set.remove(it);
+                }
+
+
+                // get the lost word on the list and make operations
+                String word = subAnswer.get(subAnswer.size() - 1);
+
+            }
+
+            return answer;
+        }
 }
