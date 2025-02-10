2dff0c2aab4255fee47de2030f578193d4dad4dd
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-10 21:36:53 +0530

    modified

--- src/GRAPH/ShortestPathInUndirectedGraphWithUnitDistance.java
+++ src/GRAPH/ShortestPathInUndirectedGraphWithUnitDistance.java
@@ -52,4 +52,17 @@ public class ShortestPathInUndirectedGraphWithUnitDistance {
 
         return distance;
     }
+
+    public static void main(String[] args){
+        int n=9, m=10;
+        int[][] edge = {{0,1},{0,3},{3,4},{4,5},{5,6},{1,2},{2,6},{6,7},{7,8},{6,8}};
+
+        ShortestPathInUndirectedGraphWithUnitDistance obj = new ShortestPathInUndirectedGraphWithUnitDistance();
+        int res[] = obj.shortestPath(edge,n,m,0);
+        for(int i=0;i<n;i++){
+            System.out.print(res[i]+" ");
+        }
+        System.out.println();
+    }
+
 }
