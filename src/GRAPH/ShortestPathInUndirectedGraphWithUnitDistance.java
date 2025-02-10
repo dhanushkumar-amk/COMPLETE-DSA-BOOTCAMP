4e3b76eb0c843cdecd614a48dde1ad08e4c7ac74
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-10 21:25:45 +0530

    modified

--- src/GRAPH/ShortestPathInUndirectedGraphWithUnitDistance.java
+++ src/GRAPH/ShortestPathInUndirectedGraphWithUnitDistance.java
@@ -13,9 +13,18 @@ public class ShortestPathInUndirectedGraphWithUnitDistance {
         }
 
 
+        // get all the elements from the matrix and added to the list
         for (int i = 0; i < m; i++) {
-            adj.get(edges[i][0]).add(edges[i][1]);
-            
+            adj.get(edges[i][0]).add(edges[i][1]); // 2 -> 1
+            adj.get(edges[i][1]).add(edges[i][0]); // 1 -> 2
         }
+
+        // create the distance array and make as a infinity
+        int[] distance = new int[n];
+        for (int i = 0; i < n; i++)
+            distance[i] = (int) 1e9;
+
+
+
     }
 }
