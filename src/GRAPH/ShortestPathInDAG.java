666c5d9e2f218db88e32e4ea49051e729ac5faea
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-10 05:42:59 +0530

    modified

--- src/GRAPH/ShortestPathInDAG.java
+++ src/GRAPH/ShortestPathInDAG.java
@@ -1,5 +1,18 @@
 package GRAPH;
 
 public class ShortestPathInDAG {
+
+    class Pair{
+        int first;
+        int second;
+
+        public Pair(int first, int second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+
+
     
+
 }
