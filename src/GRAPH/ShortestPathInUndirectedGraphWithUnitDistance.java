2741a10a9683e2d992c707d678537a8f1630b5f8
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-10 21:20:47 +0530

    modified

--- src/GRAPH/ShortestPathInUndirectedGraphWithUnitDistance.java
+++ src/GRAPH/ShortestPathInUndirectedGraphWithUnitDistance.java
@@ -1,5 +1,21 @@
 package GRAPH;
 
+import java.util.ArrayList;
+
 public class ShortestPathInUndirectedGraphWithUnitDistance {
-    
+
+    public int[] shortestPath(int[][] edges, int n, int m,int src){
+
+        // convert  matrix into graph
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+
+        for (int i = 0; i < m; i++) {
+            adj.get(edges[i][0]).add(edges[i][1]);
+            
+        }
+    }
 }
