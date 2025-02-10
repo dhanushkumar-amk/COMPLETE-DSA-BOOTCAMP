c029b3abc9f2f8448f6ada549f69cd0a65b68f6f
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-10 06:12:29 +0530

    modified

--- src/GRAPH/ShortestPathInDAG.java
+++ src/GRAPH/ShortestPathInDAG.java
@@ -91,4 +91,15 @@ public class ShortestPathInDAG {
         stack.add(node);
     }
 
+    public static void main(String[] args) throws IOException {
+        int n = 6, m = 7;
+        int[][] edge = {{0,1,2},{0,4,1},{4,5,4},{4,2,2},{1,2,3},{2,3,6},{5,3,1}};
+        ShortestPathInDAG obj = new ShortestPathInDAG();
+        int res[] = obj.shortestPath(n, m, edge);
+        for (int i = 0; i < n; i++) {
+            System.out.print(res[i] + " ");
+        }
+        System.out.println();
+    }
+
 }
