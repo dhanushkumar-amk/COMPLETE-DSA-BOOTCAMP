194f329f874274dc23e26ede6c8cb48f5dc8cc1c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-10 22:20:28 +0530

    modified

--- src/GRAPH/WordLadder1.java
+++ src/GRAPH/WordLadder1.java
@@ -2,7 +2,7 @@ package GRAPH;
 
 public class WordLadder1 {
     public static void main(String[] args) {
-        int two = 2 << 30;
+        int two = 1 << 30;
 
         System.out.println(two);
     }
