e2fa0eb1d4d1da3bb073a5da54974df0a6732a53
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-12 04:52:10 +0530

    modified

--- src/GRAPH/NumberOfWaysToArriveTheDestination.java
+++ src/GRAPH/NumberOfWaysToArriveTheDestination.java
@@ -1,5 +1,30 @@
 package GRAPH;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NumberOfWaysToArriveTheDestination {
-    
+    class Pair {
+        int first;
+        int second;
+        public Pair(int first, int second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+
+    static int countPath(int n, List<List<Integer>> roads){
+
+        // create the graph
+        ArrayList<ArrayList<Pair>> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++)
+            list.add(new ArrayList<>());
+
+        int m = roads.size();
+        for (int i = 0; i < m; i++) {
+            list.get(roads.get(i).get(0)).add(new Pair(roads.get(i).get(1), roads.get(i).get(2)));
+        }
+
+    }
 }
