6bb423299768c0895d8525b021bdf98d562593a7
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-11 09:15:45 +0530

    modified

--- src/GRAPH/ShortestPathInWeightedUndirectedGraph.java
+++ src/GRAPH/ShortestPathInWeightedUndirectedGraph.java
@@ -1,2 +1,34 @@
-package GRAPH;public class ShortestPathInWeightedUndirectGraph {
+package GRAPH;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShortestPathInWeightedUndirectedGraph {
+
+    class Pair{
+        int weight;
+        int node;
+
+        public Pair(int weight, int node) {
+            this.weight = weight;
+            this.node = node;
+        }
+    }
+    public  List<Integer> shortestPath(int n, int m, int[][] matrix){
+
+        // create the graph
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        for (int i = 0; i <= n; i++)
+            list.add(new ArrayList<>());
+
+        // added the edges given in matrix to list of graph
+        for (int i = 0; i < m; i++) {
+           list.get(matrix[i][0]).add(new Pair(matrix[i][1], matrix[i][2]));
+           list.get(matrix[i][1]).add(new Pair(matrix[i][0], matrix[i][2]));
+        }
+
+
+
+
+    }
 }
