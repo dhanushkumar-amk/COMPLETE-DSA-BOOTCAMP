38d12575493977becc53719f7a0c98a44268e158
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-11 09:21:47 +0530

    modified

--- src/GRAPH/ShortestPathInWeightedUndirectedGraph.java
+++ src/GRAPH/ShortestPathInWeightedUndirectedGraph.java
@@ -2,10 +2,11 @@ package GRAPH;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class ShortestPathInWeightedUndirectedGraph {
 
-    class Pair{
+    static class Pair{
         int weight;
         int node;
 
@@ -14,22 +15,32 @@ public class ShortestPathInWeightedUndirectedGraph {
             this.node = node;
         }
     }
-    public  List<Integer> shortestPath(int n, int m, int[][] matrix){
 
-        // create the graph
-        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-        for (int i = 0; i <= n; i++)
-            list.add(new ArrayList<>());
+        public static List<Integer> shortestPath(int n, int m, int[][] matrix) {
 
-        // added the edges given in matrix to list of graph
-        for (int i = 0; i < m; i++) {
-            int node1 = matrix[i][0];
-            int node2 = matrix[i][1];
-            int weight = matrix[i][2];
+            // Create the graph
+            ArrayList<ArrayList<Pair>> list = new ArrayList<>();
+            for (int i = 0; i <= n; i++) {
+                list.add(new ArrayList<>());  // Initialize adjacency list
+            }
 
-            list.get(node1).add(new Pair(weight, node2));
-            list.get(node2).add(new Pair(weight, node1));
-        }
+            // Add the edges given in the matrix to the list (graph representation)
+            for (int i = 0; i < m; i++) {
+                int node1 = matrix[i][0];
+                int node2 = matrix[i][1];
+                int weight = matrix[i][2];
+
+                list.get(node1).add(new Pair(weight, node2)); // Add edge (node1 -> node2)
+                list.get(node2).add(new Pair(weight, node1)); // Add edge (node2 -> node1) if undirected
+            }
+
+
+            // init queue parent[] and distance[];
+            PriorityQueue<Pair>  pq = new PriorityQueue<>((x, y) -> x.weight - y.weight);
+            int[] distance = new int[n - 1];
+            int[] parent = new int[n - 1];
+
+
 
 
 
