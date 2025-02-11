590bac453408e1ee86c822034dcf5c6cde3904d0
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-11 12:08:07 +0530

    modified

--- src/GRAPH/PathWithMinimumEffort.java
+++ src/GRAPH/PathWithMinimumEffort.java
@@ -1,2 +1,21 @@
-package GRAPH;public class PathWithMinimumEffort {
+package GRAPH;
+
+public class PathWithMinimumEffort {
+
+    class Pair{
+        int row;
+        int col;
+        int distance;
+
+        public Pair(int distance, int row, int col) {
+            this.col = col;
+            this.distance = distance;
+            this.row = row;
+        }
+    }
+
+    
+
+
+
 }
