5f112f4074dab015469fe2492a734be5191e01f4
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-11 05:51:36 +0530

    modified

--- src/GRAPH/DijkstraAlgorithm.java
+++ src/GRAPH/DijkstraAlgorithm.java
@@ -17,7 +17,7 @@ public class DijkstraAlgorithm {
 
     public int[] dijkstra(int v, ArrayList<ArrayList<ArrayList<Integer>>> list, int src) {
 
-        PriorityQueue<Pair> pQueue = new PriorityQueue<>((x, y) -> x.distance - y.distance);
+        PriorityQueue<Pair> pQueue = new PriorityQueue<>();
 
         int[] distanceArray = new int[v];
 
