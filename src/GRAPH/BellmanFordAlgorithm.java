ff338b932bf0084cc196e8558ce1d9d3b6324a92
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-12 05:55:27 +0530

    modified

--- src/GRAPH/BellmanFordAlgorithm.java
+++ src/GRAPH/BellmanFordAlgorithm.java
@@ -43,6 +43,28 @@ public class BellmanFordAlgorithm {
         return distanceArray;
     }
 
-    
+    public static void main(String[] args) {
+        int V = 6;
+        int S = 0;
+        ArrayList<ArrayList<Integer>> edges = new ArrayList<>() {
+            {
+                add(new ArrayList<Integer>(Arrays.asList(3, 2, 6)));
+                add(new ArrayList<Integer>(Arrays.asList(5, 3, 1)));
+                add(new ArrayList<Integer>(Arrays.asList(0, 1, 5)));
+                add(new ArrayList<Integer>(Arrays.asList(1, 5, -3)));
+                add(new ArrayList<Integer>(Arrays.asList(1, 2, -2)));
+                add(new ArrayList<Integer>(Arrays.asList(3, 4, -2)));
+                add(new ArrayList<Integer>(Arrays.asList(2, 4, 3)));
+            }
+        };
+
+
+
+        int[] dist = BellmanFordAlgorithm.bellman_ford(V, edges, S);
+        for (int i = 0; i < V; i++) {
+            System.out.print(dist[i] + " ");
+        }
+        System.out.println("");
+    }
 
 }
