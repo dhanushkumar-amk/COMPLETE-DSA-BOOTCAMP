4489547e0bb829c5671907de7e57a8e643438d00
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-12 21:20:00 +0530

    modified

--- src/GRAPH/FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance.java
+++ src/GRAPH/FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance.java
@@ -3,7 +3,27 @@ package GRAPH;
 public class FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance {
 
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-            int 
+            int[][] distance = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                distance[i][j] = (int)(1e9);
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+            distance[i][i] = 0;
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (distance[i][k] == (int)(1e9) || distance[k][j] == (int)(1e9))
+                        continue;
+
+
+                }
+            }
+        }
     }
 
 }
