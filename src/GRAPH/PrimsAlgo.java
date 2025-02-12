d908ca8e1413f95b6645ac68ac6741c0816bf440
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-13 04:32:42 +0530

    modified

--- src/GRAPH/PrimsAlgo.java
+++ src/GRAPH/PrimsAlgo.java
@@ -1,2 +1,22 @@
-package GRAPH;public class PrimsAlgo {
+package GRAPH;
+
+import java.util.ArrayList;
+
+public class PrimsAlgo {
+
+    class Pair{
+        int node;
+        int distance;
+
+        public Pair(int distance, int node) {
+            this.distance = distance;
+            this.node = node;
+        }
+    }
+
+    static int spanningTree(int V, ArrayList<ArrayList<ArrayList<Integer>>> list){
+
+        
+
+    }
 }
