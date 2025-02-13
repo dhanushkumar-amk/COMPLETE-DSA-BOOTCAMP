ca1b73a48e1674c06b587e8cc9c64aef83068262
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-14 04:01:59 +0530

    modified

--- src/GRAPH/AccountMerge.java
+++ src/GRAPH/AccountMerge.java
@@ -57,57 +57,57 @@ public class AccountMerge {
         }
     }
 
-    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        public List<List<String>> accountsMerge(List<List<String>> accounts) {
 
-        int n = accounts.size();
-        AccountMerge ds = new AccountMerge(n);
+            int n = accounts.size();
+            AccountMerge ds = new AccountMerge(n);
 
-        // create hashmap and put tha value on it
-        HashMap<String, Integer> mailMap = new HashMap<>();
+            // create hashmap and put tha value on it
+            HashMap<String, Integer> mailMap = new HashMap<>();
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < accounts.get(i).size(); j++) {
-                String mail = accounts.get(i).get(j);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < accounts.get(i).size(); j++) {
+                    String mail = accounts.get(i).get(j);
 
-                if (!mailMap.containsKey(mail))
-                    mailMap.put(mail, i);
-                else
-                    ds.unionBySize(i, mailMap.get(mail));
+                    if (!mailMap.containsKey(mail))
+                        mailMap.put(mail, i);
+                    else
+                        ds.unionBySize(i, mailMap.get(mail));
+                }
             }
+
+            // no get the value from the map and merge them
+            ArrayList<String>[] mergeMail = new ArrayList[n];
+
+            for (int i = 0; i < n; i++)
+                mergeMail[i] = new ArrayList<>();
+
+            for (Map.Entry<String, Integer> it : mailMap.entrySet()){
+                String mail = it.getKey();
+                int node = ds.findUPar(it.getValue());
+                mergeMail[node].add(mail);
+            }
+
+            List<List<String>> answer = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+
+                // if list doesn't not have answer simply skip
+                if (mergeMail[i].size() == 0)
+                    continue;
+
+                Collections.sort(mergeMail[i]);
+
+                List<String> temp =  new ArrayList<>();
+                temp.add(accounts.get(i).get(0));
+
+                for(String it : mergeMail[i])
+                    temp.add(it);
+
+                answer.add(temp);
+            };
+
+            return answer;
         }
-
-        // no get the value from the map and merge them
-        ArrayList<String>[] mergeMail = new ArrayList[n];
-
-        for (int i = 0; i < n; i++)
-            mergeMail[i] = new ArrayList<>();
-
-        for (Map.Entry<String, Integer> it : mailMap.entrySet()){
-            String mail = it.getKey();
-            int node = ds.findUPar(it.getValue());
-            mergeMail[node].add(mail);
-        }
-
-        List<List<String>> answer = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-
-            // if list doesn't not have answer simply skip
-            if (mergeMail[i].size() == 0)
-                continue;
-
-            Collections.sort(mergeMail[i]);
-
-            List<String> temp =  new ArrayList<>();
-            temp.add(accounts.get(i).get(0));
-
-            for(String it : mergeMail[i])
-                temp.add(it);
-
-            answer.add(temp);
-        };
-
-        return answer;
-    }
 
 
 
