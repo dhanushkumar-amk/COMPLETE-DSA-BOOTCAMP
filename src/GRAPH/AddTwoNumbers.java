a3a79aaa929bd5b88e5d3bd77d8c2fed9a0149e4
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-13 06:03:17 +0530

    modified

--- src/GRAPH/AddTwoNumbers.java
+++ src/GRAPH/AddTwoNumbers.java
@@ -4,5 +4,24 @@ package GRAPH;
 //leetcode 2
 
 public class AddTwoNumbers {
-    
+
+    public class ListNode {
+      int val;
+      ListNode next;
+
+      ListNode() {
+
+      }
+
+      ListNode(int val) {
+          this.val = val;
+      }
+
+      ListNode(int val, ListNode next) {
+          this.val = val;
+          this.next = next;
+      }
+  }
+
+  
 }
