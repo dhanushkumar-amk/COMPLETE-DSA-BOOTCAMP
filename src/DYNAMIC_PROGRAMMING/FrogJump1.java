5e3c01019dcb2977503f4b9da1edd597e89ac536
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-15 21:38:03 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/FrogJump1.java
+++ src/DYNAMIC_PROGRAMMING/FrogJump1.java
@@ -5,12 +5,21 @@ public class FrogJump1 {
 
     }
 
-    static  int jump1(int n, int[] dp){
-
-        // base case
-        if (n == 0)
-            return 0;
-
+    static int solve(int n,int[] height,int[] dp){
         
+        
+        if(n == 0)
+            return 0;
+        
+        if( dp[n] != -1)
+            return dp[n];
+
+        int jumpOneStep = solve(n-1, height,dp)+ Math.abs(height[n]-height[n-1]);
+
+        int jumpTwoSteps = Integer.MAX_VALUE;
+        if(n>1)
+            jumpTwoSteps = solve(n-2, height,dp)+ Math.abs(height[n]-height[n-2]);
+
+        return dp[n]=Math.min(jumpOneStep, jumpTwoSteps);
     }
 }
