6540c9d0c22ec76c7fd60eb2e575847e781ffbc1
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-15 17:49:40 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/Fibonacci.java
+++ src/DYNAMIC_PROGRAMMING/Fibonacci.java
@@ -8,15 +8,9 @@ public class Fibonacci {
         int[] dp = new int[n + 1];
         Arrays.fill(dp, -1);
 
-        dp[0] = 0;
-        dp[1] = 1;
-
-        for (int i = 2; i <= n ; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-        System.out.println(dp[n]);
-
         //System.out.println(fibo(n, dp));
+
+        fibo1(n, dp);
 
     }
 
@@ -34,6 +28,15 @@ public class Fibonacci {
 
     }
 
+    public static void fibo1(int n, int[] dp){
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n ; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        System.out.println(dp[n]);
+    }
 
 
 }
