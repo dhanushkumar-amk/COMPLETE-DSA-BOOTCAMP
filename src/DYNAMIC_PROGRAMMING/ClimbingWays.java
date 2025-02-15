1833d548f10c10e24d433e06a3d676867afc55a0
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-15 20:54:53 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/ClimbingWays.java
+++ src/DYNAMIC_PROGRAMMING/ClimbingWays.java
@@ -6,5 +6,21 @@ package DYNAMIC_PROGRAMMING;
 public class ClimbingWays {
 
 
+    public static void main(String[] args) {
+        System.out.println(climbStairs(3));
+    }
+
+
+    public static int climbStairs(int n) {
+
+        if (n == 0 || n == 1)
+            return 1;
+
+        int left = climbStairs(n - 1);
+        int right = climbStairs(n - 2);
+
+        return left + right;
+    }
+
 }
 
