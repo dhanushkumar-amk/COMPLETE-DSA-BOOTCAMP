430e46849d4fc276829ee48ccc018a55df436f82
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-16 16:30:57 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/NinjasTraining.java
+++ src/DYNAMIC_PROGRAMMING/NinjasTraining.java
@@ -15,6 +15,15 @@ public class NinjasTraining {
             }
             return dp[day][last] = maximum;
         }
+
+        int maximum = 0;
+        // Loop through the three activities on the current day
+        for (int i = 0; i <= 2; i++) {
+            if (i != last) {
+                int activity = points[day][i] + f(day - 1, i, points, dp);
+                maximum = Math.max(maximum, activity);
+            }
+        }
         
 
     }
