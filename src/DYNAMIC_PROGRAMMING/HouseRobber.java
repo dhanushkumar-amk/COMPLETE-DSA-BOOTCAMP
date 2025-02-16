435c7ae20f4138158a0b994f2a1db3a1f0e2c7f2
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-16 13:32:32 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/HouseRobber.java
+++ src/DYNAMIC_PROGRAMMING/HouseRobber.java
@@ -3,6 +3,29 @@ package DYNAMIC_PROGRAMMING;
 public class HouseRobber {
 
 
+    public static void main(String[] args) {
+        
+    }
 
-    
+
+    public static int rob(int[] nums) {
+        int n = nums.length;
+        return helper(n - 1, nums);
+    }
+
+    private static int helper(int index, int[] nums) {
+
+        if (index == 0)
+            return nums[index];
+
+        if (index < 0)
+            return 0;
+
+        int pick = nums[index] + helper(index - 2, nums);
+        int notPick = 0 + helper(index - 1, nums);
+
+        return Math.max(pick, notPick);
+
+    }
+
 }
