ab9489909e9d8ffd0c1e5dcc81a01cda39f22119
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-16 16:28:45 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/NinjasTraining.java
+++ src/DYNAMIC_PROGRAMMING/NinjasTraining.java
@@ -3,6 +3,8 @@ package DYNAMIC_PROGRAMMING;
 public class NinjasTraining {
 
     static int f(int day, int last, int[][] points, int[][] dp) {
-        
+
+        if (dp[day][last] != -1)
+            return dp[day][last];
     }
 }
