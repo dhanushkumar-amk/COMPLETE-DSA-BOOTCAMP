ebfa0817de8ebae69ba237547b7a96d1e47b5a67
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-16 14:44:30 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/HouseRobber2.java
+++ src/DYNAMIC_PROGRAMMING/HouseRobber2.java
@@ -6,6 +6,24 @@ package DYNAMIC_PROGRAMMING;
 public class HouseRobber2 {
 
 
-    
+        public int rob(int[] nums) {
 
+            int n = nums.length;
+            int previous1 = nums[0];
+            int previous2 = 0;
+
+            for (int i = 1; i < n; i++) {
+
+                int pick = nums[i];
+
+                if (i > 1)
+                    pick += previous2;
+
+                int notPick = 0 + previous1;
+
+                
+
+            }
+    }
 }
+
