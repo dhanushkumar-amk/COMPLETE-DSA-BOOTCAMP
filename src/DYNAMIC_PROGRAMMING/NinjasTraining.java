015732746d8c4161bd7ac3df5ab8d61e1f27cc2a
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-16 16:29:18 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/NinjasTraining.java
+++ src/DYNAMIC_PROGRAMMING/NinjasTraining.java
@@ -6,5 +6,14 @@ public class NinjasTraining {
 
         if (dp[day][last] != -1)
             return dp[day][last];
+
+        if (day == 0) {
+            int maximum = 0;
+            for (int i = 0; i <= 2; i++) {
+                if (i != last)
+                    maximum = Math.max(maximum, points[0][i]);
+            }
+            return dp[day][last] = maximum;
+        }
     }
 }
