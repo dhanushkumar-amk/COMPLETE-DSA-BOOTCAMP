457c2f5e27d511026731d464d48344ea25e0d30d
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-16 15:19:55 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/HouseRobber2.java
+++ src/DYNAMIC_PROGRAMMING/HouseRobber2.java
@@ -7,24 +7,29 @@ public class HouseRobber2 {
 
 
         public int rob(int[] nums) {
-
             int n = nums.length;
+
+            // -1, 0, 1 previous1 = 0 and previous2 -1
             int previous1 = nums[0];
             int previous2 = 0;
 
             for (int i = 1; i < n; i++) {
 
+                // pick means = i - 2;
                 int pick = nums[i];
-
                 if (i > 1)
-                    pick += previous2;
+                    pick+= previous2;
 
-                int notPick = 0 + previous1;
+                // just go i - 1
+                int notPick = previous1;
 
-
-
-
+                int current = Math.max(pick, notPick);
+                previous2 = previous1;
+                previous1 = current;
             }
-    }
+
+            // 5,6,7, n; n= i answer is i -1 is previous1
+            return previous1;
+        }
 }
 
