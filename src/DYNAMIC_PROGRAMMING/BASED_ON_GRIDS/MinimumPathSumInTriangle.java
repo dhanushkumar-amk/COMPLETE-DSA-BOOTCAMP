e0c17ff76ff65f641673820af04a1b2fb67e11e2
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-17 05:08:39 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_GRIDS/MinimumPathSumInTriangle.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_GRIDS/MinimumPathSumInTriangle.java
@@ -1,2 +1,35 @@
-package DYNAMIC_PROGRAMMING.BASED_ON_GRIDS;public class MInimumPathSumInTriangle {
+package DYNAMIC_PROGRAMMING.BASED_ON_GRIDS;
+
+import java.util.List;
+
+public class MinimumPathSumInTriangle {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int minimumTotal(List<List<Integer>> triangle) {
+
+        int m = triangle.size();
+        int n = triangle.get(0).size();
+
+        int[][] dp = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = -1;
+            }
+        }
+
+        return minimumPath(triangle, dp);
+
+    }
+
+    private int minimumPath( List<List<Integer>> triangle, int[][] dp){
+        int m = triangle.size();
+        int n = triangle.get(0).size();
+
+
+    }
+
 }
