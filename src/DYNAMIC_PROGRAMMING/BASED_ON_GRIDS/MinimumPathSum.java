a56caa9d40fa877a54ff27227c4bf1954cdb9b6e
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-17 04:26:23 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_GRIDS/MinimumPathSum.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_GRIDS/MinimumPathSum.java
@@ -10,5 +10,24 @@ public class MinimumPathSum {
 
     }
 
-    
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = -1;
+            }
+        }
+
+        return minPath(m, n, grid, dp);
+
+    }
+
+    private int minPath(int row, int col, int[][] grid, int[][] dp) {
+
+        
+
+    }
 }
