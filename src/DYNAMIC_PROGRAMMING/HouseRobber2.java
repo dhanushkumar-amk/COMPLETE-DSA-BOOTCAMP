9b0f09b7f9bbaf066f8212206097cf42e4249f82
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-16 15:31:13 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/HouseRobber2.java
+++ src/DYNAMIC_PROGRAMMING/HouseRobber2.java
@@ -3,59 +3,52 @@ package DYNAMIC_PROGRAMMING;
 //https://leetcode.com/problems/house-robber-ii/description/
 // leetcode 213
 
-import java.util.ArrayList;
+
 
 public class HouseRobber2 {
 
-
-        public static  int rob1(int[] nums) {
+        // Helper function to solve the House Robber I problem
+        private int rob1(int[] nums) {
             int n = nums.length;
+            if (n == 0) return 0;
+            if (n == 1) return nums[0];
 
-            // -1, 0, 1 previous1 = 0 and previous2 -1
             int previous1 = nums[0];
             int previous2 = 0;
 
             for (int i = 1; i < n; i++) {
-
-                // pick means = i - 2;
-                int pick = nums[i];
-                if (i > 1)
-                    pick+= previous2;
-
-                // just go i - 1
+                int pick = nums[i] + (i > 1 ? previous2 : 0);
                 int notPick = previous1;
-
                 int current = Math.max(pick, notPick);
                 previous2 = previous1;
                 previous1 = current;
             }
 
-            // 5,6,7, n; n= i answer is i -1 is previous1
             return previous1;
         }
 
-    public int rob(int[] nums) {
-
+        // Main function to solve the House Robber II problem
+        public int rob(int[] nums) {
             int n = nums.length;
-       int[] temp1 = new int[n];
-       int[] temp2 = new int[n];
+            if (n == 0) return 0;
+            if (n == 1) return nums[0];
 
+            // Split the problem into two subproblems
+            int[] temp1 = new int[n - 1];
+            int[] temp2 = new int[n - 1];
 
-            if (n == 1)
-                return nums[0];
+            for (int i = 0; i < n - 1; i++) {
+                temp1[i] = nums[i]; // Exclude the last house
+                temp2[i] = nums[i + 1]; // Exclude the first house
+            }
 
-        for (int i = 0; i < n; i++) {
-                if (i != 0)
-                    temp1[nums[i]]++;
+            // Solve both subproblems
+            int ans1 = rob1(temp1);
+            int ans2 = rob1(temp2);
 
-                if (i != n - 1)
-                    temp2[nums[i]]++;
+            // Return the maximum of the two solutions
+            return Math.max(ans1, ans2);
         }
-
-        long ans1 = rob1(temp1);
-        long ans2 = rob1(temp2);
-
-        return (int) Math.max(ans1,ans2);
     }
 
 }
