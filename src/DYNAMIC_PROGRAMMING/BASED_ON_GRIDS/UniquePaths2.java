841bdb4b9d49f8e65af59ce36620805b4cc64fbf
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-16 21:11:54 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_GRIDS/UniquePaths2.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_GRIDS/UniquePaths2.java
@@ -1,26 +1,20 @@
 package DYNAMIC_PROGRAMMING.BASED_ON_GRIDS;
 
-//leetcode 63
-// https://leetcode.com/problems/unique-paths-ii/description/
-
 public class UniquePaths2 {
-
 
     public static void main(String[] args) {
         UniquePaths2 ans = new UniquePaths2();
 
         int[][] maze = {
                 {0, 0, 0},
-                {0, -1, 0},
+                {0, 1, 0},
                 {0, 0, 0}
         };
 
         System.out.println(ans.uniquePathsWithObstacles(maze));
-
     }
 
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
 
@@ -32,27 +26,26 @@ public class UniquePaths2 {
             }
         }
 
-        return mazeObstaclesUtil(n - 1, m - 1, obstacleGrid, dp);
+        return mazeObstaclesUtil(m - 1, n - 1, obstacleGrid, dp);
     }
 
     private int mazeObstaclesUtil(int row, int col, int[][] obstacleGrid, int[][] dp) {
-
-        if (row >= 0 && col >= 0 && obstacleGrid[row][col] == -1)
+        if (row < 0 || col < 0 || obstacleGrid[row][col] == 1) {
             return 0;
+        }
 
-        if (row == 0 || col == 0)
+        if (row == 0 && col == 0) {
             return 1;
+        }
 
-        if(dp[row][col] != -1)
+        if (dp[row][col] != -1) {
             return dp[row][col];
+        }
 
         int upDirection = mazeObstaclesUtil(row - 1, col, obstacleGrid, dp);
         int leftDirection = mazeObstaclesUtil(row, col - 1, obstacleGrid, dp);
 
         dp[row][col] = upDirection + leftDirection;
-
         return dp[row][col];
-
     }
-
 }
