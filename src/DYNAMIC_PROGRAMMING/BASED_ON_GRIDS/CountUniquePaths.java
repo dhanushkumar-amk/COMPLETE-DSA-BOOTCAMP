c8bcad38837c7575bebd81db51076b651a9c2019
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-16 20:23:38 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_GRIDS/CountUniquePaths.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_GRIDS/CountUniquePaths.java
@@ -1,9 +1,40 @@
 package DYNAMIC_PROGRAMMING.BASED_ON_GRIDS;
 
+import java.util.Arrays;
+
 public class CountUniquePaths {
     public static void main(String[] args) {
 
     }
 
-    static int countPath()
+    static int countWays(int m, int n) {
+
+        int[][] dp = new int[m][n];
+
+
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
+
+        // Start the recursive calculation from the bottom-right cell (m-1, n-1)
+        return countWaysUtil(m - 1, n - 1, dp);
+    }
+
+    private static int countWaysUtil(int row, int col, int[][] dp) {
+
+        // reach the destination
+        if (row == 0  && col == 0)
+            return 1;
+
+        // validation
+        if (row < 0 ||col < 1)
+            return 0;
+
+        if (dp[row][col] != -1)
+            return dp[row][col];
+
+        int upDirection = countWaysUtil(row - 1, col, dp);
+        int leftDirection = countWaysUtil(row, col - 1, dp);
+
+        return dp[row][col] = upDirection + leftDirection;
+    }
 }
