68a485ee19c7dc750e81cbd93f395e14316287bd
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-18 04:51:21 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_GRIDS/CheeryPickup.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_GRIDS/CheeryPickup.java
@@ -30,6 +30,16 @@ public class CheeryPickup {
             return (int) (Math.pow(-10, 9));
 
 
+        // base case => destination
+        if (i == n - 1) {
+            // both go on same destination
+            if (j1 == j2)
+                return grid[i][j1];
+            else
+                return grid[i][j1] + grid[i][j2];
+        }
+
         
+
     }
 }
