2791d75ba34747b6bf09eab70f9907e6207c31cc
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-17 20:42:59 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_GRIDS/MinimumFallingPathSum.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_GRIDS/MinimumFallingPathSum.java
@@ -1,8 +1,47 @@
 package DYNAMIC_PROGRAMMING.BASED_ON_GRIDS;
 
+import java.util.Arrays;
+
 public class MinimumFallingPathSum {
 
 
-    public int minFallingPathSum(int[][] matrix) {
+    static int getMaxUtil(int i, int j, int m, int[][] matrix, int[][] dp) {
+        // Base Conditions
+        if (j < 0 || j >= m)
+            return (int) Math.pow(-10, 9);
+        if (i == 0)
+            return matrix[0][j];
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        // Calculate three possible paths: moving up, left diagonal, and right diagonal
+        int up = matrix[i][j] + getMaxUtil(i - 1, j, m, matrix, dp);
+        int leftDiagonal = matrix[i][j] + getMaxUtil(i - 1, j - 1, m, matrix, dp);
+        int rightDiagonal = matrix[i][j] + getMaxUtil(i - 1, j + 1, m, matrix, dp);
+
+        // Store the maximum of the three paths in dp
+        return dp[i][j] = Math.max(up, Math.max(leftDiagonal, rightDiagonal));
     }
+
+    // Function to find the maximum path sum in the matrix
+    static int getMaxPathSum(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int dp[][] = new int[n][m];
+        for (int row[] : dp)
+            Arrays.fill(row, -1);
+
+        int maxi = Integer.MIN_VALUE;
+
+        // For each starting column, find the maximum path sum and update maxi
+        for (int j = 0; j < m; j++) {
+            int ans = getMaxUtil(n - 1, j, m, matrix, dp);
+            maxi = Math.max(maxi, ans);
+        }
+
+        return maxi;
+    }
+
 }
