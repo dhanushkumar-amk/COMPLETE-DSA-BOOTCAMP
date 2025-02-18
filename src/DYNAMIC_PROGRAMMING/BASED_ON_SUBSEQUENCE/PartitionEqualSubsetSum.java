d8324a45d3438aa35e8e818bbf2ad1f6614cc2d5
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-19 04:32:01 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_SUBSEQUENCE/PartitionEqualSubsetSum.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_SUBSEQUENCE/PartitionEqualSubsetSum.java
@@ -1,2 +1,44 @@
-package DYNAMIC_PROGRAMMING.BASED_ON_SUBSEQUENCE;public class PartitionEqualSubsetSum {
+package DYNAMIC_PROGRAMMING.BASED_ON_SUBSEQUENCE;
+
+public class PartitionEqualSubsetSum {
+
+
+
+    
+
+
+    boolean subsetSumToK(int n, int k, int[] arr) {
+        // Create a boolean DP table with dimensions [n][k+1]
+        boolean dp[][] = new boolean[n][k + 1];
+
+        // Initialize the first row of the DP table
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+
+        // Initialize the first column of the DP table
+        if (arr[0] <= k) {
+            dp[0][arr[0]] = true;
+        }
+
+        // Fill in the DP table using bottom-up approach
+        for (int ind = 1; ind < n; ind++) {
+            for (int target = 1; target <= k; target++) {
+                // Calculate if the current target can be achieved without taking the current element
+                boolean notTaken = dp[ind - 1][target];
+
+                // Calculate if the current target can be achieved by taking the current element
+                boolean taken = false;
+                if (arr[ind] <= target) {
+                    taken = dp[ind - 1][target - arr[ind]];
+                }
+
+                // Store the result in the DP table
+                dp[ind][target] = notTaken || taken;
+            }
+        }
+
+        // The final result is stored in the bottom-right cell of the DP table
+        return dp[n - 1][k];
+    }
 }
