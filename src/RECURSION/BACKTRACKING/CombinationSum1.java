ce5a982c5794fa7f6eb51ee001024eca16706482
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-18 06:30:14 +0530

    modified

--- src/RECURSION/BACKTRACKING/CombinationSum1.java
+++ src/RECURSION/BACKTRACKING/CombinationSum1.java
@@ -11,7 +11,29 @@ public class CombinationSum1 {
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
 
-        List<List<Integer>> ans = new ArrayList<>();
-        findCombinations(0, candidates, target, )
+        List<List<Integer>> answer = new ArrayList<>();
+        findCombinations(0, candidates, target, answer, new ArrayList<>());
+        return answer;
     }
+
+    private void findCombinations(int index, int[] candidates, int target, List<List<Integer>> answer,  ArrayList<Integer> dataStructure) {
+
+        // base case
+        if (index == candidates.length){
+            if (target == 0)
+                answer.add(new ArrayList<>(dataStructure));  // add  if the target is 0 otherwise backtrack
+            return;
+        }
+
+        //pick
+        if (candidates[index] <= target){
+            dataStructure.add(candidates[index]);
+            findCombinations(index, candidates,target - candidates[index], answer, dataStructure);
+            dataStructure.remove(candidates[index]);  // backtrack 
+        }
+
+        // not pick
+        findCombinations(index + 1, candidates, target, answer, dataStructure);
+    }
+
 }
