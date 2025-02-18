0ce6e4091c69e81595c5ee70b77f5a28291abf3c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-18 05:35:30 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_SUNSEQUENCE/SubsetSumEqualToTarget.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_SUNSEQUENCE/SubsetSumEqualToTarget.java
@@ -2,9 +2,16 @@ package DYNAMIC_PROGRAMMING.BASED_ON_SUNSEQUENCE;
 
 public class SubsetSumEqualToTarget {
 
-    static boolean subsetSumToK(int n, int k, int[] arr){
+    static boolean subsetSumToK(int n, int target, int[] arr){
 
+        // create the dp array of n and k + 1 element
+        boolean[][] dp = new boolean[n][target + 1];
+
+        if (target == 0)
+            return true;
 
         
+
+
     }
 }
