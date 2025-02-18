17f177fbf8b1591e778fab438e600aa5603f0988
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-18 05:43:47 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_SUNSEQUENCE/SubsetSumEqualToTarget.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_SUNSEQUENCE/SubsetSumEqualToTarget.java
@@ -13,16 +13,16 @@ public class SubsetSumEqualToTarget {
             }
         }
 
-        
 
 
-       return fun(n -1, target, arr);
+
+       return fun(n -1, target, arr, dp);
 
 
 
     }
 
-    private static boolean fun(int index, int target, int[] arr) {
+    private static boolean fun(int index, int target, int[] arr, int[][] dp) {
 
         // base case
         if (target == 0)
@@ -31,12 +31,12 @@ public class SubsetSumEqualToTarget {
         if (index == 0)
             return (arr[0] == target);
 
-        boolean notPick = fun(index -1, target, arr);
+        boolean notPick = fun(index -1, target, arr, dp);
         boolean pick = false;
 
         // bounty validation target 3 and arr[i] = 6 means ony taken other wise can't be taken 9 target 3 and the value is 6 maens the value is greater than target so can't taken the target can'br gone on negative
         if (target >= arr[index])
-            pick = fun(index - 1, target - arr[index], arr);
+            pick = fun(index - 1, target - arr[index], arr,dp);
 
         return pick || notPick;
 
