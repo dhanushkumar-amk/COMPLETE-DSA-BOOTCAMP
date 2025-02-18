55b72eff9b7bf9b38412a155da78b611249ee57d
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-18 05:41:24 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_SUNSEQUENCE/SubsetSumEqualToTarget.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_SUNSEQUENCE/SubsetSumEqualToTarget.java
@@ -4,14 +4,29 @@ public class SubsetSumEqualToTarget {
 
     static boolean subsetSumToK(int n, int target, int[] arr){
 
-        // create the dp array of n and k + 1 element
-        boolean[][] dp = new boolean[n][target + 1];
+       return fun(n -1, target, arr);
 
+
+
+    }
+
+    private static boolean fun(int index, int target, int[] arr) {
+
+        // base case
         if (target == 0)
             return true;
 
-        
+        if (index == 0)
+            return (arr[0] == target);
 
+        boolean notPick = fun(index -1, target, arr);
+        boolean pick = false;
+
+        // bounty validation target 3 and arr[i] = 6 means ony taken other wise can't be taken 9 target 3 and the value is 6 maens the value is greater than target so can't taken the target can'br gone on negative
+        if (target >= arr[index])
+            pick = fun(index - 1, target - arr[index], arr);
+
+        return pick || notPick;
 
     }
 }
