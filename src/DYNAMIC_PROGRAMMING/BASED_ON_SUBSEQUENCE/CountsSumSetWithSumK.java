0b4d84f4ae49ac2774395a0b24251229d07d53c5
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-20 19:01:22 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_SUBSEQUENCE/CountsSumSetWithSumK.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_SUBSEQUENCE/CountsSumSetWithSumK.java
@@ -2,5 +2,11 @@ package DYNAMIC_PROGRAMMING.BASED_ON_SUBSEQUENCE;
 
 public class CountsSumSetWithSumK {
 
-    
+    public static void main(String args[]) {
+        int arr[] = {1, 2, 2, 3};
+        int k = 3;
+
+        // Calculate and print the number of subsets that sum up to k
+        System.out.println("The number of subsets found are " + findWays(arr, k));
+    }
 }
