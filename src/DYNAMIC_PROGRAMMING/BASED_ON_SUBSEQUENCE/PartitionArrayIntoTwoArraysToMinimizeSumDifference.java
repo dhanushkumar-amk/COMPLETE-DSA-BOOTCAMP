e2ceb8b3687b130a98c67bb5984a4c63fcdc777d
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-20 18:18:57 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_SUBSEQUENCE/PartitionArrayIntoTwoArraysToMinimizeSumDifference.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_SUBSEQUENCE/PartitionArrayIntoTwoArraysToMinimizeSumDifference.java
@@ -1,9 +1,16 @@
 package DYNAMIC_PROGRAMMING.BASED_ON_SUBSEQUENCE;
 
 public class PartitionArrayIntoTwoArraysToMinimizeSumDifference {
-
     public static void main(String[] args) {
 
+        PartitionArrayIntoTwoArraysToMinimizeSumDifference answer = new PartitionArrayIntoTwoArraysToMinimizeSumDifference();
+        System.out.println(answer.minimumDifference(nums));
     }
-    
+
+
+    public int minimumDifference(int[] nums) {
+
+    }
+
+
 }
