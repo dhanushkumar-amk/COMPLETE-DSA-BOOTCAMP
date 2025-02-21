b33d10e662ee3bd6566f3e29138529e07df5b0c7
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-21 22:07:52 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_SUBSEQUENCE/UnboundedKnapsack.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_SUBSEQUENCE/UnboundedKnapsack.java
@@ -1,6 +1,13 @@
 package DYNAMIC_PROGRAMMING.BASED_ON_SUBSEQUENCE;
 
+import javax.xml.transform.stax.StAXResult;
+
 public class UnboundedKnapsack {
+
+
+    public static void main(String[] args) {
+        
+    }
 
     static int function(int index, int bagWeight, int[] value, int[] weight){
 
@@ -8,13 +15,17 @@ public class UnboundedKnapsack {
             return ( (int) (bagWeight/ weight[0])) * value[0];
         }
 
-        
+        int notPick = function(index - 1, bagWeight, value, weight);
 
+        int pick = 0;
 
+        if (weight[index] <= bagWeight)
+            pick = value[index] +  function(index, bagWeight - weight[index], value, weight);
 
+        return pick + notPick;
     }
 
     static  int unbounded(int n, int bagWeight, int[] value, int[] weight){
-
+         return function(n - 1, bagWeight, value, weight);
     }
 }
