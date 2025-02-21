423754accab0dc3172fe7853a79a686ca32a3550
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-22 04:44:03 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_STRINGS/LongestCommonSubsequence.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_STRINGS/LongestCommonSubsequence.java
@@ -2,5 +2,11 @@ package DYNAMIC_PROGRAMMING.BASED_ON_STRINGS;
 
 public class LongestCommonSubsequence {
 
-    
+    public static void main(String[] args) {
+
+    }
+
+   public static int longestCommonSubsequence(String text1, String text2) {
+
+    }
 }
