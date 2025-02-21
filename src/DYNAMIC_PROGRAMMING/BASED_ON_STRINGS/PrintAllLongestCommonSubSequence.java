c8b93f3c70b68dd0f9eeff4797f6932b70cb7c56
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-22 05:16:59 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_STRINGS/PrintAllLongestCommonSubSequence.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_STRINGS/PrintAllLongestCommonSubSequence.java
@@ -1,2 +1,61 @@
-package DYNAMIC_PROGRAMMING.BASED_ON_STRINGS;public class PrintAllLongestCommonSubSequence {
-}
+package DYNAMIC_PROGRAMMING.BASED_ON_STRINGS;
+
+
+import java.util.Arrays;
+
+public class PrintAllLongestCommonSubSequence {
+
+        public static void main(String[] args) {
+            String s1 = "acd";
+            String s2 = "ced";
+
+            int[][] dp = new int[s1.length()][s2.length()];
+            for (int[] row : dp)
+                Arrays.fill(row, -1);
+
+            int length = longestCommonSubsequence(s1, s2, dp);
+            System.out.println("The Length of Longest Common Subsequence is " + length);
+
+            String lcs = printLCS(s1, s2, dp);
+            System.out.println("The Longest Common Subsequence is: " + lcs);
+        }
+
+        public static int longestCommonSubsequence(String text1, String text2, int[][] dp) {
+            return function(text1.length() - 1, text2.length() - 1, text1, text2, dp);
+        }
+
+        private static int function(int i, int j, String text1, String text2, int[][] dp) {
+            // base case
+            if (i < 0 || j < 0)
+                return 0;
+
+            if (dp[i][j] != -1)
+                return dp[i][j];
+
+            if (text1.charAt(i) == text2.charAt(j))
+                return dp[i][j] = 1 + function(i - 1, j - 1, text1, text2, dp);
+
+            return dp[i][j] = Math.max(function(i - 1, j, text1, text2, dp), function(i, j - 1, text1, text2, dp));
+        }
+
+        public static String printLCS(String text1, String text2, int[][] dp) {
+            int i = text1.length() - 1;
+            int j = text2.length() - 1;
+
+            StringBuilder lcs = new StringBuilder();
+
+            while (i >= 0 && j >= 0) {
+                if (text1.charAt(i) == text2.charAt(j)) {
+                    lcs.append(text1.charAt(i)); // Add the matching character
+                    i--;
+                    j--;
+                } else if (i > 0 && (j == 0 || dp[i - 1][j] > dp[i][j - 1])) {
+                    i--; // Move up in the DP table
+                } else {
+                    j--; // Move left in the DP table
+                }
+            }
+
+            return lcs.reverse().toString(); // Reverse to get the correct order
+        }
+    }
