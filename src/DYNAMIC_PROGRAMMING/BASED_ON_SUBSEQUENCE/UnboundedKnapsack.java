6616ffa32cce281a79f9ff5e1dab0d8d3e0ecaee
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-21 22:03:47 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_SUBSEQUENCE/UnboundedKnapsack.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_SUBSEQUENCE/UnboundedKnapsack.java
@@ -5,10 +5,10 @@ public class UnboundedKnapsack {
     static int function(int index, int bagWeight, int[] value, int[] weight){
 
         if (index == 0){
-            return ( (int) (bagWeight/ weight[0]) * value[0]);
+            return ( (int) (bagWeight/ weight[0])) * value[0];
         }
 
-        
+
 
 
     }
