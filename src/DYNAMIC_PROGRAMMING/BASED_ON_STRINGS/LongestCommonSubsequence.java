ddf0aa03880594e1921d04a68d58de6f56643ade
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-22 04:51:34 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_STRINGS/LongestCommonSubsequence.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_STRINGS/LongestCommonSubsequence.java
@@ -1,9 +1,11 @@
 package DYNAMIC_PROGRAMMING.BASED_ON_STRINGS;
 
+import java.util.Arrays;
+
 public class LongestCommonSubsequence {
 
     public static void main(String[] args) {
-
+        String text1 =  
     }
 
    public static int longestCommonSubsequence(String text1, String text2) {
@@ -11,21 +13,28 @@ public class LongestCommonSubsequence {
         int n = text1.length();
         int m = text2.length();
 
-        return function(n - 1, m -1, text1, text2);
+        int[][] dp = new int[n][m + 1];
+
+        for(int[] row : dp)
+            Arrays.fill(row, -1);
+
+        return function(n - 1, m -1, text1, text2, dp);
     }
 
-    private static int function(int i, int j, String text1, String text2) {
+    private static int function(int i, int j, String text1, String text2, int[][] dp) {
 
         // base case
         if (i < 0 || j < 0)
             return 0;
 
+        if (dp[i][j] != -1)
+            return dp[i][j];
 
         if (text1.charAt(i) == text2.charAt(j))
-            return 1 + function(i -1, j - 1, text1, text2);
+            return dp[i][j] =  1 + function(i -1, j - 1, text1, text2, dp);
 
 
-        return 0 + Math.max(function(i -1, j, text1, text2), function(i, j -1, text1, text2));
+        return dp[i][j] =  Math.max(function(i - 1, j, text1, text2, dp), function(i, j - 1, text1, text2, dp));
 
     }
 }
