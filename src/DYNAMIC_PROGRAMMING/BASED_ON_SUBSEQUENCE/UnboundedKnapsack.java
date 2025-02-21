001a2b0547989059c2026c3fcadcd07b25fbb9f0
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-21 22:03:29 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_SUBSEQUENCE/UnboundedKnapsack.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_SUBSEQUENCE/UnboundedKnapsack.java
@@ -1,2 +1,19 @@
-package DYNAMIC_PROGRAMMING.BASED_ON_SUBSEQUENCE;public class UnboundedKnapsack {
+package DYNAMIC_PROGRAMMING.BASED_ON_SUBSEQUENCE;
+
+public class UnboundedKnapsack {
+
+    static int function(int index, int bagWeight, int[] value, int[] weight){
+
+        if (index == 0){
+            return ( (int) (bagWeight/ weight[0]) * value[0]);
+        }
+
+        
+
+
+    }
+
+    static  int unbounded(int n, int bagWeight, int[] value, int[] weight){
+
+    }
 }
