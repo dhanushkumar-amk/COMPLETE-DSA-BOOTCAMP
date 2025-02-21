b6b2e9802cb6ee47751974efd87c9f25c019449d
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-21 19:22:15 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_SUBSEQUENCE/CoinChange.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_SUBSEQUENCE/CoinChange.java
@@ -7,5 +7,26 @@ package DYNAMIC_PROGRAMMING.BASED_ON_SUBSEQUENCE;
 
 public class CoinChange {
 
-    
+    public static int coinChange(int[] coins, int amount) {
+
+        return function(coins.length - 1, coins, amount);
+    }
+
+    static int function(int index, int[] coins, int target){
+
+        if (index == 0){
+            if (target % coins[0] == 0)
+                return target % coins[0];
+            else
+                return (int) 1e9;
+        }
+
+        int notPick = 0 + function(index -1, coins, target);
+
+        int pick = Integer.MAX_VALUE;
+        if (target <= coins[index])
+            pick = 1 + function(index, coins, target - coins[index]);
+
+        return Math.min(pick, notPick);
+    }
 }
