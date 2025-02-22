0651f57516b12ff19de6b1877d1ed2b387ec4ca7
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-22 15:12:45 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_STRINGS/WildcardMatching.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_STRINGS/WildcardMatching.java
@@ -2,5 +2,12 @@ package DYNAMIC_PROGRAMMING.BASED_ON_STRINGS;
 
 public class WildcardMatching {
 
-    
+    public boolean isMatch(String string, String pattern) {
+
+        int n = pattern.length();
+        int m = string.length();
+
+        return function(n - 1, m - 1, string, pattern);
+
+    }
 }
