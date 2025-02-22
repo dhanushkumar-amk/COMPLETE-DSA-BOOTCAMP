0312b58fb6456ce5a27aabb59222f73e9868b42c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-22 05:55:24 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_STRINGS/LongestPalindromicSubsequence.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_STRINGS/LongestPalindromicSubsequence.java
@@ -1,5 +1,7 @@
 package DYNAMIC_PROGRAMMING.BASED_ON_STRINGS;
 
+import java.util.Arrays;
+
 public class LongestPalindromicSubsequence {
 
     public int longestPalindromeSubseq(String s) {
@@ -7,7 +9,11 @@ public class LongestPalindromicSubsequence {
         String reversed = new StringBuilder(s).reverse().toString();
         int n = s.length();
 
-        
+        int[][] dp = new int[n + 1][n + 1];
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
+
+        return function()
 
     }
 }
