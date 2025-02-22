954d17b9ae0ee14a95d773ed3c5086b31f771320
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-22 11:24:50 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_STRINGS/DeleteOperationForTwoStrings.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_STRINGS/DeleteOperationForTwoStrings.java
@@ -4,9 +4,46 @@ package DYNAMIC_PROGRAMMING.BASED_ON_STRINGS;
 //leetcode : 583
 // https://leetcode.com/problems/delete-operation-for-two-strings/description/
 
+import java.util.Arrays;
+
 public class DeleteOperationForTwoStrings {
 
     public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+
+        return n + m - 2 * longestCommonSubsequence(word1, word2);
+    }
+
+
+    public  int longestCommonSubsequence(String text1, String text2) {
+
+        int n = text1.length();
+        int m = text2.length();
+
+        int[][] dp = new int[n][m + 1];
+
+        for(int[] row : dp)
+            Arrays.fill(row, -1);
+
+        return function(n - 1, m -1, text1, text2, dp);
+    }
+
+    private  int function(int i, int j, String text1, String text2, int[][] dp) {
+
+        // base case
+        if (i < 0 || j < 0)
+            return 0;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        if (text1.charAt(i) == text2.charAt(j))
+            return dp[i][j] =  1 + function(i -1, j - 1, text1, text2, dp);
+
+
+        return dp[i][j] =  Math.max(function(i - 1, j, text1, text2, dp), function(i, j - 1, text1, text2, dp));
 
     }
+
 }
