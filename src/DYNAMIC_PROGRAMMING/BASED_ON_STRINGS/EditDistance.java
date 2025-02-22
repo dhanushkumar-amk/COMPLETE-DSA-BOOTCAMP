b2d7532761c95f76ea44b4d81c46dcdcc7740449
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-22 14:42:56 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_STRINGS/EditDistance.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_STRINGS/EditDistance.java
@@ -52,7 +52,7 @@ public class EditDistance {
         for (int i = 1; i <= n ; i++) {
             for (int j = 1; j <= m ; j++) {
 
-                if (word1.charAt(i) == word2.charAt(j)) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
                     dp[i][j] = dp[i- 1][j - 1]; // skip both
                 }
                 else {
@@ -61,12 +61,7 @@ public class EditDistance {
                 }
             }
         }
-
-
-
-
-        return function(n -1, m - 1, word1, word2, dp);
-
+        return dp[n][m];
     }
 
 
