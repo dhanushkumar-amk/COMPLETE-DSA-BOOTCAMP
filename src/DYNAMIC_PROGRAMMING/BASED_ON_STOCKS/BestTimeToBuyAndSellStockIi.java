5cd9c4ff8d2b9dbca286e80f501fd56424598c44
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-22 21:18:27 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_STOCKS/BestTimeToBuyAndSellStockIi.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_STOCKS/BestTimeToBuyAndSellStockIi.java
@@ -14,7 +14,7 @@ public class BestTimeToBuyAndSellStockIi {
 
         int n = prices.length;
 
-        int[][] dp = new int[n][n];
+        int[][] dp = new int[n][2];
 
         for(int[] row : dp)
             Arrays.fill(row, -1);
