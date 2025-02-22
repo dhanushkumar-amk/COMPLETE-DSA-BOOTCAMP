d649be5b7ee0b280549b9a5de2b40e609479892e
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-22 23:04:49 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_STOCKS/BestTimeToBuyAndSellStock3.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_STOCKS/BestTimeToBuyAndSellStock3.java
@@ -75,7 +75,27 @@ public class BestTimeToBuyAndSellStock3 {
 //            }
 //        }
 
-        
+
+        // Loop through the dp array, starting from the second last stock (ind=n-1)
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int cap = 1; cap <= 2; cap++) {
+
+                    if (buy == 0) { // We can buy the stock
+                        dp[ind][buy][cap] = Math.max(0 + dp[ind + 1][0][cap],
+                                -prices[ind] + dp[ind + 1][1][cap]);
+                    }
+
+                    if (buy == 1) { // We can sell the stock
+                        dp[ind][buy][cap] = Math.max(0 + dp[ind + 1][1][cap],
+                                prices[ind] + dp[ind + 1][0][cap - 1]);
+                    }
+                }
+            }
+        }
+
+        // The maximum profit with 2 transactions is stored in dp[0][0][2]
+        return dp[0][0][2];
 
 
         
