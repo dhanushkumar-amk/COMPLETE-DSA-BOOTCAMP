af5e14dffe5a81df55dd3321ddf124f2b32fb7c1
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-22 22:37:01 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_STOCKS/BestTimeToBuyAndSellStock3.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_STOCKS/BestTimeToBuyAndSellStock3.java
@@ -2,4 +2,15 @@ package DYNAMIC_PROGRAMMING.BASED_ON_STOCKS;
 
 public class BestTimeToBuyAndSellStock3 {
 
+
+    public int maxProfit(int[] prices) {
+
+        return function(0);
+    }
+
+    int function(int index, int buy, int transaction, int[] price, int[][] dp){
+
+        if (index)
+    }
+
 }
