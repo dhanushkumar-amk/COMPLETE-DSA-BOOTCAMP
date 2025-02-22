bcf974a260df5529aaa2f59f3b5bd2c691f2f9eb
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-22 15:17:36 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_STRINGS/WildcardMatching.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_STRINGS/WildcardMatching.java
@@ -10,4 +10,31 @@ public class WildcardMatching {
         return function(n - 1, m - 1, string, pattern);
 
     }
+
+    private boolean function(int i, int j, String string, String pattern) {
+
+        // base case
+        if (i < 0  && j < 0)
+            return true;
+
+        if (i < 0 && j >= 0)
+            return false;
+
+        if (j < 0 && i >= 0){
+            for (int k = 0; k <= i; k++) {
+                if (pattern.charAt(i) != '*')
+                    return false;
+            }
+            return true;
+        }
+
+        if (pattern.charAt(i) == string.charAt(j) ||  pattern.charAt(i) == '?')
+            return function(i - 1, j - 1, string, pattern);
+
+        if (pattern.charAt(i) == '*')
+            return function(i - 1, j, string, pattern) || function(i, j -1, string, pattern);
+
+        // not match
+        return false;
+    }
 }
