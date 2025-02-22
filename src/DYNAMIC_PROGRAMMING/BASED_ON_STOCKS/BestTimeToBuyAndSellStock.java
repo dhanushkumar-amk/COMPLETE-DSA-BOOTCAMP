f820aa6c5a7dd8c2df22f6f4206bdad04875503a
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-22 19:53:00 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_STOCKS/BestTimeToBuyAndSellStock.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_STOCKS/BestTimeToBuyAndSellStock.java
@@ -2,5 +2,20 @@ package DYNAMIC_PROGRAMMING.BASED_ON_STOCKS;
 
 public class BestTimeToBuyAndSellStock {
 
-    
+    public static void main(String[] args) {
+        
+    }
+
+    public static int maxProfit(int[] prices) {
+
+        int minimumPrice = prices[0];
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+        int cost = minimumPrice - prices[i];
+            profit = Math.max(profit, cost);
+            minimumPrice = Math.min(minimumPrice, prices[i]);
+        }
+        return profit;
+    }
 }
