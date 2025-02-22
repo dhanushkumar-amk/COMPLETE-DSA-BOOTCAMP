6c9ce1c6deb06a084cc18b780059a22c0392873c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-22 23:20:28 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_STOCKS/BestTimeToBuyAndSellStocks4.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_STOCKS/BestTimeToBuyAndSellStocks4.java
@@ -9,7 +9,7 @@ public class BestTimeToBuyAndSellStocks4 {
 
         BestTimeToBuyAndSellStocks4  answer = new BestTimeToBuyAndSellStocks4();
 
-        int[] prices = {3, 3, 5, 0, 0, 3, 1, 4};
+        int[] prices = {3,2,6,5,0,3};
 
         // Calculate and print the maximum profit
         System.out.println("The maximum profit that can be generated is " +answer.maxProfit(2, prices));
