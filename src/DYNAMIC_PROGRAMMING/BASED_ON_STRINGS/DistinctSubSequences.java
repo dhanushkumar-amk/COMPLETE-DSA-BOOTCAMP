cdbb8e47eba863888ee6900db757fb321f1584eb
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-22 13:29:06 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_STRINGS/DistinctSubSequences.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_STRINGS/DistinctSubSequences.java
@@ -3,6 +3,24 @@ package DYNAMIC_PROGRAMMING.BASED_ON_STRINGS;
 public class DistinctSubSequences {
 
 
-    
+
+    public int numDistinct(String s, String t) {
+
+    }
+
+
+    private int function(int i, int j, String string1, String string2){
+
+        // base case
+        if(i < 0)
+            return 0;
+
+        if (j < 0)
+            return 1;
+
+        if (string2[j] == string1[i]){
+            
+        }
+    }
 
 }
