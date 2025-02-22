a1118a47a740eea604b41fce170a4c7ddc20813d
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-22 23:34:53 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_STOCKS/BestTimeToBuyAndSellStockWithCooldown.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_STOCKS/BestTimeToBuyAndSellStockWithCooldown.java
@@ -2,6 +2,14 @@ package DYNAMIC_PROGRAMMING.BASED_ON_STOCKS;
 
 public class BestTimeToBuyAndSellStockWithCooldown {
 
+
+    // just copy and paste of BestTimeToBuyAndSellStock2
+
+    public int maxProfit(int[] prices) {
+
+    }
+
     
+
 
 }
