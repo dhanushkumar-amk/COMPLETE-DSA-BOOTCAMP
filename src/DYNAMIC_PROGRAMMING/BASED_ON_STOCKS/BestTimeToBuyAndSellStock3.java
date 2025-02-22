a78e405f80308e675fe901aabddb0302454f144b
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-22 22:40:35 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_STOCKS/BestTimeToBuyAndSellStock3.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_STOCKS/BestTimeToBuyAndSellStock3.java
@@ -8,7 +8,7 @@ public class BestTimeToBuyAndSellStock3 {
         return function(0);
     }
 
-    int function(int index, int buy, int transaction, int[] price, int[][] dp){
+    int function(int index, int buy, int transaction, int[] price, int[][][] dp){
 
         int n = price.length;
 
@@ -16,7 +16,14 @@ public class BestTimeToBuyAndSellStock3 {
         if (index == n || transaction == 0)
             return 0;
 
-        if (buy)
+        if (buy == 1)
+            // buy
+            return dp[index][buy][transaction] =  Math.max(-price[index] + function(index -1, 0, transaction, price,dp),
+                    0 + function(index - 1, 1, transaction, price, dp)
+                    );
+        else
+            // sell
+            return dp[index][buy][transaction] =
 
     }
 
