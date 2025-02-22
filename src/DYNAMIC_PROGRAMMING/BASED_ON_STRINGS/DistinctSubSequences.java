da952dea27f74ea05261238bfddbd37aeceee64c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-22 13:37:17 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_STRINGS/DistinctSubSequences.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_STRINGS/DistinctSubSequences.java
@@ -1,5 +1,7 @@
 package DYNAMIC_PROGRAMMING.BASED_ON_STRINGS;
 
+import java.util.Arrays;
+
 public class DistinctSubSequences {
 
 
@@ -9,13 +11,16 @@ public class DistinctSubSequences {
         int n = s.length();
         int m = t.length();
 
-        int[][] dp = new int[][]
+        int[][] dp = new int[n][m];
 
-        return function(n -1, m-1, s, t);
+        for(int[] row : dp)
+            Arrays.fill(row, -1);
+
+        return function(n -1, m-1, s, t, dp);
     }
 
 
-    private int function(int i, int j, String string1, String string2){
+    private int function(int i, int j, String string1, String string2, int[][] dp){
 
         // base case
         if(i < 0)
@@ -24,16 +29,19 @@ public class DistinctSubSequences {
         if (j < 0)
             return 1;
 
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
         if (string1.charAt(i) == string2.charAt(j)) {
 
-            int leaveOneOnBoth = function(i - 1, j - 1, string1, string2);
-            int leaveOneOnS1 = function(i - 1, j, string1, string2);  // s2 is same as bag
+            int leaveOneOnBoth = function(i - 1, j - 1, string1, string2, dp);
+            int leaveOneOnS1 = function(i - 1, j, string1, string2, dp);  // s2 is same as bag
 
-            return leaveOneOnBoth + leaveOneOnS1;
+            return dp[i][j] =  leaveOneOnBoth + leaveOneOnS1;
         }
 
         //else
-        return function(i -1, j, string1, string2);
+        return dp[i][j] =  function(i -1, j, string1, string2, dp);
     }
 
 }
