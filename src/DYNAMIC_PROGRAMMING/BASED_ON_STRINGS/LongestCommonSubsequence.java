d0a986a19f70beecb22b58f2416c99a588b0ccc0
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-22 11:25:44 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_STRINGS/LongestCommonSubsequence.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_STRINGS/LongestCommonSubsequence.java
@@ -15,33 +15,33 @@ public class LongestCommonSubsequence {
 
     }
 
-   public static int longestCommonSubsequence(String text1, String text2) {
+       public static int longestCommonSubsequence(String text1, String text2) {
 
-        int n = text1.length();
-        int m = text2.length();
+            int n = text1.length();
+            int m = text2.length();
 
-        int[][] dp = new int[n][m + 1];
+            int[][] dp = new int[n][m + 1];
 
-        for(int[] row : dp)
-            Arrays.fill(row, -1);
+            for(int[] row : dp)
+                Arrays.fill(row, -1);
 
-        return function(n - 1, m -1, text1, text2, dp);
-    }
+            return function(n - 1, m -1, text1, text2, dp);
+        }
 
-    private static int function(int i, int j, String text1, String text2, int[][] dp) {
+        private static int function(int i, int j, String text1, String text2, int[][] dp) {
 
-        // base case
-        if (i < 0 || j < 0)
-            return 0;
+            // base case
+            if (i < 0 || j < 0)
+                return 0;
 
-        if (dp[i][j] != -1)
-            return dp[i][j];
+            if (dp[i][j] != -1)
+                return dp[i][j];
 
-        if (text1.charAt(i) == text2.charAt(j))
-            return dp[i][j] =  1 + function(i -1, j - 1, text1, text2, dp);
+            if (text1.charAt(i) == text2.charAt(j))
+                return dp[i][j] =  1 + function(i -1, j - 1, text1, text2, dp);
 
 
-        return dp[i][j] =  Math.max(function(i - 1, j, text1, text2, dp), function(i, j - 1, text1, text2, dp));
+            return dp[i][j] =  Math.max(function(i - 1, j, text1, text2, dp), function(i, j - 1, text1, text2, dp));
 
-    }
+        }
 }
