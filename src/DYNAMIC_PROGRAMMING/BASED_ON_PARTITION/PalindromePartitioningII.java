3c23a962a04dae7a51e8617483553ac520f38b01
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-23 20:35:48 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_PARTITION/PalindromePartitioningII.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_PARTITION/PalindromePartitioningII.java
@@ -21,25 +21,28 @@ public class PalindromePartitioningII {
         int[] dp = new int[n];
         Arrays.fill(dp, -1);
 
-        return function(0, s) - 1;
+        return function(0, s, dp) - 1;
     }
 
-    private int function(int i, String s){
+    private int function(int i, String s, int[] dp){
 
         int n = s.length();
 
         if (i == n)
             return 0;
 
+        if (dp[i] != -1)
+            return dp[i];
+
         int minimumCost = Integer.MAX_VALUE;
 
         for (int j = i; j < n ; j++) {
             if (isPalindrome(i, j, s)) {
-               int cost = 1 + function(j + 1, s);
+               int cost = 1 + function(j + 1, s, dp);
                 minimumCost = Math.min(cost, minimumCost);
             }
         }
-    return minimumCost;
+    return dp[i] = minimumCost;
     }
 
     private boolean isPalindrome(int i, int j, String s) {
