eb1016f0bc066164f949ebe57d83775e09540240
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-23 19:43:40 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_PARTITION/BurstBalloons.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_PARTITION/BurstBalloons.java
@@ -2,7 +2,7 @@ package DYNAMIC_PROGRAMMING.BASED_ON_PARTITION;
 
 
 // https://leetcode.com/problems/burst-balloons/description/
-// leetocde : 312
+// leetcode : 312
 
 public class BurstBalloons {
 
