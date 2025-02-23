c4bdccd206f0d7ae4a87589abda11657bf642a84
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-23 13:42:18 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_PARTITION/MatrixChainMultiplication.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_PARTITION/MatrixChainMultiplication.java
@@ -32,6 +32,6 @@ public class MatrixChainMultiplication {
             if (steps < minimumSteps)
                 minimumSteps = steps;
         }
-        return minimumSteps;
+        return dp[i][j] =  minimumSteps;
     }
 }
