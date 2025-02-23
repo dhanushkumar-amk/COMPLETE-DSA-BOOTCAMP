5b3ac87d55f412030cf740cfcd871e2588b8e0cf
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-23 22:46:23 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_GRIDS/CountSquareSubmatricesWithAllOnes.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_GRIDS/CountSquareSubmatricesWithAllOnes.java
@@ -4,45 +4,43 @@ public class CountSquareSubmatricesWithAllOnes {
 
     public static void main(String[] args) {
         int[][] matrix = {
-                {1,1,1},
-                {1,1,1},
-                {1,1,1},
+                {1, 1, 1},
+                {1, 1, 1},
+                {1, 1, 1},
         };
 
         System.out.println(countSquares(matrix));
     }
 
     public static int countSquares(int[][] matrix) {
-
         int n = matrix.length;
         int m = matrix[0].length;
 
         int[][] dp = new int[n][m];
+        int sum = 0;
 
-        for (int col = 0; col < m; col++)
-            dp[0][col] = matrix[0][col];
+        // Initialize dp array and calculate sum
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = matrix[i][0];
+            sum += dp[i][0];
+        }
+        for (int j = 1; j < m; j++) {
+            dp[0][j] = matrix[0][j];
+            sum += dp[0][j];
+        }
 
-        for (int row = 0; row < n; row++)
-            dp[row][0] = matrix[row][0];
-
+        // Build the dp array
         for (int i = 1; i < n; i++) {
             for (int j = 1; j < m; j++) {
-
-                if (matrix[i][j] == 0)
+                if (matrix[i][j] == 1) {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                    sum += dp[i][j];
+                } else {
                     dp[i][j] = 0;
-                else
-                    dp[i][j] = Math.min(dp[i - 1][j],
-                              Math.min(dp[i - 1][j - 1], dp[i][j - 1]));
+                }
+            }
+        }
 
-            }
-        }
-        // sum all values to get answer
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                sum += dp[i][j];
-            }
-        }
         return sum;
     }
 }
