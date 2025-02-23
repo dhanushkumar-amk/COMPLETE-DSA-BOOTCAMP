2654aee986b4823dece89cf1be289449a94d3d8c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-23 13:41:21 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_PARTITION/MatrixChainMultiplication.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_PARTITION/MatrixChainMultiplication.java
@@ -1,17 +1,22 @@
 package DYNAMIC_PROGRAMMING.BASED_ON_PARTITION;
 
+import java.util.Arrays;
+
 public class MatrixChainMultiplication {
 
     static int matrixMultiplication(int[] arr){
 
         int n = arr.length;
 
-        int[][] dp = new int[n][]
+        int[][] dp = new int[n][n];
 
-        return function(1, n - 1, arr);
+        for(int[] row:dp)
+            Arrays.fill(row,-1);
+
+        return function(1, n - 1, arr, dp);
     }
 
-    static int function(int i, int j, int[] arr){
+    static int function(int i, int j, int[] arr, int[][] dp){
 
         // base case
         if (i == j)
@@ -19,7 +24,7 @@ public class MatrixChainMultiplication {
 
         int minimumSteps = Integer.MAX_VALUE;
         for (int k = i; k < j - 1; k++) {
-            int steps = arr[i -1] * arr[k] * arr[j] + function(i, k-1, arr) + function(k , j, arr);
+            int steps = arr[i -1] * arr[k] * arr[j] + function(i, k-1, arr, dp) + function(k , j, arr,dp );
 
             if (steps < minimumSteps)
                 minimumSteps = steps;
