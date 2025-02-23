b859e266a2fe115d4c97969b36523c0d923c0235
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-23 13:33:58 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_PARTITION/MatrixChainMultiplication.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_PARTITION/MatrixChainMultiplication.java
@@ -2,5 +2,12 @@ package DYNAMIC_PROGRAMMING.BASED_ON_PARTITION;
 
 public class MatrixChainMultiplication {
 
-    
+    static int matrixMultiplication(int[] arr){
+
+        int n = arr.length;
+
+        return function(1, n - 1, arr);
+    }
+
+    static int function()
 }
