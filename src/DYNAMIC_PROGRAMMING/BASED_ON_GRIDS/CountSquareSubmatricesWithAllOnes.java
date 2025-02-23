0862cff481267aefcef6a0e4357a05918178971a
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-23 22:22:07 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_GRIDS/CountSquareSubmatricesWithAllOnes.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_GRIDS/CountSquareSubmatricesWithAllOnes.java
@@ -1,5 +1,12 @@
 package DYNAMIC_PROGRAMMING.BASED_ON_GRIDS;
 
 public class CountSquareSubmatricesWithAllOnes {
-    
+
+    public static void main(String[] args) {
+        
+    }
+
+    public int countSquares(int[][] matrix) {
+
+    }
 }
