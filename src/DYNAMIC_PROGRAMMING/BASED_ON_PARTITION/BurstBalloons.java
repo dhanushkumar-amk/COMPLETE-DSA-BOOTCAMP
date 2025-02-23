8a74cbad781a628df7994e1b3ea7f0e241bf1828
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-23 20:05:17 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_PARTITION/BurstBalloons.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_PARTITION/BurstBalloons.java
@@ -62,44 +62,36 @@ public class BurstBalloons {
 
 
 //    tabulation
-    public int maxCoins(int[] nums) {
+public int maxCoins(int[] nums) {
+    int n = nums.length;
+    int[] newArray = new int[n + 2];
 
-        int n = nums.length;
-        int[] newArray = new int[n+2];
-
-        // create the array and add the 1 on both end
-        for(int i=0; i<n; i++){
-            newArray[i+1] = nums[i];
-        }
-        newArray[0] = 1;
-        newArray[n+1] = 1;
-
-
-        int[][] dp = new int[n + 1][n + 1];
-
-
-        for (int i = n; i >= 1; i--) {
-            for (int j = 1; j <= n ; j++) {
-
-                if (i > j)
-                    continue;
-
-                // copy and paste recurance
-                int maximumCoins  = Integer.MIN_VALUE;
-
-                for (int index = i; index <= j; index++) {
-                    int coins = newArray[i - 1] * newArray[index] * newArray[j + 1]
-                            + dp[i][index - 1]
-                            + dp[index + 1][j];
-                            ;
-
-                    maximumCoins = Math.max(maximumCoins, coins);
-                }
-                dp[i][j] = maximumCoins;
-            }
-        }
-
-        return dp[1][n];
+    // create the array and add the 1 on both ends
+    for (int i = 0; i < n; i++) {
+        newArray[i + 1] = nums[i];
     }
+    newArray[0] = 1;
+    newArray[n + 1] = 1;
+
+    int[][] dp = new int[n + 2][n + 2]; // Adjusted size to n + 2
+
+    for (int len = 1; len <= n; len++) {
+        for (int i = 1; i <= n - len + 1; i++) {
+            int j = i + len - 1;
+            int maximumCoins = 0;
+
+            for (int k = i; k <= j; k++) {
+                int coins = newArray[i - 1] * newArray[k] * newArray[j + 1]
+                        + dp[i][k - 1]
+                        + dp[k + 1][j];
+                maximumCoins = Math.max(maximumCoins, coins);
+            }
+            dp[i][j] = maximumCoins;
+        }
+    }
+
+    return dp[1][n];
+}
+
 
 }
