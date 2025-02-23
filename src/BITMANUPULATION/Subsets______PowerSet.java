3d415c564967328a77a8b2fca0561188b5ee1a91
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-23 23:13:23 +0530

    modified

--- src/BITMANUPULATION/Subsets______PowerSet.java
+++ src/BITMANUPULATION/Subsets______PowerSet.java
@@ -1,11 +1,31 @@
 package BITMANUPULATION;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Subsets______PowerSet {
 
     public static void main(String[] args) {
 
     }
 
-    
+    public List<List<Integer>> subsets(int[] nums) {
+
+        List<List<Integer>> answer = new ArrayList<>();
+
+        int n = nums.length;
+        int subsets = 1 << n;
+
+        for (int num = 0; num < subsets - 1; num++) {
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < n - 1; i++) {
+                if ((num & ((1 << i)))) == true
+                        list.add(nums[i]);
+            }
+            answer.add(list);
+        }
+
+        return answer;
+    }
 
 }
