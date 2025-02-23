685d00bdc4ab9e159d36f7217f4e90c8549db8fb
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-23 22:03:57 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_PARTITION/PartitionArrayForMaximumSum.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_PARTITION/PartitionArrayForMaximumSum.java
@@ -1,5 +1,7 @@
 package DYNAMIC_PROGRAMMING.BASED_ON_PARTITION;
 
+import java.util.Arrays;
+
 public class PartitionArrayForMaximumSum {
 
     public static void main(String[] args) {
@@ -14,16 +16,23 @@ public class PartitionArrayForMaximumSum {
     public int maxSumAfterPartitioning(int[] arr, int k) {
 
         int n = arr.length;
-        return function(0, arr, k);
+
+        int[]dp = new int[n];
+        Arrays.fill(dp, -1);
+
+        return function(0, arr, k, dp);
     }
 
-    private int function(int index, int[] arr, int k){
+    private int function(int index, int[] arr, int k, int[] dp){
 
         int n = arr.length;
 
         // base case
         if (index == n)
             return 0;
+
+        if (dp[index] != -1)
+            return dp[index];
 
         int length = 0;
         int maximum = Integer.MIN_VALUE;
@@ -32,10 +41,10 @@ public class PartitionArrayForMaximumSum {
         for (int j = index; j < Math.min(n, index + k); j++) {
             length++;
             maximum = Math.max(maximum, arr[j]);
-            int sum = length * maximum + function(j + 1, arr, k);
+            int sum = length * maximum + function(j + 1, arr, k, dp);
             maximumAnswer = Math.max(maximumAnswer, sum);
         }
-        return maximumAnswer;
+        return dp[index] =  maximumAnswer;
     }
 
 }
