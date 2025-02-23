ee2c0c960dec952c8f0ac5ad3dc88cc260dfc620
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-23 19:54:13 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_PARTITION/BurstBalloons.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_PARTITION/BurstBalloons.java
@@ -4,6 +4,8 @@ package DYNAMIC_PROGRAMMING.BASED_ON_PARTITION;
 // https://leetcode.com/problems/burst-balloons/description/
 // leetcode : 312
 
+import java.util.Arrays;
+
 public class BurstBalloons {
 
     public static void main(String[] args) {
@@ -19,34 +21,43 @@ public class BurstBalloons {
         int m = nums.length;
         int[] newArray = new int[m+2];
 
+        // create the array and add the 1 on both end
         for(int i=0; i<m; i++){
             newArray[i+1] = nums[i];
         }
         newArray[0] = 1;
         newArray[m+1] = 1;
 
-        return function(1, m, newArray);
 
+        int[][] dp = new int[m + 1][m + 1];
+
+        for(int[] row : dp)
+            Arrays.fill(row, -1);
+
+        return function(1, m, newArray, dp);
     }
 
-    private int function(int i, int j, int[] arr){
+    private int function(int i, int j, int[] arr, int[][] dp){
 
         // base case
         if (i > j)
             return 0;
 
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
         int maximumCoins  = Integer.MIN_VALUE;
 
         for (int index = i; index <= j; index++) {
             int coins = arr[i - 1] * arr[index] * arr[j + 1]
-                    + function(i, index - 1, arr)
-                    + function(index + 1, j, arr)
+                    + function(i, index - 1, arr, dp)
+                    + function(index + 1, j, arr, dp)
                     ;
 
             maximumCoins = Math.max(maximumCoins, coins);
         }
 
-        return maximumCoins;
+        return dp[i][j] =  maximumCoins;
     }
 
 
