4509f560a47967e737f5556deb1c05f511659406
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-23 09:47:59 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_LIS/LongestIncreasingSubSequence.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_LIS/LongestIncreasingSubSequence.java
@@ -47,10 +47,32 @@ public class LongestIncreasingSubSequence {
     }
 
 
+    // tabuulation method
 
-    public static int tabulation(int[] nums){
+    public static int tabulation(int[] arr){
 
-        
+        int n = arr.length;
+
+        int dp[][]=new int[n+1][n+1];
+
+        for(int ind = n-1; ind>=0; ind --){
+            for (int prev_index = ind-1; prev_index >=-1; prev_index --){
+
+                int notTake = 0 + dp[ind+1][prev_index +1];
+
+                int take = 0;
+
+                if(prev_index == -1 || arr[ind] > arr[prev_index]){
+
+                    take = 1 + dp[ind+1][ind+1];
+                }
+
+                dp[ind][prev_index+1] = Math.max(notTake,take);
+
+            }
+        }
+
+        return dp[0][0];
     }
 
 }
