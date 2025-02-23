4dfa09ab6a70886d4a7ba42457a4152caec3e7da
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-23 17:58:23 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_PARTITION/MinimumCostToCutAStick.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_PARTITION/MinimumCostToCutAStick.java
@@ -4,14 +4,18 @@ package DYNAMIC_PROGRAMMING.BASED_ON_PARTITION;
 // https://leetcode.com/problems/minimum-cost-to-cut-a-stick/description/
 // leetcode 1547
 
+import java.util.ArrayList;
+
 public class MinimumCostToCutAStick {
 
-    public int minCost(int n, int[] cuts) {
+    static int cost(int n, int c, ArrayList<Integer> cuts) {
 
-        return function(n, cuts);
+        return function(n,c,cuts);
     }
 
-    int function(int n, int[] cuts){
+    int function(int n,int c, ArrayList<Integer> cuts ){
+
+        cuts.add(n);
 
     }
 
