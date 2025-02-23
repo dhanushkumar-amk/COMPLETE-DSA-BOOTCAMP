a26a4dc6f4b657b2f1a19519459e26b9fd88b89e
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-23 17:30:03 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_PARTITION/MinimumCostToCutAStick.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_PARTITION/MinimumCostToCutAStick.java
@@ -2,6 +2,6 @@ package DYNAMIC_PROGRAMMING.BASED_ON_PARTITION;
 
 
 // https://leetcode.com/problems/minimum-cost-to-cut-a-stick/description/
-// 
+// leetcode 154
 public class MinimumCostToCutAStick {
 }
