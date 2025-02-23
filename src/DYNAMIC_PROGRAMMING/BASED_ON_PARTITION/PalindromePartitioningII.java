862dd5973cd3002f836e7d92dfaa2104d7805a83
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-23 20:28:21 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_PARTITION/PalindromePartitioningII.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_PARTITION/PalindromePartitioningII.java
@@ -3,6 +3,8 @@ package DYNAMIC_PROGRAMMING.BASED_ON_PARTITION;
 // leetcode 132
 // https://leetcode.com/problems/palindrome-partitioning-ii/
 
+import java.util.Arrays;
+
 public class PalindromePartitioningII {
 
     public static void main(String[] args) {
@@ -10,8 +12,25 @@ public class PalindromePartitioningII {
     }
 
     public int minCut(String s) {
+        int n = s.length();
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+
+        return function(0, n, s);
+    }
+
+    private int function(int i, String s){
 
         int n = s.length();
-        int[] dp = new int[n];
+
+        if (i == n)
+            return 0;
+
+        String
+        for (int j = i; j < n ; j++) {
+
+        }
+
     }
 }
