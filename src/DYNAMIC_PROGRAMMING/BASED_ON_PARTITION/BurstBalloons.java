56be36de93e670ab4e01a4d28d977d487ef5f9f6
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-23 19:46:52 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_PARTITION/BurstBalloons.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_PARTITION/BurstBalloons.java
@@ -8,6 +8,16 @@ public class BurstBalloons {
 
     public int maxCoins(int[] nums) {
 
+        int m = nums.length;
+        int[] newCuts = new int[m+2];
+
+        for(int i=0; i<m; i++){
+            newCuts[i+1] = nums[i];
+        }
+        newCuts[0] = 0;
+        newCuts[m+1] = n;
+
+
     }
 
 
