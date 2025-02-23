f8afe5fac2b6c646401afeeaa7d0ab8a09962c9a
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-23 17:55:52 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_PARTITION/MinimumCostToCutAStick.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_PARTITION/MinimumCostToCutAStick.java
@@ -7,7 +7,8 @@ package DYNAMIC_PROGRAMMING.BASED_ON_PARTITION;
 public class MinimumCostToCutAStick {
 
     public int minCost(int n, int[] cuts) {
-        
+
+        return function()
     }
 
 }
