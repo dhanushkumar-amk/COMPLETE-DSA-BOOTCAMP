deb92dc23f46fe6254bd4fb3519976df6ff49589
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-24 10:29:29 +0530

    modified

--- src/STRING/ZigZagConversion.java
+++ src/STRING/ZigZagConversion.java
@@ -1,2 +1,46 @@
-package STRING;public class ZigZagConversion {
+package STRING;
+
+public class ZigZagConversion {
+
+    public static void main(String[] args) {
+        
+    }
+
+    public String convert(String s, int numRows) {
+
+        int row = numRows;
+        int col =  s.length();
+
+        char[][] matrix = new char[row][col];
+
+        int j = 0;
+        int k = 0;
+
+        while (k < s.length()){
+            for (int i = 0; i < row; i++) {
+                if (k == s.length())
+                    break;
+                matrix[i][j] = s.charAt(k);
+                k++;
+            }
+            j++;
+
+            for (int i = row - 2; i >= 1 ; i++) {
+                if (k == s.length())
+                    break;
+                matrix[i][j] = s.charAt(k);
+                j++;
+                k++;
+            }
+        }
+
+        String answer = "";
+
+        for (int i = 0; i < row; i++) {
+            for (int l = 0; l < col; l++) {
+                answer += matrix[i][l];
+            }
+        }
+        return answer;
+    }
 }
