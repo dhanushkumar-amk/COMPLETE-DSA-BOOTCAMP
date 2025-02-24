4a6012484bed005b2975f57836bc285b49579847
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-25 05:27:54 +0530

    modified

--- src/GREEDY/MinimumNumberOfMovesToSeatEveryone.java
+++ src/GREEDY/MinimumNumberOfMovesToSeatEveryone.java
@@ -1,4 +1,19 @@
 package GREEDY;
 
+import java.util.Arrays;
+
 public class MinimumNumberOfMovesToSeatEveryone {
+
+
+    public int minMovesToSeat(int[] seats, int[] students) {
+
+        Arrays.sort(seats);
+        Arrays.sort(students);
+
+        int answer = 0;
+        for (int i = 0; i < seats.length; i++) {
+            answer += Math.abs(seats[i] - students[i]);
+        }
+        return answer;
+    }
 }
