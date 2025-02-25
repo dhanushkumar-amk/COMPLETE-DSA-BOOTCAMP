f4874838ed768f350fb207971f8b18b30c6f4ac7
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-25 20:56:07 +0530

    modified

--- src/PREFIXSUM/NumberOfSubArraysWithOddSum.java
+++ src/PREFIXSUM/NumberOfSubArraysWithOddSum.java
@@ -2,6 +2,10 @@ package PREFIXSUM;
 
 public class NumberOfSubArraysWithOddSum {
     public static void main(String[] args) {
-        
+
+    }
+
+    public int numOfSubarrays(int[] arr) {
+
     }
 }
