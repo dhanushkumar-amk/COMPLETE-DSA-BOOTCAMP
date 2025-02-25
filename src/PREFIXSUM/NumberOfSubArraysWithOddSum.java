0fe173b4025fc0d9d1b9edbc9a257bc44aa627b7
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-25 21:07:16 +0530

    modified

--- src/PREFIXSUM/NumberOfSubArraysWithOddSum.java
+++ src/PREFIXSUM/NumberOfSubArraysWithOddSum.java
@@ -3,10 +3,12 @@ package PREFIXSUM;
 public class NumberOfSubArraysWithOddSum {
     public static void main(String[] args) {
 
+        int[] arr = {1,2,3,4,5,6,7};
+        System.out.println(numOfSubarrays(arr));
     }
 
     static int mod  =(int) 1e9 + 7;
-    public int numOfSubarrays(int[] arr) {
+    public static int numOfSubarrays(int[] arr) {
 
         int result = 0;
         int prefixSum = 0;
