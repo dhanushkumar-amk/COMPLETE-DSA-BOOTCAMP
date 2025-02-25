a67d31de05345b9d6fdec437328394ff716c9a75
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-25 20:59:47 +0530

    modified

--- src/PREFIXSUM/NumberOfSubArraysWithOddSum.java
+++ src/PREFIXSUM/NumberOfSubArraysWithOddSum.java
@@ -5,7 +5,27 @@ public class NumberOfSubArraysWithOddSum {
 
     }
 
+    static int mod  =(int) 1e9 + 7;
     public int numOfSubarrays(int[] arr) {
 
+        int result = 0;
+        int prefixSum = 0;
+        int oddCount = 0;
+        int evenCount = 0;
+
+        for(int num : arr){
+
+            prefixSum += num;
+
+            if (prefixSum % 2 == 0){
+                result = (result + oddCount) % mod;
+                evenCount++;
+            }
+            else{
+                result = (result + evenCount) % mod;
+                oddCount++;
+            }
+        }
+    return result;
     }
 }
