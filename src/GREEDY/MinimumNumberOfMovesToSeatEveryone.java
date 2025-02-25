874c8782f7feb522386180d9a0a733eb33967ba7
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-25 05:31:35 +0530

    modified

--- src/GREEDY/MinimumNumberOfMovesToSeatEveryone.java
+++ src/GREEDY/MinimumNumberOfMovesToSeatEveryone.java
@@ -5,10 +5,13 @@ import java.util.Arrays;
 public class MinimumNumberOfMovesToSeatEveryone {
 
     public static void main(String[] args) {
-        
+        int[] seats = {3,1,5};
+        int[] students = {2,7,4};
+
+        System.out.println(minMovesToSeat(seats, students));
     }
 
-    public int minMovesToSeat(int[] seats, int[] students) {
+    public static int minMovesToSeat(int[] seats, int[] students) {
 
         Arrays.sort(seats);
         Arrays.sort(students);
