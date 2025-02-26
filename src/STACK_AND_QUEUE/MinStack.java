33ef14d5e6e821d05445b3fac270c02dff9c8cd8
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-27 04:40:16 +0530

    modified

--- src/STACK_AND_QUEUE/MinStack.java
+++ src/STACK_AND_QUEUE/MinStack.java
@@ -4,5 +4,36 @@ package STACK_AND_QUEUE;
 // https://leetcode.com/problems/min-stack/
 
 
+import java.util.Stack;
+
 public class MinStack {
+
+
+    Stack<Integer> orginalStack;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        orginalStack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        orginalStack.push(val);
+
+        if (minStack.isEmpty() || val <= minStack.peek())
+            minStack.push(val);
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+
+    }
+
+    public int getMin() {
+
+    }
+
 }
