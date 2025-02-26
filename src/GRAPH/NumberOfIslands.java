dc553313eb61a0069d304321389d28c476e933c4
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-27 05:16:36 +0530

    modified

--- src/GRAPH/NumberOfIslands.java
+++ src/GRAPH/NumberOfIslands.java
@@ -19,7 +19,7 @@ public class NumberOfIslands {
 
         for (int i = 0; i < n ; i++) {
             for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1) {
+                if (grid[i][j] == '1') {
                     dfs(i, j, grid);
                     count++;
                 }
@@ -30,8 +30,16 @@ public class NumberOfIslands {
 
 
     // dfs function
-    private  int dfs(int i, int j, int[][] grid){
+    private  void dfs(int i, int j, char[][] grid){
+
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0')
+            return;
+
+        grid[i][j] = '0'; // mark the cell as visited
+
         
+
+
     }
 
 }
