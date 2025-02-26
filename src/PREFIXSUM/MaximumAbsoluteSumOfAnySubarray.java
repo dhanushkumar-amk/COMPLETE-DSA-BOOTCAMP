4735720b8f9875bb2b91e4dff9a2dd41ec22961c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-02-26 18:53:19 +0530

    modified

--- src/PREFIXSUM/MaximumAbsoluteSumOfAnySubarray.java
+++ src/PREFIXSUM/MaximumAbsoluteSumOfAnySubarray.java
@@ -1,8 +1,22 @@
 package PREFIXSUM;
 
 // leetcode :
-
+//
 
 public class MaximumAbsoluteSumOfAnySubarray {
 
+    public int maxAbsoluteSum(int[] nums){
+
+        int minSum = 0;
+        int maxSum = 0;
+        int currentSum = 0;
+        int maximumAbsoluteSum = 0;
+
+        for(int num : nums){
+
+            currentSum += num;
+
+            maximumAbsoluteSum = Math.max(maximumAbsoluteSum, Math.max(Math.abs(currentSum - minSum), Math.abs(currentSum - maxSum)))
+        }
+    }
 }
