4342066a9b504e58975e3b687c8fd8adf1fb8677
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-01 17:25:45 +0530

    modified

--- src/ARRAYS/ApplyOperationsToAnArray.java
+++ src/ARRAYS/ApplyOperationsToAnArray.java
@@ -2,6 +2,8 @@ package ARRAYS;
 
 
 // leetcode 2640
+// https://leetcode.com/problems/apply-operations-to-an-array/description/?envType=daily-question&envId=2025-03-01
+
 
 public class ApplyOperationsToAnArray {
 }
