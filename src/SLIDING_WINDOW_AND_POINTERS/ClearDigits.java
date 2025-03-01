64de05b62fa71841a7753e4c04daaf8bb961694d
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-01 18:00:04 +0530

    modified

--- src/SLIDING_WINDOW_AND_POINTERS/ClearDigits.java
+++ src/SLIDING_WINDOW_AND_POINTERS/ClearDigits.java
@@ -3,9 +3,23 @@ package SLIDING_WINDOW_AND_POINTERS;
 // leetcode 3174;
 // https://leetcode.com/problems/clear-digits/description/
 
+import java.util.Stack;
+
 public class ClearDigits {
 
     public String clearDigits(String s) {
-
+        Stack<Character> characterStack = new Stack<>();
+        for (char character : s.toCharArray()) {
+            if (Character.isDigit(character)) {
+                characterStack.pop();
+            } else {
+                characterStack.push(character);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!characterStack.isEmpty()) {
+            sb.append(characterStack.pop());
+        }
+        return sb.reverse().toString();
     }
 }
