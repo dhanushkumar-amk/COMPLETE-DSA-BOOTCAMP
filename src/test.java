9918999b7632917a647fff762da8084c8909932a
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-02 10:58:37 +0530

    modified

--- src/test.java
+++ src/test.java
@@ -1,9 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class test {
 
     public static void main(String[] args) {
 
 
         int number = 10;
+        int number2 = 20;
+        int number3 = 30;
+        int number4 = 40;
         String myName = "dhanabal";
         char letter = 'a';
         float mark = 99.5f;
@@ -12,6 +18,9 @@ public class test {
         System.out.println(myName);
         System.out.println(letter);
         System.out.println(mark);
+
+        int[] arr = {0, 20, 30,40};
+        System.out.println(Arrays.toString(arr));
     }
 
 }
