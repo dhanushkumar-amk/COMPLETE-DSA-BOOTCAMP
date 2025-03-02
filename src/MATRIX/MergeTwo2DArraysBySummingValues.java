bf3c6338f6618f24ca06807165b89400b2be17dd
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-02 15:06:43 +0530

    modified

--- src/MATRIX/MergeTwo2DArraysBySummingValues.java
+++ src/MATRIX/MergeTwo2DArraysBySummingValues.java
@@ -5,6 +5,11 @@ package MATRIX;
 
 public class MergeTwo2DArraysBySummingValues {
 
+    public static void main(String[] args) {
+        MergeTwo2DArraysBySummingValues answer = new MergeTwo2DArraysBySummingValues();
+    }
 
-    
+    public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+
+    }
 }
