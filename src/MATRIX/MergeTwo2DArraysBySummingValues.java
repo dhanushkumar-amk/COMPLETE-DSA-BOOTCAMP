021490a80ebe58fa1b9df0558e6ecc978414a6ea
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-02 15:17:34 +0530

    modified

--- src/MATRIX/MergeTwo2DArraysBySummingValues.java
+++ src/MATRIX/MergeTwo2DArraysBySummingValues.java
@@ -3,6 +3,8 @@ package MATRIX;
 //leetcode 2570
 // https://leetcode.com/problems/merge-two-2d-arrays-by-summing-values/description/?envType=daily-question&envId=2025-03-02
 
+import java.util.ArrayList;
+
 public class MergeTwo2DArraysBySummingValues {
 
     public static void main(String[] args) {
@@ -32,20 +34,29 @@ public class MergeTwo2DArraysBySummingValues {
     }
 
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-
-        int n = nums1.length;
-        int m = nums2.length;
-
-        int[][] answer = new int[n][m];
-
-        int i = 0;
-        int j = 0;
-
-        while(i < n && j < m){
-            if (nums1[i][0] == nums2[j][0]){
-                    
+        int n1=nums1.length,n2=nums2.length,i=0,j=0;
+        ArrayList<int []> ans=new ArrayList();
+        while(i<n1&& j<n2){
+            if(nums1[i][0]==nums2[j][0]){
+                ans.add(new int[]{nums1[i][0],nums1[i][1]+nums2[j][1]});
+                i++;
+                j++;
+            }
+            else if(nums1[i][0]<nums2[j][0]){
+                ans.add(nums1[i]);
+                i++;
+            }
+            else{
+                ans.add(nums2[j]);
+                j++;
             }
         }
-
+        while(i<n1){
+            ans.add(nums1[i++]);
+        }
+        while(j<n2){
+            ans.add(nums2[j++]);
+        }
+        return ans.toArray(new int[ans.size()][]);
     }
 }
