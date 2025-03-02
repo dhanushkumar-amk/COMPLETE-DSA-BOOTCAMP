08e15546b9ed4a258f6e02b658ae0405e811380b
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-03 04:40:08 +0530

    modified

--- src/GREEDY/GasStation.java
+++ src/GREEDY/GasStation.java
@@ -6,8 +6,10 @@ package GREEDY;
 public class GasStation {
 
     public static void main(String[] args) {
-        int[] gas = {1,2,3,4,5}l;
-        int[] cost = 
+        int[] gas = {1,2,3,4,5};
+        int[] cost = {3,4,5,1,2};
+
+        System.out.println(canCompleteCircuit(gas, cost));
     }
 
     public static int canCompleteCircuit(int[] gas, int[] cost) {
