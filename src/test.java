5f62a8fb2e6df34c9ee4c19ea3d2547d83348fa6
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-02 11:00:20 +0530

    modified

--- src/test.java
+++ src/test.java
@@ -5,22 +5,12 @@ public class test {
 
     public static void main(String[] args) {
 
+        int n = 20;
 
-        int number = 10;
-        int number2 = 20;
-        int number3 = 30;
-        int number4 = 40;
-        String myName = "dhanabal";
-        char letter = 'a';
-        float mark = 99.5f;
-
-        System.out.println(number);
-        System.out.println(myName);
-        System.out.println(letter);
-        System.out.println(mark);
-
-        int[] arr = {0, 20, 30,40};
-        System.out.println(Arrays.toString(arr));
+        if (n % 2 == 0)
+            System.out.println("even");
+        else
+            System.out.println("odd");
     }
 
 }
