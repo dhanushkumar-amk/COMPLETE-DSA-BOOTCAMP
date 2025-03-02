fbd888c0a8c68631545410c4d4d34bec08221f93
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-02 11:04:55 +0530

    modified

--- src/test.java
+++ src/test.java
@@ -1,17 +1,7 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-
-public class test {
+public class test{
 
     public static void main(String[] args) {
 
-
-        int a = 20;
-        int b = 30;
-        int c = 40;
-
-    int answer = Math.max(a, Math.max(b, c));
-        System.out.println(answer);
-
+        System.out.println(20);
     }
 }
