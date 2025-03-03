1847b9cf1d2575ac8a68971bc1b22e64287f568f
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-03 19:45:19 +0530

    modified

--- src/SLIDING_WINDOW_AND_POINTERS/PartitionArrayAccordingToGivenPivot.java
+++ src/SLIDING_WINDOW_AND_POINTERS/PartitionArrayAccordingToGivenPivot.java
@@ -7,7 +7,32 @@ package SLIDING_WINDOW_AND_POINTERS;
 public class PartitionArrayAccordingToGivenPivot {
 
     public int[] pivotArray(int[] nums, int pivot) {
-        
+
+        int n = nums.length;
+
+        int[]  result = new int[n];
+
+        int left = 0;
+        int right = n - 1;
+
+        for (int i = 0, j = n - 1; i < n; i++, j--) {
+
+            if (nums[i] < pivot) {
+                result[left] = nums[i];
+                left++;
+            }
+
+            if (nums[j] > pivot) {
+                result[right] = nums[i];
+                right--;
+            }
+        }
+            while (left <= right){
+                result[left] = pivot;
+                left++;
+        }
+
+            return result;
     }
 
 
