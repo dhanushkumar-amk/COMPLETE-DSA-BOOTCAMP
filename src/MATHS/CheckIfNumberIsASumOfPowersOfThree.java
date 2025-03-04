29aa2100e8ace37bcb076aae2c6932a46e339cf4
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-04 20:23:25 +0530

    modified

--- src/MATHS/CheckIfNumberIsASumOfPowersOfThree.java
+++ src/MATHS/CheckIfNumberIsASumOfPowersOfThree.java
@@ -1,8 +1,13 @@
 package MATHS;
 
+// https://leetcode.com/problems/check-if-number-is-a-sum-of-powers-of-three/description/?envType=daily-question&envId=2025-03-04
+// leetcode 1780
+
 public class CheckIfNumberIsASumOfPowersOfThree {
 
+    public static void main(String[] args) {
+        
+    }
 
-    
 
 }
