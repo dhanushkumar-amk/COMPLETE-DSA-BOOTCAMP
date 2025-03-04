ed7f93e78cce539497a1ab0c2b5614489d1c89ed
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-04 20:24:09 +0530

    modified

--- src/MATHS/CheckIfNumberIsASumOfPowersOfThree.java
+++ src/MATHS/CheckIfNumberIsASumOfPowersOfThree.java
@@ -6,8 +6,18 @@ package MATHS;
 public class CheckIfNumberIsASumOfPowersOfThree {
 
     public static void main(String[] args) {
-        
+        int n = 91;
+        System.out.println(checkPowersOfThree(n));
     }
 
+    public static boolean checkPowersOfThree(int n) {
+        while (n > 0) {
+            if (n % 3 == 2) {
+                return false;
+            }
+            n /= 3;
+        }
+        return true;
+    }
 
 }
