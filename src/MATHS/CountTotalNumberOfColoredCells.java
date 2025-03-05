c20b50d373acc2dc121b5a1ea9219eb47d685664
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-05 11:05:39 +0530

    modified

--- src/MATHS/CountTotalNumberOfColoredCells.java
+++ src/MATHS/CountTotalNumberOfColoredCells.java
@@ -3,4 +3,20 @@ package MATHS;
 public class CountTotalNumberOfColoredCells {
 
 
+    public long coloredCells(int n) {
+
+        long answer = 1;
+        long value = 4;
+
+        if (n == 1)
+            return 1;
+
+        for (int i = 2; i <= n ; i++) {
+            answer += value;
+            value = 4L * i;
+        }
+
+        return answer;
+    }
+
 }
