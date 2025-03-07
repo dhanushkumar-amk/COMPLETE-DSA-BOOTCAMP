5be3e0f284a316f98c6b2f24b9f7ff9cb4fb3933
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-07 18:55:11 +0530

    modified

--- src/MATHS/ClosestPrimeNumbersInRange.java
+++ src/MATHS/ClosestPrimeNumbersInRange.java
@@ -4,4 +4,8 @@ package MATHS;
 // https://leetcode.com/problems/closest-prime-numbers-in-range/description/?envType=daily-question&envId=2025-03-07
 
 public class ClosestPrimeNumbersInRange {
+
+    public int[] closestPrimes(int left, int right) {
+
+    }
 }
