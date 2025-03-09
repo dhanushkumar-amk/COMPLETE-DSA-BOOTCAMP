f07b94e4914c214c69a466ebbd873e870ea1f0aa
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-09 21:30:04 +0530

    modified

--- src/SLIDING_WINDOW_AND_POINTERS/AlternatingGroupsII.java
+++ src/SLIDING_WINDOW_AND_POINTERS/AlternatingGroupsII.java
@@ -7,6 +7,20 @@ public class AlternatingGroupsII {
 
     public int numberOfAlternatingGroups(int[] colors, int k) {
 
+        int windowSize = 1;
+        int answer = 0;
+        int n = colors.length;
+
+        for (int i = 1; i <= n - 1 + k - 1; i++) {
+            if (colors[i] != colors[(i - 1 + n) % n])
+                windowSize++;
+            else
+                windowSize = 1;
+
+            if (windowSize >= k)
+                answer++;
+        }
+        return answer;
     }
 
 }
