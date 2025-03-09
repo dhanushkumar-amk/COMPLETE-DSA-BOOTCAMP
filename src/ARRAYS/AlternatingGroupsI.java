7baccd98c449b41d2e6075f4c331f99fb1a38abb
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-09 21:16:30 +0530

    modified

--- src/ARRAYS/AlternatingGroupsI.java
+++ src/ARRAYS/AlternatingGroupsI.java
@@ -7,5 +7,17 @@ package ARRAYS;
 
 public class AlternatingGroupsI {
 
+    public int numberOfAlternatingGroups(int[] colors) {
+
+        int n = colors.length;
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            if (colors[(i + 1) % n] != colors[i] && colors[i] != colors[(i - 1 + n) % n])
+                answer++;
+        }
+        return answer;
+    }
 
 }
