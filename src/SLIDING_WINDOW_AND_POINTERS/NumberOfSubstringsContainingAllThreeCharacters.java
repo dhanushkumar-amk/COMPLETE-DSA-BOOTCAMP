079ad48a9a1ebdcf3a744d056ea3d7d74d990209
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-11 20:28:05 +0530

    modified

--- src/SLIDING_WINDOW_AND_POINTERS/NumberOfSubstringsContainingAllThreeCharacters.java
+++ src/SLIDING_WINDOW_AND_POINTERS/NumberOfSubstringsContainingAllThreeCharacters.java
@@ -8,9 +8,21 @@ public class NumberOfSubstringsContainingAllThreeCharacters {
     public int numberOfSubstrings(String s) {
 
         int left = 0;
-        int right =0;
-        ibt valid
+        int right = 0;
+        int validSubStringCount = 0;
 
+        int[] charFrequency = new int[3];
+
+        while (right < s.length()){
+            charFrequency[s.charAt(right) - 'a']++;
+
+            while (charFrequency[0] && charFrequency[1] && charFrequency[2]){
+                validSubStringCount += s.length() - right;
+                charFrequency[s.charAt(left++) - 'a']--;
+            }
+            right++;
+        }
+        return validSubStringCount;
     }
 
 }
