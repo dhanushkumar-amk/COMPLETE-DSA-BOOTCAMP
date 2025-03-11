39d2608c9b2de1aeb2c19d5193359d58480996d6
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-11 20:19:22 +0530

    modified

--- src/SLIDING_WINDOW_AND_POINTERS/NumberOfSubstringsContainingAllThreeCharacters.java
+++ src/SLIDING_WINDOW_AND_POINTERS/NumberOfSubstringsContainingAllThreeCharacters.java
@@ -1,6 +1,9 @@
 package SLIDING_WINDOW_AND_POINTERS;
 
+// leetocde 1358
+// https://leetcode.com/problems/number-of-substrings-containing-all-three-characters/description/?envType=daily-question&envId=2025-03-11
+
 public class NumberOfSubstringsContainingAllThreeCharacters {
 
-    
+
 }
