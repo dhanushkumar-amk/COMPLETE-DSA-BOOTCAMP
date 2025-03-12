bdf64d3ea2a2130910fe2d69410c4def499cf273
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-12 20:52:37 +0530

    modified

--- src/SEARCHING/MaximumCountOfPositiveIntegerAndNegativeInteger.java
+++ src/SEARCHING/MaximumCountOfPositiveIntegerAndNegativeInteger.java
@@ -10,5 +10,20 @@ public class MaximumCountOfPositiveIntegerAndNegativeInteger {
 
     }
 
+    public int maximumCount(int[] nums) {
+        int positiveCount = 0;
+        int negativeCount = 0;
 
+        for (int i = 0; i < nums.length ; i++) {
+
+            if (nums[i] > 0)
+                positiveCount++;
+            else if(nums[i] < 0)
+                negativeCount++;
+            else
+                continue;
+        }
+
+        return Math.max(positiveCount, negativeCount);
+    }
 }
