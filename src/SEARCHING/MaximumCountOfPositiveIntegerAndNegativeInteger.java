278926d61ff1f7b6b12af6d8b1b4ceca93e9b044
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-12 21:02:27 +0530

    modified

--- src/SEARCHING/MaximumCountOfPositiveIntegerAndNegativeInteger.java
+++ src/SEARCHING/MaximumCountOfPositiveIntegerAndNegativeInteger.java
@@ -14,20 +14,64 @@ public class MaximumCountOfPositiveIntegerAndNegativeInteger {
         System.out.println(answer.maximumCount(nums));
     }
 
-    public int maximumCount(int[] nums) {
-        int positiveCount = 0;
-        int negativeCount = 0;
+//    public int maximumCount(int[] nums) {
+//        int positiveCount = 0;
+//        int negativeCount = 0;
+//
+//        for (int i = 0; i < nums.length ; i++) {
+//
+//            if (nums[i] > 0)
+//                positiveCount++;
+//            else if(nums[i] < 0)
+//                negativeCount++;
+//            else
+//                continue;
+//        }
+//
+//        return Math.max(positiveCount, negativeCount);
+//    }
 
-        for (int i = 0; i < nums.length ; i++) {
+    // efficient way
+    public int maximumCount(int[] nums){
 
-            if (nums[i] > 0)
-                positiveCount++;
-            else if(nums[i] < 0)
-                negativeCount++;
-            else
-                continue;
-        }
+        int n = nums.length;
 
-        return Math.max(positiveCount, negativeCount);
+        int lowerBound = lower(nums, 0);
+        int upperBound = upper(nums, 0);
+
+        return Math.max(upperBound, lowerBound);
     }
+
+    private int upper(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+
+        while (left < right){
+            int mid = left + right / 2;
+
+            if (nums[mid] <= target)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
+    }
+
+    private int lower(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+
+        while (left < right){
+            int mid = left + right / 2;
+
+            if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
+    }
+
+
+
 }
