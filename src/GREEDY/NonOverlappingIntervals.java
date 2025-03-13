071d939118a107668cca6ebaaf467c27777f7836
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-13 18:43:24 +0530

    modified

--- src/GREEDY/NonOverlappingIntervals.java
+++ src/GREEDY/NonOverlappingIntervals.java
@@ -4,9 +4,40 @@ package GREEDY;
 // leetcode : 435
 // https://leetcode.com/problems/non-overlapping-intervals/description/
 
+import java.util.*;
+
 public class NonOverlappingIntervals {
+
+    public static void main(String[] args) {
+        int[][] intervals = {
+                {1,2},
+                {2,3},
+                {3,4},
+                {1,3},
+        };
+
+        NonOverlappingIntervals answer = new NonOverlappingIntervals();
+        System.out.println(answer.eraseOverlapIntervals(intervals));
+    }
 
     public int eraseOverlapIntervals(int[][] intervals) {
 
+        // base case
+        if (intervals.length == 0)
+            return 0;
+
+        // sort the intervals based on en endTime
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[1]));
+
+        int count = 1;
+        int previousInterval = 0;
+
+        for (int i = 1; i < intervals.length; i++) {
+
+            if (intervals[i][0] >= intervals[previousInterval][1]) {
+                previousInterval = i;
+                count++;
+            }
+        }
     }
 }
