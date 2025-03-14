a4614b0ad18581574ce38e154b63aaa9f133eb21
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-14 20:28:26 +0530

    modified

--- src/SEARCHING/MaximumCandiesAllocatedToKChildren.java
+++ src/SEARCHING/MaximumCandiesAllocatedToKChildren.java
@@ -2,6 +2,12 @@ package SEARCHING;
 
 public class MaximumCandiesAllocatedToKChildren {
 
+    public static void main(String[] args) {
+        MaximumCandiesAllocatedToKChildren answer = new MaximumCandiesAllocatedToKChildren();
+
+        
+    }
+
     public int maximumCandies(int[] candies, long k) {
 
     }
