f24acdc60e0c2c5eed87f51cbf742f8360d94c58
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-16 20:18:12 +0530

    modified

--- src/SEARCHING/MinimumTimeToRepairCars.java
+++ src/SEARCHING/MinimumTimeToRepairCars.java
@@ -1,7 +1,9 @@
 package SEARCHING;
 
 // leetcode: 2594
+// https://leetcode.com/problems/minimum-time-to-repair-cars/description/?envType=daily-question&envId=2025-03-16
 
 
 public class MinimumTimeToRepairCars {
+    
 }
