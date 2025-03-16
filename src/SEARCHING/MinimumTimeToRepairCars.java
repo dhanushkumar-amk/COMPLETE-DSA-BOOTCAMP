1ca6e0d0ecb4956e7bb173dbd86c1f6f80f5ccf6
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-16 20:52:55 +0530

    modified

--- src/SEARCHING/MinimumTimeToRepairCars.java
+++ src/SEARCHING/MinimumTimeToRepairCars.java
@@ -4,6 +4,9 @@ package SEARCHING;
 // https://leetcode.com/problems/minimum-time-to-repair-cars/description/?envType=daily-question&envId=2025-03-16
 
 
+import java.util.Arrays;
+import java.util.Map;
+
 public class MinimumTimeToRepairCars {
 
     public static void main(String[] args) {
@@ -19,5 +22,31 @@ public class MinimumTimeToRepairCars {
 
     public long repairCars(int[] ranks, int cars) {
 
+        long n = ranks.length;
+        long low = 1; // 1 rank and 1 car;
+        long high = (long)Arrays.stream(ranks).min().getAsInt() * cars * cars;
+
+        long answer = 0;
+
+        while (low <= high){
+            long mid = low + (high - low) / 2;
+            if (canAssign(mid, ranks, cars)){
+                answer = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return answer;
+    }
+
+    private boolean canAssign(long mid, int[] ranks, int cars) {
+        long count = 0;
+
+        for (long rank : ranks)
+            count += (long) Math.sqrt(mid / rank);
+
+        return count >= cars;
     }
 }
