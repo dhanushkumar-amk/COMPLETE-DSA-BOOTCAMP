9f964649f58efc4209e64c46f7fe6d8e9039234d
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-17 18:18:09 +0530

    modified

--- src/ARRAYS/sortt.java
+++ src/ARRAYS/sortt.java
@@ -1,2 +1,15 @@
-package ARRAYS;public class sortt {
+package ARRAYS;
+
+public class sortt {
+
+    public boolean divideArray(int[] nums) {
+
+        int n = nums.length;
+
+        for (int i = 1; i <= n ; i++) {
+            if (nums[i] != nums[i - 1])
+                return false;
+        }
+        return true;
+    }
 }
