1640eefc92210c11a8805cee7f0d55221f5b9ceb
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-18 04:43:52 +0530

    modified

--- src/STACK_AND_QUEUE/ImplementStackUsingQueues.java
+++ src/STACK_AND_QUEUE/ImplementStackUsingQueues.java
@@ -10,29 +10,33 @@ import java.util.Queue;
 
 public class ImplementStackUsingQueues {
 
-      private Queue<Integer> queue;
+}
 
-      public ImplementStackUsingQueues() {
-         queue = new LinkedList<>();
-     }
+class MyStack {
 
+    private Queue<Integer> queue;
+
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
 
     public void push(int x) {
+        queue.add(x);
+        for (int i = 1; i < queue.size() ; i++) {
+            queue.add(queue.remove());  // remove the element and added to the back of the queue by n - 1 times
+        }
 
     }
 
     public int pop() {
-
+        return queue.remove();
     }
 
     public int top() {
-
+        return queue.peek();
     }
 
     public boolean empty() {
-
+        return queue.isEmpty();
     }
-
 }
-
-class M
