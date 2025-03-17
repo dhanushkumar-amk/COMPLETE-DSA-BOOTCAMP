7887ac2472f8173ccc82e969cca06f54794fd5d4
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-18 04:24:36 +0530

    modified

--- src/STACK_AND_QUEUE/ImplementStackUsingQueues.java
+++ src/STACK_AND_QUEUE/ImplementStackUsingQueues.java
@@ -4,6 +4,26 @@ package STACK_AND_QUEUE;
 //https://leetcode.com/problems/implement-stack-using-queues/description/
 
 public class ImplementStackUsingQueues {
-    
+
+
+    public MyStack() {
+
+    }
+
+    public void push(int x) {
+
+    }
+
+    public int pop() {
+
+    }
+
+    public int top() {
+
+    }
+
+    public boolean empty() {
+
+    }
 
 }
