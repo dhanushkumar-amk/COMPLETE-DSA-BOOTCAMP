d9a22337232e621d717006fc4bd352da41f8a775
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-18 20:03:08 +0530

    modified

--- src/BITMANUPULATION/LongestNicSubArray.java
+++ src/BITMANUPULATION/LongestNicSubArray.java
@@ -1,2 +1,7 @@
-package BITMANUPULATION;public class LongestNicSubArray {
+package BITMANUPULATION;
+
+// https://leetcode.com/problems/longest-nice-subarray/description/?envType=daily-question&envId=2025-03-18
+// leetcode : 2401
+
+public class LongestNicSubArray {
 }
