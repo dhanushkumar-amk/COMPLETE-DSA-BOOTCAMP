3855192a0a6dcea5fcb40924d85b6b478d7ae154
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-18 20:23:16 +0530

    modified

--- src/BITMANUPULATION/LongestNicSubArray.java
+++ src/BITMANUPULATION/LongestNicSubArray.java
@@ -4,4 +4,31 @@ package BITMANUPULATION;
 // leetcode : 2401
 
 public class LongestNicSubArray {
+
+    public int longestNiceSubarray(int[] nums) {
+
+        int n = nums.length;
+        int left = 0;
+        int right = 0;
+
+        int maxWindowSize = 0;
+        int currentSum = 0;
+        int xorSum = 0;
+
+        while (right < n){
+
+            currentSum += nums[right];
+            xorSum ^= nums[right];
+
+            while (xorSum != currentSum){
+                currentSum -= nums[left];
+                xorSum ^= nums[left];
+                left++;
+            }
+            maxWindowSize = Math.max(maxWindowSize, right - left + 1);
+            right++;
+        }
+        return maxWindowSize;
+    }
+
 }
