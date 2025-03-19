ece44533968a6f45c23200e9ce22654db67333fd
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-19 19:51:20 +0530

    modified

--- src/BITMANUPULATION/MinimumOperationsToMakeBinaryArrayElementsEqualToOneI.java
+++ src/BITMANUPULATION/MinimumOperationsToMakeBinaryArrayElementsEqualToOneI.java
@@ -1,7 +1,7 @@
 package BITMANUPULATION;
 
 
-// leetcode : 3121
+// leetcode : 3191
 // https://leetcode.com/problems/minimum-operations-to-make-binary-array-elements-equal-to-one-i/description/?envType=daily-question&envId=2025-03-19
 
 public class MinimumOperationsToMakeBinaryArrayElementsEqualToOneI {
