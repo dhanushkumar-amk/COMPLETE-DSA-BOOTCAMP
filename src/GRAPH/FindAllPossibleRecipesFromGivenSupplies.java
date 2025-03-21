61a3dbb2516a34bb8c81ae1e077d714b88e82696
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-21 17:52:18 +0530

    modified

--- src/GRAPH/FindAllPossibleRecipesFromGivenSupplies.java
+++ src/GRAPH/FindAllPossibleRecipesFromGivenSupplies.java
@@ -3,7 +3,11 @@ package GRAPH;
 // leetcode : 2115
 // https://leetcode.com/problems/find-all-possible-recipes-from-given-supplies/description/?envType=daily-question&envId=2025-03-21
 
+import java.util.List;
+
 public class FindAllPossibleRecipesFromGivenSupplies {
 
-    
+    public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
+
+    }
 }
