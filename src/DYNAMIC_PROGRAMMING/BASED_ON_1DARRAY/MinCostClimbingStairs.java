597b3c62acfc2c3d4bd4a38c918f4731d3f2c982
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-23 12:09:27 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_1DARRAY/MinCostClimbingStairs.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_1DARRAY/MinCostClimbingStairs.java
@@ -1,7 +1,8 @@
 package DYNAMIC_PROGRAMMING.BASED_ON_1DARRAY;
 
 
-// leetcode :7746
+// leetcode :746
+// https://leetcode.com/problems/min-cost-climbing-stairs/description/
 
 
 public class MinCostClimbingStairs {
