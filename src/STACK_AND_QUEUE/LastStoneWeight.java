b70b553417ad0b8ebf2bac9bcd0c08bd1a56f840
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-23 12:46:03 +0530

    modified

--- src/STACK_AND_QUEUE/LastStoneWeight.java
+++ src/STACK_AND_QUEUE/LastStoneWeight.java
@@ -5,6 +5,10 @@ package STACK_AND_QUEUE;
 // https://leetcode.com/problems/last-stone-weight/description/
 
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
 public class LastStoneWeight {
 
     public static void main(String[] args) {
@@ -15,5 +19,25 @@ public class LastStoneWeight {
 
     public int lastStoneWeight(int[] stones) {
 
+        // step1 init the priority queue stores all the element in reverse order
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+
+        // step 2 add all elements to a priority queue
+        for (int stone : stones)
+            priorityQueue.add(stone);
+
+        // step 3 get the largest element  and take difference and also add it to the queue
+        while (priorityQueue.size() > 1){
+
+            int firstGreatest = priorityQueue.poll();
+            int secondGreatest = priorityQueue.poll();
+
+            // not equal means take difference and add it to the queue otherwise it is equal then simply ignore it
+            if (firstGreatest != secondGreatest) {
+                int difference = firstGreatest - secondGreatest;
+                priorityQueue.add(difference);
+            }
+        }
+        return priorityQueue.isEmpty() ? 0 : priorityQueue.poll();
     }
 }
