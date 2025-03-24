b368dacb9a84e7b61fa754b1e8499483fb18b311
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-24 20:08:00 +0530

    modified

--- src/GREEDY/CountDaysWithoutMeeting.java
+++ src/GREEDY/CountDaysWithoutMeeting.java
@@ -1,52 +1,44 @@
 package GREEDY;
 
-// https://leetcode.com/problems/count-days-without-meetings/description/?envType=daily-question&envId=2025-03-24
-// leetcode 3169
-
 import java.util.Arrays;
-import java.util.Collections;
 
 public class CountDaysWithoutMeeting {
-
     public static void main(String[] args) {
         int[][] meetings = {
                 {5, 7},
                 {1, 3},
-                {9, 10},
+                {9, 10}
         };
 
         int days = 10;
         CountDaysWithoutMeeting answer = new CountDaysWithoutMeeting();
 
-        System.out.println(answer.countDays(days, meetings));
-
-
+        System.out.println(answer.countDays(days, meetings)); // Expected Output: 4
     }
 
     public int countDays(int days, int[][] meetings) {
-        int n = meetings.length;
-        int answer = 0;
+        // Sort meetings by start day
+        Arrays.sort(meetings, (a, b) -> Integer.compare(a[0], b[0]));
 
-        Collections.sort(meetings);
+        int freeDays = 0;
+        int last = 0;
 
-        int last = 1;
-        for (int i = 0; i < n; i++) {
-            int start = meetings[i][0];
-            int end = meetings[i][1];
+        for (int[] meeting : meetings) {
+            int start = meeting[0];
+            int end = meeting[1];
 
-            if (start > last){
-                if (i == 0)
-                    answer += (start - last);
-                else
-                    answer += (start - last - 1);
+            // Count free days between last meeting and current meeting
+            if (start > last + 1) {
+                freeDays += (start - last - 1);
             }
             last = Math.max(last, end);
         }
 
-        if (days > last)
-            answer += (days - last);
+        // Count free days after the last meeting
+        if (days > last) {
+            freeDays += (days - last);
+        }
 
-
-        return answer;
+        return freeDays;
     }
 }
