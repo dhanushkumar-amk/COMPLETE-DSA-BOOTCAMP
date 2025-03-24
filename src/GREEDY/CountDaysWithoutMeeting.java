32a9878298a42769a0514d51849edcb57f53596d
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-24 19:45:35 +0530

    modified

--- src/GREEDY/CountDaysWithoutMeeting.java
+++ src/GREEDY/CountDaysWithoutMeeting.java
@@ -1,5 +1,4 @@
-package ARRAYS;
-
+package GREEDY;
 
 // https://leetcode.com/problems/count-days-without-meetings/description/?envType=daily-question&envId=2025-03-24
 // leetcode 3169
@@ -13,11 +12,14 @@ public class CountDaysWithoutMeeting {
                 {9, 10},
         };
 
-        
+
 
     }
 
     public int countDays(int days, int[][] meetings) {
-
+        
+        return -1;
     }
+
+
 }
