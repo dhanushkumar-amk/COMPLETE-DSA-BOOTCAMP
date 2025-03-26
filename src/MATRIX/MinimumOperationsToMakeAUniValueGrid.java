509acd27e179d3179542874c7348babfbb735f38
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-26 16:08:06 +0530

    modified

--- src/MATRIX/MinimumOperationsToMakeAUniValueGrid.java
+++ src/MATRIX/MinimumOperationsToMakeAUniValueGrid.java
@@ -1,7 +1,7 @@
 package MATRIX;
 
 // leetcode 2033
-// 
+// https://leetcode.com/problems/minimum-operations-to-make-a-uni-value-grid/description/?envType=daily-question&envId=2025-03-26
 
 public class MinimumOperationsToMakeAUniValueGrid {
 
