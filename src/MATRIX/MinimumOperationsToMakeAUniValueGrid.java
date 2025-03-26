15da5eaf8f4c03eeb3496ba9852681df810a3796
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-26 16:27:11 +0530

    modified

--- src/MATRIX/MinimumOperationsToMakeAUniValueGrid.java
+++ src/MATRIX/MinimumOperationsToMakeAUniValueGrid.java
@@ -3,6 +3,9 @@ package MATRIX;
 // leetcode 2033
 // https://leetcode.com/problems/minimum-operations-to-make-a-uni-value-grid/description/?envType=daily-question&envId=2025-03-26
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MinimumOperationsToMakeAUniValueGrid {
 
 
@@ -12,7 +15,8 @@ public class MinimumOperationsToMakeAUniValueGrid {
 
     public int minOperations(int[][] grid, int x) {
 
-        int[] 
+        List<Integer> oneDArray = new ArrayList<>();
+        
 
     }
 
