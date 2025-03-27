19839569c95b9413934cae2a28ef92a9daa1b5d2
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-27 19:41:53 +0530

    modified

--- src/ARRAYS/MinimumIndexOfAValidSplit.java
+++ src/ARRAYS/MinimumIndexOfAValidSplit.java
@@ -13,6 +13,21 @@ public class MinimumIndexOfAValidSplit {
     }
 
 
-    
+    private int mooresVotingAlgo(List<Integer> nums) {
+        int majority_element = nums.get(0);
+        int freq = 1;
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums.get(i) != majority_element) {
+                freq--;
+            } else {
+                freq++;
+            }
+            if (freq == 0) {
+                majority_element = nums.get(i);
+                freq = 1;
+            }
+        }
+        return majority_element;
+    }
 
 }
