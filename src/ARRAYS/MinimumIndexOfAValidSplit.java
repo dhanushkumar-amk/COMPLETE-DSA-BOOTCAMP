9aa119db10b37740d7fe02d4a6c7cb5f331975e9
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-27 19:42:55 +0530

    modified

--- src/ARRAYS/MinimumIndexOfAValidSplit.java
+++ src/ARRAYS/MinimumIndexOfAValidSplit.java
@@ -7,10 +7,35 @@ import java.util.*;
 
 public class MinimumIndexOfAValidSplit {
 
+    public static void main(String[] args) {
+        
+    }
+
     public int minimumIndex(List<Integer> nums) {
         int n = nums.size();
         // Step-1: Find Majority element
         int majority_element = mooresVotingAlgo(nums);
+
+        // Step-2: Count frequency of majority_element
+        int max_freq = 0;
+        for (int num : nums) {
+            if (num == majority_element) {
+                max_freq++;
+            }
+        }
+
+        // Step-3: Find minimum valid split
+        int prefix_count = 0;
+        for (int i = 0; i < n - 1; ++i) {
+            if (nums.get(i) == majority_element) {
+                prefix_count++;
+                max_freq--;
+            }
+            if ((prefix_count > (i + 1) / 2) && (max_freq > (n - i - 1) / 2)) {
+                return i;
+            }
+        }
+        return -1;
 
     }
 
