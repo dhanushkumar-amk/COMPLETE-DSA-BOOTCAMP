02282bb1cdf9dc684cae1a3fc3959468e2333768
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-27 19:18:29 +0530

    modified

--- src/ARRAYS/MinimumIndexOfAValidSplit.java
+++ src/ARRAYS/MinimumIndexOfAValidSplit.java
@@ -3,8 +3,12 @@ package ARRAYS;
 // leetcode : 2780
 // https://leetcode.com/problems/minimum-index-of-a-valid-split/description/?envType=daily-question&envId=2025-03-27
 
+import java.util.*;
+
 public class MinimumIndexOfAValidSplit {
 
-    
+    public int minimumIndex(List<Integer> nums) {
+
+    }
 
 }
