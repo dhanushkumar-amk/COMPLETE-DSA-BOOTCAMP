119243b6b76ca40fa6fb62717af3d178e9a66039
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-28 16:34:45 +0530

    modified

--- src/GRAPH/MaximumNumberOfPointsFromGridQueries.java
+++ src/GRAPH/MaximumNumberOfPointsFromGridQueries.java
@@ -3,6 +3,8 @@ package GRAPH;
 // leetcode : 2503
 // https://leetcode.com/problems/maximum-number-of-points-from-grid-queries/description/?envType=daily-question&envId=2025-03-28
 
+import java.util.*;
+
 public class MaximumNumberOfPointsFromGridQueries {
 
     static class Pair {
@@ -29,8 +31,17 @@ public class MaximumNumberOfPointsFromGridQueries {
 
     public int[] maxPoints(int[][] grid, int[] queries) {
 
+        // step 1 create the treeset and add the queries it removes the duplicates and gives the sorted form
+        Set<Integer> sortedQueries = new TreeSet<>();
+        for(int query : queries)
+            sortedQueries.add(query);
 
+        Map<Integer, Integer> queryCount = new HashMap<>();
 
+        // create the minheap that compares the values in an grid
+        PriorityQueue<Triple> minHeap = new PriorityQueue<>((a, b) -> a.first - b.first);
+
+        
     }
 
 }
