c43e5b2082195572ff1a1267b8873fa02dad7fae
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-03-28 16:07:12 +0530

    modified

--- src/GRAPH/MaximumNumberOfPointsFromGridQueries.java
+++ src/GRAPH/MaximumNumberOfPointsFromGridQueries.java
@@ -4,4 +4,9 @@ package GRAPH;
 // https://leetcode.com/problems/maximum-number-of-points-from-grid-queries/description/?envType=daily-question&envId=2025-03-28
 
 public class MaximumNumberOfPointsFromGridQueries {
+
+    public int[] maxPoints(int[][] grid, int[] queries) {
+
+    }
+
 }
