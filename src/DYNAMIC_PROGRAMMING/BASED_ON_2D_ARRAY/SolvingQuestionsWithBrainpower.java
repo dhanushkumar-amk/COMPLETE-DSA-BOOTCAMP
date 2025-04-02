141981431742ae756d42e635107abd26cd8d019c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-02 15:51:54 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_2D_ARRAY/SolvingQuestionsWithBrainpower.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_2D_ARRAY/SolvingQuestionsWithBrainpower.java
@@ -1,8 +1,9 @@
 package DYNAMIC_PROGRAMMING.BASED_ON_2D_ARRAY;
 
-
+//leetcode : 2140
+// https://leetcode.com/problems/solving-questions-with-brainpower/description/?envType=daily-question&envId=2025-04-01
 
 public class SolvingQuestionsWithBrainpower {
 
-
+    `
 }
