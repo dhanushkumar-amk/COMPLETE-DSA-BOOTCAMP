672cd071636f14add14500a9bddee92340a52831
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-02 16:24:08 +0530

    modified

--- src/ARRAYS/MaximumValueOfAnOrderedTripletI.java
+++ src/ARRAYS/MaximumValueOfAnOrderedTripletI.java
@@ -1,7 +1,8 @@
 package ARRAYS;
 
 
-// leetcode 28
+// leetcode 2873
+// https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-i/?envType=daily-question&envId=2025-04-02
 
 public class MaximumValueOfAnOrderedTripletI {
 
