574606e87a78cb9bc98948cdb8b92fd7bdbc6256
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-02 16:45:30 +0530

    modified

--- src/ARRAYS/MaximumValueOfAnOrderedTripletI.java
+++ src/ARRAYS/MaximumValueOfAnOrderedTripletI.java
@@ -1,13 +1,23 @@
 package ARRAYS;
 
 
-// leetcode 2873
+// leetcode 2873 && 2874 both has same answer
 // https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-i/?envType=daily-question&envId=2025-04-02
 
 public class MaximumValueOfAnOrderedTripletI {
 
-    public long maximumTripletValue(int[] nums){
+    public static void main(String[] args) {
         
+    }
+
+    public long maximumTripletValue(int[] nums) {
+        long maxTriplet = 0, maxElement = 0, maxDiff = 0;
+        for (int num : nums) {
+            maxTriplet = Math.max(maxTriplet, maxDiff * num);
+            maxDiff = Math.max(maxDiff, maxElement - num);
+            maxElement = Math.max(maxElement, num);
+        }
+        return maxTriplet;
     }
 
 }
