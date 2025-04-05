d16f584f3ffc8ca8dd322419f7496dd488058a68
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-05 20:54:27 +0530

    modified

--- src/BITMANUPULATION/SumOfAllSubsetXORTotals.java
+++ src/BITMANUPULATION/SumOfAllSubsetXORTotals.java
@@ -1,2 +1,8 @@
-package BITMANUPULATION;public class SumOfAllSubsetXORTotals {
+package BITMANUPULATION;
+
+// leetcode 1863
+// https://leetcode.com/problems/sum-of-all-subset-xor-totals/description/?envType=daily-question&envId=2025-04-05
+
+public class SumOfAllSubsetXORTotals {
+
 }
