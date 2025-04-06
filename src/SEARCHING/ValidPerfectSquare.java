60daee4d988a332cb6f5d8aebf4052504ebbf5c8
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-06 15:40:02 +0530

    modified

--- src/SEARCHING/ValidPerfectSquare.java
+++ src/SEARCHING/ValidPerfectSquare.java
@@ -2,6 +2,29 @@ package SEARCHING;
 
 public class ValidPerfectSquare {
 
-    
+    public boolean isPerfectSquare(int num){
+        if (num < 0)
+            return false;
+
+        if (num == 1 || num == 0)
+            return true;
+
+        long left = 1;
+        long right = num;
+
+        while (left < right){
+
+            long mid = left + (right - left)/2;
+            long square = mid * mid;
+
+            if (square == mid)
+                return true;
+            else if (square < num)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return false;
+    }
 
 }
