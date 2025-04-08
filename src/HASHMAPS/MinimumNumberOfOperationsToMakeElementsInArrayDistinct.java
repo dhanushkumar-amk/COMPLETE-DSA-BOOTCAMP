6148569655a9bcd549cd396b4ebeccc509dee2f7
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-08 15:22:10 +0530

    modified

--- src/HASHMAPS/MinimumNumberOfOperationsToMakeElementsInArrayDistinct.java
+++ src/HASHMAPS/MinimumNumberOfOperationsToMakeElementsInArrayDistinct.java
@@ -8,13 +8,42 @@ public class MinimumNumberOfOperationsToMakeElementsInArrayDistinct {
     public static void main(String[] args) {
 
         MinimumNumberOfOperationsToMakeElementsInArrayDistinct answer = new MinimumNumberOfOperationsToMakeElementsInArrayDistinct();
-        
+        int[] nums = {1,2,3,4,2,3,3,5,7};
+
+        System.out.println(answer.minimumOperations(nums));
 
     }
 
 
     public int minimumOperations(int[] nums) {
 
+        int n = nums.length;
+        // step 1 create the custom hash map size 101
+        int[] hash = new int[101];
+        for (int num : nums)
+            hash[num]++;
+
+        //step 2 do operations
+        int answer = 0;
+        int index = 0;
+        while (!uniqueElement(hash)){
+            int count = 3;  // remove three elements maximum
+
+            while( index <= 10 && count-- > 0){
+                hash[nums[index]]--;
+                index++;
+            }
+            answer++;
+        }
+
+        return answer;
+    }
+
+    private boolean uniqueElement(int[] hash) {
+        for (int i = 0; i < hash.length; i++)
+            if (hash[i] > 1)
+                return false;
+        return true;
     }
 
 }
