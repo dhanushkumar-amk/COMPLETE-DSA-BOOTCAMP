55a0ad363111dd36cadc9b259b16d87aa90b5e8c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-08 15:15:02 +0530

    modified

--- src/HASHMAPS/MinimumNumberOfOperationsToMakeElementsInArrayDistinct.java
+++ src/HASHMAPS/MinimumNumberOfOperationsToMakeElementsInArrayDistinct.java
@@ -1,7 +1,7 @@
 package HASHMAPS;
 
 // leetcode 3396
-// 
+// https://leetcode.com/problems/minimum-number-of-operations-to-make-elements-in-array-distinct/description/?envType=daily-question&envId=2025-04-08
 
 public class MinimumNumberOfOperationsToMakeElementsInArrayDistinct {
 
