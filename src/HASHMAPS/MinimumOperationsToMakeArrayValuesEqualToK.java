224835b5493be9eae53851a6befb88095a6f05d0
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-09 23:09:38 +0530

    modified

--- src/HASHMAPS/MinimumOperationsToMakeArrayValuesEqualToK.java
+++ src/HASHMAPS/MinimumOperationsToMakeArrayValuesEqualToK.java
@@ -1,2 +1,17 @@
-package HASHMAPS;public class MinimumOperationsToMakeArrayValuesEqualToK {
+package HASHMAPS;
+
+// leetcode : 3375
+// https://leetcode.com/problems/minimum-operations-to-make-array-values-equal-to-k/description/?envType=daily-question&envId=2025-04-09
+
+public class MinimumOperationsToMakeArrayValuesEqualToK {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public int minOperations(int[] nums, int k) {
+
+    }
+
 }
