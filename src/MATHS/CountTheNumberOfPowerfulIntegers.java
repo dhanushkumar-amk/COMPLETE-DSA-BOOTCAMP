d3254a3c448ddb5f29f5ad919b79f7ec7c69bb31
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-12 13:59:35 +0530

    modified

--- src/MATHS/CountTheNumberOfPowerfulIntegers.java
+++ src/MATHS/CountTheNumberOfPowerfulIntegers.java
@@ -5,4 +5,13 @@ package MATHS;
 // https://leetcode.com/problems/count-the-number-of-powerful-integers/description/?envType=daily-question&envId=2025-04-10
 
 public class CountTheNumberOfPowerfulIntegers {
+
+    public static void main(String[] args) {
+        long start = 
+    }
+
+    public long numberOfPowerfulInt(long start, long finish, int limit, String s) {
+
+    }
+
 }
