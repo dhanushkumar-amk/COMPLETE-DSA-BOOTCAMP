d0e2c8ab4c1eb5675f89a40589c17deb5148a147
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-12 13:59:13 +0530

    modified

--- src/MATHS/CountTheNumberOfPowerfulIntegers.java
+++ src/MATHS/CountTheNumberOfPowerfulIntegers.java
@@ -1,4 +1,8 @@
 package MATHS;
 
+
+// leetcode 2999
+// https://leetcode.com/problems/count-the-number-of-powerful-integers/description/?envType=daily-question&envId=2025-04-10
+
 public class CountTheNumberOfPowerfulIntegers {
 }
