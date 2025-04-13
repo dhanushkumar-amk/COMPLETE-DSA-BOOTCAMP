6fb3b2c500dd0a63d349eba11d305b056d1e6fd1
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-13 22:50:36 +0530

    modified

--- src/RECURSION/CountGoodNumbers.java
+++ src/RECURSION/CountGoodNumbers.java
@@ -17,9 +17,14 @@ public class CountGoodNumbers {
     }
 
     private long binaryExponentiation(int base, long exp) {
-
-        
-
+        long res = 1;
+        while (exp > 0) {
+            if ((exp & 1) == 1)
+                res = (res * base) % MOD;
+            base = (base * base) % MOD;
+            exp >>= 1;
+        }
+        return res;
     }
 
 }
