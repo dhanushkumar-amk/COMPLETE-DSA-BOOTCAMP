dc4e30dc53ae61268beafa99dd64c5f94ade9ef0
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-13 22:49:39 +0530

    modified

--- src/RECURSION/CountGoodNumbers.java
+++ src/RECURSION/CountGoodNumbers.java
@@ -11,11 +11,15 @@ public class CountGoodNumbers {
         System.out.println(answer.countGoodNumbers(4));
     }
 
+    final int MOD = 1000000007;
     public int countGoodNumbers(long n) {
         return (int)((binaryExponentiation(4, n / 2) * binaryExponentiation(5, n - n / 2)) % MOD);
     }
 
     private long binaryExponentiation(int base, long exp) {
+
+        
+
     }
 
 }
