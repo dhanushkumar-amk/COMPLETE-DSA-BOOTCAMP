be7e2d7d0cf832bd284a82c57dac212b4a736c51
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-13 22:46:45 +0530

    modified

--- src/RECURSION/CountGoodNumbers.java
+++ src/RECURSION/CountGoodNumbers.java
@@ -1,2 +1,11 @@
-package RECURSION;public class CountGoodNumbers {
+package RECURSION;
+
+
+// leetcode 1922
+// https://leetcode.com/problems/count-good-numbers/?envType=daily-question&envId=2025-04-13
+
+public class CountGoodNumbers {
+
+    
+
 }
