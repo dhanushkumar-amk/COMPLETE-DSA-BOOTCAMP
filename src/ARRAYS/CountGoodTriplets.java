b85392c75cbb495e5f9cd584f2fdc5789d958f18
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-14 22:21:29 +0530

    modified

--- src/ARRAYS/CountGoodTriplets.java
+++ src/ARRAYS/CountGoodTriplets.java
@@ -5,4 +5,19 @@ package ARRAYS;
 // https://leetcode.com/problems/count-good-triplets/description/?envType=daily-question&envId=2025-04-14
 
 public class CountGoodTriplets {
+
+
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int n = arr.length;
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (Math.abs())
+                }
+            }
+        }
+
+    }
 }
