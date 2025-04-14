75592b684cfca16de594d74851c0923f31631301
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-14 22:20:36 +0530

    modified

--- src/ARRAYS/CountGoodTriplets.java
+++ src/ARRAYS/CountGoodTriplets.java
@@ -1,4 +1,8 @@
 package ARRAYS;
 
+
+// leetcode 1534
+// https://leetcode.com/problems/count-good-triplets/description/?envType=daily-question&envId=2025-04-14
+
 public class CountGoodTriplets {
 }
