26043416b67c622700c85f76cc8ea94e1b6ce1c8
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-14 22:23:05 +0530

    modified

--- src/ARRAYS/CountGoodTriplets.java
+++ src/ARRAYS/CountGoodTriplets.java
@@ -14,10 +14,15 @@ public class CountGoodTriplets {
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 for (int k = j + 1; k < n; k++) {
-                    if (Math.abs())
+                    if (
+                            Math.abs(arr[i] - arr[j]) <= a
+                          && Math.abs(arr[j] - arr[k]) <= b
+                          && Math.abs(arr[i] - arr[k]) <= c
+                    )
+                        answer++;
                 }
             }
         }
-
+    return answer;
     }
 }
