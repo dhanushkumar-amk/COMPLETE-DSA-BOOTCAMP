70f67a280f6b22f6413a79cae170c90f6fc8312d
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-16 21:41:04 +0530

    modified

--- src/SLIDING_WINDOW_AND_POINTERS/CountTheNumberOfGoodSubarrays.java
+++ src/SLIDING_WINDOW_AND_POINTERS/CountTheNumberOfGoodSubarrays.java
@@ -1,5 +1,11 @@
 package SLIDING_WINDOW_AND_POINTERS;
 
+
+//leetcode 2573
+// https://leetcode.com/problems/count-the-number-of-good-subarrays/description/?envType=daily-question&envId=2025-04-16
+
 public class CountTheNumberOfGoodSubarrays {
+
     
+
 }
