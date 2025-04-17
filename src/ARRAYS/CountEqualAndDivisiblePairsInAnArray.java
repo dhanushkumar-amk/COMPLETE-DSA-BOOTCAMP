80c0bf2b366fbdeb744522154314fe5e491c2ae3
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-17 20:05:34 +0530

    modified

--- src/ARRAYS/CountEqualAndDivisiblePairsInAnArray.java
+++ src/ARRAYS/CountEqualAndDivisiblePairsInAnArray.java
@@ -3,6 +3,18 @@ package ARRAYS;
 public class CountEqualAndDivisiblePairsInAnArray {
 
 
-    
+    public int countPairs(int[] nums, int k) {
+
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+
+                if (nums[i] == nums[j] && nums[i] * nums[j] % k == 0)
+                    count++;
+            }
+        }
+        return count;
+    }
 
 }
