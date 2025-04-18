a6c7567d65f6ffc6cbdd6eec78f77a3aa251284b
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-18 21:15:21 +0530

    modified

--- src/STRING/CountAndSay.java
+++ src/STRING/CountAndSay.java
@@ -16,8 +16,29 @@ public class CountAndSay {
 
         String s = "1";
         for (int i = 1; i < n; i++) {
-            s = RE
+            s = RLE(s);
         }
+        return  s;
+    }
 
+    private String RLE(String s) {
+        char ch = s.charAt(0);
+        int count = 1;
+        int i = 1;
+        int n = s.length();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while (i < n){
+            if (s.charAt(i) == ch)
+                count++;
+            else{
+                stringBuilder.append(count).append(ch);
+                ch = s.charAt(i);
+                count = 1;
+            }
+            i++;
+        }
+        stringBuilder.append(count).append(ch);
+        return stringBuilder.toString();
     }
 }
