cf97817d540f3c6f2fd3623ad3ecc1e8d1f86547
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-19 21:48:32 +0530

    modified

--- src/SLIDING_WINDOW_AND_POINTERS/CountTheNumberOfFairPairs.java
+++ src/SLIDING_WINDOW_AND_POINTERS/CountTheNumberOfFairPairs.java
@@ -1,4 +1,18 @@
 package SLIDING_WINDOW_AND_POINTERS;
 
 public class CountTheNumberOfFairPairs {
+
+
+    public long countFairPairs(int[] nums, int lower, int upper) {
+
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j <nums.length ; j++) {
+                if (nums[i] + nums[j] >)
+            }
+        }
+
+    }
+
 }
