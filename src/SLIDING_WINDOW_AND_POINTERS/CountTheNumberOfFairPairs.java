b65a9c7bfb01b67ee05fa0a2512973c727e20174
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-19 22:00:21 +0530

    modified

--- src/SLIDING_WINDOW_AND_POINTERS/CountTheNumberOfFairPairs.java
+++ src/SLIDING_WINDOW_AND_POINTERS/CountTheNumberOfFairPairs.java
@@ -10,11 +10,13 @@ public class CountTheNumberOfFairPairs {
 
     public static void main(String[] args) {
 
+        CountTheNumberOfFairPairs answer = new CountTheNumberOfFairPairs();
+
         int[] nums = {0,1,7,4,4,5};
         int lower = 3;
         int upper = 6;
-        
 
+        System.out.println(answer.countFairPairs(nums, lower, upper));
     }
 
     public long countFairPairs(int[] nums, int lower, int upper){
@@ -26,7 +28,22 @@ public class CountTheNumberOfFairPairs {
     }
 
     private long helper(int[] nums, int target){
+        int left = 0;
+        int right = nums.length - 1;
 
+        long answer = 0;;
+
+        while (left < right){
+            int sum = nums[left] + nums[right];
+            if (sum < target){
+                answer += (right - left);
+                left++;
+            }else{
+                right--;
+            }
+
+        }
+        return answer;
     }
 
     /*
