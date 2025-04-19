df175ca3d0b7863892ddece16b5221c682af804e
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-19 21:56:38 +0530

    modified

--- src/SLIDING_WINDOW_AND_POINTERS/CountTheNumberOfFairPairs.java
+++ src/SLIDING_WINDOW_AND_POINTERS/CountTheNumberOfFairPairs.java
@@ -4,16 +4,26 @@ package SLIDING_WINDOW_AND_POINTERS;
 // https://leetcode.com/problems/count-the-number-of-fair-pairs/submissions/?envType=daily-question&envId=2025-04-19
 // leetcode  2563;
 
+import java.util.Arrays;
+
 public class CountTheNumberOfFairPairs {
 
     public static void main(String[] args) {
 
         int[] nums = {0,1,7,4,4,5};
-        
+
     }
 
     public long countFairPairs(int[] nums, int lower, int upper){
 
+        Arrays.sort(nums);
+
+        return helper(nums, upper + 1) - helper(nums, lower);
+
+    }
+
+    private  int helper(int[] nums, int target){
+        
     }
 
     /*
