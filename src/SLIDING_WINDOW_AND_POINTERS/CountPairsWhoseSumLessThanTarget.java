7a7e285c6ac0b1d30c94ae967ba317dd97c733b2
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-19 21:40:20 +0530

    modified

--- src/SLIDING_WINDOW_AND_POINTERS/CountPairsWhoseSumLessThanTarget.java
+++ src/SLIDING_WINDOW_AND_POINTERS/CountPairsWhoseSumLessThanTarget.java
@@ -1,9 +1,13 @@
 package SLIDING_WINDOW_AND_POINTERS;
 
+// leetcode 2824
+// https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target/description/
+
+
 public class CountPairsWhoseSumLessThanTarget {
     public static void main(String[] args) {
 
     }
 
-    
+
 }
