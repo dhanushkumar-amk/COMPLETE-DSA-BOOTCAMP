a3655c8cd83f1ba0dd4625fe365ebc6e2ecdbd18
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-19 21:41:45 +0530

    modified

--- src/SLIDING_WINDOW_AND_POINTERS/CountPairsWhoseSumLessThanTarget.java
+++ src/SLIDING_WINDOW_AND_POINTERS/CountPairsWhoseSumLessThanTarget.java
@@ -4,8 +4,22 @@ package SLIDING_WINDOW_AND_POINTERS;
 // https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target/description/
 
 
+import java.util.List;
+
 public class CountPairsWhoseSumLessThanTarget {
     public static void main(String[] args) {
+
+    }
+
+    public int countPairs(List<Integer> nums, int target) {
+
+        int sum = 0;
+
+        for (int i = 0; i < nums.size();i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums.get(i) + nums.get(j) <)
+            }
+        }
 
     }
 
