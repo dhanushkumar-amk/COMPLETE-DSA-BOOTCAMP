fa5a24e1d885bdbecceb2d0968eddcf648305e8a
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-20 20:21:05 +0530

    modified

--- src/HASHMAPS/RabbitsInForest.java
+++ src/HASHMAPS/RabbitsInForest.java
@@ -5,13 +5,27 @@ package HASHMAPS;
 
 public class RabbitsInForest {
     public static void main(String[] args) {
-     int[] answers = {};
+     int[] answers = {10,10,10};
      RabbitsInForest answer = new RabbitsInForest();
         System.out.println(answer.numRabbits(answers));
     }
 
     public int numRabbits(int[] answers) {
 
+        // create the hash and find the frequency
+        int[] hash = new int[1001];
+        for(int answer: answers)
+            hash[answer]++;
+
+        int ans = 0;
+        for (int i = 0; i < 1001; i++) {
+            if (hash[i] == 0)
+                continue;
+            else
+                ans += Math.ceil( (double) hash[i]/ (i + 1)) * (i + 1);
+        }
+
+        return ans;
     }
 
 }
