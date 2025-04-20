d0702847cb4e0f6bbdd036282e8dba8f9595c16c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-20 20:16:16 +0530

    modified

--- src/HASHMAPS/RabbitsInForest.java
+++ src/HASHMAPS/RabbitsInForest.java
@@ -1,7 +1,17 @@
 package HASHMAPS;
 
 // https://leetcode.com/problems/rabbits-in-forest/description/?envType=daily-question&envId=2025-04-20
-// leetcode 781 
+// leetcode 781
 
 public class RabbitsInForest {
+    public static void main(String[] args) {
+     int[] answers = {};
+     RabbitsInForest answer = new RabbitsInForest();
+        System.out.println(answer.numRabbits(answers));
+    }
+
+    public int numRabbits(int[] answers) {
+
+    }
+
 }
