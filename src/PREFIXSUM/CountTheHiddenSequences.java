f906992451f55496346c7dddc0840119c8552665
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-21 17:51:26 +0530

    modified

--- src/PREFIXSUM/CountTheHiddenSequences.java
+++ src/PREFIXSUM/CountTheHiddenSequences.java
@@ -1,7 +1,8 @@
 package PREFIXSUM;
 
 
-
+// https://leetcode.com/problems/count-the-hidden-sequences/description/?envType=daily-question&envId=2025-04-21
+// leetcode : 2145
 
 public class CountTheHiddenSequences {
 
