0bc6bcb36c204f0167a65ad7d299aa1ddd87221f
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-21 17:51:53 +0530

    modified

--- src/PREFIXSUM/CountTheHiddenSequences.java
+++ src/PREFIXSUM/CountTheHiddenSequences.java
@@ -6,5 +6,13 @@ package PREFIXSUM;
 
 public class CountTheHiddenSequences {
 
+    public static void main(String[] args) {
+        CountTheHiddenSequences answer = new CountTheHiddenSequences();
+        
+    }
+
+    public int numberOfArrays(int[] differences, int lower, int upper) {
+
+    }
 
 }
