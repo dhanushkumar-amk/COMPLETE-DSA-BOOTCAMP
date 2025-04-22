af169bda9336927b9f72b069cb3a16c8e6b976fd
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-22 18:35:21 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_MATHS/CountTheNumberOfIdealArrays.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_MATHS/CountTheNumberOfIdealArrays.java
@@ -1,2 +1,19 @@
-package DYNAMIC_PROGRAMMING.BASED_ON_MATHS;public class CountTheNumberOfIdealArrays {
+package DYNAMIC_PROGRAMMING.BASED_ON_MATHS;
+
+//
+// leetcode : 2338
+
+
+public class CountTheNumberOfIdealArrays {
+
+    public static void main(String[] args) {
+        CountTheNumberOfIdealArrays answer = new CountTheNumberOfIdealArrays();
+        int n = 2;
+        int maxValue = 5;
+        System.out.println();
+    }
+
+
+    
+
 }
