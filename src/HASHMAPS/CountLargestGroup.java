a12826a6555592406afb6903290498f3b1605824
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-23 20:47:08 +0530

    modified

--- src/HASHMAPS/CountLargestGroup.java
+++ src/HASHMAPS/CountLargestGroup.java
@@ -1,10 +1,15 @@
 package HASHMAPS;
 
-
 // leetcode 1399
 // https://leetcode.com/problems/count-largest-group/description/?envType=daily-question&envId=2025-04-23
 
 public class CountLargestGroup {
+    public static void main(String[] args) {
+        CountLargestGroup answer = new CountLargestGroup();
+        System.out.println(answer.countLargestGroup(13));
+    }
 
+    public int countLargestGroup(int n) {
 
+    }
 }
