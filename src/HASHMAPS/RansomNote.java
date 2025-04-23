87d7b8c2e33a20cd71de0adf4b848462ed30ab6d
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-24 05:16:54 +0530

    modified

--- src/HASHMAPS/RansomNote.java
+++ src/HASHMAPS/RansomNote.java
@@ -7,7 +7,8 @@ package HASHMAPS;
 public class RansomNote {
 
     public static void main(String[] args) {
-        
+        RansomNote answer = new RansomNote();
+        String ransomNote = ""
     }
 
     public boolean canConstruct(String ransomNote, String magazine) {
