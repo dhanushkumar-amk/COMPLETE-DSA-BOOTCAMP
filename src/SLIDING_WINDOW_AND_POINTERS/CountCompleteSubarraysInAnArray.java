190081269259e7a9c942acb4825ac14789cb0c63
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-24 21:04:02 +0530

    modified

--- src/SLIDING_WINDOW_AND_POINTERS/CountCompleteSubarraysInAnArray.java
+++ src/SLIDING_WINDOW_AND_POINTERS/CountCompleteSubarraysInAnArray.java
@@ -1,5 +1,9 @@
 package SLIDING_WINDOW_AND_POINTERS;
 
+
+// leetcode 2799
+// https://leetcode.com/problems/count-complete-subarrays-in-an-array/description/?envType=daily-question&envId=2025-04-24
+
 public class CountCompleteSubarraysInAnArray {
 
     public int countCompleteSubarrays(int[] nums) {
