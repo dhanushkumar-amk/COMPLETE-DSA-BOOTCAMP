a7a665d19fcd0462969965b5a57833fe4452c725
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-24 21:40:35 +0530

    modified

--- src/SLIDING_WINDOW_AND_POINTERS/CountCompleteSubarraysInAnArray.java
+++ src/SLIDING_WINDOW_AND_POINTERS/CountCompleteSubarraysInAnArray.java
@@ -4,6 +4,11 @@ package SLIDING_WINDOW_AND_POINTERS;
 // leetcode 2799
 // https://leetcode.com/problems/count-complete-subarrays-in-an-array/description/?envType=daily-question&envId=2025-04-24
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class CountCompleteSubarraysInAnArray {
 
     public static void main(String[] args) {
@@ -14,5 +19,28 @@ public class CountCompleteSubarraysInAnArray {
 
     public int countCompleteSubarrays(int[] nums) {
 
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums)
+            set.add(num);
+        int total = set.size();
+
+        int i = 0;
+        int j = 0;
+        int answer = 0;
+        int n = nums.length;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        while (j < n){
+            map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
+
+            while(map.size() == total){
+                answer += n - j;
+                map.put(nums[i], map.get(nums[i]) - 1);
+                if (map.get(nums[i]) == 0)
+                    map.remove(nums[i]);
+            }
+            j++;
+        }
+        return answer;
     }
 }
