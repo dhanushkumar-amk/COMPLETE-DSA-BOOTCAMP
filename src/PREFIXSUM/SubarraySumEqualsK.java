282ff88dcf022285d96a98318b3ca41e73a08c82
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-25 19:14:14 +0530

    modified

--- src/PREFIXSUM/SubarraySumEqualsK.java
+++ src/PREFIXSUM/SubarraySumEqualsK.java
@@ -1,4 +1,12 @@
 package PREFIXSUM;
 
 public class SubarraySumEqualsK {
+
+    public static void main(String[] args) {
+        
+    }
+
+    public int subarraySum(int[] nums, int k) {
+
+    }
 }
