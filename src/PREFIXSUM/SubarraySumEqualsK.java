95489f0d504ada62cfab30eaae0efaa69ce53b90
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-25 19:14:59 +0530

    modified

--- src/PREFIXSUM/SubarraySumEqualsK.java
+++ src/PREFIXSUM/SubarraySumEqualsK.java
@@ -5,10 +5,12 @@ public class SubarraySumEqualsK {
     public static void main(String[] args) {
         SubarraySumEqualsK ans = new SubarraySumEqualsK();
         int[] nums = {1,1,1};
-        
+        int k = 2;
+
+        System.out.println(ans.subarraySum(nums, k));
     }
 
     public int subarraySum(int[] nums, int k) {
-
+        
     }
 }
