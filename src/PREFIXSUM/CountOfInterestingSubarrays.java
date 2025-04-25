e408e04fe4e9dc69546e37c5efce2ff49e86dec5
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-25 18:41:44 +0530

    modified

--- src/PREFIXSUM/CountOfInterestingSubarrays.java
+++ src/PREFIXSUM/CountOfInterestingSubarrays.java
@@ -1,4 +1,20 @@
 package PREFIXSUM;
 
+// https://leetcode.com/problems/count-of-interesting-subarrays/description/?envType=daily-question&envId=2025-04-25
+// leetcode 2845
+
+import  java.util.*;
+
 public class CountOfInterestingSubarrays {
+
+    public static void main(String[] args) {
+        CountOfInterestingSubarrays answer = new CountOfInterestingSubarrays();
+        List<Integer> nums = new ArrayList<>();
+        
+    }
+
+    public long countInterestingSubarrays(List<Integer> nums, int modulo, int k) {
+
+    }
+
 }
