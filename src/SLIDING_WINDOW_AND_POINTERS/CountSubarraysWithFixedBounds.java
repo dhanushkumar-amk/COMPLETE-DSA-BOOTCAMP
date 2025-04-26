3af98030bbcccbe763528904824667f82c6f5760
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-26 19:19:51 +0530

    modified

--- src/SLIDING_WINDOW_AND_POINTERS/CountSubarraysWithFixedBounds.java
+++ src/SLIDING_WINDOW_AND_POINTERS/CountSubarraysWithFixedBounds.java
@@ -1,4 +1,7 @@
 package SLIDING_WINDOW_AND_POINTERS;
 
+// leetcode 2444
+// https://leetcode.com/problems/count-subarrays-with-fixed-bounds/description/?envType=daily-question&envId=2025-04-26
+
 public class CountSubarraysWithFixedBounds {
 }
