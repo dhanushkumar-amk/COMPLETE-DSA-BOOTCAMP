30215630f799776410a5b3da171485efee3e047c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-26 19:21:25 +0530

    modified

--- src/SLIDING_WINDOW_AND_POINTERS/CountSubarraysWithFixedBounds.java
+++ src/SLIDING_WINDOW_AND_POINTERS/CountSubarraysWithFixedBounds.java
@@ -4,15 +4,14 @@ package SLIDING_WINDOW_AND_POINTERS;
 // https://leetcode.com/problems/count-subarrays-with-fixed-bounds/description/?envType=daily-question&envId=2025-04-26
 
 public class CountSubarraysWithFixedBounds {
-
-
     public static void main(String[] args) {
+        CountSubarraysWithFixedBounds answer = new CountSubarraysWithFixedBounds();
         int[] nums = {1,3,5,2,7,5};
-        int minK = 2;
-
+        int minK = 1;
+        int maxK = 5;
+        System.out.println(answer.countSubarrays(nums, minK, maxK));
     }
-
     public long countSubarrays(int[] nums, int minK, int maxK) {
-
+        
     }
 }
