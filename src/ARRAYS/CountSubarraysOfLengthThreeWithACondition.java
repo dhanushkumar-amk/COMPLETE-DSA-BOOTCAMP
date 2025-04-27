b3537fbb711b083e299496df7ffd4d626024fb0c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-04-27 11:50:27 +0530

    modified

--- src/ARRAYS/CountSubarraysOfLengthThreeWithACondition.java
+++ src/ARRAYS/CountSubarraysOfLengthThreeWithACondition.java
@@ -1,4 +1,17 @@
 package ARRAYS;
 
+
+// leetcode : 3392
+// https://leetcode.com/problems/count-subarrays-of-length-three-with-a-condition/description/?envType=daily-question&envId=2025-04-27
+
 public class CountSubarraysOfLengthThreeWithACondition {
+
+    public static void main(String[] args) {
+        CountSubarraysOfLengthThreeWithACondition answer = new CountSubarraysOfLengthThreeWithACondition();
+        int[] nums = {1,2,1,4,1};
+    }
+
+    public int countSubarrays(int[] nums) {
+
+    }
 }
