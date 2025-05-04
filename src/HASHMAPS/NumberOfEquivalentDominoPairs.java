dc4eccb66d3b9d2a932bea0953fec6d3dc5061de
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-04 19:20:25 +0530

    modified

--- src/HASHMAPS/NumberOfEquivalentDominoPairs.java
+++ src/HASHMAPS/NumberOfEquivalentDominoPairs.java
@@ -1,7 +1,10 @@
- package HASHMAPS; 
+package HASHMAPS;
 
 public class NumberOfEquivalentDominoPairs {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
+        NumberOfEquivalentDominoPairs answer = new NumberOfEquivalentDominoPairs();
+        in
 
     }
 }
