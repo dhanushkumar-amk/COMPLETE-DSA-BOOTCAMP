7be6970a33f6fc925a2d447b510330579870ea07
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-04 19:21:12 +0530

    modified

--- src/HASHMAPS/NumberOfEquivalentDominoPairs.java
+++ src/HASHMAPS/NumberOfEquivalentDominoPairs.java
@@ -4,7 +4,14 @@ public class NumberOfEquivalentDominoPairs {
     public static void main(String[] args) {
 
         NumberOfEquivalentDominoPairs answer = new NumberOfEquivalentDominoPairs();
-        in
+        int[][] dominoes = {
+                {1, 2},
+                {2, 1},
+                {3, 4},
+                {5, 6}
+        };
+
+        System.out.println();
 
     }
 }
