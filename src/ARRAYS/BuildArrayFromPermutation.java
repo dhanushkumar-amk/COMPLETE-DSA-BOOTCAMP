de55029cc22b0cf4fefbb626b9e613f6ddf1f432
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-06 16:48:18 +0530

    modified

--- src/ARRAYS/BuildArrayFromPermutation.java
+++ src/ARRAYS/BuildArrayFromPermutation.java
@@ -1,4 +1,7 @@
 package ARRAYS;
 
+// https://leetcode.com/problems/build-array-from-permutation/description/?envType=daily-question&envId=2025-05-06
+//leetcode : 1920
+
 public class BuildArrayFromPermutation {
 }
