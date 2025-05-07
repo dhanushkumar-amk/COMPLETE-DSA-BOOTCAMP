5d7b634d384afd659a67ed9d1bd055a078a200af
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-07 20:57:28 +0530

    modified

--- src/RECURSION/BACKTRACKING/Permutations.java
+++ src/RECURSION/BACKTRACKING/Permutations.java
@@ -4,10 +4,14 @@ package RECURSION.BACKTRACKING;
 // leetcode : 46
 // https://leetcode.com/problems/permutations/description/
 
+import java.util.*;
+
 public class Permutations {
     public static void main(String[] args) {
 
     }
 
-    
+    public List<List<Integer>> permute(int[] nums) {
+
+    }
 }
