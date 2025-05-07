3b94539a5ba022e2542b383c35298b93bb3d8d92
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-07 18:00:03 +0530

    modified

--- src/GRAPH/FindMinimumTimeToReachLastRoomI.java
+++ src/GRAPH/FindMinimumTimeToReachLastRoomI.java
@@ -3,6 +3,8 @@ package GRAPH;
 // https://leetcode.com/problems/find-minimum-time-to-reach-last-room-i/description/?envType=daily-question&envId=2025-05-07
 // leetcode : 3341
 
+import java.util.Arrays;
+
 public class FindMinimumTimeToReachLastRoomI {
     public static void main(String[] args) {
     }
@@ -11,7 +13,11 @@ public class FindMinimumTimeToReachLastRoomI {
         int n = moveTime.length;
         int m = moveTime[0].length;
 
-        int[][] visited = new int[][]
+        int[][] visited = new int[n][m];
+        for(var row : visited)
+            Arrays.fill(row, Integer.MAX_VALUE);
+
+
 
     }
 }
