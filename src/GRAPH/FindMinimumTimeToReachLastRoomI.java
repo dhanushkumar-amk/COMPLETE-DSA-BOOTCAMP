0370eefc269a89854897d8aa4651e2c4486cf5fe
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-07 17:34:23 +0530

    modified

--- src/GRAPH/FindMinimumTimeToReachLastRoomI.java
+++ src/GRAPH/FindMinimumTimeToReachLastRoomI.java
@@ -1,7 +1,7 @@
 package GRAPH;
 
-// 
-
+// https://leetcode.com/problems/find-minimum-time-to-reach-last-room-i/description/?envType=daily-question&envId=2025-05-07
+// leetcode : 3341
 
 public class FindMinimumTimeToReachLastRoomI {
     public static void main(String[] args) {
