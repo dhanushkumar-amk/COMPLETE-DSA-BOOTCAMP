7dc4903ae1a7493c2efc8a3f0454e243e983e5e5
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-09 21:53:05 +0530

    modified

--- src/MATHS/PowXN.java
+++ src/MATHS/PowXN.java
@@ -1,4 +1,14 @@
 package MATHS;
 
+// leetcode 50
+//https://leetcode.com/problems/powx-n/description/
+
 public class PowXN {
+    public static void main(String[] args) {
+
+    }
+
+    public double myPow(double x, int n) {
+
+    }
 }
