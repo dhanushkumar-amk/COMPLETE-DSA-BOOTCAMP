2399a1b3b05c9d31af2951ba7f8b8dbbf79321a7
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-10 10:01:50 +0530

    modified

--- src/GREEDY/MinimumEqualSumOfTwoArraysAfterReplacingZeros.java
+++ src/GREEDY/MinimumEqualSumOfTwoArraysAfterReplacingZeros.java
@@ -8,7 +8,10 @@ public class MinimumEqualSumOfTwoArraysAfterReplacingZeros {
     public static void main(String[] args) {
         MinimumEqualSumOfTwoArraysAfterReplacingZeros answer = new MinimumEqualSumOfTwoArraysAfterReplacingZeros();
         int[] nums1 = {3,2,0,1,0};
-        int[] nums2 = {}
+        int[] nums2 = {6,5,0};
+
+        System.out.println(answer.minSum(nums1, nums2));
+
     }
 
     public long minSum(int[] nums1, int[] nums2) {
