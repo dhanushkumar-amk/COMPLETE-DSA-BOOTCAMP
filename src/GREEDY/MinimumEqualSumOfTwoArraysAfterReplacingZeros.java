68b1c16f899b6ae730dd75dffab96cad5a67377f
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-10 09:49:47 +0530

    modified

--- src/GREEDY/MinimumEqualSumOfTwoArraysAfterReplacingZeros.java
+++ src/GREEDY/MinimumEqualSumOfTwoArraysAfterReplacingZeros.java
@@ -5,4 +5,11 @@ package GREEDY;
 // https://leetcode.com/problems/minimum-equal-sum-of-two-arrays-after-replacing-zeros/?envType=daily-question&envId=2025-05-10
 
 public class MinimumEqualSumOfTwoArraysAfterReplacingZeros {
+    public static void main(String[] args) {
+
+    }
+
+    public long minSum(int[] nums1, int[] nums2) {
+
+    }
 }
