4107c64f2a80490549ef672bd3211a350ddda10d
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-10 09:49:33 +0530

    modified

--- src/GREEDY/MinimumEqualSumOfTwoArraysAfterReplacingZeros.java
+++ src/GREEDY/MinimumEqualSumOfTwoArraysAfterReplacingZeros.java
@@ -1,4 +1,8 @@
 package GREEDY;
 
+
+// leetcode 2918
+// https://leetcode.com/problems/minimum-equal-sum-of-two-arrays-after-replacing-zeros/?envType=daily-question&envId=2025-05-10
+
 public class MinimumEqualSumOfTwoArraysAfterReplacingZeros {
 }
