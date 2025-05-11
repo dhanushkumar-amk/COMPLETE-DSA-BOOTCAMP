bc18dd28e5d27b92b33b679b4ea5447f76648a1f
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-11 21:23:55 +0530

    modified

--- src/ARRAYS/ThreeConsecutiveOdds.java
+++ src/ARRAYS/ThreeConsecutiveOdds.java
@@ -12,7 +12,6 @@ public class ThreeConsecutiveOdds {
     }
 
     public boolean threeConsecutiveOdds(int[] arr) {
-
         for (int i = 0; i < arr.length - 2 ; i++) {
             if (arr[i] % 2 != 0 && arr[i+1] % 2 != 0 && arr[i + 2] % 2 != 0)
                 return true;
