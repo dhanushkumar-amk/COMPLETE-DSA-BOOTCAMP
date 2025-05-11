6e6e3b5838c34058b60c8eeebdf47469db71cc2f
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-11 21:20:58 +0530

    modified

--- src/ARRAYS/ThreeConsecutiveOdds.java
+++ src/ARRAYS/ThreeConsecutiveOdds.java
@@ -1,5 +1,9 @@
 package ARRAYS;
 
+// leetcode 1550
+// https://leetcode.com/problems/three-consecutive-odds/description/?envType=daily-question&envId=2025-05-11
+
+
 public class ThreeConsecutiveOdds {
     public static void main(String[] args) {
 
