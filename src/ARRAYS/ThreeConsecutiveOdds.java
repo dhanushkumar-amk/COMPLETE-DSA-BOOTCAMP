4538062ca5c26f30f09ff939da46f418838aa463
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-11 21:23:24 +0530

    modified

--- src/ARRAYS/ThreeConsecutiveOdds.java
+++ src/ARRAYS/ThreeConsecutiveOdds.java
@@ -6,10 +6,17 @@ package ARRAYS;
 
 public class ThreeConsecutiveOdds {
     public static void main(String[] args) {
-
+        ThreeConsecutiveOdds answer = new ThreeConsecutiveOdds();
+        int[] arr ={2,6,4,1};
+        System.out.println(answer.threeConsecutiveOdds(arr));
     }
 
     public boolean threeConsecutiveOdds(int[] arr) {
 
+        for (int i = 0; i < arr.length - 2 ; i++) {
+            if (arr[i] % 2 != 0 && arr[i+1] % 2 != 0 && arr[i + 2] % 2 != 0)
+                return true;
+        }
+        return false;
     }
 }
