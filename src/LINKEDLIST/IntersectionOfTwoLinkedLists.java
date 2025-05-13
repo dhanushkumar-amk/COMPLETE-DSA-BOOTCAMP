7b77497a35a5f0f7fc7ae0e631b78532ee3786d9
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-13 20:28:33 +0530

    modified

--- src/LINKEDLIST/IntersectionOfTwoLinkedLists.java
+++ src/LINKEDLIST/IntersectionOfTwoLinkedLists.java
@@ -1,4 +1,17 @@
 package LINKEDLIST;
 
 public class IntersectionOfTwoLinkedLists {
+
+
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+
+  
+    
 }
