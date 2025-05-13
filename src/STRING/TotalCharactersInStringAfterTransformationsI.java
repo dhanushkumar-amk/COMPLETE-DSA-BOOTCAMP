bcdc86bb28925a14a22c92b029fdd9daf01ca5df
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-13 19:56:10 +0530

    modified

--- src/STRING/TotalCharactersInStringAfterTransformationsI.java
+++ src/STRING/TotalCharactersInStringAfterTransformationsI.java
@@ -1,7 +1,10 @@
- package STRING; 
+package STRING;
+
+// leetcode 3335
+// https://leetcode.com/problems/total-characters-in-string-after-transformations-i/description/?envType=daily-question&envId=2025-05-13
 
 public class TotalCharactersInStringAfterTransformationsI {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
     }
 }
