d34dd0e6bcfb9442a3a65b01f03c16674cbcc0eb
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-13 20:43:54 +0530

    modified

--- src/LINKEDLIST/IntersectionOfTwoLinkedLists.java
+++ src/LINKEDLIST/IntersectionOfTwoLinkedLists.java
@@ -13,7 +13,33 @@ public class IntersectionOfTwoLinkedLists {
   }
 
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        
+        int lengthA = getLitLength(headA);
+        int lengthB = getLitLength(headB);
+
+        while (lengthA > lengthB){
+            lengthA--;
+            headA = headA.next;
+        }
+
+        while(lengthB > lengthA){
+            lengthB--;
+            headB = headB.next;
+        }
+
+        while (headA != headB){
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return headA;
+    }
+
+    private int getLitLength(ListNode head){
+        int length = 0;
+        while(head.next != null){
+            length++;
+            head = head.next;
+        }
+        return length;
     }
     
 }
