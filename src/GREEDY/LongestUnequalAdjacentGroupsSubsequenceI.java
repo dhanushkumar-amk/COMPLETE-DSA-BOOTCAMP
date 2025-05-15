9ebd7a00dd97d864f3a9a9243225b25f58713322
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-15 20:02:28 +0530

    modified

--- src/GREEDY/LongestUnequalAdjacentGroupsSubsequenceI.java
+++ src/GREEDY/LongestUnequalAdjacentGroupsSubsequenceI.java
@@ -1,7 +1,7 @@
 package GREEDY;
 
 // leetcode : 2900
-// 
+// https://leetcode.com/problems/longest-unequal-adjacent-groups-subsequence-i/description/?envType=daily-question&envId=2025-05-15
 
 
 public class LongestUnequalAdjacentGroupsSubsequenceI {
