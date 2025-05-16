3ec8533eee112fc4018acae0d475926e374fe8e5
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-16 08:32:32 +0530

    modified

--- src/TREES/RangeSumOfBST.java
+++ src/TREES/RangeSumOfBST.java
@@ -23,7 +23,28 @@ public class RangeSumOfBST {
     }
 
     public int rangeSumBST(TreeNode root, int low, int high) {
-            
+
+        int sum = 0;
+
+        if (root == null)
+            return 0;
+
+        // InOrder traversal
+        // left -> root -> right
+
+        //left tree traversal
+        if (root.val > low)
+            sum += rangeSumBST(root.left, low, high);
+
+        // the current value
+        if (root.val >= low && root.val <= high)
+            sum += root.val;
+
+        // right tree traversal
+        if (root.val < high)
+            sum += rangeSumBST(root.right, low, high);
+
+        return sum;
     }
 
 }
