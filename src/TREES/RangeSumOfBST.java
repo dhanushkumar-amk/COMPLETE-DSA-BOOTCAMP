b89643a5011c147750f4ac523e8ca73e540e4967
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-16 08:25:19 +0530

    modified

--- src/TREES/RangeSumOfBST.java
+++ src/TREES/RangeSumOfBST.java
@@ -5,9 +5,25 @@ package TREES;
 
 public class RangeSumOfBST {
 
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+    public static void main(String[] args) {
+
+    }
 
     public int rangeSumBST(TreeNode root, int low, int high) {
-
+            
     }
 
 }
