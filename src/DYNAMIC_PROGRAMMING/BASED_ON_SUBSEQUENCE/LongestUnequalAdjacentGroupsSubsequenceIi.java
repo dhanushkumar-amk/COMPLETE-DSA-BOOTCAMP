9c16a9e7fb3b15db82d7c013175d31ec80bfe6a8
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-16 14:10:32 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_SUBSEQUENCE/LongestUnequalAdjacentGroupsSubsequenceIi.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_SUBSEQUENCE/LongestUnequalAdjacentGroupsSubsequenceIi.java
@@ -1,7 +1,17 @@
 package DYNAMIC_PROGRAMMING.BASED_ON_SUBSEQUENCE;
 
 // leetcode : 2901
-// 
+// https://leetcode.com/problems/longest-unequal-adjacent-groups-subsequence-ii/description/?envType=daily-question&envId=2025-05-16
+
+import java.util.List;
 
 public class LongestUnequalAdjacentGroupsSubsequenceIi {
+
+    public static void main(String[] args) {
+        
+    }
+
+    public List<String> getWordsInLongestSubsequence(String[] words, int[] groups) {
+
+    }
 }
