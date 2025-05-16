c7ca5fcfdee655cc7ac40d651d93487fbdd22d51
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-16 09:08:27 +0530

    modified

--- src/STRING/SortingTheSentence.java
+++ src/STRING/SortingTheSentence.java
@@ -3,6 +3,9 @@ package STRING;
 // leetcode : 1859
 //https://leetcode.com/problems/sorting-the-sentence/description/
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SortingTheSentence {
     public static void main(String[] args) {
         SortingTheSentence answer = new SortingTheSentence();
@@ -11,6 +14,22 @@ public class SortingTheSentence {
     }
 
     public String sortSentence(String s) {
-        
+        Map<Integer, String> map = new HashMap<>();
+
+        for(String word : s.split(" ")){
+            int lastIndex = word.length() - 1;
+
+            int index = word.charAt(lastIndex) - '0';
+            String actualWord = word.substring(0, lastIndex);
+
+            map.put(index, actualWord);
+        }
+
+        StringBuilder answer = new StringBuilder();
+        for(Map.Entry<Integer, String> indexWord : map.entrySet()){
+            answer.append(indexWord.getValue());
+            answer.append(" ");
+        }
+        return answer.toString().trim();
     }
 }
