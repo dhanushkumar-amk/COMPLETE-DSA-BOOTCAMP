14f2cc659f58c6bdf0d316fb5483f6ff0020f6f7
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-17 05:51:09 +0530

    modified

--- src/SLIDING_WINDOW_AND_POINTERS/SortColors.java
+++ src/SLIDING_WINDOW_AND_POINTERS/SortColors.java
@@ -3,12 +3,17 @@ package SLIDING_WINDOW_AND_POINTERS;
 // leetcode : 75
 // https://leetcode.com/problems/sort-colors/description/?envType=daily-question&envId=2025-05-17
 
+import java.util.Arrays;
+
 public class SortColors {
     public static void main(String[] args) {
-
+        SortColors answer = new SortColors();
+        int[] nums = {2,0,2,1,1,0};
+        answer.sortColors(nums);
+        System.out.println(Arrays.toString(nums));
     }
 
     public void sortColors(int[] nums) {
-
+        
     }
 }
