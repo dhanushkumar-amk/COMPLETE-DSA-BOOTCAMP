ccb4d18b6303d8050e83086514d2c28ddc6628d8
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-17 06:31:58 +0530

    modified

--- src/STRING/ReorganizeString.java
+++ src/STRING/ReorganizeString.java
@@ -7,9 +7,47 @@ public class ReorganizeString {
 
     public static void main(String[] args) {
         ReorganizeString answer = new ReorganizeString();
-        String s = "public String reorganizeString(String s) {\n" +
-                "        \n" +
-                "    }"
+        String s = "aab";
+        System.out.println(answer.reorganizeString(s));
+    }
+
+    public String reorganizeString(String s) {
+        int[] hash = new int[26];
+
+        for (int i = 0; i < s.length(); i++)
+            hash[s.charAt(i) - 'a']++;
+
+        int max = 0;
+        int letter = 0;
+
+        for (int i = 0; i < hash.length; i++) {
+            if (hash[i] > max) {
+                max = hash[i];
+                letter = i;
+            }
+        }
+
+        if (max > (s.length() + 1 ) / 2 )
+            return "";
+
+        char[] result = new char[s.length()];
+
+        int index = 0;
+        while (hash[letter]-- > 0){
+            result[index] = (char) (letter + 'a');
+            index += 2;
+        }
+
+        for (int i = 0; i < hash.length; i++) {
+            while (hash[i]--  > 0){
+                if (index >= result.length)
+                    index = 1;
+
+                result[index] = (char) (i + 'a');
+                index += 2;
+            }
+        }
+        return String.valueOf(result);
     }
 
 }
