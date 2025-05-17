3e592eeaf5c753ad7cb4eab32da4b320d79dc4a3
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-17 05:55:42 +0530

    modified

--- src/SLIDING_WINDOW_AND_POINTERS/SortColors.java
+++ src/SLIDING_WINDOW_AND_POINTERS/SortColors.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class SortColors {
     public static void main(String[] args) {
         SortColors answer = new SortColors();
-        int[] nums = {2,0,2,1,1,0};
+        int[] nums = {2,0,0,1,1,0};
         answer.sortColors(nums);
         System.out.println(Arrays.toString(nums));
     }
