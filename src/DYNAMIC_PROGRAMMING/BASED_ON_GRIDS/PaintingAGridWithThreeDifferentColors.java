17578eda5e1d73d6cdfa2b910848ae6703b5bc56
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-18 14:59:12 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_GRIDS/PaintingAGridWithThreeDifferentColors.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_GRIDS/PaintingAGridWithThreeDifferentColors.java
@@ -6,7 +6,8 @@ package DYNAMIC_PROGRAMMING.BASED_ON_GRIDS;
 public class PaintingAGridWithThreeDifferentColors {
     public static void main(String[] args) {
         PaintingAGridWithThreeDifferentColors answer = new PaintingAGridWithThreeDifferentColors();
-        int m = 
+        int m = 5;
+        int n = 
     }
 
     public int colorTheGrid(int m, int n) {
