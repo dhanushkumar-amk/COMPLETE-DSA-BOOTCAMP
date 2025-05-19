cdb0501f13efa517d7735532f308fa2b760ace01
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-19 10:07:43 +0530

    modified

--- src/STRING/TypeofTriangle.java
+++ src/STRING/TypeofTriangle.java
@@ -5,4 +5,11 @@ package STRING;
 // https://leetcode.com/problems/type-of-triangle/description/?envType=daily-question&envId=2025-05-19
 
 public class TypeofTriangle {
+    public static void main(String[] args) {
+
+    }
+
+    public String triangleType(int[] nums) {
+
+    }
 }
