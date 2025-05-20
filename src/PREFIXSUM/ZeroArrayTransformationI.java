42ce245788f7d206da4bbacf0443018ff701b30f
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-20 20:37:40 +0530

    modified

--- src/PREFIXSUM/ZeroArrayTransformationI.java
+++ src/PREFIXSUM/ZeroArrayTransformationI.java
@@ -1,7 +1,7 @@
 package PREFIXSUM;
 
 // leetcode 3355
-
+// https://leetcode.com/problems/zero-array-transformation-i/?envType=daily-question&envId=2025-05-20
 
 public class ZeroArrayTransformationI {
     public static void main(String[] args) {
