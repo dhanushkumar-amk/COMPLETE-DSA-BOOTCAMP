a5ea7ba0dbd5e0daeba2099e2158cb78f0a334d0
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-24 19:40:32 +0530

    modified

--- src/STRING/FindWordsContainingCharacter.java
+++ src/STRING/FindWordsContainingCharacter.java
@@ -3,8 +3,19 @@ package STRING;
 //leetcode : 2942
 // https://leetcode.com/problems/find-words-containing-character/description/?envType=daily-question&envId=2025-05-24
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FindWordsContainingCharacter {
     public static void main(String[] args) {
 
+    }
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].indexOf(x) != -1)
+                ans.add(i);
+        }
+        return ans;
     }
 }
