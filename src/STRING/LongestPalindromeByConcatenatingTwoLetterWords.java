2411f48899171a0e511c05e2f44d4087648485ee
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-25 21:46:08 +0530

    modified

--- src/STRING/LongestPalindromeByConcatenatingTwoLetterWords.java
+++ src/STRING/LongestPalindromeByConcatenatingTwoLetterWords.java
@@ -2,7 +2,7 @@ package STRING;
 
 
 // leetcode : 2131
-//
+// https://leetcode.com/problems/longest-palindrome-by-concatenating-two-letter-words/description/?envType=daily-question&envId=2025-05-25
 
 public class LongestPalindromeByConcatenatingTwoLetterWords {
     public static void main(String[] args) {
