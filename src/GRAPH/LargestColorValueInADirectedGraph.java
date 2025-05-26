76cbdcdf826b29a68e6c8ad0421f213ac1e02eaa
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-26 20:50:34 +0530

    modified

--- src/GRAPH/LargestColorValueInADirectedGraph.java
+++ src/GRAPH/LargestColorValueInADirectedGraph.java
@@ -1,7 +1,11 @@
 package GRAPH;
 
-// leetcode : 157
+// leetcode : 1857
 // https://leetcode.com/problems/largest-color-value-in-a-directed-graph/description/?envType=daily-question&envId=2025-05-26
 
 public class LargestColorValueInADirectedGraph {
+
+    public int largestPathValue(String colors, int[][] edges) {
+
+    }
 }
