83c79379cb2b6d4ec597b83b101ceff40f702159
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-28 19:43:55 +0530

    modified

--- src/MATHS/DivisibleAndNonDivisibleSumsDifference.java
+++ src/MATHS/DivisibleAndNonDivisibleSumsDifference.java
@@ -4,4 +4,9 @@ package MATHS;
 // https://leetcode.com/problems/divisible-and-non-divisible-sums-difference/description/?envType=daily-question&envId=2025-05-27
 
 public class DivisibleAndNonDivisibleSumsDifference {
+
+    public static void main(String[] args) {
+        
+    }
+
 }
