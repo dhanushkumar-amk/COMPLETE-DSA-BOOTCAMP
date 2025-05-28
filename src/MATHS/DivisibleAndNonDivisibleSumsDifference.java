779c7d5702435940b3cf69f4b8088c4018d95c8a
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-28 19:45:44 +0530

    modified

--- src/MATHS/DivisibleAndNonDivisibleSumsDifference.java
+++ src/MATHS/DivisibleAndNonDivisibleSumsDifference.java
@@ -14,10 +14,13 @@ public class DivisibleAndNonDivisibleSumsDifference {
         int num2 = 0;
 
         int result = 0;
-        for (int i = ; i < ; i++) {
-
+        for (int i = 1; i <= n; i++) {
+            if(i % 3 == 0)
+                num1 += i;
+            else
+                num2 += i;
         }
-
+    return num1 - num2;
     }
 
 }
