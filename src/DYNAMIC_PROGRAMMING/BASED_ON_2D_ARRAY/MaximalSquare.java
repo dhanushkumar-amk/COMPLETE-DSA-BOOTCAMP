10c54f595948006d814f24117292edd96bb40cb6
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-30 12:34:14 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_2D_ARRAY/MaximalSquare.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_2D_ARRAY/MaximalSquare.java
@@ -1,2 +1,11 @@
-package DYNAMIC_PROGRAMMING.BASED_ON_2D_ARRAY;public class MaximalSquare {
+package DYNAMIC_PROGRAMMING.BASED_ON_2D_ARRAY;
+
+
+// leetcode : 221
+// https://leetcode.com/problems/maximal-square/description/
+
+public class MaximalSquare {
+    public static void main(String[] args) {
+
+    }
 }
