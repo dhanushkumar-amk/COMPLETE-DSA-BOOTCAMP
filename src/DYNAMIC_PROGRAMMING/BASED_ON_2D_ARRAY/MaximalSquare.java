83e174125227fb72e9f45d2e0c6aef56b2971575
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-30 12:52:12 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_2D_ARRAY/MaximalSquare.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_2D_ARRAY/MaximalSquare.java
@@ -18,6 +18,28 @@ public class MaximalSquare {
     }
 
     public int maximalSquare(char[][] matrix) {
+        int row = matrix.length;
+        int column  = matrix[0].length;
 
+        int[][] dp = new int[row][column];
+
+        int maxSum = 0;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+
+                if(matrix[i][j] == '1'){
+
+                    // special case for first row and column when it is an value 1
+                    if(i == 0 || j == 0)
+                        dp[i][j] = '1';
+                    else
+//                        find the min valueof the neighbour and add + 1
+                        dp[i][j] = Math.min(dp[i -1][j], Math.min(matrix[i - 1][j - 1], matrix[i][j - 1])) + 1;
+                }
+                maxSum = Math.max(maxSum, dp[i][j]);
+            }
+        }
+        return maxSum;
     }
 }
