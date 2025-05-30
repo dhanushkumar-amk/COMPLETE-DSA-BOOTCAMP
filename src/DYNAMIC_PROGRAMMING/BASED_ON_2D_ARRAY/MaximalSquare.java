a1a54c3a96672f5a70eacd028a273ec6af79e022
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-30 12:39:10 +0530

    modified

--- src/DYNAMIC_PROGRAMMING/BASED_ON_2D_ARRAY/MaximalSquare.java
+++ src/DYNAMIC_PROGRAMMING/BASED_ON_2D_ARRAY/MaximalSquare.java
@@ -6,6 +6,18 @@ package DYNAMIC_PROGRAMMING.BASED_ON_2D_ARRAY;
 
 public class MaximalSquare {
     public static void main(String[] args) {
+        MaximalSquare answer = new MaximalSquare();
+        char[][] matrix = {
+                {'1','0','1','0', '0'},
+                {'1','0','1','1', '1'},
+                {'1','1','1','1', '1'},
+                {'1','0','0','1', '0'},
+        };
+
+        System.out.println(answer.maximalSquare(matrix));
+    }
+
+    public int maximalSquare(char[][] matrix) {
 
     }
 }
