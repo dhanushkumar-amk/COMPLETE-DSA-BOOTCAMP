25bcbd6e269271fdc603ed7feb075cb2a0b55c70
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-06-07 08:24:59 +0530

    modified

--- src/PATTERNS/pattern5.java
+++ src/PATTERNS/pattern5.java
@@ -4,8 +4,11 @@ public class pattern5 {
     public static void main(String[] args) {
         int n = 7;
 
-        for (int i = 0; i < ; i++) {
-            
+        for (int i = 0; i < n ; i++) {
+            for (int j = i; j >= 0; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
         }
     }
 }
