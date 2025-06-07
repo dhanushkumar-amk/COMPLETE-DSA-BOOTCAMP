58e19ca596c8987cc64223df767a8a263b0e1a20
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-06-07 08:20:37 +0530

    modified

--- src/PATTERNS/NUMBER_PATTERN/pattern3.java
+++ src/PATTERNS/NUMBER_PATTERN/pattern3.java
@@ -4,9 +4,10 @@ public class pattern3 {
     public static void main(String[] args) {
         int n = 7;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < ; j++) {
-                
+            for (int j = 0; j < n; j++) {
+                System.out.print(i + " ");
             }
+            System.out.println();
         }
     }
 }
