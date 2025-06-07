8b1da8ffef01fdfd5f4c66c618ec32918a4a52de
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-06-07 08:07:52 +0530

    modified

--- src/PATTERNS/NUMBER_PATTERN/Pattern1.java
+++ src/PATTERNS/NUMBER_PATTERN/Pattern1.java
@@ -2,7 +2,7 @@ package PATTERNS.NUMBER_PATTERN;
 
 public class Pattern1 {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 7;
 
         for (int i = 0; i <n ; i++) {
             for (int j = 0; j <i + 1 ; j++) {
