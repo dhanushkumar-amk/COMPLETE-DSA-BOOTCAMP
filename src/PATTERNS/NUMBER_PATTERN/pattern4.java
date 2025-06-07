64710c562b0a075a66885094df72f9f8666b5248
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-06-07 08:23:34 +0530

    modified

--- src/PATTERNS/NUMBER_PATTERN/pattern4.java
+++ src/PATTERNS/NUMBER_PATTERN/pattern4.java
@@ -3,8 +3,8 @@ package PATTERNS.NUMBER_PATTERN;
 public class pattern4 {
     public static void main(String[] args) {
         int n = 7;
-        for (int i = n; i >= 0 ; i--) {
-            for (int j = 0; j < i + 1; j++) {
+        for (int i = n; i > 0 ; i--) {
+            for (int j = 1; j < i + 1; j++) {
                 System.out.print(i + " ");
             }
             System.out.println();
