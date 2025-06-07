d3374fa3099a4b181e0d1f92ba38b8c338a00206
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-06-07 08:09:00 +0530

    modified

--- src/PATTERNS/NUMBER_PATTERN/Pattern1.java
+++ src/PATTERNS/NUMBER_PATTERN/Pattern1.java
@@ -3,13 +3,19 @@ package PATTERNS.NUMBER_PATTERN;
 public class Pattern1 {
     public static void main(String[] args) {
         int n = 7;
-
         for (int i = 0; i <n ; i++) {
             for (int j = 0; j <i + 1 ; j++) {
                 System.out.print(j + 1 + " ");
             }
             System.out.println();
         }
-
     }
 }
+
+// 1
+//1 2
+//1 2 3
+//1 2 3 4
+//1 2 3 4 5
+//1 2 3 4 5 6
+//1 2 3 4 5 6 7
--- src/PATTERNS/NUMBER_PATTERN/pattern2.java
+++ src/PATTERNS/NUMBER_PATTERN/pattern2.java
@@ -1,2 +1,12 @@
-package PATTERNS.NUMBER_PATTERN;public class pattern2 {
+package PATTERNS.NUMBER_PATTERN;
+
+public class pattern2 {
+    public static void main(String[] args) {
+        int n = 7;
+
+        for (int i = 0; i < ; i++) {
+            
+        }
+        
+    }
 }
