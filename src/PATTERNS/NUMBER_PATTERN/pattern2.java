62f73c805bf3f9a8aff05e99234fe7f9a9c17bca
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-06-07 08:12:52 +0530

    modified

--- src/PATTERNS/NUMBER_PATTERN/pattern2.java
+++ src/PATTERNS/NUMBER_PATTERN/pattern2.java
@@ -4,8 +4,8 @@ public class pattern2 {
     public static void main(String[] args) {
         int n = 7;
 
-        for (int i = n ; i >= 0; i--) {
-            for (int j = n - 1; j >= i; j--) {
+        for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j > i; j--) {
                 System.out.print(j + " ");
             }
             System.out.println();
