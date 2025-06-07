47baa78e499346cae05c769997802ed7a375ad03
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-06-07 08:21:37 +0530

    modified

--- src/PATTERNS/NUMBER_PATTERN/pattern3.java
+++ src/PATTERNS/NUMBER_PATTERN/pattern3.java
@@ -3,7 +3,7 @@ package PATTERNS.NUMBER_PATTERN;
 public class pattern3 {
     public static void main(String[] args) {
         int n = 7;
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print(i + " ");
             }
