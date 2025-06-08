e92705bb2df716e7b4eb9fa651d86abf4c9428e4
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-06-08 17:16:37 +0530

    modified

--- src/ARRAYS/LexicographicalNumbers.java
+++ src/ARRAYS/LexicographicalNumbers.java
@@ -1,8 +1,21 @@
 package ARRAYS;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class LexicographicalNumbers {
     public static void main(String[] args) {
         LexicographicalNumbers ans = new LexicographicalNumbers();
+        int n = 13;
+        System.out.println(ans.lexicalOrder(n));
+    }
+    public List<Integer> lexicalOrder(int n) {
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = i + 1;
+        }
 
+        Arrays.sort(arr, (a, b) -> a.toString().compareTo(b.toString()));
+        return Arrays.asList(arr);
     }
 }
