a5403d65f2aba293cd28ebd97b82f1739b029382
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-06-08 17:14:40 +0530

    modified

--- src/ARRAYS/LexicographicalNumbers.java
+++ src/ARRAYS/LexicographicalNumbers.java
@@ -1,2 +1,8 @@
-package ARRAYS;public class LexicographicalNumbers {
+package ARRAYS;
+
+public class LexicographicalNumbers {
+    public static void main(String[] args) {
+        LexicographicalNumbers ans = new LexicographicalNumbers();
+
+    }
 }
