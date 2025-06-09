ba23d48704b560417efa8215c60f6637c453369c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-06-09 20:46:56 +0530

    modified

--- src/PATTERNS/STAR_PATTERN/pattern4.java
+++ src/PATTERNS/STAR_PATTERN/pattern4.java
@@ -5,10 +5,8 @@ public class pattern4 {
         int n = 5;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if(i == j )
+                if(i == j)
                     System.out.print("* ");
-                else
-                    System.out.print("  ");
             }
             System.out.println();
         }
