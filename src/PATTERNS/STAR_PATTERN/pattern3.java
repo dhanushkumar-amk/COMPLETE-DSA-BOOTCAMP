553e9ce06835ad29109f89c4cc6039433998efd2
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-06-09 20:43:29 +0530

    modified

--- src/PATTERNS/STAR_PATTERN/pattern3.java
+++ src/PATTERNS/STAR_PATTERN/pattern3.java
@@ -5,10 +5,10 @@ public class pattern3 {
         int n = 5;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if(i == 0 || j == 0 || i == n || j == n)
+                if(i == 0 || j == 0 || i == n-1 || j == n - 1)
                     System.out.print("* ");
                 else
-                    System.out.print(" ");
+                    System.out.print("");
             }
             System.out.println();
         }
