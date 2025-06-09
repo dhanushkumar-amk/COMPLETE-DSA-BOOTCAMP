f1bca3bcc3c02c3dd3dd0b8f6308df8988fc1f24
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-06-09 20:47:44 +0530

    modified

--- src/PATTERNS/STAR_PATTERN/pattern4.java
+++ src/PATTERNS/STAR_PATTERN/pattern4.java
@@ -7,7 +7,18 @@ public class pattern4 {
             for (int j = 0; j < n; j++) {
                 if(i == j)
                     System.out.print("* ");
+                else
+                    System.out.print("  ");
             }
+
+            for (int j = n; j > 0; j--) {
+                if(i == j)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+
+            }
+
             System.out.println();
         }
     }
