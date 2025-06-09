46e95ba33261d02acefd614e5879e52cf88d9ec1
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-06-09 20:31:20 +0530

    modified

--- src/PATTERNS/STAR_PATTERN/pattern2.java
+++ src/PATTERNS/STAR_PATTERN/pattern2.java
@@ -2,7 +2,7 @@ package PATTERNS.STAR_PATTERN;
 
 public class pattern2 {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 5; i > 0; i--) {
             for (int j = 5; j >= i ; j--) {
                 System.out.println("* ");
             }
