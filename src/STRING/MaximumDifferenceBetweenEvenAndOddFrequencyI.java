8e2fa1cc1966539fecdd9cb528da80c23b3d019a
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-06-11 21:31:00 +0530

    modified

--- src/STRING/MaximumDifferenceBetweenEvenAndOddFrequencyI.java
+++ src/STRING/MaximumDifferenceBetweenEvenAndOddFrequencyI.java
@@ -7,10 +7,25 @@ package STRING;
 public class MaximumDifferenceBetweenEvenAndOddFrequencyI {
 
     public static void main(String[] args) {
-        
+        MaximumDifferenceBetweenEvenAndOddFrequencyI ans = new MaximumDifferenceBetweenEvenAndOddFrequencyI();
+        String s = "aaaaabbc";
+        System.out.println(ans.maxDifference(s));
     }
 
     public int maxDifference(String s) {
+        int[] freq = new int[26];
+        int minFreqOdd = Integer.MIN_VALUE;
+        int maxFreqEven = Integer.MAX_VALUE;
 
+        for(char element : s.toCharArray())
+            freq[element - 'a']++;
+
+        for (int i = 0; i < freq.length; i++) {
+            if( freq[i] % 2 != 0 &&freq[i] > maxFreqEven) {
+                minFreqOdd = freq[i];
+            } else if(freq[i] % 2 == 0 && freq[i] < minFreqOdd)
+                maxFreqEven = freq[i];
+        }
+        return  minFreqOdd - maxFreqEven;
     }
 }
