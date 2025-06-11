234295a0bc5df44eca67f943e8061417efa6150b
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-06-11 21:21:45 +0530

    modified

--- src/STRING/MaximumDifferenceBetweenEvenAndOddFrequencyI.java
+++ src/STRING/MaximumDifferenceBetweenEvenAndOddFrequencyI.java
@@ -2,7 +2,7 @@ package STRING;
 
 
 // https://leetcode.com/problems/maximum-difference-between-even-and-odd-frequency-i/description/?envType=daily-question&envId=2025-06-10
-// 3442
+// leetcode : 3442
 
 public class MaximumDifferenceBetweenEvenAndOddFrequencyI {
 }
