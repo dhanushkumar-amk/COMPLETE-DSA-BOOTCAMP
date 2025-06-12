1cf9393b54e182b5f8c278109d2a6f22c75eb36a
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-06-12 20:43:07 +0530

    modified

--- src/ARRAYS/MaximumDifferenceBetweenAdjacentElementsInACircularArray.java
+++ src/ARRAYS/MaximumDifferenceBetweenAdjacentElementsInACircularArray.java
@@ -1,7 +1,11 @@
- package ARRAYS; 
+package ARRAYS;
+
+import STRING.MaximumDifferenceBetweenEvenAndOddFrequencyI;
 
 public class MaximumDifferenceBetweenAdjacentElementsInACircularArray {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+        MaximumDifferenceBetweenEvenAndOddFrequencyI ans = new MaximumDifferenceBetweenEvenAndOddFrequencyI();
+        
     }
 }
