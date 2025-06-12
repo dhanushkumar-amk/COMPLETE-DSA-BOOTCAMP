34324360a17957f637626232cb959449658da46d
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-06-12 20:48:31 +0530

    modified

--- src/ARRAYS/MaximumDifferenceBetweenAdjacentElementsInACircularArray.java
+++ src/ARRAYS/MaximumDifferenceBetweenAdjacentElementsInACircularArray.java
@@ -1,15 +1,21 @@
 package ARRAYS;
 
-import STRING.MaximumDifferenceBetweenEvenAndOddFrequencyI;
+
 
 public class MaximumDifferenceBetweenAdjacentElementsInACircularArray {
     public static void main(String[] args) {
 
-        MaximumDifferenceBetweenEvenAndOddFrequencyI ans = new MaximumDifferenceBetweenEvenAndOddFrequencyI();
-        int[] nums = 
+        MaximumDifferenceBetweenAdjacentElementsInACircularArray ans = new MaximumDifferenceBetweenAdjacentElementsInACircularArray();
+        int[] nums = {1,2,4};
+        System.out.println(ans.maxAdjacentDistance(nums));
 
     }
     public int maxAdjacentDistance(int[] nums) {
-
+        int n = nums.length;
+        int ans = Math.abs(nums[0] - nums[n - 1]);
+        for (int i = 0; i < n; i++) {
+            ans = Math.max(ans, Math.abs(nums[i] - nums[i + 1]));
+        }
+        return ans;
     }
 }
