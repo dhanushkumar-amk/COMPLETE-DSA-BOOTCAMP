656ce2bfb4856ca4d8dd1927147b0e235824bae6
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-06-15 17:16:09 +0530

    modified

--- src/MATHS/MaxDifferenceYouCanGetFromChangingAnInteger.java
+++ src/MATHS/MaxDifferenceYouCanGetFromChangingAnInteger.java
@@ -19,8 +19,9 @@ public class MaxDifferenceYouCanGetFromChangingAnInteger {
 
         // convert the max value
         for (int i = 0; i < n; ++i) {
-            if(maxChar[i] != '9')
+            if(maxChar[i] != '9') {
                 char target = maxChar[i];
+            }
         }
 
 
