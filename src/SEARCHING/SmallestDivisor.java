217394e4a1192138f6cd8a977af08ddeb2128057
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-06-15 17:51:56 +0530

    modified

--- src/SEARCHING/SmallestDivisor.java
+++ src/SEARCHING/SmallestDivisor.java
@@ -1,12 +1,15 @@
 package SEARCHING;
 
+import java.util.Arrays;
+
 public class SmallestDivisor {
     public static void main(String[] args) {
         SmallestDivisor ans = new SmallestDivisor();
-        
+
     }
 
     int smallestDivisor(int[] arr, int k) {
-
+        int low = 1;
+        int high = 
     }
 }
