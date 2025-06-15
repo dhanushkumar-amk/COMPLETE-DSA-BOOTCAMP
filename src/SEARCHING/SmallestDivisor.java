f6e901c1d92613820e689de673b896a4f63400b4
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-06-15 17:58:20 +0530

    modified

--- src/SEARCHING/SmallestDivisor.java
+++ src/SEARCHING/SmallestDivisor.java
@@ -10,6 +10,28 @@ public class SmallestDivisor {
 
     int smallestDivisor(int[] arr, int k) {
         int low = 1;
-        int high = 
+        int high = Arrays.stream(arr).max().getAsInt();
+
+        while (low <= high){
+            int mid = low + high / 2;
+
+            if(sumByDiv(arr, mid) <= k){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return low;
     }
+
+    private int sumByDiv(int[] arr, int mid) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += Math.ceil((double) arr[i] / mid);
+        }
+        return sum;
+    }
+
+
 }
