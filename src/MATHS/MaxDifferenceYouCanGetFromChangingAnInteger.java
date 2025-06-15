c18c2d85e44233ec36557036d65b6097c3a8e939
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-06-15 16:58:56 +0530

    modified

--- src/MATHS/MaxDifferenceYouCanGetFromChangingAnInteger.java
+++ src/MATHS/MaxDifferenceYouCanGetFromChangingAnInteger.java
@@ -1,5 +1,8 @@
 package MATHS;
 
+
+// leetcode : 1432
+// https://leetcode.com/problems/max-difference-you-can-get-from-changing-an-integer/?envType=daily-question&envId=2025-06-15
 public class MaxDifferenceYouCanGetFromChangingAnInteger {
     public static void main(String[] args) {
         MaxDifferenceYouCanGetFromChangingAnInteger ans = new MaxDifferenceYouCanGetFromChangingAnInteger();
@@ -8,6 +11,6 @@ public class MaxDifferenceYouCanGetFromChangingAnInteger {
 
     }
     public int maxDiff(int num) {
-
+        
     }
 }
