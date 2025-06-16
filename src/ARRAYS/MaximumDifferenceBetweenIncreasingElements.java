f6c5819edaddefcce2b45e41f779c98d53dd28c9
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-06-16 19:13:23 +0530

    modified

--- src/ARRAYS/MaximumDifferenceBetweenIncreasingElements.java
+++ src/ARRAYS/MaximumDifferenceBetweenIncreasingElements.java
@@ -11,7 +11,7 @@ public class MaximumDifferenceBetweenIncreasingElements {
     int ans = Integer.MIN_VALUE;
         for (int i = 0; i < nums.length - 1; i++) {
             if(nums[i] < nums[i + 1]){
-               ans = 
+               ans =
             }
         }
 
