b35568593af89e8fbd55f811579c2225bb216fbf
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-06-16 19:04:57 +0530

    modified

--- src/ARRAYS/MaximumDifferenceBetweenIncreasingElements.java
+++ src/ARRAYS/MaximumDifferenceBetweenIncreasingElements.java
@@ -3,10 +3,15 @@ package ARRAYS;
 public class MaximumDifferenceBetweenIncreasingElements {
     public static void main(String[] args) {
         MaximumDifferenceBetweenIncreasingElements ans = new MaximumDifferenceBetweenIncreasingElements();
-        int[] nums = {};
+        int[] nums = {7,1,5,4};
+        System.out.println(ans.maximumDifference(nums));
 
     }
     public int maximumDifference(int[] nums) {
-
+    int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if(nums[i] < nums[i + 1])
+                max = Math.max()
+        }
     }
 }
