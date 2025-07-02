af6b5925d3cb89263a0ebc13c5907d7e3b661a92
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-02 17:28:11 +0530

    modified

--- src/MATHS/ReverseInteger.java
+++ src/MATHS/ReverseInteger.java
@@ -3,12 +3,19 @@ package MATHS;
 public class ReverseInteger {
     public static void main(String[] args) {
         ReverseInteger ans = new ReverseInteger();
-        
+        int x = 123;
+        System.out.println(ans.reverse(x));
     }
 
 
     public int reverse(int x) {
-
+        int ans = 0;
+        while (x > 0){
+            int rem = x % 10;
+            ans = ans * 10 + rem;
+            x /= 10;
+        }
+        return ans;
     }
 
 }
