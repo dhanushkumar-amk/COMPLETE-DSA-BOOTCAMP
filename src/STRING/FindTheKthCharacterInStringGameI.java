eaeb5701253cb8871890dcbae5fc755eaad15b14
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-03 21:35:37 +0530

    modified

--- src/STRING/FindTheKthCharacterInStringGameI.java
+++ src/STRING/FindTheKthCharacterInStringGameI.java
@@ -3,7 +3,7 @@ package STRING;
 public class FindTheKthCharacterInStringGameI {
     public static void main(String[] args) {
         FindTheKthCharacterInStringGameI ans = new FindTheKthCharacterInStringGameI();
-        int k = 10;
+        int k = 5;
         System.out.println(ans.kthCharacter(k));
     }
     public char kthCharacter(int k) {
