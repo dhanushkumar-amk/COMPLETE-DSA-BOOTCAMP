5c0e8a0bd83985bee1ee2931541ff3b669f0b5a2
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-03 21:30:14 +0530

    modified

--- src/STRING/FindTheKthCharacterInStringGameI.java
+++ src/STRING/FindTheKthCharacterInStringGameI.java
@@ -1,4 +1,8 @@
 package STRING;
 
 public class FindTheKthCharacterInStringGameI {
+    public static void main(String[] args) {
+        FindTheKthCharacterInStringGameI ans = new FindTheKthCharacterInStringGameI();
+        
+    }
 }
