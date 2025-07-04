98764036b146e07fe65de0556a507d0fb1cb20d3
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-04 21:36:34 +0530

    modified

--- src/STRING/LongestPalindromeByConcatenatingTwoLetterWords.java
+++ src/STRING/LongestPalindromeByConcatenatingTwoLetterWords.java
@@ -2,7 +2,7 @@ package STRING;
 
 
 // leetcode : 2131
-// https://leetcode.com/problems/longest-palindrome-by-concatenating-two-letter-words/description/?envType=daily-question&envId=2025-05-25
+// https://leetcode.com/problems/longest-palindrome-by-concatenating-two-letter-words/description/?envTykpe=daily-question&envId=2025-05-25
 
 import java.util.HashMap;
 import java.util.Map;
