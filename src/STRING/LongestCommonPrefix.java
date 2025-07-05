e54e9e88eb427a6776fc9d02fc466bd6587d244b
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-05 17:53:23 +0530

    modified

--- src/STRING/LongestCommonPrefix.java
+++ src/STRING/LongestCommonPrefix.java
@@ -3,6 +3,6 @@ package STRING;
 public class LongestCommonPrefix {
     public static void main(String[] args) {
         LongestCommonPrefix ans = new LongestCommonPrefix();
-        String
+        String[] strs = 
     }
 }
