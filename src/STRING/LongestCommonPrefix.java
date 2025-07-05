7531ac1482e3932f50e26867baef5afec04c920a
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-05 19:02:33 +0530

    modified

--- src/STRING/LongestCommonPrefix.java
+++ src/STRING/LongestCommonPrefix.java
@@ -17,6 +17,8 @@ public class LongestCommonPrefix {
         String s2 = strs[strs.length - 1];
         int index;
 
+        if(s1.length() == 1 && s2.length() == 1)
+            return 
 
         if(s1.length() == 1 && s2.length() == 1 && s1.charAt(0) == s2.charAt(0))
             return "";
