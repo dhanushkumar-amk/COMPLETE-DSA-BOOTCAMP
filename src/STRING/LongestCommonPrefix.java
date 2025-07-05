8a8bcc66fb0a506b3050306d4a65c1ef8dbc160c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-05 18:51:02 +0530

    modified

--- src/STRING/LongestCommonPrefix.java
+++ src/STRING/LongestCommonPrefix.java
@@ -23,6 +23,6 @@ public class LongestCommonPrefix {
             else
                 break;
         }
-        return s1.substring(0,index);
+        return s1.length() > 2 ? s1.substring(0,index) : s1.toString();
     }
 }
