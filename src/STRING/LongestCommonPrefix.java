0da86f3f2aba78ddfd7a0c88ac019a65ab90278c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-05 19:12:29 +0530

    modified

--- src/STRING/LongestCommonPrefix.java
+++ src/STRING/LongestCommonPrefix.java
@@ -10,32 +10,18 @@ public class LongestCommonPrefix {
     }
 
     public String longestCommonPrefix(String[] strs) {
-        if(strs.length == 1)
-            return strs[0].toString();
+
         Arrays.sort(strs);
         String s1 = strs[0];
         String s2 = strs[strs.length - 1];
-        int index;
-
-        if(s1.length() == 1 && s2.length() == 1)
-            return "";
-
-        if(s1.length() == 1 && s2.length() == 1 && s1.charAt(0) == s2.charAt(0)) {
-            return "";
-        }
+        int index = 0;
 
 
-
-        if(s1.equals(s2))
-            return s1.toString();
-
-        for( index = 0; index < s1.length()-1 ;index++){
+        while(index < s1.length()){
             if(s1.charAt(index) == s2.charAt(index))
-                continue;
-            else
-                break;
+                index++;
+            else break;
         }
-
-        return s1.length() > 2 ? s2.substring(0,index) : s1.toString();
+        return s1.substring(0, index);
     }
 }
