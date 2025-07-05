6d26da644f080da1ace2fba7758bbd3bc96e3af6
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-05 18:33:18 +0530

    modified

--- src/STRING/LongestCommonPrefix.java
+++ src/STRING/LongestCommonPrefix.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class LongestCommonPrefix {
     public static void main(String[] args) {
         LongestCommonPrefix ans = new LongestCommonPrefix();
-        String[] strs = {"flower","flow","flight"};
+        String[] strs = {"flower"};
         System.out.println(ans.longestCommonPrefix(strs));
     }
 
