d97f7d830c845a4a5296a7fcd82669572d35fcb2
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-05 21:20:41 +0530

    modified

--- src/ARRAYS/FindLuckyIntegerInAnArray.java
+++ src/ARRAYS/FindLuckyIntegerInAnArray.java
@@ -11,7 +11,7 @@ public class FindLuckyIntegerInAnArray {
         int[] freqArray = new int[501];
 
         for (int i = 0; i < arr.length; i++) {
-            freqArray[i]++;
+            freqArray[arr[i]]++;
         }
 
         for (int i = freqArray.length - 1; i > 0; i--) {
