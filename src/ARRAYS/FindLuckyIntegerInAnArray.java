dccd447f8a04c4394e259fc1c828c8c8e6d22edc
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-05 21:05:07 +0530

    modified

--- src/ARRAYS/FindLuckyIntegerInAnArray.java
+++ src/ARRAYS/FindLuckyIntegerInAnArray.java
@@ -14,7 +14,7 @@ public class FindLuckyIntegerInAnArray {
             freqArray[i]++;
         }
         for (int i = freqArray.length; i >= 0; i--) {
-           if(i == freqArray[i-1])
+           if(i-1 == freqArray[i])
                maxFreq = i;
         }
         return maxFreq;
