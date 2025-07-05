fadcc582c634de5e9ffae5dd1a8b58029f5939d6
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-05 20:57:24 +0530

    modified

--- src/ARRAYS/FindLuckyIntegerInAnArray.java
+++ src/ARRAYS/FindLuckyIntegerInAnArray.java
@@ -3,7 +3,7 @@ package ARRAYS;
 public class FindLuckyIntegerInAnArray {
     public static void main(String[] args) {
         FindLuckyIntegerInAnArray ans = new FindLuckyIntegerInAnArray();
-        
+        int[] arr ={};
     }
 
     public int findLucky(int[] arr) {
