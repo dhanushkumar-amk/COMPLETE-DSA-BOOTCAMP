0bfd663fc0ce30865af94cfcafe1cce5c41931a9
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-05 21:13:49 +0530

    modified

--- src/ARRAYS/FindLuckyIntegerInAnArray.java
+++ src/ARRAYS/FindLuckyIntegerInAnArray.java
@@ -8,15 +8,15 @@ public class FindLuckyIntegerInAnArray {
     }
 
     public int findLucky(int[] arr) {
-        int maxFreq = Integer.MIN_VALUE;
+
         int[] freqArray = new int[501];
         for (int i = 0; i < arr.length; i++) {
             freqArray[arr[i]]++;
         }
         for (int i = freqArray.length - 1; i > 0; i--) {
            if(freqArray[i] == i);
-               maxFreq = freqArray[i];
+               return freqArray[i];
         }
-        return maxFreq;
+        return -1;
     }
 }
