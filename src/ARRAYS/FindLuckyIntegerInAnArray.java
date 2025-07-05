3c819f51c11ef84aaf606b17075e68675f66c1a4
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-05 21:00:08 +0530

    modified

--- src/ARRAYS/FindLuckyIntegerInAnArray.java
+++ src/ARRAYS/FindLuckyIntegerInAnArray.java
@@ -3,10 +3,21 @@ package ARRAYS;
 public class FindLuckyIntegerInAnArray {
     public static void main(String[] args) {
         FindLuckyIntegerInAnArray ans = new FindLuckyIntegerInAnArray();
-        int[] arr ={};
+        int[] arr ={2,2,3,4};
+        System.out.println(ans.findLucky(arr));
     }
 
     public int findLucky(int[] arr) {
-
+        int maxFreq = Integer.MIN_VALUE;
+        int[] freqArray = new int[500];
+        for (int i = 1; i < arr.length; i++) {
+            freqArray[i]++;
+        }
+        for (int i = 0; i < freqArray.length; i++) {
+            if(freqArray[i]== i){
+                maxFreq = Math.max(maxFreq, freqArray[i]);
+            }
+        }
+        return maxFreq;
     }
 }
