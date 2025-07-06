7cfb434579003a38ac88bec9a5a22184cf4bc0e1
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-06 14:47:52 +0530

    modified

--- src/STRING/CheckifNumbersAreAscendinginASentence.java
+++ src/STRING/CheckifNumbersAreAscendinginASentence.java
@@ -7,25 +7,15 @@ public class CheckifNumbersAreAscendinginASentence {
     public static void main(String[] args) {
         CheckifNumbersAreAscendinginASentence ans = new CheckifNumbersAreAscendinginASentence();
         String s = "1 box has 3 blue 4 red 6 green and 12 yellow marbles";
-        System.out.println(ans.areNumbersAscending(s));
+//        System.out.println(ans.areNumbersAscending(s));
+        System.out.println(s.trim());
 
     }
 
-    public boolean areNumbersAscending(String s) {
-        String[] arr = s.split("");
-        int prevValue = 0;
-       for(String word : arr){
-           int num = Integer.parseInt(word);
-
-           if(num > prevValue){
-               prevValue = num;
-               continue;
-           }
-           else
-               return false;
-       }
-    return true;
-    }
+//    public boolean areNumbersAscending(String s) {
+//
+//
+//    }
 
 
 }
