3ddae7d2c81ad0374522af27bd521b3848076849
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-06 14:10:54 +0530

    modified

--- src/STRING/SumOfNumbersInString.java
+++ src/STRING/SumOfNumbersInString.java
@@ -2,11 +2,24 @@ package STRING;
 
 public class SumOfNumbersInString {
     public static void main(String[] args) {
-        String s = "0";
-        System.out.println(('a' - s.charAt(0)));
+        String s = "abc21";
+        System.out.println(findSum(s));
+
     }
-//    public static int findSum(String s) {
-//
-//
-//    }
+    public static int findSum(String s) {
+        int result  = 0;
+        int currentValue = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch > 0 && !Character.isDigit(ch)){
+                result += currentValue;
+                currentValue = 0;
+            }
+            if(Character.isDigit(ch)){
+                currentValue *= 10 + ch;
+            }
+        }
+        return currentValue > 0 ? result += currentValue : result;
+    }
 }
