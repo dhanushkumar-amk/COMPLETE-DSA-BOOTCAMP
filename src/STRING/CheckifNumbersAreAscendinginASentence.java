1feed7cab9c4e523699bcd841a6b8438b2a1825d
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-06 14:29:06 +0530

    modified

--- src/STRING/CheckifNumbersAreAscendinginASentence.java
+++ src/STRING/CheckifNumbersAreAscendinginASentence.java
@@ -1,11 +1,13 @@
 package STRING;
 
+import java.util.Arrays;
+
 public class CheckifNumbersAreAscendinginASentence {
 
     public static void main(String[] args) {
         CheckifNumbersAreAscendinginASentence ans = new CheckifNumbersAreAscendinginASentence();
         String s = "1 box has 3 blue 4 red 6 green and 12 yellow marbles";
-        
+        String[] arr = 
     }
 
 
