2f307ae19eb28e7f165434201077a559e99d7f6c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-06 13:49:26 +0530

    modified

--- src/STRING/SumOfNumbersInString.java
+++ src/STRING/SumOfNumbersInString.java
@@ -2,6 +2,11 @@ package STRING;
 
 public class SumOfNumbersInString {
     public static void main(String[] args) {
-        
+        String s = "a";
+        System.out.println(Integer.parseInt(s));
     }
+//    public static int findSum(String s) {
+//
+//
+//    }
 }
