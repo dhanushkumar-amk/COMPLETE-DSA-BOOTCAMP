98600c19ff2ff20d010ed55389e9d4f278ec7da8
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-06 13:50:48 +0530

    modified

--- src/STRING/SumOfNumbersInString.java
+++ src/STRING/SumOfNumbersInString.java
@@ -3,7 +3,7 @@ package STRING;
 public class SumOfNumbersInString {
     public static void main(String[] args) {
         String s = "a";
-        System.out.println((s.charAt(0) - 'a'));
+        System.out.println((s.charAt(0) - 'b'));
     }
 //    public static int findSum(String s) {
 //
