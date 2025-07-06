1921bc6239bd9c28195ceb6a279fab4adebd1b88
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-06 14:15:36 +0530

    modified

--- src/STRING/SumOfNumbersInString.java
+++ src/STRING/SumOfNumbersInString.java
@@ -12,12 +12,14 @@ public class SumOfNumbersInString {
 
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if(ch > 0 && !Character.isDigit(ch)){
+
+            if(Character.isDigit(ch)){
+                currentValue *= 10;
+                currentValue += ch - 48;
+            }
+            if(currentValue > 0 && !Character.isDigit(ch)){
                 result += currentValue;
                 currentValue = 0;
-            }
-            if(Character.isDigit(ch)){
-                currentValue *= 10 + ch;
             }
         }
         return currentValue > 0 ? result += currentValue : result;
