45355fdb153b2ca8779f5bdaf2d50e98f214e211
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-06 14:54:10 +0530

    modified

--- src/STRING/CheckifNumbersAreAscendinginASentence.java
+++ src/STRING/CheckifNumbersAreAscendinginASentence.java
@@ -6,7 +6,7 @@ public class CheckifNumbersAreAscendinginASentence {
 
     public static void main(String[] args) {
         CheckifNumbersAreAscendinginASentence ans = new CheckifNumbersAreAscendinginASentence();
-        String s = "1 box has 3 blue 4 red 6 green and 12 yellow marbles";
+        String s = "1 box has 3 blue 6 red 6 green and 12 yellow marbles";
        System.out.println(ans.areNumbersAscending(s));
 
     }
