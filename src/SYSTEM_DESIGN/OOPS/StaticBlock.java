974f2e1b7aa09a012ea0d423d0733be4069cc198
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-07 15:16:05 +0530

    modified

--- src/SYSTEM_DESIGN/OOPS/StaticBlock.java
+++ src/SYSTEM_DESIGN/OOPS/StaticBlock.java
@@ -11,5 +11,11 @@ public class StaticBlock {
     public static void main(String[] args) {
         StaticBlock obj = new StaticBlock();
         System.out.println(StaticBlock.a + " " + StaticBlock.b);
+
+        StaticBlock.b += 3;
+
+        StaticBlock obj2 = new StaticBlock();
+        System.out.println(StaticBlock.a + " " + StaticBlock.b);
+        
     }
 }
