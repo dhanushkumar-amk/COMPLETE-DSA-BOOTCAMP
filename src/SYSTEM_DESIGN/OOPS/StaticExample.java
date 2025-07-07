8d7f9b1619a90f133548b4937738e1361f6aeecb
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-07 14:17:22 +0530

    modified

--- src/SYSTEM_DESIGN/OOPS/StaticExample.java
+++ src/SYSTEM_DESIGN/OOPS/StaticExample.java
@@ -3,7 +3,7 @@ package SYSTEM_DESIGN.OOPS;
 public class StaticExample {
     public static void main(String[] args) {
         Human dhanush = new Human(20, "dhanushkumar", 100000);
-        System.out.println(dhanush.name);
+        System.out.println(dhanush.population);
     }
 }
 
@@ -11,10 +11,12 @@ class Human {
     String name;
     int age;
     int salary;
+    static long population;
 
     public Human(int age, String name, int salary) {
         this.age = age;
         this.name = name;
         this.salary = salary;
+        Human.population += 1;
     }
 }
