72c7b89a4f4887a2de2ba31d125dfa9dffb54bb5
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-07 15:20:23 +0530

    modified

--- src/SYSTEM_DESIGN/OOPS/StaticBlock.java
+++ src/SYSTEM_DESIGN/OOPS/StaticBlock.java
@@ -3,10 +3,14 @@ package SYSTEM_DESIGN.OOPS;
 public class StaticBlock {
     static int a = 4;
     static  int b;
+
+    // it will only runs when the object created first time on class loaded
     static {
         System.out.println("I am in static block");
         b = a * 5;
     }
+
+    
 
     public static void main(String[] args) {
         StaticBlock obj = new StaticBlock();
@@ -16,6 +20,6 @@ public class StaticBlock {
 
         StaticBlock obj2 = new StaticBlock();
         System.out.println(StaticBlock.a + " " + StaticBlock.b);
-        
+
     }
 }
