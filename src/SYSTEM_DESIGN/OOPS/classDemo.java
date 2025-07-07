a000246e93dde54c78c4f6a9020386e2189584e6
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-07 12:34:40 +0530

    modified

--- src/SYSTEM_DESIGN/OOPS/classDemo.java
+++ src/SYSTEM_DESIGN/OOPS/classDemo.java
@@ -1,2 +1,21 @@
-package SYSTEM_DESIGN.OOPS;public class classDemo {
+package SYSTEM_DESIGN.OOPS;
+
+public class classDemo {
+
+    int rollNo;
+    String name;
+    int age ;
+
+    public classDemo(int age, String name, int rollNo) {
+        this.age = age;
+        this.name = name;
+        this.rollNo = rollNo;
+    }
+
+    public static void main(String[] args) {
+        classDemo obj = new classDemo(10, "dhanushkumar", 21);
+        System.out.println(obj.name);
+        System.out.println(obj.age);
+        System.out.println(obj.rollNo);
+    }
 }
