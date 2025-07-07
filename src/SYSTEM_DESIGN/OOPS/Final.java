695b0261c54d66235172a7aa05320681150a8a88
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-07 13:51:01 +0530

    modified

--- src/SYSTEM_DESIGN/OOPS/Final.java
+++ src/SYSTEM_DESIGN/OOPS/Final.java
@@ -1,11 +1,22 @@
 package SYSTEM_DESIGN.OOPS;
 
 public class Final {
+    String name;
+
+    public Final(String name) {
+        this.name = name;
+    }
+
+
     public static void main(String[] args) {
-        final  int ONE = 1;
+        final int ONE = 1;
         System.out.println(ONE);
 
-        // cannot modified
-//        ONE++;
+        final Final one = new Final("Dhanushkumar");
+        System.out.println(one.name);
+        one.name = "arun";
+        System.out.println(one.name);
     }
 }
+
+
