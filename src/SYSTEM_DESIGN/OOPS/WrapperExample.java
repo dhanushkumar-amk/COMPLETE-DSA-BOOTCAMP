806107a505257d7651beffd8f11fec354d1902f3
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-07 13:27:42 +0530

    modified

--- src/SYSTEM_DESIGN/OOPS/WrapperExample.java
+++ src/SYSTEM_DESIGN/OOPS/WrapperExample.java
@@ -1,2 +1,10 @@
-package SYSTEM_DESIGN.OOPS;public class WrapperExample {
+package SYSTEM_DESIGN.OOPS;
+
+public class WrapperExample {
+
+    public static void main(String[] args) {
+        Integer a = 20;
+        System.out.println(a.longValue());
+    }
+
 }
