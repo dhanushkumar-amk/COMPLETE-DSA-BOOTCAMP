66b81cd667630b6bc1005a1d9147f9457660e05e
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-07 14:56:04 +0530

    modified

--- src/SYSTEM_DESIGN/OOPS/StaticExample.java
+++ src/SYSTEM_DESIGN/OOPS/StaticExample.java
@@ -2,10 +2,12 @@ package SYSTEM_DESIGN.OOPS;
 
 public class StaticExample {
     public static void main(String[] args) {
-        Human dhanush = new Human(20, "dhanushkumar", 100000);
-        Human arun = new Human(20, "arunkumar", 200000);
-        Human sanjay = new Human(20, "arunkumar", 200000);
+//        Human dhanush = new Human(20, "dhanushkumar", 100000);
+//        Human arun = new Human(20, "arunkumar", 200000);
+//        Human sanjay = new Human(20, "arunkumar", 200000);
         System.out.println(Human.population);
+        
+
     }
 }
 
