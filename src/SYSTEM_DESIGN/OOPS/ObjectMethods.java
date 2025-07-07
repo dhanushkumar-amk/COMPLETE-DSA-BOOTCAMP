73e1c6d74769e2728db51565ee8fdac8ecca23d6
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-07 20:49:20 +0530

    modified

--- src/SYSTEM_DESIGN/OOPS/ObjectMethods.java
+++ src/SYSTEM_DESIGN/OOPS/ObjectMethods.java
@@ -1,8 +1,39 @@
 package SYSTEM_DESIGN.OOPS;
 
 public class ObjectMethods {
-    public ObjectMethods() {
+    
+    int num;
+
+    public ObjectMethods(int num) {
+        this.num = num;
     }
 
-    
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    public ObjectMethods() {
+        super();
+    } 
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
