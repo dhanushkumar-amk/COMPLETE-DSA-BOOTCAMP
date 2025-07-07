d26afa7ccc2aa55a5bd30c4eb959ef22db5ead71
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-07 13:27:54 +0530

    modified

--- src/SYSTEM_DESIGN/OOPS/WrapperExample.java
+++ src/SYSTEM_DESIGN/OOPS/WrapperExample.java
@@ -4,7 +4,7 @@ public class WrapperExample {
 
     public static void main(String[] args) {
         Integer a = 20;
-        System.out.println(a.longValue());
+        System.out.println(a.byteValue());
     }
 
 }
