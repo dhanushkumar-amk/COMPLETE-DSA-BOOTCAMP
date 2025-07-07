804efee51b9ec85bfb74836b76e7190b34d3a240
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-07 21:19:01 +0530

    modified

--- src/SYSTEM_DESIGN/OOPS/AbstractClassExample.java
+++ src/SYSTEM_DESIGN/OOPS/AbstractClassExample.java
@@ -2,27 +2,41 @@ package SYSTEM_DESIGN.OOPS;
 
 public class AbstractClassExample {
     public static void main(String[] args) {
-        Hp hp = new Hp();
-        hp.turnOn();
-        hp.turnOff();
+        son obj1 = new son();
+        daughter obj2 = new daughter();
+        
+        obj1.career("Developer");
+        obj1.partner("nothing");
+
+        obj2.career("doctor");
+        obj2.partner("john");
     }
 }
 
-abstract class computer{
-    void turnOn(){
-        System.out.println("Turning on");
-    }
-    abstract void turnOff();
+abstract class parent{
+    abstract void career(String career);
+    abstract void partner(String partnerName);
 }
 
-class Hp extends computer {
+class son extends parent{
     @Override
-    void turnOn() {
-        System.out.println("HP is turning on");
+    void career(String career) {
+        System.out.println("My career is become a " + career);
     }
 
     @Override
-    void turnOff() {
-        System.out.println("HP is turning off");
+    void partner(String partnerName) {
+        System.out.println("I don't have a partner yet" + partnerName);
+    }
+}
+class daughter extends parent{
+    @Override
+    void career(String career) {
+        System.out.println("I am become a" + career);
+    }
+
+    @Override
+    void partner(String partnerName) {
+        System.out.println("my name is " + partnerName);
     }
 }
