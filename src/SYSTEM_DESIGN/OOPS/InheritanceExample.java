fe2194aee2fd3ebf5e1e443c5327d87ee6323526
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-07 16:01:57 +0530

    modified

--- src/SYSTEM_DESIGN/OOPS/InheritanceExample.java
+++ src/SYSTEM_DESIGN/OOPS/InheritanceExample.java
@@ -5,19 +5,21 @@ public class InheritanceExample {
     String name;
     int age;
     int rollNo;
+    private boolean isMarried;
 
-    public InheritanceExample(int age, String name, int rollNo) {
+    public InheritanceExample(int age, String name, int rollNo, boolean isMarried) {
         this.age = age;
         this.name = name;
         this.rollNo = rollNo;
+        this.isMarried = isMarried;
     }
 }
 
 class child extends InheritanceExample {
     int no;
 
-    public child(int age, String name, int rollNo, int no) {
-        super(age, name, rollNo);
+    public child(int age, String name, int rollNo, int no, boolean isMarried) {
+        super(age, name, rollNo, isMarried);
         this.no = no;
     }
 
@@ -29,7 +31,8 @@ class child extends InheritanceExample {
     }
 
     public static void main(String[] args) {
-        child obj = new child(10, "dhanush", 21, 10);
+        child obj = new child(10, "dhanush", 21, 10, false);
         System.out.println(obj);
+//        System.out.println(obj.isMarried); // can't be accces tge private variables
     }
 }
