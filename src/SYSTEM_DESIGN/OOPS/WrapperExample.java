f2e56f8487d33ce333cffb733b411e0e1b3ed65a
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-07 13:32:12 +0530

    modified

--- src/SYSTEM_DESIGN/OOPS/WrapperExample.java
+++ src/SYSTEM_DESIGN/OOPS/WrapperExample.java
@@ -3,8 +3,8 @@ package SYSTEM_DESIGN.OOPS;
 public class WrapperExample {
 
     public static void main(String[] args) {
+//      a is assume as a object
         Integer a = 20;
-        System.out.println(a.toString());
+        System.out.println(a);
     }
-
 }
