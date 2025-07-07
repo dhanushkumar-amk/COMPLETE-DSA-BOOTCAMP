7e8e763a10e40759eb7dbc4b652f1796b02d546a
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-07 13:34:29 +0530

    modified

--- src/SYSTEM_DESIGN/OOPS/WrapperExample.java
+++ src/SYSTEM_DESIGN/OOPS/WrapperExample.java
@@ -5,6 +5,16 @@ public class WrapperExample {
     public static void main(String[] args) {
 //      a is assume as a object
         Integer a = 20;
-        System.out.println(a);
+        Integer b = 30;
+
+        swap(a, b);
+
+        System.out.println(a + " " + b);
+    }
+
+    static void swap(Integer a, Integer b){
+        Integer temp = a;
+        a = b;
+        b = temp;
     }
 }
