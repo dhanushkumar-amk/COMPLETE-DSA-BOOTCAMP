c6c645f3eb39d0c90fd24b60e3bf7bcdb2d5aa40
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-08 09:28:21 +0530

    modified

--- src/STRIVER_SHEET/PATTERNS/Pattern1.java
+++ src/STRIVER_SHEET/PATTERNS/Pattern1.java
@@ -2,9 +2,16 @@ package STRIVER_SHEET.PATTERNS;
 
 public class Pattern1 {
     public static void main(String[] args) {
-
+        nForest(5);
     }
 
-    
+    public static void nForest(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
 
 }
