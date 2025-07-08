7608e8c01bdde4d9854fcc88d803755614510d05
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-08 18:11:00 +0530

    modified

--- src/STRIVER_SHEET/PATTERNS/Pattern20.java
+++ src/STRIVER_SHEET/PATTERNS/Pattern20.java
@@ -13,10 +13,11 @@ public class Pattern20 {
             }
 
             char ch = 'A';
-            int breakPoint = (n * i + 1)/2;
-            for (int letter = 1; letter <= n * i + 1; letter++) {
+            int breakpoint = (2*i+1)/2;
+            for(int j=1;j<=2*i+1;j++){
+
                 System.out.print(ch);
-                if(letter <= breakPoint) ch++;
+                if(j <= breakpoint) ch++;
                 else ch--;
             }
 
