2cb3b22988d277a4fdae3af037c6b2a3c02506bb
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-08 09:38:57 +0530

    modified

--- src/STRIVER_SHEET/PATTERNS/pattern3.java
+++ src/STRIVER_SHEET/PATTERNS/pattern3.java
@@ -1,2 +1,15 @@
-package STRIVER_SHEET.PATTERNS;public class pattern3 {
+package STRIVER_SHEET.PATTERNS;
+
+public class pattern3 {
+    public static void main(String[] args) {
+        nTriangle(5);
+    }
+    public static void nTriangle(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
 }
