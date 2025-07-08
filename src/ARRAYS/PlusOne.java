3066fed206db2b12a7812077d30abf4ccca52af8
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-08 20:39:53 +0530

    modified

--- src/ARRAYS/PlusOne.java
+++ src/ARRAYS/PlusOne.java
@@ -13,48 +13,49 @@ public class PlusOne {
 
     public static int[] plusOne(int[] digits) {
 
-        try {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < digits.length; i++) {
-                sb.append(digits[i]);
-            }
 
-            String val = sb.toString();
-            int valOne = Integer.parseInt(val);
-
-
-            int ans = valOne + 1;
-
-            ArrayList<Integer> list = new ArrayList<>();
-            int index = 0;
-            while (ans > 0) {
-                int lastVal = ans % 10;
-                //arr[index] = lastVal;
-                list.add(index, lastVal);
-                ans /= 10;
-                index++;
-            }
-
-            int[] result = new int[list.size()];
-            for (int i = 0; i < list.size(); i++) {
-                result[i] = list.get(i);
-            }
-
-            int right = 0;
-            int left = result.length - 1;
-
-            while (right < left) {
-                int temp = result[right];
-                result[right] = result[left];
-                result[left] = temp;
-                right++;
-                left--;
-            }
-
-            return result;
-        }catch (NumberFormatException e){
-
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < digits.length; i++) {
+            sb.append(digits[i]);
         }
+
+        int valOne;
+        String val = sb.toString();
+        try {
+            valOne = Integer.parseInt(val);
+        }catch (NumberFormatException e){
+            
+        }
+        int ans = valOne + 1;
+
+        ArrayList<Integer> list = new ArrayList<>();
+        int index = 0;
+        while (ans > 0){
+            int lastVal = ans % 10;
+            //arr[index] = lastVal;
+            list.add(index, lastVal);
+            ans /= 10;
+            index++;
+        }
+
+        int[] result =new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+
+        int right = 0;
+        int left = result.length - 1;
+
+        while( right < left){
+            int temp = result[right];
+            result[right] = result[left];
+            result[left] = temp;
+            right++;
+            left--;
+        }
+
+        return result;
+
     }
 
 }
