cde0016db76901f2e5239789b4eacf144feb6175
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-08 09:51:37 +0530

    modified

--- src/STRIVER_SHEET/PATTERNS/Pattern5.java
+++ src/STRIVER_SHEET/PATTERNS/Pattern5.java
@@ -6,7 +6,7 @@ public class Pattern5 {
     }
     public static void seeding(int n) {
         for (int i = n; i > 0; ++i) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
