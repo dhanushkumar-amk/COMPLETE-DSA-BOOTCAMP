9212eba126c5509f22b908af64b57dc9a0005ff0
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-08 10:21:30 +0530

    modified

--- src/STRIVER_SHEET/PATTERNS/Pattern7.java
+++ src/STRIVER_SHEET/PATTERNS/Pattern7.java
@@ -1,2 +1,15 @@
-package STRIVER_SHEET.PATTERNS;public class Pattern7 {
+package STRIVER_SHEET.PATTERNS;
+
+public class Pattern7 {
+    public static void main(String[] args) {
+
+    }
+    public static void nStarTriangle(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i * 2 - 1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
