d163e026c4b1e37540c8aa9ec8e2dbf3428a7804
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-08 17:51:58 +0530

    modified

--- src/STRIVER_SHEET/PATTERNS/Pattern20.java
+++ src/STRIVER_SHEET/PATTERNS/Pattern20.java
@@ -7,10 +7,7 @@ public class Pattern20 {
 
     public static void alphaHill(int n) {
         for (int i = 0; i < n; i++) {
-            for (char j = 'A'; j <= 'A' + (n - i - 1); j++) {
-                System.out.print(j + " ");
-            }
-            System.out.println();
+        
         }
     }
 }
