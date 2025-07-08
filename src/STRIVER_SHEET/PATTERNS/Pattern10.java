fb634e6394e41f02fe8e0b57a2a566c62e6161e3
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-08 13:43:11 +0530

    modified

--- src/STRIVER_SHEET/PATTERNS/Pattern10.java
+++ src/STRIVER_SHEET/PATTERNS/Pattern10.java
@@ -9,7 +9,11 @@ public class Pattern10 {
     public static void upperPath(int n) {
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < i; ++j) {
-                System.out.print("* ");
+                System.out.print("*");
+            }
+
+            for (int j = 0; j < n; ++j) {
+                System.out.print(" ");
             }
             System.out.println();
         }
