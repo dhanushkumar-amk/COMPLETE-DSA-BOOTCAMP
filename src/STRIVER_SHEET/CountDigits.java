2b25658ccf6c68fcf217813142b1c60d06bcbc65
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-08 09:15:04 +0530

    modified

--- src/STRIVER_SHEET/CountDigits.java
+++ src/STRIVER_SHEET/CountDigits.java
@@ -2,9 +2,18 @@ package STRIVER_SHEET;
 
 public class CountDigits {
     public static void main(String[] args) {
-
+        int n = 123456;
+        System.out.println(countDigit(n));
     }
 
-    public 
+    public static int countDigit(int n){
+        int count = 0;
+        while(n > 0){
+            int lastDigit = n % 10;
+            count++;
+            n = n/10;
+        }
+        return count;
+    }
 
 }
