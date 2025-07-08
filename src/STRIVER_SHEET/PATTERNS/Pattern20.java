d74f8e9333f389a44b46879a2855828951775aac
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-08 18:09:16 +0530

    modified

--- src/STRIVER_SHEET/PATTERNS/Pattern20.java
+++ src/STRIVER_SHEET/PATTERNS/Pattern20.java
@@ -12,7 +12,14 @@ public class Pattern20 {
                 System.out.print(" ");
             }
 
-            
+            char ch = 'A';
+            int breakpoint = (2*i+1)/2;
+            for(int j=1;j<=2*i+1;j++){
+
+                System.out.print(ch);
+                if(j <= breakpoint) ch++;
+                else ch--;
+            }
 
             for (int suffixSpace = 0; suffixSpace < n - i - 1; suffixSpace++) {
                 System.out.print(" ");
