9e1dd32c77c5e5efdf1401cd9ef93df2da7f4457
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-08 13:09:16 +0530

    modified

--- src/STRIVER_SHEET/PATTERNS/Pattern8.java
+++ src/STRIVER_SHEET/PATTERNS/Pattern8.java
@@ -7,6 +7,20 @@ public class Pattern8 {
 
     public static void nStarTriangle(int n) {
         for (int i = 0; i < n; i++) {
+
+            for (int prefixSpace = 0; prefixSpace <  i; prefixSpace++) {
+                System.out.print(" ");
+            }
+
+            for (int star = 0; star < ; star++) {
+                
+            }
+            
+            for (int suffixSpace = 0; suffixSpace < i; suffixSpace++) {
+                System.out.print(" ");
+            }
+            
+            
             
         }
     }
