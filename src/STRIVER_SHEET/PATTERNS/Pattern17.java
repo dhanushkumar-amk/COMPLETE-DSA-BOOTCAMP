63adab85d1c96ffa234e3111e5a7d479630942fb
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-08 16:16:56 +0530

    modified

--- src/STRIVER_SHEET/PATTERNS/Pattern17.java
+++ src/STRIVER_SHEET/PATTERNS/Pattern17.java
@@ -5,5 +5,12 @@ public class Pattern17 {
 
     }
 
-    
+    public static void alphaRamp(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print((char) (65 + i) + " ");
+            }
+            System.out.println();
+        }
+    }
 }
