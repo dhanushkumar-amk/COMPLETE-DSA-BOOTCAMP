3ad5951c309f56c2f999a5e266d8df64b5da1187
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-08 20:21:04 +0530

    modified

--- src/ARRAYS/PlusOne.java
+++ src/ARRAYS/PlusOne.java
@@ -5,6 +5,27 @@ public class PlusOne {
 
     }
 
-    
+    public int[] plusOne(int[] digits) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < digits.length; i++) {
+            sb.append(digits[i]);
+        }
+
+        String val = sb.toString();
+        int valOne = Integer.parseInt(val);
+
+        int ans = valOne + 1;
+
+        int[] arr = new int[100];
+        int index = 0;
+        while (ans > 0){
+            int lastVal = ans % 10;
+            arr[index] = lastVal;
+            ans /= 10;
+            index++;
+        }
+        return arr;
+
+    }
 
 }
