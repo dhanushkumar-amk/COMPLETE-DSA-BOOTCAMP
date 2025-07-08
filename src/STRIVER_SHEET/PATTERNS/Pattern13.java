1a2163b2e248075508e5a386a961cc0022c8d4a4
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-08 14:22:15 +0530

    modified

--- src/STRIVER_SHEET/PATTERNS/Pattern13.java
+++ src/STRIVER_SHEET/PATTERNS/Pattern13.java
@@ -6,14 +6,16 @@ public class Pattern13 {
     }
 
     public static void nNumberTriangle(int n) {
-        for (int i = 0; i < n; i++) {
-            int startValue = 1;
-            for (int j = 0; j <= i; j++) {
-                System.out.print(startValue++ + " ");
-
+        int current=1;
+        for(int i=1;i<=n;++i)
+        {
+            for(int j=1;j<=i;++j)
+            {
+                System.out.print(current);
+                ++current;
             }
-
             System.out.println();
         }
+        
     }
 }
