b92a74a501022a45219c30231903c862215e17ca
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-08 09:42:34 +0530

    modified

--- src/STRIVER_SHEET/PATTERNS/Pattern4.java
+++ src/STRIVER_SHEET/PATTERNS/Pattern4.java
@@ -3,7 +3,7 @@ package STRIVER_SHEET.PATTERNS;
 
 public class Pattern4 {
     public static void main(String[] args) {
-        nTriangle(5);
+        nTriangle(3);
     }
     public static void nTriangle(int n) {
         for (int i = 0; i < n; i++) {
