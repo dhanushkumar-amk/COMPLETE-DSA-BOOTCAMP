0277bc70248f36aa1587140f754696362c3c120a
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-08 20:45:13 +0530

    modified

--- src/ARRAYS/PlusOne.java
+++ src/ARRAYS/PlusOne.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 
 public class PlusOne {
     public static void main(String[] args) {
-        int[] digits = {0};
+        int[] digits = {9,9,9};
         int[] ans = plusOne(digits);
 
         System.out.println(Arrays.toString(ans));
