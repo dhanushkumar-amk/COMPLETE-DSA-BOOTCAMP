03aa5777f7648f77e039661e47d7f45fbe613544
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-08 10:10:18 +0530

    modified

--- src/STRIVER_SHEET/PATTERNS/Pattern6.java
+++ src/STRIVER_SHEET/PATTERNS/Pattern6.java
@@ -8,7 +8,7 @@ public class Pattern6 {
         for (int i = 0; i < n; i++) {
             for(int j = n; j > 0; --j)
                 System.out.print(j + " ");
+            System.out.println();
         }
-       
     }
 }
