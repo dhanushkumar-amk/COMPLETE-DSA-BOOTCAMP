218816a41473693e2e03d45540cd875ec5e9952a
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-08 14:04:41 +0530

    modified

--- src/STRIVER_SHEET/PATTERNS/Pattern11.java
+++ src/STRIVER_SHEET/PATTERNS/Pattern11.java
@@ -1,2 +1,23 @@
-package STRIVER_SHEET.PATTERNS;public class Pattern11 {
+package STRIVER_SHEET.PATTERNS;
+
+public class Pattern11 {
+    public static void main(String[] args) {
+        nBinaryTriangle(5);
+    }
+
+    public static void nBinaryTriangle(int n) {
+        for (int i = 0; i < n; ++i) {
+            int firstValue = i % 2 == 0 ? 1 : 0;
+            for (int j = 0; j <= i ; ++j) {
+                if(firstValue == 1){
+                    System.out.print(1 + " ");
+                    firstValue = 0;
+                }else{
+                    System.out.print(0 + " ");
+                    firstValue = 1;
+                }
+            }
+            System.out.println();
+        }
+    }
 }
