eba26a40ba220b043911e52d083658ce992c12f8
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-08 13:32:02 +0530

    modified

--- src/STRIVER_SHEET/PATTERNS/Pattern9.java
+++ src/STRIVER_SHEET/PATTERNS/Pattern9.java
@@ -3,10 +3,47 @@ package STRIVER_SHEET.PATTERNS;
 public class Pattern9 {
 
     public static void main(String[] args) {
-
+        upperPart(5);
+        lowerPart(5);
     }
 
 
-    
+    public static void upperPart(int n){
+        for (int i = 0; i < n; i++) {
+
+
+            for (int prefixSpace = 0; prefixSpace < n - i - 1; prefixSpace++) {
+                System.out.print(" ");
+            }
+
+            for (int star = 0; star < n * i + 1; star++) {
+                System.out.print("*");
+            }
+
+            for (int suffixSpace = 0; suffixSpace < n - i - 1; suffixSpace++) {
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void lowerPart(int n){
+        for (int i = 0; i < n; i++) {
+
+            for (int prefixStar = 0; prefixStar < i; prefixStar++) {
+                System.out.print(" ");
+            }
+
+            for (int star = 0; star < 2 * n - (2 * i + 1); star++) {
+                System.out.print("*");
+            }
+
+            for (int suffixStar = 0; suffixStar < i; suffixStar++) {
+                System.out.print(" ");
+            }
+
+        }
+    }
 
 }
