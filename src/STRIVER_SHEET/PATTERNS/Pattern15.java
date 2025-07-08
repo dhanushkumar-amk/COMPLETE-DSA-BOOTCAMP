7412ff6b8372512b0bbe189db0f68173bcf7383e
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-08 15:41:37 +0530

    modified

--- src/STRIVER_SHEET/PATTERNS/Pattern15.java
+++ src/STRIVER_SHEET/PATTERNS/Pattern15.java
@@ -2,10 +2,15 @@ package STRIVER_SHEET.PATTERNS;
 
 public class Pattern15 {
     public static void main(String[] args) {
-
+        nLetterTriangle(5);
     }
 
     public static void nLetterTriangle(int n) {
-        
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j <= i; ++j) {
+                System.out.print((char)  'A' + j);
+            }
+            System.out.println();
+        }
     }
 }
