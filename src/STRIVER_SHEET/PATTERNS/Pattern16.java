ceef0e2d63787898dd60359bfa11724e7c00070e
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-08 16:10:40 +0530

    modified

--- src/STRIVER_SHEET/PATTERNS/Pattern16.java
+++ src/STRIVER_SHEET/PATTERNS/Pattern16.java
@@ -1,2 +1,9 @@
-package STRIVER_SHEET.PATTERNS;public class pattern16 {
+package STRIVER_SHEET.PATTERNS;
+
+public class Pattern16 {
+    public static void main(String[] args) {
+
+    }
+
+    
 }
