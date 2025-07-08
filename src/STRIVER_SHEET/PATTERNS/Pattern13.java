9a6d9f168b92aa824ae12652d2ab42a4a5ae6f6d
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-08 14:25:52 +0530

    modified

--- src/STRIVER_SHEET/PATTERNS/Pattern13.java
+++ src/STRIVER_SHEET/PATTERNS/Pattern13.java
@@ -7,11 +7,13 @@ public class Pattern13 {
 
     public static void nNumberTriangle(int n) {
         int current=1;
+        byte [] arr ={1,2,5,5};
         for(byte  i=1;i<=n;++i)
         {
             for(byte j=1;j<=i;++j)
             {
                 System.out.print(current + " ");
+                System.out.println(i==1 ? arr[i]:"");
                 ++current;
             }
             System.out.println();
