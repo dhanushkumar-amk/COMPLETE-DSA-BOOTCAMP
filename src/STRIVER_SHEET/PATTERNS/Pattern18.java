12c93c73d404ce61886d60472af819ec28ed8976
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-08 16:25:40 +0530

    modified

--- src/STRIVER_SHEET/PATTERNS/Pattern18.java
+++ src/STRIVER_SHEET/PATTERNS/Pattern18.java
@@ -5,16 +5,16 @@ public class Pattern18 {
         getStarPattern(5);
     }
 
-    public static void getStarPattern(int n) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if(i == 0 || i == n - 1 || j == 0 || j == n - 1){
-                    System.out.print("*");
-                }else{
-                    System.out.print(" ");
+        public static void getStarPattern(int n) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if(i == 0 || i == n - 1 || j == 0 || j == n - 1){
+                        System.out.print("*");
+                    }else{
+                        System.out.print(" ");
+                    }
                 }
+                System.out.println();
             }
-            System.out.println();
         }
-    }
 }
