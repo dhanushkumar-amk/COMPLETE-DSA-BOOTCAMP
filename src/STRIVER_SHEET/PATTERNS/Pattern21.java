26a09f4893ef7971d95fd3f2c526c1464e0435cd
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-09 22:30:59 +0530

    modified

--- src/STRIVER_SHEET/PATTERNS/Pattern21.java
+++ src/STRIVER_SHEET/PATTERNS/Pattern21.java
@@ -3,7 +3,6 @@ package STRIVER_SHEET.PATTERNS;
 public class Pattern21 {
     public static void main(String[] args) {
         upperPart(5);
-        lowerPart(5);
     }
 
     public static void upperPart(int n) {
@@ -27,7 +26,19 @@ public class Pattern21 {
         }
         int initialSpace = 2 * n - 2;
         for (int i = 0; i < n; i++) {
+            for (int prefixStars = 0; prefixStars < n - i; ++prefixStars) {
+                System.out.print("*");
+            }
 
+            for (int space = 0; space < initialSpace; ++space) {
+                System.out.print(" ");
+            }
+
+            for (int suffixStar = 0; suffixStar < n - i; ++suffixStar) {
+                System.out.print("*");
+            }
+            initialSpace -= 2;
+            System.out.println();
         }
     }
 
