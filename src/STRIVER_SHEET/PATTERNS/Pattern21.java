a6c963829db046ad3edeb3d958f8a16c5e06f76f
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-09 21:41:32 +0530

    modified

--- src/STRIVER_SHEET/PATTERNS/Pattern21.java
+++ src/STRIVER_SHEET/PATTERNS/Pattern21.java
@@ -2,11 +2,25 @@ package STRIVER_SHEET.PATTERNS;
 
 public class Pattern21 {
     public static void main(String[] args) {
-
+        symmetry(5);
     }
 
     public static void symmetry(int n) {
-        
+        for (int i = 0; i < n; ++i) {
+
+            for (int prefixStars = 0; prefixStars < n - i; ++prefixStars) {
+                System.out.print("*");
+            }
+
+            for (int space = 0; space < i * 2; space++) {
+                System.out.print(" ");
+            }
+
+            for (int suffixStar = 0; suffixStar < n - i; ++suffixStar) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
 }
