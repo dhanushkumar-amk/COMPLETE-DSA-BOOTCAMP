cb117781ba9b77647a28a33f948433c17888da6c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-09 22:30:32 +0530

    modified

--- src/STRIVER_SHEET/PATTERNS/Pattern21.java
+++ src/STRIVER_SHEET/PATTERNS/Pattern21.java
@@ -15,29 +15,35 @@ public class Pattern21 {
                 System.out.print("*");
             }
 
-            for (int space = 0; space < i * 2; ++space) {
+            for (int space = 0; space < initialSpace; ++space) {
                 System.out.print(" ");
             }
 
             for (int suffixStar = 0; suffixStar < n - i; ++suffixStar) {
                 System.out.print("*");
             }
+            initialSpace += 2;
             System.out.println();
+        }
+        int initialSpace = 2 * n - 2;
+        for (int i = 0; i < n; i++) {
+
         }
     }
 
     public static void lowerPart(int n){
         for (int i = 0; i < n; ++i) {
 
-            for (int prefixStar = 0; prefixStar < i; ++prefixStar) {
+            int initialSpace = 0;
+            for (int prefixStar = 0; prefixStar < i + 1; ++prefixStar) {
                 System.out.print("*");
             }
 
-            for (int spaces = 0; spaces < n * 2 -2; spaces++) {
+            for (int spaces = 0; spaces < ; spaces++) {
 
             }
 
-            for (int suffixStar = 0; suffixStar < i; suffixStar++) {
+            for (int suffixStar = 0; suffixStar < i + 1; suffixStar++) {
                 System.out.print("*");
             }
 
