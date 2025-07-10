3e61ad7384e90ec98da0f015dd2a1f82ee6cf833
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-10 20:03:51 +0530

    modified

--- src/STRIVER_SHEET/MATHS/CountDigits.java
+++ src/STRIVER_SHEET/MATHS/CountDigits.java
@@ -3,10 +3,10 @@ package STRIVER_SHEET.MATHS;
 public class CountDigits {
     public static void main(String[] args) {
         int n = 336;
-        System.out.println(countDigit(n));
+        System.out.println(countDigits(n));
     }
 
-    public static int countDigit(int n){
+    public static int countDigits(int n){
         int count = 0;
         while(n > 0){
             count++;
