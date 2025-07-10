ad9783ef9d2c018abab279c49d40537f7623cd78
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-10 21:43:18 +0530

    modified

--- src/STRIVER_SHEET/MATHS/GCD.java
+++ src/STRIVER_SHEET/MATHS/GCD.java
@@ -1,2 +1,17 @@
-package STRIVER_SHEET.MATHS;public class GCD {
+package STRIVER_SHEET.MATHS;
+
+public class GCD {
+    public static void main(String[] args) {
+        System.out.println(gcd(20, 40));
+    }
+    public static  int gcd(int a, int b){
+
+        while (a > 0 && b > 0){
+            if(a > b)
+                a = a % b;
+            else
+                b = b % a;
+        }
+        return a == 0 ? b : a;
+    }
 }
