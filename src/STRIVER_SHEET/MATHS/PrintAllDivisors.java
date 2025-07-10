2254ad4700004490bc97477a2b0f94c3fdc4a893
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-10 20:37:51 +0530

    modified

--- src/STRIVER_SHEET/MATHS/PrintAllDivisors.java
+++ src/STRIVER_SHEET/MATHS/PrintAllDivisors.java
@@ -1,2 +1,15 @@
-package STRIVER_SHEET.MATHS;public class PrintAllDivisors {
+package STRIVER_SHEET.MATHS;
+
+public class PrintAllDivisors {
+    public static void main(String[] args) {
+        printDivisors(36);
+    }
+
+    public static void printDivisors(int n){
+
+        for (int i = 1; i <= n; i++) {
+            if(n % i == 0)
+                System.out.print(i + " ");
+        }
+    }
 }
