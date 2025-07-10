767ef538ffbcc4bb650458855b417b82a5acfd85
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-10 21:03:07 +0530

    modified

--- src/STRIVER_SHEET/MATHS/PrimeNumber.java
+++ src/STRIVER_SHEET/MATHS/PrimeNumber.java
@@ -1,2 +1,19 @@
-package STRIVER_SHEET.MATHS;public class PrimeNumber {
+package STRIVER_SHEET.MATHS;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        System.out.println(isPrime(11));
+    }
+
+    public  static boolean isPrime(int n){
+        int count = 0;
+        for (int i = 1; i <= n ; i++) {
+            if(n % i == 0){
+                count++;
+                if(n / i != i)
+                    count++;
+            }
+        }
+        return count == 2 ? true : false;
+    }
 }
