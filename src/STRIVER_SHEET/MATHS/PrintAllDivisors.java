8df5e28cefc717f021998b498af618b249f87c40
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-10 20:44:36 +0530

    modified

--- src/STRIVER_SHEET/MATHS/PrintAllDivisors.java
+++ src/STRIVER_SHEET/MATHS/PrintAllDivisors.java
@@ -7,9 +7,11 @@ public class PrintAllDivisors {
 
     public static void printDivisors(int n){
 
-        for (int i = 1; i <= n; i++) {
-            if(n % i == 0)
-                System.out.print(i + " ");
+        for (int i = 1; i <= Math.sqrt(36); i++) {
+            if(n % i == 0){
+                System.out.println(i+ " "+  n / i + " ");
+            }
+
         }
     }
 }
