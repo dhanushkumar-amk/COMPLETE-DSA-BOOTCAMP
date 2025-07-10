de443f34ca75d24f9be5a0a4deb93754b054643f
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-10 20:06:49 +0530

    modified

--- src/STRIVER_SHEET/MATHS/ReverseANumber.java
+++ src/STRIVER_SHEET/MATHS/ReverseANumber.java
@@ -1,4 +1,13 @@
 package STRIVER_SHEET.MATHS;
 
 public class ReverseANumber {
+    public static void main(String[] args) {
+
+    }
+
+    public static int reverse(){
+        int ans = 0;
+        w
+    }
+
 }
