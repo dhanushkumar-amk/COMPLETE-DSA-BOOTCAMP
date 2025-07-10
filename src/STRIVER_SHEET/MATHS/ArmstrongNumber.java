3d9e979f59425d336402e03915a0990790f800cc
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-10 20:32:00 +0530

    modified

--- src/STRIVER_SHEET/MATHS/ArmstrongNumber.java
+++ src/STRIVER_SHEET/MATHS/ArmstrongNumber.java
@@ -1,4 +1,19 @@
 package STRIVER_SHEET.MATHS;
 
-public class AmstrongNumber {
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        System.out.println(isArmstrongNumber(153));
+    }
+
+    public static  boolean isArmstrongNumber(int n){
+        int x = n;
+        int temp = 0;
+        while(n != 0){
+            int rem = n % 10;
+            temp += rem * rem * rem;
+            n /= 10;
+        }
+        return temp == x ? true : false;
+    }
+
 }
