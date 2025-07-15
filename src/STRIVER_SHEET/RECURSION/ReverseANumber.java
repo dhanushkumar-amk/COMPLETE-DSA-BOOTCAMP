9c1e557b7f552cbc56e51c3d0e30a0cf6858f90a
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-16 05:21:31 +0530

    modified

--- src/STRIVER_SHEET/RECURSION/ReverseANumber.java
+++ src/STRIVER_SHEET/RECURSION/ReverseANumber.java
@@ -1,2 +1,28 @@
-package STRIVER_SHEET.RECURSION;public class ReverseANumber {
+package STRIVER_SHEET.RECURSION;
+
+
+import java.util.Arrays;
+
+public class ReverseANumber {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+
+        reverse(0, arr, arr.length);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void reverse(int index, int[] arr, int n){
+        if(index >= n / 2)
+            return;
+
+        swap(arr[index], arr[n - index - 1]);
+
+        reverse(index + 1, arr, n);
+    }
+
+    static void swap(int left, int right){
+        int temp = left;
+        left = right;
+        right = temp;
+    }
 }
