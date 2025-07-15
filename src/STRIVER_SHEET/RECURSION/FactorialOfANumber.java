d58bf1b233be1cd4a9cc85157338e3b892bb9d85
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-16 05:02:08 +0530

    modified

--- src/STRIVER_SHEET/RECURSION/FactorialOfANumber.java
+++ src/STRIVER_SHEET/RECURSION/FactorialOfANumber.java
@@ -1,2 +1,14 @@
-package STRIVER_SHEET.RECURSION;public class FactorialOfANumber {
+package STRIVER_SHEET.RECURSION;
+
+public class FactorialOfANumber {
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+
+    static int factorial(int n){
+        if(n == 1)
+            return 1;
+
+        return n * factorial(n - 1);
+    }
 }
