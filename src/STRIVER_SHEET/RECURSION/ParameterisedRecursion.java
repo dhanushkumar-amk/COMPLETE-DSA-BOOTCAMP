dcd5a1cbc251a9f08ba0b437fc9f3ab5e0ba2418
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-16 04:52:26 +0530

    modified

--- src/STRIVER_SHEET/RECURSION/ParameterisedRecursion.java
+++ src/STRIVER_SHEET/RECURSION/ParameterisedRecursion.java
@@ -1,2 +1,18 @@
-package STRIVER_SHEET.RECURSION;public class ParameterisedRecursion {
+package STRIVER_SHEET.RECURSION;
+
+public class ParameterisedRecursion {
+    public static void main(String[] args) {
+        func(5, 0) ;
+    }
+
+    static void func(int i, int sum){
+        if( i < 1){
+            System.out.println("Sum : " + sum);
+            return;
+        }
+
+        func(i - 1, sum + i);
+
+    }
+
 }
