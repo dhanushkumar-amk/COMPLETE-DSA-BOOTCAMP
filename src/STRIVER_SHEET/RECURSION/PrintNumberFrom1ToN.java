7f018715728b406816d51f4236be78094bd8fda9
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-16 04:29:37 +0530

    modified

--- src/STRIVER_SHEET/RECURSION/PrintNumberFrom1ToN.java
+++ src/STRIVER_SHEET/RECURSION/PrintNumberFrom1ToN.java
@@ -2,6 +2,14 @@ package STRIVER_SHEET.RECURSION;
 
 public class PrintNumberFrom1ToN {
     public static void main(String[] args) {
+        func(1, 5);
+    }
 
+    static void func(int i, int n){
+        if(i > n)
+            return;
+
+        System.out.println("i : " +  i);
+        func(i - 1, n);
     }
 }
