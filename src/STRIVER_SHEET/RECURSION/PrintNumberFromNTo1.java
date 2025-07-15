900d81bbab463cac87407ea912e885af6ed66338
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-16 04:31:31 +0530

    modified

--- src/STRIVER_SHEET/RECURSION/PrintNumberFromNTo1.java
+++ src/STRIVER_SHEET/RECURSION/PrintNumberFromNTo1.java
@@ -1,2 +1,16 @@
-package STRIVER_SHEET.RECURSION;public class PrintNumberFromNTo1 {
+package STRIVER_SHEET.RECURSION;
+
+public class PrintNumberFromNTo1 {
+    public static void main(String[] args) {
+        func(1, 5);
+    }
+
+    static void func(int i, int n){
+        if(i > n)
+            return;
+
+        func(i + 1, n);
+        System.out.println(i);
+
+    }
 }
