fd6bdaaa193d85cdd506e596fac696f78334aa59
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-16 04:58:23 +0530

    modified

--- src/STRIVER_SHEET/RECURSION/FunctionalRecursion.java
+++ src/STRIVER_SHEET/RECURSION/FunctionalRecursion.java
@@ -1,2 +1,15 @@
-package STRIVER_SHEET.RECURSION;public class FunctionalRecursion {
+package STRIVER_SHEET.RECURSION;
+
+public class FunctionalRecursion {
+    public static void main(String[] args) {
+        System.out.println(func(5));
+    }
+
+    static  int func(int n){
+        if(n == 0)
+            return 0;
+
+        return n + func(n - 1);
+    }
+
 }
