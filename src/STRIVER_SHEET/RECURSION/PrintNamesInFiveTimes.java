a3d95386d897410d841e22f3aba327d49928a3d5
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-16 04:22:34 +0530

    modified

--- src/STRIVER_SHEET/RECURSION/PrintNamesInFiveTimes.java
+++ src/STRIVER_SHEET/RECURSION/PrintNamesInFiveTimes.java
@@ -1,2 +1,18 @@
-package STRIVER_SHEET.RECURSION;public class PrintNamesInFiveTimes {
+package STRIVER_SHEET.RECURSION;
+
+public class PrintNamesInFiveTimes {
+    public static void main(String[] args) {
+        func();
+    }
+
+    static int count = 0;
+ public  static void func(){
+        if(count == 5)
+            return;
+
+        System.out.println("dhanushkumar");
+        count++;
+        func();
+    }
+
 }
