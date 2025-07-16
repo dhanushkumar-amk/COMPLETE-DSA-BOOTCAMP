dff16f6acebd9cb9978c4fe93e59fc7398e3b90f
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-16 05:32:34 +0530

    modified

--- src/STRIVER_SHEET/RECURSION/Palindrome.java
+++ src/STRIVER_SHEET/RECURSION/Palindrome.java
@@ -3,7 +3,7 @@ package STRIVER_SHEET.RECURSION;
 public class Palindrome {
 
     public static void main(String[] args) {
-        System.out.println(isPalindrome(0, "madam", "madam".length()));
+        System.out.println(isPalindrome(0, "madams", "madams".length()));
     }
 
     static boolean isPalindrome(int index, String s, int n){
