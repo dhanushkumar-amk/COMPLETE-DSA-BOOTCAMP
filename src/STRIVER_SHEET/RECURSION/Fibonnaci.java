8f9db8684cdc56f14531747a8a4c850364155c9d
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-16 06:05:24 +0530

    modified

--- src/STRIVER_SHEET/RECURSION/Fibonnaci.java
+++ src/STRIVER_SHEET/RECURSION/Fibonnaci.java
@@ -1,2 +1,16 @@
-package STRIVER_SHEET.RECURSION;public class Fibonnaci {
+package STRIVER_SHEET.RECURSION;
+
+public class Fibonnaci {
+    public static void main(String[] args) {
+        System.out.println(fibo(5));
+    }
+
+    static int fibo(int n){
+        if(n <= 1)
+            return n;
+
+        int last = fibo(n - 1);
+        int secondLast = fibo(n - 2);
+        return last + secondLast;
+    }
 }
