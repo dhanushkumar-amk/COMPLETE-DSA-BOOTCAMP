7d755ea9627d33683a4ca4816b144588e87c216d
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-17 05:26:50 +0530

    modified

--- src/STRIVER_SHEET/HASHMAP/CountFrequency.java
+++ src/STRIVER_SHEET/HASHMAP/CountFrequency.java
@@ -1,2 +1,21 @@
-package STRIVER_SHEET.HASHMAP;public class CountFrequency {
+package STRIVER_SHEET.HASHMAP;
+
+import java.util.HashMap;
+
+public class CountFrequency {
+    public static void main(String[] args) {
+
+    }
+
+    static int countFreq(int[] arr, int n){
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            hashMap.put(i, hashMap.getOrDefault(hashMap.get(i) + 1), 0);
+        }
+
+
+
+    }
+
 }
