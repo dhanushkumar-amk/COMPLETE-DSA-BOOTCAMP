19dedac51553c953d5ceb1010b7fcf7bf5e0d125
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-16 05:32:22 +0530

    modified

--- src/STRIVER_SHEET/RECURSION/Palindrome.java
+++ src/STRIVER_SHEET/RECURSION/Palindrome.java
@@ -1,2 +1,20 @@
-package STRIVER_SHEET.RECURSION;public class Palindrome {
+package STRIVER_SHEET.RECURSION;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(0, "madam", "madam".length()));
+    }
+
+    static boolean isPalindrome(int index, String s, int n){
+
+        if(index >= n / 2)
+            return true;
+
+        if(s.charAt(index) != s.charAt(n - index - 1))
+            return false;
+
+        return isPalindrome(index + 1, s, n);
+
+    }
 }
