67555612e0092e6f4444970edfa490f8ab43bc81
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-17 16:53:08 +0530

    modified

--- src/STRIVER_SHEET/SORTING/SelectionSort.java
+++ src/STRIVER_SHEET/SORTING/SelectionSort.java
@@ -1,2 +1,26 @@
-package STRIVER_SHEET.SORTING;public class SelectionSort {
+package STRIVER_SHEET.SORTING;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1};
+        selectionSort(arr);
+
+        for(int ar : arr)
+            System.out.print(arr);
+    }
+
+    static void selectionSort(int[] arr){
+
+        int currentStartIndex = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            int minValue = Integer.MAX_VALUE;
+            if(arr[i] < minValue)
+                minValue = i;
+
+           int temp = arr[currentStartIndex];
+           arr[currentStartIndex] = arr[minValue];
+           arr[minValue] = temp;
+        }
+    }
 }
