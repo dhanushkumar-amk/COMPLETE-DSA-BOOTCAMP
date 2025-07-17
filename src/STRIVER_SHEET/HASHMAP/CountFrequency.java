f8975913b9cc18084d3b0080dc07ccc98fc3e8b1
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-17 05:30:06 +0530

    modified

--- src/STRIVER_SHEET/HASHMAP/CountFrequency.java
+++ src/STRIVER_SHEET/HASHMAP/CountFrequency.java
@@ -1,25 +1,24 @@
 package STRIVER_SHEET.HASHMAP;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class CountFrequency {
     public static void main(String[] args) {
-
+        int[] arr = {1,1,1,1,1,1,2,3,4,4,5,5};
+        countFreq(arr);
     }
 
-    static int countFreq(int[] arr, int n){
+    static void countFreq(int[] arr){
         HashMap<Integer, Integer> hashMap = new HashMap<>();
 
         for (int i = 0; i < arr.length; i++) {
             hashMap.put(arr[i], hashMap.getOrDefault(arr[i], 0) + 1);
         }
-
-        for (int i = 0; i < hashMap.size(); i++) {
-
+        // Print the frequencies
+        for (Map.Entry<Integer, Integer> entry : hashMap.entrySet()) {
+            System.out.println("Element: " + entry.getKey() + " -> Frequency: " + entry.getValue());
         }
-
-
-
     }
 
 }
