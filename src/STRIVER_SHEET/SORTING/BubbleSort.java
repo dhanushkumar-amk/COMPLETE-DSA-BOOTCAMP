bb57fce72ab34aa66817ac774d095c9b6c1057c3
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-17 17:16:39 +0530

    modified

--- src/STRIVER_SHEET/SORTING/BubbleSort.java
+++ src/STRIVER_SHEET/SORTING/BubbleSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {5,4,3,2,1};
+        int[] arr = {5,4,3,2,1,7, 6};
         bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
     }
