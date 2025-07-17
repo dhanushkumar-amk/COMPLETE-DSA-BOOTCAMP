4d344c8ab97ef666d41af553715a23f02efbc8e5
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-17 17:38:27 +0530

    modified

--- src/STRIVER_SHEET/SORTING/InsertionSort.java
+++ src/STRIVER_SHEET/SORTING/InsertionSort.java
@@ -1,2 +1,7 @@
-package STRIVER_SHEET.SORTING;public class InsertionSort {
+package STRIVER_SHEET.SORTING;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        
+    }
 }
