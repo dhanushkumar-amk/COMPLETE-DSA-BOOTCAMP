cdeecd9d355d90bc6e10507e76e3186e977f9e5e
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-17 17:16:12 +0530

    modified

--- src/STRIVER_SHEET/SORTING/BubbleSort.java
+++ src/STRIVER_SHEET/SORTING/BubbleSort.java
@@ -1,2 +1,24 @@
-package STRIVER_SHEET.SORTING;public class BubbleSort {
+package STRIVER_SHEET.SORTING;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubbleSort(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if(arr[j] > arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+
+            }
+        }
+    }
 }
