22e162a4fa3853baa2e8d906ac10f6969a4f48a8
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-17 06:15:15 +0530

    modified

--- src/STRIVER_SHEET/HASHMAP/HighestAndLowestFrequencyElements.java
+++ src/STRIVER_SHEET/HASHMAP/HighestAndLowestFrequencyElements.java
@@ -1,2 +1,27 @@
-package STRIVER_SHEET.HASHMAP;public class HighestAndLowestFrequencyElements {
+package STRIVER_SHEET.HASHMAP;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HighestAndLowestFrequencyElements {
+    public static void main(String[] args) {
+        int[] arr = {1,1,1,1,1,2,2,2,2,2,2,3,4,4,4};
+        freq(arr);
+
+    }
+
+    static void freq(int[] arr){
+
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            hashMap.put(arr[i], hashMap.getOrDefault(arr[i], 0) + 1);
+        }
+
+        int maxValue = Integer.MIN_VALUE;
+        int minValue = Integer.MAX_VALUE;
+
+       for(Map.entry(Integer))
+
+    }
 }
