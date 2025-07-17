a233b516684bb8d0426d096703deb542060499b5
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-17 17:18:06 +0530

    modified

--- src/STRIVER_SHEET/SORTING/BubbleSort.java
+++ src/STRIVER_SHEET/SORTING/BubbleSort.java
@@ -11,14 +11,17 @@ public class BubbleSort {
 
     static void bubbleSort(int[] arr){
         for (int i = 0; i < arr.length; i++) {
+            boolean flag = false;
             for (int j = 0; j < arr.length - i - 1; j++) {
                 if(arr[j] > arr[j + 1]){
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+                    flag = true;
                 }
-
             }
+            if(flag == false)
+                break;
         }
     }
 }
