845edac1179dcffc0762b40e2bfd6300b337077c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-17 17:40:21 +0530

    modified

--- src/STRIVER_SHEET/SORTING/InsertionSort.java
+++ src/STRIVER_SHEET/SORTING/InsertionSort.java
@@ -12,7 +12,7 @@ public class InsertionSort {
 
     private static void insertionSort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j >=0; j--) {
+            for (int j = i + 1; j > 0; j--) {
                 if(arr[j] < arr[j - 1]){
                     int temp = arr[j];
                     arr[j] = arr[j - 1];
