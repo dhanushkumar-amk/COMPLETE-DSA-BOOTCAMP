cc23f06452471be3b25398adf85bcb0ef60b3f31
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-17 18:15:19 +0530

    modified

--- src/STRIVER_SHEET/SORTING/MergeSort.java
+++ src/STRIVER_SHEET/SORTING/MergeSort.java
@@ -1,2 +1,17 @@
-package STRIVER_SHEET.SORTING;public class MergeSort {
+package STRIVER_SHEET.SORTING;
+
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = {2,5,1,4,3,2};
+        mergeSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void mergeSort(int[] arr) {
+        
+    }
+
+
 }
