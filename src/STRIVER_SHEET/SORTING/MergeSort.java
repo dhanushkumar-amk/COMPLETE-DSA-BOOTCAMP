ae574deee5bb2d7bfcd966bd0730b3a6ab868e2d
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-18 06:50:14 +0530

    modified

--- src/STRIVER_SHEET/SORTING/MergeSort.java
+++ src/STRIVER_SHEET/SORTING/MergeSort.java
@@ -34,11 +34,9 @@ public class MergeSort {
     }
 
     private static void merge(int[] arr, int low, int mid, int high) {
-        ArrayList<Integer> temp = new ArrayList<>(); // temporary array
-        int left = low;      // starting index of left half of arr
-        int right = mid + 1;   // starting index of right half of arr
-
-        //storing elements in the temporary array in a sorted manner//
+        ArrayList<Integer> temp = new ArrayList<>();
+        int left = low;
+        int right = mid + 1;
 
         while (left <= mid && right <= high) {
             if (arr[left] <= arr[right]) {
@@ -50,25 +48,18 @@ public class MergeSort {
             }
         }
 
-        // if elements on the left half are still left //
-
         while (left <= mid) {
             temp.add(arr[left]);
             left++;
         }
 
-        //  if elements on the right half are still left //
         while (right <= high) {
             temp.add(arr[right]);
             right++;
         }
 
-        // transfering all elements from temporary to arr //
         for (int i = low; i <= high; i++) {
-            arr[i] = temp.get(i - low);
+            arr[i] = temp.get(i);
         }
-
     }
-
-
 }
