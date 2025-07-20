e5cd79e20a65cbb0ba29f266d8e71f2a862b3e64
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-21 03:56:16 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/LargestElementInArray.java
+++ src/STRIVER_SHEET/ARRAYS/LargestElementInArray.java
@@ -1,2 +1,18 @@
-package STRIVER_SHEET.ARRAYS;public class LargestElementInArray {
+package STRIVER_SHEET.ARRAYS;
+
+public class LargestElementInArray {
+    public static void main(String[] args) {
+        int[] arr = {2,5,1,3,9,7,10,2,5,6};
+        System.out.println(largest(arr));
+    }
+
+    static int largest(int[] arr){
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] > max)
+                max = arr[i];
+        }
+        return max;
+    }
+
 }
