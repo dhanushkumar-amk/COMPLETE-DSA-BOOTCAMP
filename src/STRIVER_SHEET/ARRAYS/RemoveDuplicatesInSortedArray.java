6d763af2576bce961619277a553d85bb72986851
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-21 04:39:59 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/RemoveDuplicatesInSortedArray.java
+++ src/STRIVER_SHEET/ARRAYS/RemoveDuplicatesInSortedArray.java
@@ -2,8 +2,10 @@ package STRIVER_SHEET.ARRAYS;
 
 public class RemoveDuplicatesInSortedArray {
     public static void main(String[] args) {
-
+        int[] arr = 
     }
 
-    
+    static int removeDuplicates(int[] arr){
+
+    }
 }
