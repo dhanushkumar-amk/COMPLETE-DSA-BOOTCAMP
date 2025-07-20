0e6352e91ddb1a028e75ccbfa4d4fa9d02d3a334
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-21 04:12:28 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/CheckIfArrayIsSorted.java
+++ src/STRIVER_SHEET/ARRAYS/CheckIfArrayIsSorted.java
@@ -1,2 +1,15 @@
-package STRIVER_SHEET.ARRAYS;public class CheckIfArrayIsSorted {
+package STRIVER_SHEET.ARRAYS;
+
+public class CheckIfArrayIsSorted {
+    public static void main(String[] args) {
+        int[] arr = {1,4,6,8,9,10};
+        System.out.println(isSorted(arr));
+    }
+
+    static boolean isSorted(int[] arr){
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i] > arr[i - 1])
+                
+        }
+    }
 }
