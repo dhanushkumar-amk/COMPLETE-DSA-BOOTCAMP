5a1bd5804ac24876f456d3ff1f79d6b7e15d8957
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-21 04:47:28 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/RemoveDuplicatesInSortedArray.java
+++ src/STRIVER_SHEET/ARRAYS/RemoveDuplicatesInSortedArray.java
@@ -1,12 +1,29 @@
 package STRIVER_SHEET.ARRAYS;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RemoveDuplicatesInSortedArray {
     public static void main(String[] args) {
         int[] arr = {4,6,7,8,9,9,10};
-        System.out.println(removeDuplicates(arr));
+
     }
 
-    static int removeDuplicates(int[] arr){
-        
+    static int[] removeDuplicates(int[] arr){
+
+        Set<Integer> set = new HashSet<>();
+
+        for(int num : arr){
+            set.add(num);
+        }
+
+        int[] ans = new int[set.size() + 1];
+
+        for (int i = 0; i < set.size(); i++) {
+            arr[i] = set.
+        }
+
+
+
     }
 }
