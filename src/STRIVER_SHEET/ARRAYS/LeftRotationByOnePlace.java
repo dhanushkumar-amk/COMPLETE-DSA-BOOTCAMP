af14011472a2bb5b764d9963d5f36e65aac8e830
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-21 05:16:18 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/LeftRotationByOnePlace.java
+++ src/STRIVER_SHEET/ARRAYS/LeftRotationByOnePlace.java
@@ -3,7 +3,21 @@ package STRIVER_SHEET.ARRAYS;
 public class LeftRotationByOnePlace {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5};
+        leftRotate(arr);
+    }
+    static void leftRotate(int[] arr){
+        int temp = arr[0];
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+
+        arr[arr.length - 1] = temp;
+
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
 
     }
-    static void leftRotate(int[] arr)
 }
