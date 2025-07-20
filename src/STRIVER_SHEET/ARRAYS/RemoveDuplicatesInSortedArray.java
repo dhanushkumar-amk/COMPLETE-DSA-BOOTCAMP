f66b8c179120cd78273b2276f350e25d32f274a4
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-21 04:54:44 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/RemoveDuplicatesInSortedArray.java
+++ src/STRIVER_SHEET/ARRAYS/RemoveDuplicatesInSortedArray.java
@@ -6,10 +6,20 @@ import java.util.Set;
 public class RemoveDuplicatesInSortedArray {
     public static void main(String[] args) {
         int[] arr = {4,6,7,8,9,9,10};
-
+        int range = removeDuplicates(arr);
+        for (int i = 0; i < range; ++i) {
+            System.out.print(arr[i] + " ");
+        }
     }
 
     static int removeDuplicates(int[] arr){
-        
+        int i = 0;
+        for (int j = 1; j < arr.length; ++j) {
+            if(arr[i] != arr[j]){
+                arr[i + 1]  = arr[j];
+                i++;
+            }
+        }
+        return i + 1;
     }
 }
