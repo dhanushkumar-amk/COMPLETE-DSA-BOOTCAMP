6556cdb849905582d494230208b7e6bc4d8412e2
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-21 04:02:19 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/SecondLargestElementInArray.java
+++ src/STRIVER_SHEET/ARRAYS/SecondLargestElementInArray.java
@@ -1,2 +1,23 @@
-package STRIVER_SHEET.ARRAYS;public class SecondLargestElementInarray {
+package STRIVER_SHEET.ARRAYS;
+
+public class SecondLargestElementInArray {
+    public static void main(String[] args) {
+        int[] arr = {17,19, 57, 43,55, 36,47,98};
+        System.out.println(secondLargest(arr));
+    }
+
+    static int secondLargest(int[] arr){
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = largest;
+
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] > largest){
+                secondLargest = arr[i];
+                largest = arr[i];
+            }
+            return secondLargest;
+        }
+
+    }
+
 }
