ab8b7e7ca4138e9353b4bd839b34e41c192e0fe2
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-21 03:58:59 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/SmallestElementInArray.java
+++ src/STRIVER_SHEET/ARRAYS/SmallestElementInArray.java
@@ -1,2 +1,17 @@
-package STRIVER_SHEET.ARRAYS;public class SmallestElementInArray {
+package STRIVER_SHEET.ARRAYS;
+
+public class SmallestElementInArray {
+    public static void main(String[] args) {
+        int[] arr = {2,5,1,3,9,7,10,2,5,6};
+        System.out.println(smallest(arr));
+    }
+
+    private static int smallest(int[] arr) {
+    int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; ++i) {
+            if(arr[i] < min)
+                min = arr[i];
+        }
+        return min;
+    }
 }
