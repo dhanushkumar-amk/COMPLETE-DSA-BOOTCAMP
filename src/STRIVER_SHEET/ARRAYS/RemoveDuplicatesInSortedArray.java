9b311ec8bf9003fe6c17dfb0e91316ac770654c6
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-21 05:01:01 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/RemoveDuplicatesInSortedArray.java
+++ src/STRIVER_SHEET/ARRAYS/RemoveDuplicatesInSortedArray.java
@@ -12,6 +12,7 @@ public class RemoveDuplicatesInSortedArray {
             System.out.print(arr[i] + " ");
         }
 
+        System.out.println();
         ArrayList<Integer> ans = removeDuplicates1(arr);
         for (int i = 0; i < ans.size(); i++) {
             System.out.print(ans.get(i) + " ");
