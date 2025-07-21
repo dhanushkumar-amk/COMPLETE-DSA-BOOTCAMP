09464d55ce043bf706324c7ea1c0df9bc4f566c7
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-21 06:05:14 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/RotateTheArrayByDPlaces.java
+++ src/STRIVER_SHEET/ARRAYS/RotateTheArrayByDPlaces.java
@@ -2,10 +2,17 @@ package STRIVER_SHEET.ARRAYS;
 
 public class RotateTheArrayByDPlaces {
     public static void main(String[] args) {
-
+        int[] arr = {1,2,3,4,5,6,7};
+        int n = 7;
+        int k = 2;
+        rotate(arr, k);
+        System.out.print("After Rotating the k elements to right ");
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
     }
 
-    public void rotate(int[] arr, int k) {
+    public static  void rotate(int[] arr, int k) {
         int n = arr.length;
         // Reverse first n-k elements
         Reverse(arr, 0, n - k - 1);
