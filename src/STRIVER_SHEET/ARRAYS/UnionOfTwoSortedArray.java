23833c0f77fe7d826dd6231128ee0e8a0d51d64e
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-21 18:55:59 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/UnionOfTwoSortedArray.java
+++ src/STRIVER_SHEET/ARRAYS/UnionOfTwoSortedArray.java
@@ -2,9 +2,22 @@ package STRIVER_SHEET.ARRAYS;
 
 public class UnionOfTwoSortedArray {
     public static void main(String[] args) {
+        int[] arr1 = {1,1,2,3,4,5};
+        int[] arr2 = {2,3,4,4,5,6};
+
 
     }
 
-    
+    static  int[] unionOfArray(int[] arr1, int[] arr2){
+        int n1 = arr1.length;
+        int n2 = arr2.length;
+
+        int i = 0;
+        int j = 0;
+
+        
+
+    }
+
 
 }
