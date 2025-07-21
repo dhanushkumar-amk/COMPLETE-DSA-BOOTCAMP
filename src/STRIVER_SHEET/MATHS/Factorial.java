da78603df45390400d109f6754936bdf9b3fec00
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-21 06:31:56 +0530

    modified

--- src/STRIVER_SHEET/MATHS/Factorial.java
+++ src/STRIVER_SHEET/MATHS/Factorial.java
@@ -1,2 +1,22 @@
-package STRIVER_SHEET.MATHS;public class Factorial {
+package STRIVER_SHEET.MATHS;
+
+import java.util.ArrayList;
+
+public class Factorial {
+    public static void main(String[] args) {
+        ArrayList<Long> ans = factorialNumbers(5);
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print(ans.get(i) + " ");
+        }
+    }
+
+    public static ArrayList<Long> factorialNumbers(long n) {
+        ArrayList<Long> ans = new ArrayList<>();
+        long fact = 1;
+        ans.add(fact);
+        for (long i = 2; i <= n ; ++i) {
+            fact = fact * i;
+        }
+    return ans;
+    }
 }
