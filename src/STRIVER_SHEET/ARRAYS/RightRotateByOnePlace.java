c461ad71c942937f2f4abdd82164a1fad2ef044a
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-21 05:30:49 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/RightRotateByOnePlace.java
+++ src/STRIVER_SHEET/ARRAYS/RightRotateByOnePlace.java
@@ -11,7 +11,7 @@ public class RightRotateByOnePlace {
         int n = arr.length;
         int temp = arr[n - 1];
 
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i < n - 1; i++) {
             arr[i + 1] = arr[i];
         }
 
