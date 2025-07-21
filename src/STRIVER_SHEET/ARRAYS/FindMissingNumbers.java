a96bfbd0ae14d066573ed9b97ab7a0a9cda9e685
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-21 19:17:30 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/FindMissingNumbers.java
+++ src/STRIVER_SHEET/ARRAYS/FindMissingNumbers.java
@@ -2,7 +2,7 @@ package STRIVER_SHEET.ARRAYS;
 
 public class FindMissingNumbers {
     public static void main(String[] args) {
-        int[] arr = {1,2,4,5,6};
+        int[] arr = {1,2,3,4,5,6,7,9};
 
         System.out.println(missingNumber(arr));
     }
