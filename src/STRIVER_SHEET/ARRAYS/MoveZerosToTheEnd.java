e7ae9bbed7ca60d8e2c18ceb5b2a887cd90b3869
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-21 06:11:04 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/MoveZerosToTheEnd.java
+++ src/STRIVER_SHEET/ARRAYS/MoveZerosToTheEnd.java
@@ -1,2 +1,11 @@
-package STRIVER_SHEET.ARRAYS;public class MoveZerosToTheEnd {
+package STRIVER_SHEET.ARRAYS;
+
+public class MoveZerosToTheEnd {
+    public static void main(String[] args) {
+
+    }
+
+    static void zeroToEnd(int[] arr){
+        
+    }
 }
