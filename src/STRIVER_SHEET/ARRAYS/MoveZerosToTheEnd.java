31fa7930b05dee138de870eb80c9514c2c6e0463
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-21 18:29:04 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/MoveZerosToTheEnd.java
+++ src/STRIVER_SHEET/ARRAYS/MoveZerosToTheEnd.java
@@ -2,10 +2,33 @@ package STRIVER_SHEET.ARRAYS;
 
 public class MoveZerosToTheEnd {
     public static void main(String[] args) {
-
+        int[] arr = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
+        int n = 10;
+        int[] ans = moveZeros(n, arr);
+        for (int i = 0; i < n; i++) {
+            System.out.print(ans[i] + " ");
+        }
+        System.out.println("");
     }
 
-    static void zeroToEnd(int[] arr){
-        
+    static int[] moveZeros(int n, int[] arr){
+
+        int[] ans = new int[arr.length];
+
+        int i = 0;
+        int j = ans.length - 1;
+
+        for (int k = 0; k < arr.length; k++) {
+            if(arr[k] != 0){
+                ans[i] = arr[k];
+                i++;
+            } else if (arr[k] == 0) {
+                arr[j] = arr[k];
+                j--;
+            }
+
+        }
+
+
     }
 }
