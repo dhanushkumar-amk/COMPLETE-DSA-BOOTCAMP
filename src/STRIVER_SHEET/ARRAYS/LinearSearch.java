594dc9e763a916d0c0dd9dfc288b0a8e8f19e829
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-21 05:46:11 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/LinearSearch.java
+++ src/STRIVER_SHEET/ARRAYS/LinearSearch.java
@@ -1,2 +1,16 @@
-package STRIVER_SHEET.ARRAYS;public class LinearSearch {
+package STRIVER_SHEET.ARRAYS;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {2,8,23,14,46,5};
+        System.out.println(linearSearch(arr, 14));
+    }
+
+    static int linearSearch(int[] arr, int target){
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == target)
+                return i;
+        }
+        return -1;
+    }
 }
