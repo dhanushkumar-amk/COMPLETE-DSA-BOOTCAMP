28b0585d592d4970f31434003a594626af073545
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-21 06:04:40 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/RotateTheArrayByDPlaces.java
+++ src/STRIVER_SHEET/ARRAYS/RotateTheArrayByDPlaces.java
@@ -5,12 +5,18 @@ public class RotateTheArrayByDPlaces {
 
     }
 
-    public void rotate(int[] nums, int k) {
-        
+    public void rotate(int[] arr, int k) {
+        int n = arr.length;
+        // Reverse first n-k elements
+        Reverse(arr, 0, n - k - 1);
+        // Reverse last k elements
+        Reverse(arr, n - k, n - 1);
+        // Reverse whole array
+        Reverse(arr, 0, n - 1);
     }
 
-    static void reverse(int[] arr, int start, int end){
-        while(start < end) {
+    static void Reverse(int[] arr, int start, int end){
+        while(start <= end) {
             int temp = arr[start];
             arr[start] = arr[end];
             arr[end] = temp;
