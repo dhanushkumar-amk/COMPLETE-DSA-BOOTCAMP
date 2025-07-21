64aa643704eee34fc41f147c2370ece1530584f1
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-21 18:59:12 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/UnionOfTwoSortedArray.java
+++ src/STRIVER_SHEET/ARRAYS/UnionOfTwoSortedArray.java
@@ -1,5 +1,8 @@
 package STRIVER_SHEET.ARRAYS;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class UnionOfTwoSortedArray {
     public static void main(String[] args) {
         int[] arr1 = {1,1,2,3,4,5};
@@ -8,15 +11,22 @@ public class UnionOfTwoSortedArray {
 
     }
 
-    static  int[] unionOfArray(int[] arr1, int[] arr2){
+    static ArrayList<Integer> FindUnion(int[] arr1, int[] arr2){
         int n1 = arr1.length;
         int n2 = arr2.length;
 
         int i = 0;
         int j = 0;
 
-        
+        ArrayList<Integer> ans = new ArrayList<>();
 
+        while (i < n1 && j < n2){
+            if(arr1[i] <= arr2[j]){
+                ans.get(i - 1)
+            }
+        }
+
+        return ans;
     }
 
 
