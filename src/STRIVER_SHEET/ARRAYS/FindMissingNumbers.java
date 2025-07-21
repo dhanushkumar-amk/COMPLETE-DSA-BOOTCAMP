061bce6aefa7fa88c6485ac50f4c423cf4043822
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-21 19:16:20 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/FindMissingNumbers.java
+++ src/STRIVER_SHEET/ARRAYS/FindMissingNumbers.java
@@ -1,2 +1,17 @@
-package STRIVER_SHEET.ARRAYS;public class FindMissingNumbers {
+package STRIVER_SHEET.ARRAYS;
+
+public class FindMissingNumbers {
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,5,6};
+
+        System.out.println(missingNumber(arr));
+    }
+
+    static int missingNumber(int[] arr){
+        int n = arr.length;
+
+        int sumOfNaturalNumber = (n * (n + 1) / 2);
+        return sumOfNaturalNumber;
+
+    }
 }
