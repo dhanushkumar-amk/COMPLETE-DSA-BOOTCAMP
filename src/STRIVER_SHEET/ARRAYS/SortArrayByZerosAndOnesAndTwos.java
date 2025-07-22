87dfdd382136c885933c9312a6bbd6a8c88f0974
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-22 17:08:18 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/SortArrayByZerosAndOnesAndTwos.java
+++ src/STRIVER_SHEET/ARRAYS/SortArrayByZerosAndOnesAndTwos.java
@@ -12,7 +12,20 @@ public class SortArrayByZerosAndOnesAndTwos {
     }
 
     static void sort(int[] arr){
-       
+        int low = 0;
+        int mid = 0;
+        int high = arr.length - 1;
+
+        while (mid <= high){
+            if(arr[mid] == 0){
+
+            } else if (arr[mid] == 1) {
+
+            }else{
+                
+            }
+        }
+
     }
 
 }
