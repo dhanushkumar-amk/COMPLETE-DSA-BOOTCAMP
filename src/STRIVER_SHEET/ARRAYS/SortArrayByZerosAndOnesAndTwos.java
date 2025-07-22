f7ad14a8568c8550f80f7ba3e72c32e95081e97c
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-22 16:40:45 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/SortArrayByZerosAndOnesAndTwos.java
+++ src/STRIVER_SHEET/ARRAYS/SortArrayByZerosAndOnesAndTwos.java
@@ -1,2 +1,18 @@
-package STRIVER_SHEET.ARRAYS;public class SortArrayByZerosAndOnesAndTwos {
+package STRIVER_SHEET.ARRAYS;
+
+import java.util.Arrays;
+
+public class SortArrayByZerosAndOnesAndTwos {
+    public static void main(String[] args) {
+    int[] arr = {1,2,0,0,2,2,1,2,1,0};
+    sort(arr);
+    for(int num : arr){
+        System.out.print(num + " ");
+    }
+    }
+
+    static void sort(int[] arr){
+        Arrays.sort(arr);
+    }
+
 }
