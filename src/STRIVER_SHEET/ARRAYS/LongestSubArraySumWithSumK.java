412516d8ebbf0bf392823af10ba06893db61d538
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-22 06:25:08 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/LongestSubArraySumWithSumK.java
+++ src/STRIVER_SHEET/ARRAYS/LongestSubArraySumWithSumK.java
@@ -1,2 +1,36 @@
-package STRIVER_SHEET.ARRAYS;public class LongestSubArraySumWithSumK {
+package STRIVER_SHEET.ARRAYS;
+
+public class LongestSubArraySumWithSumK {
+    public static void main(String[] args) {
+    int[] arr = {1,2,3,1,1,1,3,3};
+    int k = 6;
+        System.out.println(longestSubarraySum(arr, k));
+    }
+
+    static long longestSubarraySum(int[] arr, int k ){
+        int left = 0;
+        int right = 0;
+
+        long sum = arr[0];
+        int max = 0;
+        int n = arr.length;
+
+
+        while (right < n){
+
+            while (sum > k){
+                sum -= arr[left];
+                left++;
+            }
+            if(sum == k){
+                max = Math.max(max, right - left + 1);
+            }
+            right++;
+            if(right < n)
+                sum += arr[right];
+        }
+        return max;
+    }
+
+
 }
