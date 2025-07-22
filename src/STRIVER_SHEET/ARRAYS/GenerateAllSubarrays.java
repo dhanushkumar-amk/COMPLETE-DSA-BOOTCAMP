7e86b6544fa9bdd4895f29ed3fdf1163ee267464
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-22 05:53:23 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/GenerateAllSubarrays.java
+++ src/STRIVER_SHEET/ARRAYS/GenerateAllSubarrays.java
@@ -1,2 +1,19 @@
-package STRIVER_SHEET.ARRAYS;public class GenerateAllSubarrays {
+package STRIVER_SHEET.ARRAYS;
+
+public class GenerateAllSubarrays {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        subArrays(arr);
+    }
+
+    static void subArrays(int[] arr){
+        for (int i = 0; i < arr.length; ++i) {
+            for (int j = i; j < arr.length; ++j) {
+                for (int k = i; k < j; ++k) {
+                    System.out.print(arr[k] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
