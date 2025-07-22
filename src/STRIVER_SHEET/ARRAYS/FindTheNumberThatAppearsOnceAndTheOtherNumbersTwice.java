1e42ee35ded873a8972abf1c53d135badc256351
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-22 05:35:48 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/FindTheNumberThatAppearsOnceAndTheOtherNumbersTwice.java
+++ src/STRIVER_SHEET/ARRAYS/FindTheNumberThatAppearsOnceAndTheOtherNumbersTwice.java
@@ -1,7 +1,12 @@
- package STRIVER_SHEET.ARRAYS; 
+package STRIVER_SHEET.ARRAYS;
 
 public class FindTheNumberThatAppearsOnceAndTheOtherNumbersTwice {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        int[] arr = {1,1,2,3,3,4,4,5,5};
+        System.out.println();
     }
+
+    public static int 
+
+
 }
