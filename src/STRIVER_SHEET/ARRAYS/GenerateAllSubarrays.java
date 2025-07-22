15e06a335463b68f0123d094c0771551ebaf4d76
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-22 05:54:54 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/GenerateAllSubarrays.java
+++ src/STRIVER_SHEET/ARRAYS/GenerateAllSubarrays.java
@@ -9,7 +9,7 @@ public class GenerateAllSubarrays {
     static void subArrays(int[] arr){
         for (int i = 0; i < arr.length; ++i) {
             for (int j = i; j < arr.length; ++j) {
-                for (int k = i; k < j; ++k) {
+                for (int k = i; k <= j; ++k) {
                     System.out.print(arr[k] + " ");
                 }
             }
