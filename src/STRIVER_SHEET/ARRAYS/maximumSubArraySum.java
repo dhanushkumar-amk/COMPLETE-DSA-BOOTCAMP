51f10ebd920a44e3f5c8cdc4bf02d7d2080dcc7f
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-23 05:46:25 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/maximumSubArraySum.java
+++ src/STRIVER_SHEET/ARRAYS/maximumSubArraySum.java
@@ -2,8 +2,23 @@ package STRIVER_SHEET.ARRAYS;
 
 public class maximumSubArraySum {
     public static void main(String[] args) {
-
+        int[] arr = {1,-2,-5, -10,34};
+        System.out.println(subArraySum(arr));
     }
 
-    static int 
+    static int subArraySum(int[] arr){
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; ++i) {
+
+            sum += arr[i];
+
+            if(sum > max)
+                max = sum;
+
+            if(sum < 0)
+                sum = 0;
+        }
+        return max;
+    }
 }
