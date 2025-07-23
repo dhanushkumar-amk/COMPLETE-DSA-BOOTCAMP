c493ec71eb723cd0db3e52eee91571aa9f54fa6f
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-23 18:59:08 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/RearrangeArrayElementsBySign.java
+++ src/STRIVER_SHEET/ARRAYS/RearrangeArrayElementsBySign.java
@@ -2,11 +2,28 @@ package STRIVER_SHEET.ARRAYS;
 
 public class RearrangeArrayElementsBySign {
     public static void main(String[] args) {
-    
+    int[] arr = {3,1,-2,-5,2,-4};
+    int[] ans = rearrangeArray(arr);
+        for (int i = 0; i < ans.length; i++) {
+            System.out.print(ans[i] + " ");
+        }
     }
 
-    public static int[] rearrangeArray(int[] nums) {
+    public static int[] rearrangeArray(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        int positiveIndex = 0;
+        int negativeIndex = 1;
 
+        for (int i = 0; i < n; ++i) {
+            if(arr[i] < 0){
+                ans[negativeIndex] = arr[i];
+                negativeIndex += 2;
+            }else{
+                ans[positiveIndex] = arr[i];
+                positiveIndex += 2;
+            }
+        }
+        return ans;
     }
-
 }
