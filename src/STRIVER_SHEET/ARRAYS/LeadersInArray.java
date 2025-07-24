b5d4568710e7b80b24148c5b6d30321b60f13f45
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-24 06:41:39 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/LeadersInArray.java
+++ src/STRIVER_SHEET/ARRAYS/LeadersInArray.java
@@ -1,8 +1,15 @@
 package STRIVER_SHEET.ARRAYS;
 
+import java.util.ArrayList;
+
 public class LeadersInArray {
     public static void main(String[] args) {
         int[] arr = {16, 17, 4, 3, 5, 2};
-        
+
+    }
+
+    static ArrayList<Integer> leaders(int arr[]) {
+
+
     }
 }
