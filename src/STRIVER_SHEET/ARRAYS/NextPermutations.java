c42ba47f01c2625a1a674a3a1f7731cedd47a7f8
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-24 06:24:23 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/NextPermutations.java
+++ src/STRIVER_SHEET/ARRAYS/NextPermutations.java
@@ -1,11 +1,24 @@
 package STRIVER_SHEET.ARRAYS;
 
+
+
 public class NextPermutations {
     public static void main(String[] args) {
 
     }
 
-    public static void nextPermutation(int[] nums) {
+    public static void nextPermutation(int[] arr) {
+    int index = -1;
+    int n = arr.length;
+    for (int i = n - 2; i >= 0; i--) {
+        if(arr[i] < arr[i + 1]){
+            index = i;
+            break;
+        }
+    }
+
+    if(index == -1)
+        reverse();
 
     }
 }
