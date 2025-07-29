b503d49ba3e3803dcbc2f1077b15853d2e3dbd7e
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-29 17:49:57 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/TransposeOfAMatrix.java
+++ src/STRIVER_SHEET/ARRAYS/TransposeOfAMatrix.java
@@ -2,6 +2,31 @@ package STRIVER_SHEET.ARRAYS;
 
 public class TransposeOfAMatrix {
     public static void main(String[] args) {
-        
+        int[][] matrix = {
+                {2, 4, -1},
+                {-10,5,11},
+                {18, -7, 6},
+        };
+
+        int[][] ans = transpose(matrix);
+        for (int i = 0; i < ans.length; i++) {
+            for (int j = 0; j < ans[0].length; j++) {
+                System.out.print(ans[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] transpose(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[][] ans = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                ans[j][i] = matrix[i][j];
+            }
+        }
+        return ans;
     }
 }
