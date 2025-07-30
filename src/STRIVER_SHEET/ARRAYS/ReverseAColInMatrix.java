0971c8c272a325b176700ed9db41ba42a638ae2a
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-30 10:21:17 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/ReverseAColInMatrix.java
+++ src/STRIVER_SHEET/ARRAYS/ReverseAColInMatrix.java
@@ -5,10 +5,29 @@ public class ReverseAColInMatrix {
         int[][] matrix = {
                 {1,6,11,16,21},
                 {2,7,12,17,22},
-                {3,8, 13, 18, },
-                {},
-                {},
+                {3,8,13,18,23},
+                {4,9,14,19,24},
+                {5,10,15,20,25},
         };
+
+        System.out.println("Before reverse : \n");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+        System.out.println("After reverse : \n");
+        reverseMatrix(matrix);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
     }
 
     static void reverseMatrix(int[][] matrix){
