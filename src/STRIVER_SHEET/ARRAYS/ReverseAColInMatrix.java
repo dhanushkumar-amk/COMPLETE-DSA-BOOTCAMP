f6e583e97bb8170aaf0654b42c8fced35e22b432
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-07-30 10:18:58 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/ReverseAColInMatrix.java
+++ src/STRIVER_SHEET/ARRAYS/ReverseAColInMatrix.java
@@ -1,2 +1,17 @@
-package STRIVER_SHEET.ARRAYS;public class ReverseAColunmInmatrix {
+package STRIVER_SHEET.ARRAYS;
+
+public class ReverseAColInMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1,6,11,16,21},
+                {2,7,12,17,22},
+                {3,8, 13, 18, },
+                {},
+                {},
+        };
+    }
+
+    static void reverseMatrix(int[][] matrix){
+
+    }
 }
