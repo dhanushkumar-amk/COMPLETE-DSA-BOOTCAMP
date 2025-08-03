4efdc427ccc451ce9755e82a3e6b75b29dcca8d2
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-08-03 21:43:00 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/MergeTwoSortedArray.java
+++ src/STRIVER_SHEET/ARRAYS/MergeTwoSortedArray.java
@@ -1,2 +1,30 @@
-package STRIVER_SHEET.ARRAYS;public class MergeTwoSortedArray {
+package STRIVER_SHEET.ARRAYS;
+
+public class MergeTwoSortedArray {
+    public static void main(String[] args) {
+
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m-1;
+        int j= n-1;
+        int k = m + n-1;
+
+        while(j>=0)
+        {
+            if(i>=0 && nums1[i] > nums2[j])
+            {
+                nums1[k] = nums1[i];
+                i--;
+                k--;
+            }
+            else {
+                nums1[k] = nums2[j];
+                j--;
+                k--;
+            }
+        }
+
+    }
+
 }
