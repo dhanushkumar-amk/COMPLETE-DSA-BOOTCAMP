1f165cc5a42560907160214c6b6cf9641286f443
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-08-03 21:44:06 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/MergeTwoSortedArray.java
+++ src/STRIVER_SHEET/ARRAYS/MergeTwoSortedArray.java
@@ -6,7 +6,7 @@ public class MergeTwoSortedArray {
     public static void main(String[] args) {
         int[] nums1 = {1,2,3,0,0,0};
         int[] nums2 = {2,5,6};
-        System.out.println(Arrays.toString(merge(nums1, )));
+        System.out.println(Arrays.toString(merge(nums1, 3, nums2, )));
     }
 
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
