f6d9925c9a607ada42fefd797123b48f93e7bdbc
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-08-03 21:54:01 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/CountInversion.java
+++ src/STRIVER_SHEET/ARRAYS/CountInversion.java
@@ -6,5 +6,16 @@ public class CountInversion {
     }
 
 
-    static 
+    public  static int inversionCount(int arr[]) {
+        int count = 0;
+
+        for (int i = 0; i < arr.length - 2; ++i) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if(arr[i] > arr[j])
+                    count++;
+            }
+        }
+
+        return count;
+    }
 }
