64c32f8d3d85a5069b67897fe3e2b516818feb50
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-08-04 17:31:47 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/MaximumProductSubarray.java
+++ src/STRIVER_SHEET/ARRAYS/MaximumProductSubarray.java
@@ -2,10 +2,29 @@ package STRIVER_SHEET.ARRAYS;
 
 public class MaximumProductSubarray {
     public static void main(String[] args) {
-        int[] nums = {}
+        int[] nums = {2,3,-2,4};
+        System.out.println(maxProduct(nums));
     }
 
     public static int maxProduct(int[] nums) {
+        int prefix = 1;
+        int suffix = 1;
+        int max = Integer.MIN_VALUE;
+        int n = nums.length;
 
+        for (int i = 0; i < n; i++) {
+
+            if(prefix == 0)
+                prefix = 1;
+
+            if(suffix == 0)
+                suffix = 1;
+
+            prefix = prefix * nums[i];
+            suffix = suffix * nums[n - i - 1];
+
+            max = Math.max(max, Math.max(prefix, suffix));
+        }
+        return max;
     }
 }
