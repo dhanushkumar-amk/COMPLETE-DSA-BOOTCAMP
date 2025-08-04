9b8c52c4703b70fbce92c773796e38c434161620
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-08-04 17:28:16 +0530

    modified

--- src/STRIVER_SHEET/ARRAYS/MaximumProductSubarray.java
+++ src/STRIVER_SHEET/ARRAYS/MaximumProductSubarray.java
@@ -2,9 +2,10 @@ package STRIVER_SHEET.ARRAYS;
 
 public class MaximumProductSubarray {
     public static void main(String[] args) {
-
+        int[] nums = {}
     }
 
+    public static int maxProduct(int[] nums) {
 
-    
+    }
 }
