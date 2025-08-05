b9ce5cabfacdeb500072efcbbd0f56bbfb386bf0
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-08-05 18:34:38 +0530

    modified

--- src/STRIVER_SHEET/BINARYSEARCH/FirstAndLastOccurrence.java
+++ src/STRIVER_SHEET/BINARYSEARCH/FirstAndLastOccurrence.java
@@ -14,5 +14,26 @@ public class FirstAndLastOccurrence {
     }
 
 
+    public static int lowerBoundIndex(int[] arr, int target){
+        int n = arr.length;
+        int low = 0;
+        int high = n - 1;
+        int ans = n;
+
+
+        while(low <= high){
+            int mid = (low + high) / 2;
+
+            if(arr[mid] >= target){
+                ans = mid;
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+
 
 }
