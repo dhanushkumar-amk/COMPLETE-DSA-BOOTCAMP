fd0e172bf66c8036fe95caea1bc2c6645e95e3d2
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-08-05 17:32:10 +0530

    modified

--- src/STRIVER_SHEET/BINARYSEARCH/UpperBond.java
+++ src/STRIVER_SHEET/BINARYSEARCH/UpperBond.java
@@ -1,2 +1,28 @@
-package STRIVER_SHEET.BINARYSEARCH;public class UpperBond {
+package STRIVER_SHEET.BINARYSEARCH;
+
+public class UpperBond {
+    public static void main(String[] args) {
+        int[] arr = {1,4,6,7,7,8,9};
+        System.out.println(upperBoundIndex(arr, 10));
+    }
+
+    public static int upperBoundIndex(int[] arr, int target){
+        int n = arr.length;
+        int low = 0;
+        int high = n - 1;
+        int ans = 0;
+
+
+        while(low <= high){
+            int mid = (low + high) / 2;
+
+            if(arr[mid] <= target){
+                ans = mid;
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
 }
