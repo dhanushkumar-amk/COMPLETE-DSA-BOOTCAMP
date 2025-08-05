b0568338c3565c0b03638cde12e91a4080190954
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-08-05 11:30:03 +0530

    modified

--- src/STRIVER_SHEET/BINARYSEARCH/BinarySearch.java
+++ src/STRIVER_SHEET/BINARYSEARCH/BinarySearch.java
@@ -2,6 +2,23 @@ package STRIVER_SHEET.BINARYSEARCH;
 
 public class BinarySearch {
     public static void main(String[] args) {
+        int[] arr = {4,77,88,99,234,345,456};
+        System.out.println(binarySearch(arr, 234));
+    }
 
+    public static int binarySearch(int[] arr, int target){
+        int low = 0;
+        int high = arr.length - 1;
+
+        while(low < high){
+            int mid = low + high/2;
+             if(arr[mid] == target)
+                 return mid;
+             else if(arr[mid] < target)
+                 high = mid - 1;
+             else
+                 low = mid + 1;
+        }
+        return -1;
     }
 }
