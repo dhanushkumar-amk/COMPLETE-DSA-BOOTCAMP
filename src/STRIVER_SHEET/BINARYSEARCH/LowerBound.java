804f03f54172933bd83684c5db00506ae4fff7d0
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-08-05 17:25:29 +0530

    modified

--- src/STRIVER_SHEET/BINARYSEARCH/LowerBound.java
+++ src/STRIVER_SHEET/BINARYSEARCH/LowerBound.java
@@ -2,10 +2,27 @@ package STRIVER_SHEET.BINARYSEARCH;
 
 public class LowerBound {
     public static void main(String[] args) {
-
+        int[] arr = {1,4,6,7,7,8,9};
+        System.out.println(lowerBoundIndex(arr, 5));
     }
 
     public static int lowerBoundIndex(int[] arr, int target){
-        
+        int n = arr.length;
+        int low = 0;
+        int high = n - 1;
+        int ans = n;
+
+
+        while(low <= high){
+            int mid = (low + high) / 2;
+
+            if(arr[mid] >= target){
+                ans = mid;
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return ans;
     }
 }
