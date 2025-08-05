316e6c0b2016e808669def4db86e65a3f359c08b
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-08-05 12:04:21 +0530

    modified

--- src/STRIVER_SHEET/BINARYSEARCH/BinarySearch.java
+++ src/STRIVER_SHEET/BINARYSEARCH/BinarySearch.java
@@ -10,15 +10,13 @@ public class BinarySearch {
         int low = 0;
         int high = arr.length - 1;
 
-        while(low <= high){
-            int mid = (low + high)/2;
-             if(arr[mid] == target)
-                 return mid;
 
-             if(arr[mid] <= target)
-                 high = mid - 1;
-             else
-                 low = mid + 1;
+            while (low <= high) {
+                int mid = (low + high) / 2;
+                if (arr[mid] == target) return mid;
+                else if (target > arr[mid]) low = mid + 1;
+                else high = mid - 1;
+
         }
         return -1;
     }
