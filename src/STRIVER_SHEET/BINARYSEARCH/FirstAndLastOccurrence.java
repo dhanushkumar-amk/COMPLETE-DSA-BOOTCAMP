7d341418e4293e0ab3e9d38714849a380134dfb4
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-08-05 18:33:46 +0530

    modified

--- src/STRIVER_SHEET/BINARYSEARCH/FirstAndLastOccurrence.java
+++ src/STRIVER_SHEET/BINARYSEARCH/FirstAndLastOccurrence.java
@@ -1,11 +1,18 @@
 package STRIVER_SHEET.BINARYSEARCH;
 
+import java.util.Arrays;
+
 public class FirstAndLastOccurrence {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,4,4,4,5,6,7};
-
+        System.out.println(Arrays.toString(searchRange(arr, 4)));
     }
 
 
-    public static 
+    public static int[] searchRange(int[] nums, int target){
+
+    }
+
+    
+
 }
