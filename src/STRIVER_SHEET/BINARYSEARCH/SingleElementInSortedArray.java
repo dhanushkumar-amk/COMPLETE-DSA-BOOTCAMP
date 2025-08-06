978b3a1b25c137e65b391436e9dfec8cd9ef6378
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-08-06 18:35:37 +0530

    modified

--- src/STRIVER_SHEET/BINARYSEARCH/SingleElementInSortedArray.java
+++ src/STRIVER_SHEET/BINARYSEARCH/SingleElementInSortedArray.java
@@ -12,7 +12,34 @@ public class SingleElementInSortedArray {
 
    public static int single(int[] arr) {
 
+        int n = arr.length;
 
+        if(n == 1)
+            return arr[0];
+
+        if(arr[0] != arr[1])
+            return arr[0];
+
+        if(arr[n - 1] != arr[n - 2])
+            return arr[n - 1];
+
+
+        int low = 1;
+        int high = n - 2;
+
+        while (low <= high){
+            int mid = (low + high)/2;
+
+            if(arr[mid] != arr[mid + 1] && arr[mid] != arr[mid - 1])
+                return mid;
+
+
+            if((mid % 2 == 1 && arr[mid] == arr[mid - 1])  || (mid % 2 == 0 && arr[mid] == arr[mid + 1]))
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+    return -1;
     }
 
 }
