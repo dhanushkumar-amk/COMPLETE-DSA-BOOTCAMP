e29c6ff583e8cc52741e1e92eab6204084c114dd
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-08-06 17:23:34 +0530

    modified

--- src/STRIVER_SHEET/BINARYSEARCH/MinimumInSortedArray.java
+++ src/STRIVER_SHEET/BINARYSEARCH/MinimumInSortedArray.java
@@ -6,29 +6,19 @@ public class MinimumInSortedArray {
         int ans = Integer.MAX_VALUE;
         while (low <= high) {
             int mid = (low + high) / 2;
-
-            //search space is already sorted
-            //then arr[low] will always be
-            //the minimum in that search space:
             if (arr[low] <= arr[high]) {
                 ans = Math.min(ans, arr[low]);
                 break;
             }
 
-            //if left part is sorted:
-            if (arr[low] <= arr[mid]) {
-                // keep the minimum:
-                ans = Math.min(ans, arr[low]);
 
-                // Eliminate left half:
+            if (arr[low] <= arr[mid]) {
+                ans = Math.min(ans, arr[low]);
                 low = mid + 1;
 
-            } else { //if right part is sorted:
-
-                // keep the minimum:
+            } else {
+                
                 ans = Math.min(ans, arr[mid]);
-
-                // Eliminate right half:
                 high = mid - 1;
             }
         }
