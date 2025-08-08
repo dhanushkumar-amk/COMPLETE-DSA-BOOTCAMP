b61fc7a1326175e712f3e4fef20f66c9aeaf285e
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-08-08 17:40:01 +0530

    modified

--- src/STRIVER_SHEET/BINARYSEARCH/FindPeakElementI.java
+++ src/STRIVER_SHEET/BINARYSEARCH/FindPeakElementI.java
@@ -2,9 +2,11 @@ package STRIVER_SHEET.BINARYSEARCH;
 
 public class FindPeakElementI {
     public static void main(String[] args) {
-
+        
     }
 
-    
+    public static int findPeakElement(int[] nums) {
+
+    }
 
 }
