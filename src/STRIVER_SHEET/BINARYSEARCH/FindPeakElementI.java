059d1757d8088e2cc3c356e13c9b7a966477ffe7
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-08-08 17:47:26 +0530

    modified

--- src/STRIVER_SHEET/BINARYSEARCH/FindPeakElementI.java
+++ src/STRIVER_SHEET/BINARYSEARCH/FindPeakElementI.java
@@ -2,11 +2,37 @@ package STRIVER_SHEET.BINARYSEARCH;
 
 public class FindPeakElementI {
     public static void main(String[] args) {
-        
+        int[] arr = {1,2,3,4,3,2,1};
+        System.out.println(findPeakElement(arr));
     }
 
-    public static int findPeakElement(int[] nums) {
+    public static int findPeakElement(int[] arr) {
+        int n = arr.length;
+        if(n == 1)
+            return 0;
 
+        if (arr[0] > arr[1])
+            return 0;
+
+        if(arr[n - 1] > arr[n - 2])
+            return n - 1;
+
+        int low = 1;
+        int high = n - 2;
+        while (low <= high){
+            int mid = (low + high)/2;
+
+            if(arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1])
+                return mid;
+            else if(arr[mid] > arr[mid - 1])
+                low = mid + 1;
+            else if(arr[mid] > arr[mid + 1])
+                high = mid  - 1;
+            else
+                low = mid + 1;
+            // high = mid - 1;
+        }
+        return -1;
     }
 
 }
