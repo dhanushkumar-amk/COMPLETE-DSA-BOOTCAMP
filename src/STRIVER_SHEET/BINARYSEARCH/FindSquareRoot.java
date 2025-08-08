8ca3dba31b40774b0665eea8122d09cba3d34ea6
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-08-08 18:23:16 +0530

    modified

--- src/STRIVER_SHEET/BINARYSEARCH/FindSquareRoot.java
+++ src/STRIVER_SHEET/BINARYSEARCH/FindSquareRoot.java
@@ -3,8 +3,23 @@ package STRIVER_SHEET.BINARYSEARCH;
 
 public class FindSquareRoot {
     public static void main(String[] args) {
-
+        int n = 7;
+        System.out.println(sqrt(n));
     }
 
-    
+    public static int sqrt(int n){
+        int ans = 1;
+        int low = 1;
+        int high = n;
+
+        while (low <= high){
+            int mid = (low + high)/2;
+            if((mid * mid) <= n){
+                ans = mid;
+                low = mid + 1;
+            }else
+                high = mid - 1;
+        }
+        return ans;
+    }
 }
