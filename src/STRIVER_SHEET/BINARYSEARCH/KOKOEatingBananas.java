0bb115a9fae935c75f62d32d66d34a671bf75fd9
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-08-10 10:26:01 +0530

    modified

--- src/STRIVER_SHEET/BINARYSEARCH/KOKOEatingBananas.java
+++ src/STRIVER_SHEET/BINARYSEARCH/KOKOEatingBananas.java
@@ -11,6 +11,29 @@ public class KOKOEatingBananas {
     public static int minEatingSpeed(int[] arr, int h) {
         int maxElementInArray = Arrays.stream(arr).max().getAsInt();
 
-        return -1;
+        int low = 1;
+        int high = maxElementInArray;
+        int ans = 0;
+
+        while (low <= high){
+            int mid = (low + high)/2;
+            int totalHour = calculateTotalHours(arr, mid);
+
+            if(totalHour <= h){
+                ans = mid;
+                high = mid - 1;
+            }
+            else
+                low = mid + 1;
+        }
+        return ans;
+    }
+
+    private static int calculateTotalHours(int[] arr, int mid) {
+        int totalHours =0;
+        for (int i = 0; i < arr.length; i++) {
+            totalHours += Math.ceil(arr[i]/ mid);
+        }
+        return totalHours;
     }
 }
