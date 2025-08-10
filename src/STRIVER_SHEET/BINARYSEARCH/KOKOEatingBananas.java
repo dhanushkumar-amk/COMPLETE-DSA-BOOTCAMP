34815c9e5dcc410780bdc9b1f6d9a0aaccc5fcd6
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-08-10 10:26:52 +0530

    modified

--- src/STRIVER_SHEET/BINARYSEARCH/KOKOEatingBananas.java
+++ src/STRIVER_SHEET/BINARYSEARCH/KOKOEatingBananas.java
@@ -4,35 +4,32 @@ import java.util.Arrays;
 
 public class KOKOEatingBananas {
     public static void main(String[] args) {
-        int[] arr = {3,6,7,11};
+        int[] arr = {3, 6, 7, 11};
         System.out.println(minEatingSpeed(arr, 8));
     }
 
     public static int minEatingSpeed(int[] arr, int h) {
         int maxElementInArray = Arrays.stream(arr).max().getAsInt();
+        int low = 1, high = maxElementInArray, ans = maxElementInArray;
 
-        int low = 1;
-        int high = maxElementInArray;
-        int ans = 0;
-
-        while (low <= high){
-            int mid = (low + high)/2;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
             int totalHour = calculateTotalHours(arr, mid);
 
-            if(totalHour <= h){
+            if (totalHour <= h) {
                 ans = mid;
                 high = mid - 1;
-            }
-            else
+            } else {
                 low = mid + 1;
+            }
         }
         return ans;
     }
 
-    private static int calculateTotalHours(int[] arr, int mid) {
-        int totalHours =0;
-        for (int i = 0; i < arr.length; i++) {
-            totalHours += Math.ceil(arr[i]/ mid);
+    private static int calculateTotalHours(int[] arr, int k) {
+        int totalHours = 0;
+        for (int num : arr) {
+            totalHours += (int) Math.ceil((double) num / k);
         }
         return totalHours;
     }
