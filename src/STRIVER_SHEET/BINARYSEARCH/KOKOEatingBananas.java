32b0110fa0022f714347ee9373be2efee04a2585
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-08-10 10:19:46 +0530

    modified

--- src/STRIVER_SHEET/BINARYSEARCH/KOKOEatingBananas.java
+++ src/STRIVER_SHEET/BINARYSEARCH/KOKOEatingBananas.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 
 public class KOKOEatingBananas {
     public static void main(String[] args) {
-
+        int[] arr = {3,6,7,11};
+        System.out.println(minEatingSpeed(arr, 8));
     }
 
     public static int minEatingSpeed(int[] arr, int h) {
