96cfca3b4098278886d3ef604cb08ad035e4d62d
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-08-10 10:19:22 +0530

    modified

--- src/STRIVER_SHEET/BINARYSEARCH/KOKOEatingBananas.java
+++ src/STRIVER_SHEET/BINARYSEARCH/KOKOEatingBananas.java
@@ -1,9 +1,15 @@
 package STRIVER_SHEET.BINARYSEARCH;
 
+import java.util.Arrays;
+
 public class KOKOEatingBananas {
     public static void main(String[] args) {
 
     }
 
-    public static int 
+    public static int minEatingSpeed(int[] arr, int h) {
+        int maxElementInArray = Arrays.stream(arr).max().getAsInt();
+
+        return -1;
+    }
 }
