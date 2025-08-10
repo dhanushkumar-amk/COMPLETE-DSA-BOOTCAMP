d827bfadcfd184b9e52bcc6603036cd34ff09417
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-08-10 09:52:15 +0530

    modified

--- src/STRIVER_SHEET/BINARYSEARCH/FindTheNThRootOfANumber.java
+++ src/STRIVER_SHEET/BINARYSEARCH/FindTheNThRootOfANumber.java
@@ -1,2 +1,40 @@
-package STRIVER_SHEET.BINARYSEARCH;public class FindTheNThRootOfANumber {
+package STRIVER_SHEET.BINARYSEARCH;
+
+public class FindTheNThRootOfANumber {
+    public static void main(String[] args) {
+        System.out.println(nthRoot(3, 27));
+    }
+
+    public static int nthRoot(int n, int m){
+
+        int low = 1;
+        int high = m;
+
+        while (low <= high)
+        {
+            int mid = (low + high)/2;
+
+            int midN = helperFunc(mid, n, m);
+            if(midN == 1)
+                return mid;
+            else if (midN == 0)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
+
+    private static int helperFunc(int mid, int n, int m) {
+        int ans = 1;
+        for (int i = 1; i <= n; i++) {
+            ans = ans * mid;
+            if(ans > m)
+                return 2;
+        }
+
+        if(ans == m)
+            return 1;
+        return 0;
+    }
 }
