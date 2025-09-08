bfc8b212753a2fe630e710480c84856efa97d817
dhanushkumar <dhanushkumaramk@gmail.com>
2025-09-08 18:20:51 +0530

    modified

--- src/STRIVER_SHEET/LINKEDLIST/lengthOfTheLinkedList.java
+++ src/STRIVER_SHEET/LINKEDLIST/lengthOfTheLinkedList.java
@@ -2,20 +2,24 @@ package STRIVER_SHEET.LINKEDLIST;
 
 import static STRIVER_SHEET.LINKEDLIST.ArrayToLinkedList.arrayToLL;
 
-public class IterateLinkedList {
+public class lengthOfTheLinkedList {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5};
-        iterate(arr);
+        System.out.println(lengthOfLL(arr));
     }
 
-    public static void iterate(int[] arr){
+    public static int lengthOfLL(int[] arr){
+
         Node head = arrayToLL(arr);
         Node temp = head;
 
+        int count = 0;
+
         while (temp != null){
-            System.out.print(temp.data + " ");
+            count++;
             temp = temp.next;
         }
 
+        return count;
     }
 }
